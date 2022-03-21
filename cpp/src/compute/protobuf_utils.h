39e6b8a45bad2a1d50998c332fc5ec561a52473e
Rui Mo <rui.mo@intel.com>
2022-03-21 20:34:23 +0800

    [GJ-75] Unify function names (#76)
    * use the aligned function names with Substrait
    
    * add a writing function
    
    * use constant variables

--- cpp/src/compute/protobuf_utils.h
+++ cpp/src/compute/protobuf_utils.h
@@ -72,3 +72,6 @@ arrow::Result<std::shared_ptr<arrow::Buffer>> SubstraitFromJSON(
     arrow::util::string_view type_name, arrow::util::string_view json);
 arrow::Result<std::string> SubstraitToJSON(arrow::util::string_view type_name,
                                            const arrow::Buffer& buf);
+// Write a Protobuf message into a specified file with JSON format.
+void MessageToJSONFile(const google::protobuf::Message& message,
+                       const std::string& file_path);
