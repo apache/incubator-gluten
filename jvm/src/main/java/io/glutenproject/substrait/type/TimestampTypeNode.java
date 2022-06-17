fe9072c5202792c6adb1a5dac78e471cd0f32026
JiaKe <ke.a.jia@intel.com>
2022-06-17 17:08:13 +0800

    Add timestamp and decimal support in jvm (#200)

---
+++ jvm/src/main/java/io/glutenproject/substrait/type/TimestampTypeNode.java
@@ -0,0 +1,27 @@
+package io.glutenproject.substrait.type;
+
+import io.substrait.proto.Type;
+
+import java.io.Serializable;
+
+public class TimestampTypeNode implements TypeNode, Serializable {
+    private final Boolean nullable;
+
+    TimestampTypeNode(Boolean nullable) {
+        this.nullable = nullable;
+    }
+
+    @Override
+    public Type toProtobuf() {
+        Type.Timestamp.Builder timestampBuilder = Type.Timestamp.newBuilder();
+        if (nullable) {
+            timestampBuilder.setNullability(Type.Nullability.NULLABILITY_NULLABLE);
+        } else {
+            timestampBuilder.setNullability(Type.Nullability.NULLABILITY_REQUIRED);
+        }
+
+        Type.Builder builder = Type.newBuilder();
+        builder.setTimestamp(timestampBuilder.build());
+        return builder.build();
+    }
+}
