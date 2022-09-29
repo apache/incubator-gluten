203d9c60154cf27a04f512ac4ded53d7152be799
lviiii <jiao.lv@intel.com>
2022-09-29 09:47:58 +0000

    Update test Velox branch for Jenkins.

--- tools/build_velox.sh
+++ tools/build_velox.sh
@@ -9,8 +9,10 @@ COMPILE_VELOX=OFF
 VELOX_HOME=${3:-/root/velox}
 ENABLE_EP_CACHE=OFF
 
-VELOX_REPO=https://github.com/oap-project/velox.git
-VELOX_BRANCH=main
+#VELOX_REPO=https://github.com/oap-project/velox.git
+#VELOX_BRANCH=main
+VELOX_REPO=https://github.com/lviiii/velox.git
+VELOX_BRANCH=row_constructor
 
 for arg in "$@"
 do
