72f7aaa7fd4c7b1f31bd6d99a6d155ddf6edf39e
Jin Chengcheng <79615558+jinchengchenghh@users.noreply.github.com>
2022-09-29 10:45:37 +0800

    Update build_velox.sh

--- tools/build_velox.sh
+++ tools/build_velox.sh
@@ -9,8 +9,8 @@ COMPILE_VELOX=OFF
 VELOX_HOME=${3:-/root/velox}
 ENABLE_EP_CACHE=OFF
 
-VELOX_REPO=https://github.com/jinchengchenghh/velox.git
-VELOX_BRANCH=datetype
+VELOX_REPO=https://github.com/oap-project/velox.git
+VELOX_BRANCH=main
 
 for arg in "$@"
 do
