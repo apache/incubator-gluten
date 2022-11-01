fa3c23d7c2b37670776602308e14153a73555244
weiting-chen <weiting.chen@intel.com>
2022-11-02 00:15:14 +0800

    Update script for prerequisite setup in ubuntu20.04

---
+++ tools/setup-ubuntu.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Minimal setup for Ubuntu 20.04
+
+# Install all dependencies for jar release
+sudo apt install -y libboost-all-dev=1.71.0.0ubuntu2 libgflags-dev=2.2.2-1build1 libgoogle-glog-dev=0.4.0-1build1 libre2-dev=20200101+dfsg-1build1 libsnappy-dev=1.1.8-1build1
