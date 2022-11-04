126b1b5847cfcb5d2d2983d9d45d06a5165f4f39
chang chen <baibaichen@gmail.com>
2022-11-04 23:40:00 +0800

    xxxxxx3

--- substrait/substrait-spark/src/test/scala/io/substrait/spark/TPCHPlan.scala
+++ substrait/substrait-spark/src/test/scala/io/substrait/spark/TPCHPlan.scala
@@ -20,11 +20,42 @@ import org.apache.spark.sql.TPCHBase
 
 class TPCHPlan extends TPCHBase with SubstraitPlanTestBase {
 
-  test("scan") {
-//    val queryString = resourceToString(s"tpch/${tpchQueries(5)}.sql",
-//      classLoader = Thread.currentThread().getContextClassLoader)
-//    logInfo(queryString)
-    assertSqlSubstraitRelRoundTrip("select * from lineitem")
+  ignore("simpleJoin") {
+    assertSqlSubstraitRelRoundTrip(
+      "select l_partkey + l_orderkey, l_extendedprice * 0.1 + 100.0, o_orderkey from lineitem " +
+        "join orders on l_orderkey = o_orderkey where l_shipdate < date '1998-01-01' ")
+    assertSqlSubstraitRelRoundTrip(
+      "select l_partkey + l_orderkey, l_extendedprice * 0.1 + 100.0, o_orderkey from lineitem " +
+        "left join orders on l_orderkey = o_orderkey where l_shipdate < date '1998-01-01' ")
+    assertSqlSubstraitRelRoundTrip(
+      "select l_partkey + l_orderkey, l_extendedprice * 0.1 + 100.0, o_orderkey from lineitem " +
+        "right join orders on l_orderkey = o_orderkey where l_shipdate < date '1998-01-01' ")
+    assertSqlSubstraitRelRoundTrip(
+      "select l_partkey + l_orderkey, l_extendedprice * 0.1 + 100.0, o_orderkey from lineitem " +
+        "full join orders on l_orderkey = o_orderkey where l_shipdate < date '1998-01-01' ")
+  }
+
+  ignore("simpleOrderByClause") {
+    assertSqlSubstraitRelRoundTrip(
+      "select l_partkey from lineitem where l_shipdate < date '1998-01-01' " +
+        "order by l_shipdate, l_discount")
+    assertSqlSubstraitRelRoundTrip(
+      "select l_partkey from lineitem where l_shipdate < date '1998-01-01' " +
+        "order by l_shipdate asc, l_discount desc")
+    assertSqlSubstraitRelRoundTrip(
+      "select l_partkey from lineitem where l_shipdate < date '1998-01-01' " +
+        "order by l_shipdate asc, l_discount desc limit 100 offset 1000")
+    assertSqlSubstraitRelRoundTrip(
+      "select l_partkey from lineitem where l_shipdate < date '1998-01-01' " +
+        "order by l_shipdate asc, l_discount desc limit 100")
+    assertSqlSubstraitRelRoundTrip(
+      "select l_partkey from lineitem where l_shipdate < date '1998-01-01' limit 100")
+    assertSqlSubstraitRelRoundTrip(
+      "select l_partkey from lineitem where l_shipdate < date '1998-01-01' " +
+        "order by l_shipdate asc, l_discount desc nulls first")
+    assertSqlSubstraitRelRoundTrip(
+      "select l_partkey from lineitem where l_shipdate < date '1998-01-01' " +
+        "order by l_shipdate asc, l_discount desc nulls last")
   }
 
   test("simpleTest") {
@@ -62,7 +93,84 @@ class TPCHPlan extends TPCHBase with SubstraitPlanTestBase {
       "select l_partkey, sum(l_extendedprice * (1.0-l_discount)) from lineitem group by l_partkey")
   }
 
+  ignore("simpleTestAggFilter") {
+    assertSqlSubstraitRelRoundTrip(
+      "select sum(l_tax) filter(WHERE l_orderkey > l_partkey) from lineitem")
+    // cast is added to avoid the difference by implicit cast
+    assertSqlSubstraitRelRoundTrip(
+      "select sum(l_tax) filter(WHERE l_orderkey > cast(10.0 as bigint)) from lineitem")
+  }
+
   test("simpleTestAggNoGB") {
     assertSqlSubstraitRelRoundTrip("select count(l_tax), count(distinct l_discount) from lineitem")
+  }
+
+  ignore("simpleTestApproxCountDistinct") {
+    val query = "select approx_count_distinct(l_tax)  from lineitem"
+    val plan = assertSqlSubstraitRelRoundTrip(query)
+  }
+
+  test("simpleTestDateInterval") {
+    assertSqlSubstraitRelRoundTrip(
+      "select l_partkey+l_orderkey, l_shipdate from lineitem where l_shipdate < date '1998-01-01' ")
+    assertSqlSubstraitRelRoundTrip(
+      "select l_partkey+l_orderkey, l_shipdate from lineitem " +
+        "where l_shipdate < date '1998-01-01' + interval '3' month ")
+    assertSqlSubstraitRelRoundTrip(
+      "select l_partkey+l_orderkey, l_shipdate from lineitem " +
+        "where l_shipdate < date '1998-01-01' + interval '1' year")
+    assertSqlSubstraitRelRoundTrip(
+      "select l_partkey+l_orderkey, l_shipdate from lineitem " +
+        "where l_shipdate < date '1998-01-01' + interval '1-3' year to month")
+  }
+
+  test("simpleTestDecimal") {
+    assertSqlSubstraitRelRoundTrip(
+      "select l_partkey + l_orderkey, l_extendedprice * 0.1 + 100.0 from lineitem" +
+        " where l_shipdate < date '1998-01-01' ")
+  }
+
+  ignore("simpleTestGroupingSets [has Expand]") {
+    assertSqlSubstraitRelRoundTrip(
+      "select sum(l_discount) from lineitem group by grouping sets " +
+        "((l_orderkey, L_COMMITDATE), l_shipdate)")
+
+    assertSqlSubstraitRelRoundTrip(
+      "select sum(l_discount) from lineitem group by grouping sets " +
+        "((l_orderkey, L_COMMITDATE), l_shipdate), l_linestatus")
+
+    assertSqlSubstraitRelRoundTrip(
+      "select sum(l_discount) from lineitem group by grouping sets " +
+        "((l_orderkey, L_COMMITDATE), l_shipdate, ())")
+
+    assertSqlSubstraitRelRoundTrip(
+      "select sum(l_discount) from lineitem group by grouping sets " +
+        "((l_orderkey, L_COMMITDATE), l_shipdate, ()), l_linestatus")
+    assertSqlSubstraitRelRoundTrip(
+      "select sum(l_discount) from lineitem group by grouping sets " +
+        "((l_orderkey, L_COMMITDATE), (l_orderkey, L_COMMITDATE, l_linestatus), l_shipdate, ())")
+  }
+
+  test("tpch_q1_variant") {
+    // difference from tpch_q1 : 1) remove order by clause; 2) remove interval date literal
+    assertSqlSubstraitRelRoundTrip(
+      "select\n"
+        + "  l_returnflag,\n"
+        + "  l_linestatus,\n"
+        + "  sum(l_quantity) as sum_qty,\n"
+        + "  sum(l_extendedprice) as sum_base_price,\n"
+        + "  sum(l_extendedprice * (1.0 - l_discount)) as sum_disc_price,\n"
+        + "  sum(l_extendedprice * (1.0 - l_discount) * (1.0 + l_tax)) as sum_charge,\n"
+        + "  avg(l_quantity) as avg_qty,\n"
+        + "  avg(l_extendedprice) as avg_price,\n"
+        + "  avg(l_discount) as avg_disc,\n"
+        + "  count(*) as count_order\n"
+        + "from\n"
+        + "  lineitem\n"
+        + "where\n"
+        + "  l_shipdate <= date '1998-12-01' \n"
+        + "group by\n"
+        + "  l_returnflag,\n"
+        + "  l_linestatus\n")
   }
 }
