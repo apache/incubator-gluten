bc9b4e6b534c7cc3736ab785faf73d740d77f336
lviiii <jiao.lv@intel.com>
2022-11-11 12:52:04 +0000

    Revert "build_velox.sh".

--- tools/build_velox.sh
+++ tools/build_velox.sh
@@ -9,10 +9,8 @@ COMPILE_VELOX=OFF
 ENABLE_EP_CACHE=OFF
 VELOX_HOME=/root/velox
 
-#VELOX_REPO=https://github.com/oap-project/velox.git
-#VELOX_BRANCH=main
-VELOX_REPO=https://github.com/lviiii/velox.git
-VELOX_BRANCH=string_functions
+VELOX_REPO=https://github.com/oap-project/velox.git
+VELOX_BRANCH=main
 
 for arg in "$@"
 do
