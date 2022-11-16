064a904ff9906e2ff5b7c188f17acf46e1007716
PHILO-HE <feilong.he@intel.com>
2022-11-16 20:09:32 +0800

    Revise branch [will revert at last]

--- ep/build-velox/src/build_velox.sh
+++ ep/build-velox/src/build_velox.sh
@@ -9,8 +9,8 @@ COMPILE_VELOX=OFF
 ENABLE_EP_CACHE=OFF
 VELOX_HOME=/root/velox
 
-VELOX_REPO=https://github.com/oap-project/velox.git
-VELOX_BRANCH=main
+VELOX_REPO=https://github.com/PHILO-HE/velox.git
+VELOX_BRANCH=bitwise-oap
 
 for arg in "$@"
 do
