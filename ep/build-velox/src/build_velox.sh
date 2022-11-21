422bec320d5a7c6775f1341a319c7c9fe3feaf69
PHILO-HE <feilong.he@intel.com>
2022-11-21 09:50:33 +0800

    Revert "Revise branch [will revert at last]"
    This reverts commit 064a904ff9906e2ff5b7c188f17acf46e1007716.

--- ep/build-velox/src/build_velox.sh
+++ ep/build-velox/src/build_velox.sh
@@ -9,8 +9,8 @@ COMPILE_VELOX=OFF
 ENABLE_EP_CACHE=OFF
 VELOX_HOME=/root/velox
 
-VELOX_REPO=https://github.com/PHILO-HE/velox.git
-VELOX_BRANCH=bitwise-oap
+VELOX_REPO=https://github.com/oap-project/velox.git
+VELOX_BRANCH=main
 
 for arg in "$@"
 do
