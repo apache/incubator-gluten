cade69e4a4b178deb4f512256c29eb1b338259bd
JiaKe <ke.a.jia@intel.com>
2022-11-29 01:00:56 +0000

    [OPPRO-331] Add timestamp and decimal validation (#612)
    * Add timestamp and decimal type validation

--- backends-velox/src/test/scala/io/glutenproject/execution/VeloxDataTypeValidationSuite.scala
+++ backends-velox/src/test/scala/io/glutenproject/execution/VeloxDataTypeValidationSuite.scala
@@ -253,4 +253,36 @@ class VeloxDataTypeValidationSuite extends WholeStageTransformerSuite {
     runQueryAndCompare("select type1.map['key'] from type1," +
       " type2 where type1.map['key'] = type2.map['key']") { _ => }
   }
+
+  test("Decimal type") {
+    // Validation: BatchScan Project Aggregate Expand Sort Limit
+    runQueryAndCompare("select int, decimal from type1 " +
+      " group by grouping sets(int, decimal) sort by decimal, int limit 1") { _ => }
+
+    // Validation: BroadHashJoin, Filter, Project
+    super.sparkConf.set("spark.sql.autoBroadcastJoinThreshold", "10M")
+    runQueryAndCompare("select type1.decimal from type1," +
+      " type2 where type1.decimal = type2.decimal") { _ => }
+
+    // Validation: ShuffledHashJoin, Filter, Project
+    super.sparkConf.set("spark.sql.autoBroadcastJoinThreshold", "-1")
+    runQueryAndCompare("select type1.decimal from type1," +
+      " type2 where type1.decimal = type2.decimal") { _ => }
+  }
+
+  test("Timestamp type") {
+    // Validation: BatchScan Project Aggregate Expand Sort Limit
+    runQueryAndCompare("select int, timestamp from type1 " +
+      " group by grouping sets(int, timestamp) sort by timestamp, int limit 1") { _ => }
+
+    // Validation: BroadHashJoin, Filter, Project
+    super.sparkConf.set("spark.sql.autoBroadcastJoinThreshold", "10M")
+    runQueryAndCompare("select type1.timestamp from type1," +
+      " type2 where type1.timestamp = type2.timestamp") { _ => }
+
+    // Validation: ShuffledHashJoin, Filter, Project
+    super.sparkConf.set("spark.sql.autoBroadcastJoinThreshold", "-1")
+    runQueryAndCompare("select type1.timestamp from type1," +
+      " type2 where type1.timestamp = type2.timestamp") { _ => }
+  }
 }
