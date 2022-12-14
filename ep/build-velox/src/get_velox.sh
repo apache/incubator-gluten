74a1cc60a9c19c5d4cd184b328bad1dd920e20b3
Jia Ke <ke.a.jia@intel.com>
2022-12-14 14:41:12 +0000

    add smj support

--- ep/build-velox/src/get_velox.sh
+++ ep/build-velox/src/get_velox.sh
@@ -2,8 +2,8 @@
 
 set -exu
 
-VELOX_REPO=https://github.com/oap-project/velox.git
-VELOX_BRANCH=main
+VELOX_REPO=https://github.com/JkSelf/velox.git
+VELOX_BRANCH=add-smj-operator
 
 for arg in "$@"
 do
