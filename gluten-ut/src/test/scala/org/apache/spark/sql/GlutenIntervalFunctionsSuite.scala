57909b5d3a3729b3c90d529f973d27a4d8bb6ba2
PHILO-HE <feilong.he@intel.com>
2022-12-15 16:12:30 +0800

    [Gluten-374] Enable GlutenIntervalFunctionsSuite for velox backend (#719)

--- gluten-ut/src/test/scala/org/apache/spark/sql/GlutenIntervalFunctionsSuite.scala
+++ gluten-ut/src/test/scala/org/apache/spark/sql/GlutenIntervalFunctionsSuite.scala
@@ -19,7 +19,4 @@ package org.apache.spark.sql
 
 class GlutenIntervalFunctionsSuite extends IntervalFunctionsSuite with GlutenSQLTestsTrait {
 
-  override def testNameBlackList: Seq[String] = Seq(
-    GlutenTestConstants.IGNORE_ALL
-  )
 }
