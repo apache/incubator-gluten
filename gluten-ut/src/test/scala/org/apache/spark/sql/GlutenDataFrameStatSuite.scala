8e594375abb9b82b33de41902f71410e227c30b6
Chengcheng Jin <chengcheng.jin@intel.com>
2022-12-20 09:19:22 +0000

    Enable some suites

--- gluten-ut/src/test/scala/org/apache/spark/sql/GlutenDataFrameStatSuite.scala
+++ gluten-ut/src/test/scala/org/apache/spark/sql/GlutenDataFrameStatSuite.scala
@@ -18,8 +18,4 @@
 package org.apache.spark.sql
 
 class GlutenDataFrameStatSuite extends DataFrameStatSuite with GlutenSQLTestsTrait {
-
-  override def testNameBlackList: Seq[String] = Seq(
-    GlutenTestConstants.IGNORE_ALL
-  )
 }
