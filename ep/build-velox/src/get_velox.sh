33d9e5b3e209c37d3b205688b85eed693c4d8e2e
Hongze Zhang <hongze.zhang@intel.com>
2022-12-27 16:17:50 +0800

    revert

--- ep/build-velox/src/get_velox.sh
+++ ep/build-velox/src/get_velox.sh
@@ -2,8 +2,8 @@
 
 set -exu
 
-VELOX_REPO=https://github.com/zhztheplayer/velox.git
-VELOX_BRANCH=wip-split-skip-fix
+VELOX_REPO=https://github.com/oap-project/velox.git
+VELOX_BRANCH=main
 
 for arg in "$@"
 do
