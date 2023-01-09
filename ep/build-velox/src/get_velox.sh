6609e7517cc116ff443818bb54d7accc92d39784
Jia Ke <ke.a.jia@intel.com>
2023-01-09 13:25:08 +0000

    revert test branch

--- ep/build-velox/src/get_velox.sh
+++ ep/build-velox/src/get_velox.sh
@@ -2,8 +2,8 @@
 
 set -exu
 
-VELOX_REPO=https://github.com/JkSelf/velox.git
-VELOX_BRANCH=agg-filter-fix
+VELOX_REPO=https://github.com/oap-project/velox.git
+VELOX_BRANCH=main
 
 for arg in "$@"
 do
