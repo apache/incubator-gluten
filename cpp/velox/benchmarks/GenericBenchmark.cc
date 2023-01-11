4d35b9799d1c7965ef1ebb54f2f4434e7801eb35
zuochunwei <zuochunwei@meituan.com>
2023-01-11 05:58:42 -0500

    GenericBenchmark

--- cpp/velox/benchmarks/GenericBenchmark.cc
+++ cpp/velox/benchmarks/GenericBenchmark.cc
@@ -20,6 +20,7 @@
 #include <benchmark/benchmark.h>
 #include <gflags/gflags.h>
 #include <operators/writer/ArrowWriter.h>
+#include <velox/exec/PlanNodeStats.h>
 #include <velox/exec/Task.h>
 
 #include <chrono>
@@ -102,7 +103,7 @@ auto BM_Generic = [](::benchmark::State& state,
     auto* rawIter = static_cast<gluten::WholeStageResultIterator*>(resultIter->GetRaw());
     const auto& task = rawIter->task_;
     const auto& planNode = rawIter->planNode_;
-    auto statsStr = ::facebook::velox::exec::printPlanWithStats(*planNode, task->taskStats(), true);
+    auto statsStr = facebook::velox::exec::printPlanWithStats(*planNode, task->taskStats(), true);
     std::cout << statsStr << std::endl;
   }
 
