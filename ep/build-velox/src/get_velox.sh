c39774d210155b199dfc48df881adc30a54cdda1
Jia Ke <ke.a.jia@intel.com>
2023-02-01 09:42:50 +0000

    partial enable GlutenSQLQueryTestSuite

--- ep/build-velox/src/get_velox.sh
+++ ep/build-velox/src/get_velox.sh
@@ -2,8 +2,8 @@
 
 set -exu
 
-VELOX_REPO=https://github.com/oap-project/velox.git
-VELOX_BRANCH=main
+VELOX_REPO=https://github.com/JkSelf/velox.git
+VELOX_BRANCH=fix_scan_push_down
 
 for arg in "$@"
 do
