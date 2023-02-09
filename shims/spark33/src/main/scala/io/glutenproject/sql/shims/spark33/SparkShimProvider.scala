350efebb93ee3231b1ecefad40b6d9b28a19a226
CodingCat <zhunansjtu@gmail.com>
2023-02-08 16:39:23 -0800

    format

--- shims/spark33/src/main/scala/io/glutenproject/sql/shims/spark33/SparkShimProvider.scala
+++ shims/spark33/src/main/scala/io/glutenproject/sql/shims/spark33/SparkShimProvider.scala
@@ -16,8 +16,8 @@
  */
 package io.glutenproject.sql.shims.spark33
 
-import io.glutenproject.sql.shims.spark33.SparkShimProvider.DESCRIPTOR
 import io.glutenproject.sql.shims.{SparkShimDescriptor, SparkShims}
+import io.glutenproject.sql.shims.spark33.SparkShimProvider.DESCRIPTOR
 
 object SparkShimProvider {
   val DESCRIPTOR = SparkShimDescriptor(3, 3, 1)
@@ -32,8 +32,9 @@ class SparkShimProvider extends io.glutenproject.sql.shims.SparkShimProvider {
     val majorMinorVersionMatch = DESCRIPTOR.toMajorMinorVersion ==
       extractMajorAndMinorVersion(version)
     if (majorMinorVersionMatch && DESCRIPTOR.toString() != version) {
-      logWarning(s"Spark runtime version $version is not matched with Gluten's fully" +
-        s" tested version ${DESCRIPTOR.toString()}")
+      logWarning(
+        s"Spark runtime version $version is not matched with Gluten's fully" +
+          s" tested version ${DESCRIPTOR.toString()}")
     }
     majorMinorVersionMatch
   }
