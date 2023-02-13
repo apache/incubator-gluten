7a6849f6ed415f4099d92c6e5416582524a6975a
taiyang-li <654010905@qq.com>
2023-02-13 15:03:59 +0800

    fix velox uts

--- gluten-core/src/main/scala/io/glutenproject/expression/UnaryExpressionTransformer.scala
+++ gluten-core/src/main/scala/io/glutenproject/expression/UnaryExpressionTransformer.scala
@@ -122,37 +122,37 @@ case class Md5Transformer(substraitExprName: String, child: ExpressionTransforme
     with Logging {
 
   override def doTransform(args: java.lang.Object): ExpressionNode = {
-    if (!BackendsApiManager.getBackendName.equalsIgnoreCase(
+    if (BackendsApiManager.getBackendName.equalsIgnoreCase(
       GlutenConfig.GLUTEN_CLICKHOUSE_BACKEND)) {
+      // In Spark: md5(str)
+      // In CH: lower(hex(md5(str)))
+      // So we need to wrap md5(str) with lower and hex in substrait plan for clickhouse backend.
+      val functionMap = args.asInstanceOf[java.util.HashMap[String, java.lang.Long]]
+
+      val md5FuncId = ExpressionBuilder.newScalarFunction( functionMap,
+        ConverterUtils.makeFuncName(substraitExprName, Seq(original.dataType), FunctionConfig.OPT))
+      val md5ChildNode = child.doTransform(args)
+      val md5ExprNodes = Lists.newArrayList(md5ChildNode)
+      /// In CH, the output type of md5 is FixedString(16)
+      val md5TypeNode = TypeBuilder.makeFixedChar(original.nullable, 16)
+      val md5FuncNode = ExpressionBuilder.makeScalarFunction(md5FuncId, md5ExprNodes, md5TypeNode)
+
+      // wrap in hex: hex(md5(str))
+      val hexFuncId = ExpressionBuilder.newScalarFunction(
+        functionMap, ConverterUtils.makeFuncName("hex", Seq(CharType(16)), FunctionConfig.OPT))
+      val hexExprNodes: ArrayList[ExpressionNode] = Lists.newArrayList(md5FuncNode)
+      val hexTypeNode = TypeBuilder.makeString(original.nullable)
+      val hexFuncNode = ExpressionBuilder.makeScalarFunction(hexFuncId, hexExprNodes, hexTypeNode)
+
+      // wrap in lower: lower(hex(md5(str)))
+      val lowerFuncId = ExpressionBuilder.newScalarFunction(
+        functionMap, ConverterUtils.makeFuncName("lower", Seq(StringType), FunctionConfig.OPT))
+      val lowerExprNodes: ArrayList[ExpressionNode] = Lists.newArrayList(hexFuncNode)
+      val lowerTypeNode = TypeBuilder.makeString(original.nullable)
+      ExpressionBuilder.makeScalarFunction(lowerFuncId, lowerExprNodes, lowerTypeNode)
+    } else {
       UnaryExpressionTransformer(substraitExprName, child, original).doTransform(args)
     }
-
-    // In Spark: md5(str)
-    // In CH: lower(hex(md5(str)))
-    // So we need to wrap md5(str) with lower and hex in substrait plan for clickhouse backend.
-    val functionMap = args.asInstanceOf[java.util.HashMap[String, java.lang.Long]]
-
-    val md5FuncId = ExpressionBuilder.newScalarFunction( functionMap,
-      ConverterUtils.makeFuncName(substraitExprName, Seq(original.dataType), FunctionConfig.OPT))
-    val md5ChildNode = child.doTransform(args)
-    val md5ExprNodes = Lists.newArrayList(md5ChildNode)
-    /// In CH, the output type of md5 is FixedString(16)
-    val md5TypeNode = TypeBuilder.makeFixedChar(original.nullable, 16)
-    val md5FuncNode = ExpressionBuilder.makeScalarFunction(md5FuncId, md5ExprNodes, md5TypeNode)
-
-    // wrap in hex: hex(md5(str))
-    val hexFuncId = ExpressionBuilder.newScalarFunction(
-      functionMap, ConverterUtils.makeFuncName("hex", Seq(CharType(16)), FunctionConfig.OPT))
-    val hexExprNodes: ArrayList[ExpressionNode] = Lists.newArrayList(md5FuncNode)
-    val hexTypeNode = TypeBuilder.makeString(original.nullable)
-    val hexFuncNode = ExpressionBuilder.makeScalarFunction(hexFuncId, hexExprNodes, hexTypeNode)
-
-    // wrap in lower: lower(hex(md5(str)))
-    val lowerFuncId = ExpressionBuilder.newScalarFunction(
-      functionMap, ConverterUtils.makeFuncName("lower", Seq(StringType), FunctionConfig.OPT))
-    val lowerExprNodes: ArrayList[ExpressionNode] = Lists.newArrayList(hexFuncNode)
-    val lowerTypeNode = TypeBuilder.makeString(original.nullable)
-    ExpressionBuilder.makeScalarFunction(lowerFuncId, lowerExprNodes, lowerTypeNode)
   }
 }
 
