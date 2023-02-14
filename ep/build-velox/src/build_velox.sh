92e63676f32059cb25c1bff1de423c903e9b586c
joey.ljy <liujiayi771@apache.org>
2023-02-14 21:28:55 +0800

    Revert "change velox repo and branch for test"
    This reverts commit 700328f9bbe02a38c37bdd8fc7df42d6726e929b.

--- ep/build-velox/src/build_velox.sh
+++ ep/build-velox/src/build_velox.sh
@@ -10,8 +10,8 @@ BUILD_PROTOBUF=ON
 BUILD_TYPE=release
 VELOX_HOME=
 #for ep cache
-VELOX_REPO=https://github.com/liujiayi771/velox.git
-VELOX_BRANCH=stddev_pop
+VELOX_REPO=https://github.com/oap-project/velox.git
+VELOX_BRANCH=main
 TARGET_BUILD_COMMIT=""
 
 LINUX_DISTRIBUTION=$(. /etc/os-release && echo ${ID})
