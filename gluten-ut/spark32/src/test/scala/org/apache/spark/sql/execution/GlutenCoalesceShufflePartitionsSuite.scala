8a24ba3fc88aba3f859dffc548fefb577ee8fa1e
PHILO-HE <feilong.he@intel.com>
2023-02-24 15:28:15 +0800

    Enable complex query 2

--- gluten-ut/spark32/src/test/scala/org/apache/spark/sql/execution/GlutenCoalesceShufflePartitionsSuite.scala
+++ gluten-ut/spark32/src/test/scala/org/apache/spark/sql/execution/GlutenCoalesceShufflePartitionsSuite.scala
@@ -322,5 +322,62 @@ class GlutenCoalesceShufflePartitionsSuite extends CoalesceShufflePartitionsSuit
       withSparkSession(test, 40000, minNumPostShufflePartitions)
     }
 
+    test(GLUTEN_TEST + s"determining the number of reducers: complex query 2$testNameNote") {
+      val test: (SparkSession) => Unit = { spark: SparkSession =>
+        val df1 =
+          spark
+              .range(0, 1000, 1, numInputPartitions)
+              .selectExpr("id % 500 as key1", "id as value1")
+              .groupBy("key1")
+              .count()
+              .toDF("key1", "cnt1")
+        val df2 =
+          spark
+              .range(0, 1000, 1, numInputPartitions)
+              .selectExpr("id % 500 as key2", "id as value2")
+
+        val join =
+          df1
+              .join(df2, col("key1") === col("key2"))
+              .select(col("key1"), col("cnt1"), col("value2"))
+
+        // Check the answer first.
+        val expectedAnswer =
+          spark
+              .range(0, 1000)
+              .selectExpr("id % 500 as key", "2 as cnt", "id as value")
+        QueryTest.checkAnswer(
+          join,
+          expectedAnswer.collect())
+
+        // Then, let's look at the number of post-shuffle partitions estimated
+        // by the ExchangeCoordinator.
+        val finalPlan = join.queryExecution.executedPlan
+            .asInstanceOf[AdaptiveSparkPlanExec].executedPlan
+        val shuffleReads = finalPlan.collect {
+          case r@CoalescedShuffleRead() => r
+          // Added for gluten.
+          case r@ColumnarCoalescedShuffleRead() => r
+        }
+
+        minNumPostShufflePartitions match {
+          case Some(numPartitions) =>
+            assert(shuffleReads.isEmpty)
+
+          case None =>
+            assert(shuffleReads.length === 2)
+            shuffleReads.foreach { read =>
+              assert(read.outputPartitioning.numPartitions === 3)
+            }
+        }
+      }
+
+      // Change the original value 12000 to 30000 for gluten. The test depends on the calculation
+      // for bytesByPartitionId in MapOutputStatistics. Gluten has a different statistic result.
+      // See ShufflePartitionsUtil.coalescePartitions & GlutenColumnarShuffleWriter's mapStatus.
+      withSparkSession(test, 30000, minNumPostShufflePartitions)
+    }
+
+
   }
 }
