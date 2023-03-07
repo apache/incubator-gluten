9e56f6f6d5468b5c047ee84c524617d050ac7f13
Chang Chen <baibaichen@gmail.com>
2023-03-07 21:03:55 +0800

    Support Q6, Q7

--- substrait/substrait-spark/src/test/scala/io/substrait/spark/TPCDSPlan.scala
+++ substrait/substrait-spark/src/test/scala/io/substrait/spark/TPCDSPlan.scala
@@ -30,7 +30,7 @@ class TPCDSPlan extends TPCDSBase with SubstraitPlanTestBase {
   }
 
   // "q9" failed in spark 3.3
-  val successfulSQL: Set[String] = Set("q1", "q3", "q4", "q41", "q62", "q93", "q96", "q99")
+  val successfulSQL: Set[String] = Set("q1", "q3", "q4", "q6", "q7", "q41", "q62", "q93", "q96", "q99")
 
   tpcdsQueries.foreach {
     q =>
