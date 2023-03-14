3d74e5d9497b43dd71600ae7d60cd7b69d3f431f
rui-mo <rui.mo@intel.com>
2023-03-14 02:45:56 +0000

    for test

--- ep/build-velox/src/get_velox.sh
+++ ep/build-velox/src/get_velox.sh
@@ -2,8 +2,8 @@
 
 set -exu
 
-VELOX_REPO=https://github.com/oap-project/velox.git
-VELOX_BRANCH=main
+VELOX_REPO=https://github.com/rui-mo/velox.git
+VELOX_BRANCH=wip_date_diff
 ENABLE_EP_CACHE=OFF
 
 for arg in "$@"
