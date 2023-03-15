64d5af26d1ea116da189d2a359e8cb240ef472c1
PHILO-HE <feilong.he@intel.com>
2023-03-15 21:18:05 +0800

    Revert "Change the code in getting conf"
    This reverts commit a9966e2b338b5c64291e0154d571fd731f20b4c1.

--- gluten-core/src/main/scala/io/glutenproject/extension/ColumnarOverrides.scala
+++ gluten-core/src/main/scala/io/glutenproject/extension/ColumnarOverrides.scala
@@ -502,8 +502,7 @@ case class ColumnarOverrideRules(session: SparkSession)
   // This is an empirical value, may need to be changed for supporting other versions of spark.
   private val aqeStackTraceIndex = 13
 
-  val wholeStageFallbackThreshold = session.sparkContext.getConf.getInt(
-    "spark.gluten.sql.columnar.wholeStage.fallback.threshold", defaultValue = -1)
+  val wholeStageFallbackThreshold = GlutenConfig.getConf.wholeStageFallbackThreshold
   private var originalPlan: SparkPlan = _
   private var fallbacks = 0
   // Do not create rules in class initialization as we should access SQLConf
