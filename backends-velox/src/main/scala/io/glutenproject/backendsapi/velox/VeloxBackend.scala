e0ac71e0d09b108eaf244f3073f5cd69e043a877
taiyang-li <654010905@qq.com>
2023-03-20 15:09:11 +0800

    fix build error

--- backends-velox/src/main/scala/io/glutenproject/backendsapi/velox/VeloxBackend.scala
+++ backends-velox/src/main/scala/io/glutenproject/backendsapi/velox/VeloxBackend.scala
@@ -33,6 +33,7 @@ import scala.util.control.Breaks.{break, breakable}
 class VeloxBackend extends Backend {
   override def name: String = GlutenConfig.GLUTEN_VELOX_BACKEND
   override def initializerApi(): InitializerApi = new VeloxInitializerApi
+  override def shutdownApi(): ShutdownApi = new VeloxShutdownApi
   override def iteratorApi(): IteratorApi = new VeloxIteratorApi
   override def sparkPlanExecApi(): SparkPlanExecApi = new VeloxSparkPlanExecApi
   override def transformerApi(): TransformerApi = new VeloxTransformerApi
