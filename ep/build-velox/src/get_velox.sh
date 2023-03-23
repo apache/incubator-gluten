86b526463c0fd108c6c4fddac75dd68616bb8860
Yuan Zhou <yuan.zhou@intel.com>
2023-03-23 20:42:37 +0800

    DNM quick test on new scan options
    Signed-off-by: Yuan Zhou <yuan.zhou@intel.com>

--- ep/build-velox/src/get_velox.sh
+++ ep/build-velox/src/get_velox.sh
@@ -2,8 +2,8 @@
 
 set -exu
 
-VELOX_REPO=https://github.com/oap-project/velox.git
-VELOX_BRANCH=main
+VELOX_REPO=https://github.com/zhouyuan/velox.git
+VELOX_BRANCH=wip_options
 ENABLE_EP_CACHE=OFF
 
 for arg in "$@"
