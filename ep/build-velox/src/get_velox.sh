2ec8d0e8b853177a2d0dcc54d4a715408283513f
zhli1142015 <zhli@pczhlich.fareast.corp.microsoft.com>
2023-03-28 09:42:20 +0800

    fix ut failure by groupings size validation fix

--- ep/build-velox/src/get_velox.sh
+++ ep/build-velox/src/get_velox.sh
@@ -2,8 +2,8 @@
 
 set -exu
 
-VELOX_REPO=https://github.com/oap-project/velox.git
-VELOX_BRANCH=main
+VELOX_REPO=https://github.com/zhli1142015/velox.git
+VELOX_BRANCH=fix-groupings-size-validation
 ENABLE_EP_CACHE=OFF
 
 for arg in "$@"
