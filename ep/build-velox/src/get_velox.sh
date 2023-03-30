6474d18ac775b3b78c96ea842b5a7e9290823da5
Jia Ke <ke.a.jia@intel.com>
2023-03-30 09:58:05 +0000

    just for test

--- ep/build-velox/src/get_velox.sh
+++ ep/build-velox/src/get_velox.sh
@@ -2,8 +2,8 @@
 
 set -exu
 
-VELOX_REPO=https://github.com/oap-project/velox.git
-VELOX_BRANCH=main
+VELOX_REPO=https://github.com/JkSelf/velox.git
+VELOX_BRANCH=parquet-write
 
 #Set on run gluten on HDFS
 ENABLE_HDFS=OFF
