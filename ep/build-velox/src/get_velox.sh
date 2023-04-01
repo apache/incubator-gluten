8cc2bb5d3719acce11340b95e4a7c53b96dd4e96
joey.ljy <liujiayi771@apache.org>
2023-04-01 16:20:34 +0800

    fix ep velox repo just for test

--- ep/build-velox/src/get_velox.sh
+++ ep/build-velox/src/get_velox.sh
@@ -2,8 +2,8 @@
 
 set -exu
 
-VELOX_REPO=https://github.com/oap-project/velox.git
-VELOX_BRANCH=main
+VELOX_REPO=https://github.com/liujiayi771/velox.git
+VELOX_BRANCH=avg-overflow
 
 #Set on run gluten on HDFS
 ENABLE_HDFS=OFF
