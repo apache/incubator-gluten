b5b7bee9d5d8397118a0b0e1cdb995fc1c776c3e
zhli1142015 <zhli@pczhlich.fareast.corp.microsoft.com>
2023-04-12 18:04:21 +0800

    test velox input stream change

--- ep/build-velox/src/get_velox.sh
+++ ep/build-velox/src/get_velox.sh
@@ -2,8 +2,8 @@
 
 set -exu
 
-VELOX_REPO=https://github.com/oap-project/velox.git
-VELOX_BRANCH=main
+VELOX_REPO=https://github.com/zhli1142015/velox.git
+VELOX_BRANCH=vread-for-input-stream
 
 #Set on run gluten on HDFS
 ENABLE_HDFS=OFF
