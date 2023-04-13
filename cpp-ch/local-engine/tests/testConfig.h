574597ddb4420eb28ef885fac99f58ab0cc7c68e
liuneng1994 <neng.liu@kyligence.io>
2023-04-13 18:38:07 +0800

    refator

---
+++ cpp-ch/local-engine/tests/testConfig.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#define TEST_DATA(file) "file:///home/admin1/github/ClickHouse/utils/local-engine/tests"#file
+#define PARQUET_DATA(file) "file://"#file
+#define MERGETREE_DATA(file) ""#file
+
