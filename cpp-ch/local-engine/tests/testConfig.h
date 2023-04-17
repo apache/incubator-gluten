91a4b7a0a882f715e531b932a989d6648ee1037c
liuneng <1398775315@qq.com>
2023-04-17 09:52:12 +0800

    remove testconfig

--- cpp-ch/local-engine/tests/testConfig.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#define TEST_DATA(file) "file:///home/admin1/github/ClickHouse/utils/local-engine/tests"#file
-#define PARQUET_DATA(file) "file://"#file
-#define MERGETREE_DATA(file) ""#file
-
