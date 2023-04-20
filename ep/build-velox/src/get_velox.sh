18e52a7424589fdc76c1446c3a81b1e8682b5a99
Rong Ma <rong.ma@intel.com>
2023-04-20 17:41:34 +0800

    test oap/velox #209

--- ep/build-velox/src/get_velox.sh
+++ ep/build-velox/src/get_velox.sh
@@ -2,8 +2,8 @@
 
 set -exu
 
-VELOX_REPO=https://github.com/oap-project/velox.git
-VELOX_BRANCH=main
+VELOX_REPO=https://github.com/marin-ma/velox-oap.git
+VELOX_BRANCH=might_contain
 
 #Set on run gluten on HDFS
 ENABLE_HDFS=OFF
