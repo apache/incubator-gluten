e768a495e2cce4f3e85b22759f4261bf2b5b6675
Jin Chengcheng <chengcheng.jin@intel.com>
2023-04-20 19:12:26 +0800

    [GLUTEN-1374] [VL] RangePartitioning supports velox columnar batch (#1375)

--- gluten-data/src/main/scala/org/apache/spark/sql/execution/utils/GlutenExecUtil.scala
+++ gluten-data/src/main/scala/org/apache/spark/sql/execution/utils/GlutenExecUtil.scala
@@ -17,11 +17,12 @@
 
 package org.apache.spark.sql.execution.utils
 
-import scala.collection.JavaConverters._
+import scala.collection.JavaConverters.asScalaIteratorConverter
 
-import io.glutenproject.columnarbatch.ArrowColumnarBatches
+import io.glutenproject.columnarbatch.{ArrowColumnarBatches, GlutenColumnarBatches}
+import io.glutenproject.memory.alloc.NativeMemoryAllocators
 import io.glutenproject.memory.arrowalloc.ArrowBufferAllocators
-import io.glutenproject.vectorized.{ArrowWritableColumnVector, NativePartitioning}
+import io.glutenproject.vectorized.{ArrowWritableColumnVector, NativeColumnarToRowInfo, NativeColumnarToRowJniWrapper, NativePartitioning}
 
 import org.apache.spark.{Partitioner, RangePartitioner, ShuffleDependency}
 import org.apache.spark.internal.Logging
@@ -29,7 +30,7 @@ import org.apache.spark.rdd.RDD
 import org.apache.spark.serializer.Serializer
 import org.apache.spark.shuffle.ColumnarShuffleDependency
 import org.apache.spark.sql.catalyst.InternalRow
-import org.apache.spark.sql.catalyst.expressions.{Attribute, BoundReference, UnsafeProjection}
+import org.apache.spark.sql.catalyst.expressions.{Attribute, BoundReference, UnsafeProjection, UnsafeRow}
 import org.apache.spark.sql.catalyst.expressions.codegen.LazilyGeneratedOrdering
 import org.apache.spark.sql.catalyst.plans.physical._
 import org.apache.spark.sql.execution.PartitionIdPassthrough
@@ -37,11 +38,54 @@ import org.apache.spark.sql.execution.exchange.ShuffleExchangeExec
 import org.apache.spark.sql.execution.metric.SQLMetric
 import org.apache.spark.sql.internal.SQLConf
 import org.apache.spark.sql.types.{IntegerType, StructType}
-import org.apache.spark.sql.vectorized.ColumnarBatch
+import org.apache.spark.sql.vectorized.{ColumnarBatch, ColumnVector}
 import org.apache.spark.util.MutablePair
 import org.apache.spark.util.memory.TaskMemoryResources
 
 object GlutenExecUtil {
+
+  def convertColumnarToRow(batch: ColumnarBatch): Iterator[InternalRow] = {
+    val jniWrapper = new NativeColumnarToRowJniWrapper()
+    var info: NativeColumnarToRowInfo = null
+    val offloaded =
+      ArrowColumnarBatches.ensureOffloaded(ArrowBufferAllocators.contextInstance(), batch)
+    val batchHandle = GlutenColumnarBatches.getNativeHandle(offloaded)
+    info = jniWrapper.nativeConvertColumnarToRow(
+      batchHandle,
+      NativeMemoryAllocators.contextInstance().getNativeInstanceId)
+
+
+    new Iterator[InternalRow] {
+      var rowId = 0
+      val row = new UnsafeRow(batch.numCols())
+      var closed = false
+
+      TaskMemoryResources.addLeakSafeTaskCompletionListener[Unit](_ => {
+        if (!closed) {
+          jniWrapper.nativeClose(info.instanceID)
+          closed = true
+        }
+      })
+
+      override def hasNext: Boolean = {
+        val result = rowId < batch.numRows()
+        if (!result && !closed) {
+          jniWrapper.nativeClose(info.instanceID)
+          closed = true
+        }
+        result
+      }
+
+      override def next: UnsafeRow = {
+        if (rowId >= batch.numRows()) throw new NoSuchElementException
+        val (offset, length) = (info.offsets(rowId), info.lengths(rowId))
+        row.pointTo(null, info.memoryAddress + offset, length.toInt)
+        rowId += 1
+        row
+      }
+    }
+  }
+
   // scalastyle:off argcount
   def genShuffleDependency(rdd: RDD[ColumnarBatch],
                            outputAttributes: Seq[Attribute],
@@ -60,10 +104,11 @@ object GlutenExecUtil {
           // Internally, RangePartitioner runs a job on the RDD that samples keys to compute
           // partition bounds. To get accurate samples, we need to copy the mutable keys.
           iter.flatMap(batch => {
-            val rows =
-              ArrowColumnarBatches
-                .ensureLoaded(ArrowBufferAllocators.contextInstance(), batch)
-                .rowIterator.asScala
+            val rows = if (GlutenColumnarBatches.isIntermediateColumnarBatch(batch)) {
+              convertColumnarToRow(batch)
+            } else ArrowColumnarBatches
+              .ensureLoaded(ArrowBufferAllocators.contextInstance(), batch)
+              .rowIterator.asScala
             val projection =
               UnsafeProjection.create(sortingExpressions.map(_.child), outputAttributes)
             val mutablePair = new MutablePair[InternalRow, Null]()
@@ -85,34 +130,63 @@ object GlutenExecUtil {
       case _ => None
     }
 
+    // will remove after arrow is removed
+    def computeArrowAddPartitionId(cb: ColumnarBatch, partitionKeyExtractor: InternalRow => Any):
+    (Int, ColumnarBatch) = {
+      val pidVec = ArrowWritableColumnVector
+        .allocateColumns(cb.numRows, new StructType().add("pid", IntegerType))
+        .head
+      (0 until cb.numRows).foreach { i =>
+        val row = cb.getRow(i)
+        val pid = rangePartitioner.get.getPartition(partitionKeyExtractor(row))
+        pidVec.putInt(i, pid)
+      }
+
+      val newColumns = (pidVec +: (0 until cb.numCols).map(
+        ArrowColumnarBatches
+          .ensureLoaded(ArrowBufferAllocators.contextInstance(),
+            cb).column)).toArray
+      val offloadCb = ArrowColumnarBatches.ensureOffloaded(
+        ArrowBufferAllocators.contextInstance(),
+        new ColumnarBatch(newColumns, cb.numRows))
+      (0, offloadCb)
+    }
+
     // only used for fallback range partitioning
     def computeAndAddPartitionId(cbIter: Iterator[ColumnarBatch],
                                  partitionKeyExtractor: InternalRow => Any
                                 ): CloseablePairedColumnarBatchIterator = {
+      var batchType: String = null
       CloseablePairedColumnarBatchIterator {
         cbIter
           .filter(cb => cb.numRows != 0 && cb.numCols != 0)
-          .map {
-            cb => ArrowColumnarBatches.ensureLoaded(ArrowBufferAllocators.contextInstance(), cb)
-          }
-          .map { cb =>
-            val pidVec = ArrowWritableColumnVector
-              .allocateColumns(cb.numRows, new StructType().add("pid", IntegerType))
-              .head
-            (0 until cb.numRows).foreach { i =>
-              val row = cb.getRow(i)
-              val pid = rangePartitioner.get.getPartition(partitionKeyExtractor(row))
-              pidVec.putInt(i, pid)
+          .map { cb => if (GlutenColumnarBatches.isIntermediateColumnarBatch(cb)) {
+            if (batchType == null) {
+              batchType = ArrowColumnarBatches.getType(cb)
             }
-
-            val newColumns = (pidVec +: (0 until cb.numCols).map(
-              ArrowColumnarBatches
-                .ensureLoaded(ArrowBufferAllocators.contextInstance(),
-                  cb).column)).toArray
-            val offloadCb = ArrowColumnarBatches.ensureOffloaded(
-              ArrowBufferAllocators.contextInstance(),
-              new ColumnarBatch(newColumns, cb.numRows))
-            (0, offloadCb)
+            if (batchType.equals("velox")) {
+              val pidVec = ArrowWritableColumnVector
+                .allocateColumns(cb.numRows, new StructType().add("pid", IntegerType))
+                .head
+              convertColumnarToRow(cb).zipWithIndex.foreach {
+                case (row, i) =>
+                  val pid = rangePartitioner.get.getPartition(partitionKeyExtractor(row))
+                  pidVec.putInt(i, pid)
+              }
+              val pidBatch = new ColumnarBatch(Array[ColumnVector](pidVec), cb.numRows)
+              val offloadColCb = ArrowColumnarBatches.ensureOffloaded(
+                ArrowBufferAllocators.contextInstance(),
+                pidBatch)
+              val newHandle = ArrowColumnarBatches.addColumn(cb, 0, offloadColCb)
+              (0, GlutenColumnarBatches.create(newHandle))
+            } else {
+              computeArrowAddPartitionId(cb, partitionKeyExtractor)
+            }
+          } else {
+            val loaded = ArrowColumnarBatches.ensureLoaded(
+              ArrowBufferAllocators.contextInstance(), cb)
+            computeArrowAddPartitionId(loaded, partitionKeyExtractor)
+          }
           }
       }
     }
