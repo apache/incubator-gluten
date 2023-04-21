41ef54185958fc40c8ae3e55899c3e2d823b57b7
Hongze Zhang <hongze.zhang@intel.com>
2023-04-21 10:41:07 +0800

    fixup

--- ep/build-velox/src/get_velox.sh
+++ ep/build-velox/src/get_velox.sh
@@ -2,8 +2,8 @@
 
 set -exu
 
-VELOX_REPO=https://github.com/zhztheplayer/velox.git
-VELOX_BRANCH=wip-fix-join-spill-threshold
+VELOX_REPO=https://github.com/oap-project/velox.git
+VELOX_BRANCH=main
 
 #Set on run gluten on HDFS
 ENABLE_HDFS=OFF
