51da596e22f18be3b3b1768fc53be9372972938e
yangchuan <yangchuan.zy@alibaba-inc.com>
2023-04-23 15:05:36 +0800

    Refactor cpp dependency management.

--- cpp/CMake/BuildGoogleBenchmark.cmake
+++ cpp/CMake/BuildGoogleBenchmark.cmake
@@ -15,65 +15,40 @@
 # specific language governing permissions and limitations
 # under the License.
 
-include(ExternalProject)
+include_guard(GLOBAL)
+include(FetchContent)
 
-macro(build_benchmark)
-  message(STATUS "Building benchmark from source")
-  if(CMAKE_VERSION VERSION_LESS 3.6)
-    message(FATAL_ERROR "Building gbenchmark from source requires at least CMake 3.6")
-  endif()
+set(GLUTEN_GBENCHMARK_BUILD_VERSION "v1.6.0")
+set(GLUTEN_GBENCHMARK_SOURCE_URL
+    "https://github.com/google/benchmark/archive/refs/tags/${GLUTEN_GBENCHMARK_BUILD_VERSION}.tar.gz"
+    "https://github.com/ursa-labs/thirdparty/releases/download/latest/gbenchmark-${GLUTEN_GBENCHMARK_BUILD_VERSION}.tar.gz")
+set(GLUTEN_GBENCHMARK_BUILD_SHA256_CHECKSUM "1f71c72ce08d2c1310011ea6436b31e39ccab8c2db94186d26657d41747c85d6")
 
-  if(DEFINED ENV{GLUTEN_GBENCHMARK_URL})
-    set(GBENCHMARK_SOURCE_URL "$ENV{GLUTEN_GBENCHMARK_URL}")
-  else()
-    set(GBENCHMARK_BUILD_VERSION "v1.6.0")
-    set(GBENCHMARK_SOURCE_URL
-            "https://github.com/google/benchmark/archive/refs/tags/${GBENCHMARK_BUILD_VERSION}.tar.gz"
-            "https://github.com/ursa-labs/thirdparty/releases/download/latest/gbenchmark-${GBENCHMARK_BUILD_VERSION}.tar.gz")
-  endif()
-  set(GBENCHMARK_BUILD_SHA256_CHECKSUM "1f71c72ce08d2c1310011ea6436b31e39ccab8c2db94186d26657d41747c85d6")
+set(GBENCHMARK_CMAKE_ARGS "-fPIC -w")
 
+message(STATUS "Building google benchmark from source")
+FetchContent_Declare(
+    gbenchmark
+    URL ${GLUTEN_GBENCHMARK_SOURCE_URL}
+    URL_HASH "SHA256=${GLUTEN_GBENCHMARK_BUILD_SHA256_CHECKSUM}"
+)
 
-  set(GBENCHMARK_PREFIX
-      "${CMAKE_CURRENT_BINARY_DIR}/gbenchmark_ep/src/gbenchmark_ep-install")
-  set(GBENCHMARK_INCLUDE_DIR "${GBENCHMARK_PREFIX}/include")
-  set(GBENCHMARK_STATIC_LIB
-      "${GBENCHMARK_PREFIX}/lib/${CMAKE_STATIC_LIBRARY_PREFIX}benchmark${CMAKE_STATIC_LIBRARY_SUFFIX}"
-      )
-  set(GBENCHMARK_MAIN_STATIC_LIB
-      "${GBENCHMARK_PREFIX}/lib/${CMAKE_STATIC_LIBRARY_PREFIX}benchmark_main${CMAKE_STATIC_LIBRARY_SUFFIX}"
-      )
-  set(GBENCHMARK_CMAKE_ARGS
-      "-DCMAKE_INSTALL_PREFIX=${GBENCHMARK_PREFIX}"
-      -DCMAKE_INSTALL_LIBDIR=lib
-      -DBENCHMARK_ENABLE_TESTING=OFF
-      -DCMAKE_CXX_FLAGS="-fPIC"
-      -DCMAKE_BUILD_TYPE=Release)
+if (NOT gbenchmark_POPULATED)
+  # We don't want to build tests.
+  set(BENCHMARK_ENABLE_TESTING
+      OFF
+      CACHE BOOL "Disable google benchmark tests" FORCE)
+  set(CMAKE_CXX_FLAGS_BKP "${CMAKE_CXX_FLAGS}")
+  string(APPEND CMAKE_CXX_FLAGS " ${GBENCHMARK_CMAKE_ARGS}")
+  set(CMAKE_BUILD_TYPE_BKP "${CMAKE_BUILD_TYPE}")
+  set(CMAKE_BUILD_TYPE "Release")
 
-  ExternalProject_Add(gbenchmark_ep
-      URL ${GBENCHMARK_SOURCE_URL}
-      URL_HASH "SHA256=${GBENCHMARK_BUILD_SHA256_CHECKSUM}"
-      BUILD_BYPRODUCTS "${GBENCHMARK_STATIC_LIB}"
-      "${GBENCHMARK_MAIN_STATIC_LIB}"
-      CMAKE_ARGS ${GBENCHMARK_CMAKE_ARGS})
+  # Fetch the content using previously declared details
+  FetchContent_Populate(gbenchmark)
 
-  # The include directory must exist before it is referenced by a target.
-  file(MAKE_DIRECTORY "${GBENCHMARK_INCLUDE_DIR}")
+  add_subdirectory(${gbenchmark_SOURCE_DIR} ${gbenchmark_BINARY_DIR})
+  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS_BKP}")
+  set(CMAKE_BUILD_TYPE "${CMAKE_BUILD_TYPE_BKP}")
+endif()
 
-  add_library(benchmark::benchmark STATIC IMPORTED)
-  set_target_properties(benchmark::benchmark
-      PROPERTIES IMPORTED_LOCATION "${GBENCHMARK_STATIC_LIB}"
-      INTERFACE_INCLUDE_DIRECTORIES
-      "${GBENCHMARK_INCLUDE_DIR}")
-
-  add_library(benchmark::benchmark_main STATIC IMPORTED)
-  set_target_properties(benchmark::benchmark_main
-      PROPERTIES IMPORTED_LOCATION "${GBENCHMARK_MAIN_STATIC_LIB}"
-      INTERFACE_INCLUDE_DIRECTORIES
-      "${GBENCHMARK_INCLUDE_DIR}")
-
-  add_dependencies(benchmark::benchmark gbenchmark_ep)
-  add_dependencies(benchmark::benchmark_main gbenchmark_ep)
-endmacro()
-
-build_benchmark()
+FetchContent_MakeAvailable(gbenchmark)
