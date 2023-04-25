05af134a6b3a6796a820770491b1a549cb7f113b
zhejiangxiaomai <zhenhui.zhao@intel.com>
2023-04-25 23:02:28 +0800

    test format_check

--- ep/build-velox/src/get_velox.sh
+++ ep/build-velox/src/get_velox.sh
@@ -2,8 +2,8 @@
 
 set -exu
 
-VELOX_REPO=https://github.com/oap-project/velox.git
-VELOX_BRANCH=main
+VELOX_REPO=https://github.com/zhejiangxiaomai/velox.git
+VELOX_BRANCH=format_check
 
 #Set on run gluten on HDFS
 ENABLE_HDFS=OFF
