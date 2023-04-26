671520842742d569e0fd0d3428d55fa0ec75732e
ccat3z <lingfeng.zhang@intel.com>
2023-04-26 03:16:12 +0000

    vcpkg: Add variable ENABLE_GLUTEN_VCPKG & Use static gflags

--- dev/vcpkg/triplets/x64-linux-avx.cmake
+++ dev/vcpkg/triplets/x64-linux-avx.cmake
@@ -8,12 +8,6 @@ set(VCPKG_C_FLAGS "-mavx2 -mfma -mavx -mf16c -mlzcnt -mbmi2")
 set(VCPKG_CXX_FLAGS "-mavx2 -mfma -mavx -mf16c -mlzcnt -std=c++17 -mbmi2")
 set(VCPKG_LINKER_FLAGS "-static-libstdc++ -static-libgcc")
 
-# gflags and all library depends on gflags should be dynamic
-if(PORT MATCHES "glog|gflags")
-    set(VCPKG_LIBRARY_LINKAGE dynamic)
-    set(VCPKG_FIXUP_ELF_RPATH ON)
-endif()
-
 # Fix folly static link libstdc++
 # See: https://github.com/facebook/folly/blob/b88123c2abf4b3244ed285e6db0d4bea2d24f95f/CMakeLists.txt#L192
 if(PORT MATCHES "folly")
