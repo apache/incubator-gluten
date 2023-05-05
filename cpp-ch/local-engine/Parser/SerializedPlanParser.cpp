261191a60a34e859b5d00f2ce130b728df6e12fb
taiyang-li <654010905@qq.com>
2023-05-05 15:56:01 +0800

    fix cast issue in https://github.com/oap-project/gluten/issues/1431

--- cpp-ch/local-engine/Parser/SerializedPlanParser.cpp
+++ cpp-ch/local-engine/Parser/SerializedPlanParser.cpp
@@ -219,66 +219,58 @@ std::shared_ptr<DB::ActionsDAG> SerializedPlanParser::expressionsToActionsDAG(
     return actions_dag;
 }
 
-std::string getDecimalFunction(const substrait::Type_Decimal & decimal, const bool null_on_overflow) {
+std::string getDecimalFunction(const substrait::Type_Decimal & decimal, bool null_on_overflow) {
     std::string ch_function_name;
     UInt32 precision = decimal.precision();
     UInt32 scale = decimal.scale();
 
     if (precision <= DataTypeDecimal32::maxPrecision())
-    {
         ch_function_name = "toDecimal32";
-    }
     else if (precision <= DataTypeDecimal64::maxPrecision())
-    {
         ch_function_name = "toDecimal64";
-    }
     else if (precision <= DataTypeDecimal128::maxPrecision())
-    {
         ch_function_name = "toDecimal128";
-    }
     else
-    {
         throw Exception(ErrorCodes::UNKNOWN_TYPE, "Spark doesn't support decimal type with precision {}", precision);
-    }
 
-    if (null_on_overflow) {
-        ch_function_name = ch_function_name + "OrNull";
-    }
-
+    if (null_on_overflow)
+      ch_function_name = ch_function_name + "OrNull";
     return ch_function_name;
 }
 
-/// TODO: This function needs to be improved for Decimal/Array/Map/Tuple types.
-std::string getCastFunction(const substrait::Type & type)
+std::string getCastFunctionName(const substrait::Type & type)
 {
+    /// As far as I known, cast type in spark is always nullable, but we can't be sure about it.
+    static constexpr auto is_nullable
+        = [](substrait::Type_Nullability nullability) -> bool { return nullability == substrait::Type_Nullability_NULLABILITY_NULLABLE; };
+
     std::string ch_function_name;
     if (type.has_fp64())
-        ch_function_name = "toFloat64";
+        ch_function_name = is_nullable(type.fp64().nullability()) ? "toFloat64OrNull" : "toFloat64OrDefault";
     else if (type.has_fp32())
-        ch_function_name = "toFloat32";
+        ch_function_name = is_nullable(type.fp32().nullability()) ? "toFloat32OrNull" : "toFloat32OrDefault";
     else if (type.has_string())
-        ch_function_name = "toString";
+        ch_function_name = "toString"; // toString can handle null input
     else if (type.has_binary())
-        ch_function_name = "reinterpretAsStringSpark";
+        ch_function_name = "reinterpretAsStringSpark"; // reinterpretAsStringSpark can handle null input
     else if (type.has_i64())
-        ch_function_name = "toInt64";
+        ch_function_name = is_nullable(type.i64().nullability()) ? "toInt64OrNull" : "toInt64OrDefault";
     else if (type.has_i32())
-        ch_function_name = "toInt32";
+        ch_function_name = is_nullable(type.i32().nullability()) ? "toInt32OrNull" : "toInt32OrDefault";
     else if (type.has_i16())
-        ch_function_name = "toInt16";
+        ch_function_name = is_nullable(type.i16().nullability()) ? "toInt16OrNull" : "toInt16OrDefault";
     else if (type.has_i8())
-        ch_function_name = "toInt8";
+        ch_function_name = is_nullable(type.i8().nullability()) ? "toInt8OrNull" : "toInt8OrDefault";
     else if (type.has_date())
-        ch_function_name = "toDate32";
-    // TODO need complete param: scale
+        ch_function_name = is_nullable(type.date().nullability()) ? "toDate32OrNull" : "toDate32OrDefault";
     else if (type.has_timestamp())
-        ch_function_name = "toDateTime64";
+        ch_function_name = is_nullable(type.timestamp().nullability()) ? "toDateTime64OrNull" : "toDateTime64OrDefault";
     else if (type.has_bool_())
-        ch_function_name = "toUInt8";
+        ch_function_name = is_nullable(type.bool_().nullability()) ? "toUInt8OrNull" : "toUInt8OrDefault";
     else if (type.has_decimal())
         ch_function_name = getDecimalFunction(type.decimal(), false);
     else
-        throw Exception(ErrorCodes::UNKNOWN_TYPE, "doesn't support cast type {}", type.DebugString());
+        throw Exception(ErrorCodes::UNKNOWN_TYPE, "Doesn't support cast type {}", type.DebugString());
 
     /// TODO(taiyang-li): implement cast functions of other types
     return ch_function_name;
@@ -1145,7 +1137,7 @@ SerializedPlanParser::getFunctionName(const std::string & function_signature, co
     std::string ch_function_name;
     if (function_name == "cast")
     {
-        ch_function_name = getCastFunction(output_type);
+        ch_function_name = getCastFunctionName(output_type);
     }
     else if (function_name == "trim")
     {
@@ -2035,7 +2027,7 @@ const ActionsDAG::Node * SerializedPlanParser::parseExpression(ActionsDAGPtr act
             if (!rel.cast().has_type() || !rel.cast().has_input())
                 throw Exception(ErrorCodes::BAD_ARGUMENTS, "Doesn't have type or input in cast node.");
 
-            std::string ch_function_name = getCastFunction(rel.cast().type());
+            std::string ch_function_name = getCastFunctionName(rel.cast().type());
             DB::ActionsDAG::NodeRawConstPtrs args;
             const auto & cast_input = rel.cast().input();
             args.emplace_back(parseExpression(action_dag, cast_input));
@@ -2567,7 +2559,7 @@ ASTPtr ASTParser::parseArgumentToAST(const Names & names, const substrait::Expre
             if (!rel.cast().has_type() || !rel.cast().has_input())
                 throw Exception(ErrorCodes::BAD_ARGUMENTS, "Doesn't have type or input in cast node.");
 
-            std::string ch_function_name = getCastFunction(rel.cast().type());
+            std::string ch_function_name = getCastFunctionName(rel.cast().type());
 
             ASTs args;
             args.emplace_back(parseArgumentToAST(names, rel.cast().input()));
