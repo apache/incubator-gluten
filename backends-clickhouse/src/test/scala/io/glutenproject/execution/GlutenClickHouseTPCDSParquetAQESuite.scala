37a3108d2b6e6cb94367d32be28548e806d78c98
Li Shuai <loneylee@live.cn>
2023-05-10 20:29:50 +0800

    add opencostInBytes

--- backends-clickhouse/src/test/scala/io/glutenproject/execution/GlutenClickHouseTPCDSParquetAQESuite.scala
+++ backends-clickhouse/src/test/scala/io/glutenproject/execution/GlutenClickHouseTPCDSParquetAQESuite.scala
@@ -40,7 +40,7 @@ class GlutenClickHouseTPCDSParquetAQESuite
       .set("spark.gluten.sql.columnar.backend.ch.use.v2", "false")
       // Currently, it can not support to read multiple partitioned file in one task.
       //      .set("spark.sql.files.maxPartitionBytes", "134217728")
-      //      .set("spark.sql.files.openCostInBytes", "134217728")
+      .set("spark.sql.files.openCostInBytes", "134217728")
       .set("spark.sql.adaptive.enabled", "true")
       .set("spark.memory.offHeap.size", "4g")
   }
