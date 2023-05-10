047725cfe26d59148f02668c50ed7d90d288444a
Hongze Zhang <hongze.zhang@intel.com>
2023-05-10 10:16:17 +0800

    [VL] Add shortcut tools/gluten-it/sbin/gluten-it.sh to run gluten-it (#1588)

---
+++ tools/gluten-it/sbin/gluten-it.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -eu
+
+GLUTEN_IT_JVM_ARGS=${GLUTEN_IT_JVM_ARGS:-"-Xmx2G -XX:ErrorFile=/var/log/java/hs_err_pid%p.log"}
+
+BASEDIR=$(dirname $0)
+
+BUILD_DIR=$BASEDIR/../target
+JAR_PATH=$BUILD_DIR/gluten-it-1.0-SNAPSHOT-jar-with-dependencies.jar
+
+if [[ ! -e $JAR_PATH ]]; then
+  echo "Please build gluten-it first. For example: mvn clean package"
+  exit 1
+fi
+
+java $GLUTEN_IT_JVM_ARGS -cp $JAR_PATH io.glutenproject.integration.tpc.Tpc $@
