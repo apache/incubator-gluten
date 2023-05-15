b3f8d08d57fc00397ad62b9fe7980cd5e330f11f
PHILO-HE <feilong.he@intel.com>
2023-05-15 19:27:16 +0800

    Set velox branch for test [will be reverted]

--- ep/build-velox/src/get_velox.sh
+++ ep/build-velox/src/get_velox.sh
@@ -2,8 +2,8 @@
 
 set -exu
 
-VELOX_REPO=https://github.com/oap-project/velox.git
-VELOX_BRANCH=main
+VELOX_REPO=https://github.com/PHILO-HE/velox.git
+VELOX_BRANCH=window-bound-type
 
 #Set on run gluten on HDFS
 ENABLE_HDFS=OFF
