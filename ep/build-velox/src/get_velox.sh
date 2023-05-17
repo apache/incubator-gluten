e0987b019f5edd79b2c18226027680df766fe963
yangyimin <yangyimin@meituan.com>
2023-05-17 11:26:23 +0800

    test velox branch

--- ep/build-velox/src/get_velox.sh
+++ ep/build-velox/src/get_velox.sh
@@ -2,8 +2,8 @@
 
 set -exu
 
-VELOX_REPO=https://github.com/oap-project/velox.git
-VELOX_BRANCH=main
+VELOX_REPO=https://github.com/yimin-yang/velox.git
+VELOX_BRANCH=RleEncoderV2
 
 #Set on run gluten on HDFS
 ENABLE_HDFS=OFF
