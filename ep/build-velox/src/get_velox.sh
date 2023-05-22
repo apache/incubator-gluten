dd4095e5e9d6d593b0d899b756aacd0fdd373ab1
joey.ljy <liujiayi771@apache.org>
2023-05-22 16:22:48 +0800

    modify velox repo just for test

--- ep/build-velox/src/get_velox.sh
+++ ep/build-velox/src/get_velox.sh
@@ -2,8 +2,8 @@
 
 set -exu
 
-VELOX_REPO=https://github.com/oap-project/velox.git
-VELOX_BRANCH=main
+VELOX_REPO=https://github.com/liujiayi771/velox.git
+VELOX_BRANCH=ts-hash
 
 #Set on run gluten on HDFS
 ENABLE_HDFS=OFF
