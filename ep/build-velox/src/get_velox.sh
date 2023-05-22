8f23c497d916cd8a2775b502f64be6595c41d95d
yangyimin <yangyimin@meituan.com>
2023-05-22 16:03:47 +0800

    test long decimal

--- ep/build-velox/src/get_velox.sh
+++ ep/build-velox/src/get_velox.sh
@@ -2,8 +2,8 @@
 
 set -exu
 
-VELOX_REPO=https://github.com/oap-project/velox.git
-VELOX_BRANCH=main
+VELOX_REPO=https://github.com/yimin-yang/velox.git
+VELOX_BRANCH=longdecimal
 
 #Set on run gluten on HDFS
 ENABLE_HDFS=OFF
