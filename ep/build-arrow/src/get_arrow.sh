8c3287e887e0cd1c801b70aeb0db82e81d2f3af8
Chengcheng Jin <chengcheng.jin@intel.com>
2023-05-24 08:53:52 +0000

    change arrow branch to tag

--- ep/build-arrow/src/get_arrow.sh
+++ ep/build-arrow/src/get_arrow.sh
@@ -4,7 +4,7 @@ set -exu
 #setting gluten root path
 ARROW_REPO=https://github.com/apache/arrow.git
 #for velox_backend
-ARROW_BRANCH=release-12.0.0-rc0
+ARROW_BRANCH=apache-arrow-12.0.0
 ENABLE_CUSTOM_CODEC=OFF
 
 for arg in "$@"; do
@@ -29,18 +29,13 @@ for arg in "$@"; do
 done
 
 function checkout_code {
-  TARGET_BUILD_COMMIT="$(git ls-remote $ARROW_REPO $ARROW_BRANCH | awk '{print $1;}')"
   ARROW_SOURCE_DIR="$CURRENT_DIR/../build/arrow_ep"
   if [ -d $ARROW_SOURCE_DIR ]; then
     echo "Arrow source folder $ARROW_SOURCE_DIR already exists..."
     cd $ARROW_SOURCE_DIR
     git init .
-    EXISTS=$(git show-ref refs/heads/build_$TARGET_BUILD_COMMIT || true)
-    if [ -z "$EXISTS" ]; then
-      git fetch $ARROW_REPO $TARGET_BUILD_COMMIT:build_$TARGET_BUILD_COMMIT
-    fi
+    git fetch $ARROW_REPO tag $ARROW_BRANCH
     git reset --hard HEAD
-    git checkout build_$TARGET_BUILD_COMMIT
   else
     git clone $ARROW_REPO -b $ARROW_BRANCH $ARROW_SOURCE_DIR
     cd $ARROW_SOURCE_DIR
