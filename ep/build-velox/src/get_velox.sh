cdede069a174528b08feb3e645b6b3c1a5945303
Jia Ke <ke.a.jia@intel.com>
2023-05-24 10:32:53 +0000

    revert velox branch

--- ep/build-velox/src/get_velox.sh
+++ ep/build-velox/src/get_velox.sh
@@ -2,11 +2,8 @@
 
 set -exu
 
-#VELOX_REPO=https://github.com/oap-project/velox.git
-#VELOX_BRANCH=main
-
-VELOX_REPO=https://github.com/JkSelf/velox.git
-VELOX_BRANCH=make-decimal
+VELOX_REPO=https://github.com/oap-project/velox.git
+VELOX_BRANCH=main
 
 #Set on run gluten on HDFS
 ENABLE_HDFS=OFF
