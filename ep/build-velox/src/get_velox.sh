191443facae060d7909d7994672de2e11503cd5b
Chen Zhang <izchen@126.com>
2023-05-25 23:20:57 +0800

    revert VELOX_BRANCH

--- ep/build-velox/src/get_velox.sh
+++ ep/build-velox/src/get_velox.sh
@@ -2,8 +2,8 @@
 
 set -exu
 
-VELOX_REPO=https://github.com/izchen/velox.git
-VELOX_BRANCH=gluten_test_tmp
+VELOX_REPO=https://github.com/oap-project/velox.git
+VELOX_BRANCH=main
 
 #Set on run gluten on HDFS
 ENABLE_HDFS=OFF
