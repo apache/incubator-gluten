8750fc8501b3fa4c8e799b43456e6aff7a9b8bb7
Paul Amonson <paul.d.amonson@intel.com>
2023-06-12 21:06:51 +0000

    [Fix] Pass arguments to the child build script.
    Signed-off-by: Paul Amonson <paul.d.amonson@intel.com>

--- dev/buildbundle-veloxbe.sh
+++ dev/buildbundle-veloxbe.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 BASEDIR=$(dirname $0)
-source "$BASEDIR/builddeps-veloxbe.sh"
+source "$BASEDIR/builddeps-veloxbe.sh" ${@}
 
 cd $GLUTEN_DIR
 mvn clean package -Pbackends-velox -Prss -Pspark-3.2 -DskipTests
