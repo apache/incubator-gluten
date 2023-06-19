da20273f8bebb491f2c5199631d53b02f5f752d0
Hongze Zhang <hongze.zhang@intel.com>
2023-06-19 22:32:25 +0800

    fixup

--- tools/gluten-it/sbin/gluten-it.sh
+++ tools/gluten-it/sbin/gluten-it.sh
@@ -2,29 +2,16 @@
 
 set -euf
 
-GLUTEN_VERSION=0.5.0-SNAPSHOT
-GLUTEN_IT_VERSION=1.0-SNAPSHOT
-
 GLUTEN_IT_JVM_ARGS=${GLUTEN_IT_JVM_ARGS:-"-Xmx2G -XX:ErrorFile=/var/log/java/hs_err_pid%p.log"}
 
 BASEDIR=$(dirname $0)
 
-BUILD_DIR=$BASEDIR/../target
-
-GLUTEN_JAR_PATH=~/.m2/repository/io/glutenproject/gluten-package/$GLUTEN_VERSION/gluten-package-$GLUTEN_VERSION.jar
-if [[ ! -e $GLUTEN_JAR_PATH ]]; then
-  echo "Please build and install gluten first."
+LIB_DIR=$BASEDIR/../dist/target/lib
+if [[ ! -e $LIB_DIR ]]; then
+  echo "Lib directory not found at $LIB_DIR. Please build gluten-it first. For example: mvn clean install"
   exit 1
 fi
 
-GLUTEN_IT_JAR_PATH=$BUILD_DIR/gluten-it-$GLUTEN_IT_VERSION-jar-with-dependencies.jar
-if [[ ! -e $GLUTEN_IT_JAR_PATH ]]; then
-  echo "Please build gluten-it first. For example: mvn clean package"
-  exit 1
-fi
-
-JAR_PATH=
-JAR_PATH=$JAR_PATH:$GLUTEN_JAR_PATH
-JAR_PATH=$JAR_PATH:$GLUTEN_IT_JAR_PATH
+JAR_PATH=$LIB_DIR/*
 
 java $GLUTEN_IT_JVM_ARGS -cp $JAR_PATH io.glutenproject.integration.tpc.Tpc $@
