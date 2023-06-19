a69dd8363be2b9f9a2f93faefad0eacfad5723bc
Yuan Zhou <yuan.zhou@intel.com>
2023-06-20 07:40:06 +0800

    add JDK 17 conf
    Signed-off-by: Yuan Zhou <yuan.zhou@intel.com>

--- tools/gluten-it/sbin/gluten-it.sh
+++ tools/gluten-it/sbin/gluten-it.sh
@@ -14,4 +14,19 @@ if [[ ! -e $JAR_PATH ]]; then
   exit 1
 fi
 
-java $GLUTEN_IT_JVM_ARGS -cp $JAR_PATH io.glutenproject.integration.tpc.Tpc $@
+java \
+    --add-opens=java.base/java.lang=ALL-UNNAMED \
+    --add-opens=java.base/java.lang.invoke=ALL-UNNAMED \
+    --add-opens=java.base/java.lang.reflect=ALL-UNNAMED \
+    --add-opens=java.base/java.io=ALL-UNNAMED \
+    --add-opens=java.base/java.net=ALL-UNNAMED \
+    --add-opens=java.base/java.nio=ALL-UNNAMED \
+    --add-opens=java.base/java.util=ALL-UNNAMED \
+    --add-opens=java.base/java.util.concurrent=ALL-UNNAMED \
+    --add-opens=java.base/java.util.concurrent.atomic=ALL-UNNAMED \
+    --add-opens=java.base/sun.nio.ch=ALL-UNNAMED \
+    --add-opens=java.base/sun.nio.cs=ALL-UNNAMED \
+    --add-opens=java.base/sun.security.action=ALL-UNNAMED \
+    --add-opens=java.base/sun.util.calendar=ALL-UNNAMED \
+    --add-opens=java.security.jgss/sun.security.krb5=ALL-UNNAMED \
+   --illegal-access=permit -Dio.netty.tryReflectionSetAccessible=true  $GLUTEN_IT_JVM_ARGS -cp $JAR_PATH io.glutenproject.integration.tpc.Tpc $@
