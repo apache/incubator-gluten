e8f8bb6ab0e8401dafb1f497dbff568fc19c805b
ulysses-you <ulyssesyou18@gmail.com>
2023-06-26 16:41:42 +0800

    revert repo

--- ep/build-velox/src/get_velox.sh
+++ ep/build-velox/src/get_velox.sh
@@ -2,8 +2,8 @@
 
 set -exu
 
-VELOX_REPO=https://github.com/ulysses-you/velox.git
-VELOX_BRANCH=row-group
+VELOX_REPO=https://github.com/oap-project/velox.git
+VELOX_BRANCH=main
 
 #Set on run gluten on HDFS
 ENABLE_HDFS=OFF
