ba377ff5f9a53efc7bdff1b37baf097d026c5542
rui-mo <rui.mo@intel.com>
2023-07-10 03:09:20 +0000

    Revert "for test"
    This reverts commit 3ad3ece944dd9d66831ec0fd35fffbf71d05a836.

--- ep/build-velox/src/get_velox.sh
+++ ep/build-velox/src/get_velox.sh
@@ -2,8 +2,8 @@
 
 set -exu
 
-VELOX_REPO=https://github.com/rui-mo/velox.git
-VELOX_BRANCH=wip_prune
+VELOX_REPO=https://github.com/oap-project/velox.git
+VELOX_BRANCH=main
 VELOX_HOME=""
 
 #Set on run gluten on HDFS
