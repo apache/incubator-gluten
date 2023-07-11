fae07ab95d1bb4282d2eeb31b3afa0968729cc63
zhanglistar <zhanglinuxstar@gmail.com>
2023-07-11 14:28:53 +0800

    Support now and current_timestamp

--- cpp-ch/local-engine/Parser/SerializedPlanParser.h
+++ cpp-ch/local-engine/Parser/SerializedPlanParser.h
@@ -51,6 +51,8 @@ static const std::map<std::string, std::string> SCALAR_FUNCTIONS
        {"to_unix_timestamp", "toUnixTimestamp"},
        {"unix_timestamp", "toUnixTimestamp"},
        {"date_format", "formatDateTimeInJodaSyntax"},
+       {"now", "now64"},
+       {"current_timestamp", "now64"},
 
        /// arithmetic functions
        {"subtract", "minus"},
