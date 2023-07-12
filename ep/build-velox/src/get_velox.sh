320dc77f7f1f609e4d99876117f31ba05dc99d38
Jia Ke <ke.a.jia@intel.com>
2023-07-12 13:51:52 +0000

    Test velox streaming window

--- ep/build-velox/src/get_velox.sh
+++ ep/build-velox/src/get_velox.sh
@@ -2,8 +2,8 @@
 
 set -exu
 
-VELOX_REPO=https://github.com/oap-project/velox.git
-VELOX_BRANCH=main
+VELOX_REPO=https://github.com/JkSelf/velox.git
+VELOX_BRANCH=streaming-window
 VELOX_HOME=""
 
 #Set on run gluten on HDFS
