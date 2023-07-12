8f27683d25d6797005ca418c8b8cf48cb0cf8a27
lhuang09287750 <liang.huang@kyligence.io>
2023-07-12 10:49:19 +0800

    add test case for roundHalfup,make 1800 times round computation

--- backends-clickhouse/src/test/scala/io/glutenproject/execution/GlutenClickHouseTPCDSParquetSuite.scala
+++ backends-clickhouse/src/test/scala/io/glutenproject/execution/GlutenClickHouseTPCDSParquetSuite.scala
@@ -342,5 +342,58 @@ class GlutenClickHouseTPCDSParquetSuite extends GlutenClickHouseTPCDSAbstractSui
         |""".stripMargin
     compareResultsAgainstVanillaSpark(sql, true, _ => {})
   }
+
+  test("GLUTEN-1626: test 'roundHalfup'") {
+    var sql =
+      """
+        |select cast(ss_wholesale_cost as Int) a, round(sum(ss_wholesale_cost),2),
+        |round(sum(ss_wholesale_cost+0.06),2), round(sum(ss_wholesale_cost-0.04),2)
+        |from store_sales
+        |group by a order by a
+        |""".stripMargin
+    compareResultsAgainstVanillaSpark(sql, true, _ => {})
+    sql =
+      """
+        |select cast(ss_sales_price as Int) a, round(sum(ss_sales_price),2),
+        |round(sum(ss_sales_price+0.06),2), round(sum(ss_sales_price-0.04),2)
+        |from store_sales
+        |group by a order by a
+        |""".stripMargin
+    compareResultsAgainstVanillaSpark(sql, true, _ => {})
+
+    sql =
+      """
+        |select cast(cs_wholesale_cost as Int) a, round(sum(cs_wholesale_cost),2),
+        |round(sum(cs_wholesale_cost+0.06),2), round(sum(cs_wholesale_cost-0.04),2)
+        |from catalog_sales
+        |group by a order by a
+        |""".stripMargin
+    compareResultsAgainstVanillaSpark(sql, true, _ => {})
+    sql =
+      """
+        |select cast(cs_sales_price as Int) a, round(sum(cs_sales_price),2),
+        |round(sum(cs_sales_price+0.06),2), round(sum(cs_sales_price-0.04),2)
+        |from catalog_sales
+        |group by a order by a
+        |""".stripMargin
+    compareResultsAgainstVanillaSpark(sql, true, _ => {})
+
+    sql =
+      """
+        |select cast(ws_wholesale_cost as Int) a, round(sum(ws_wholesale_cost),2),
+        |round(sum(ws_wholesale_cost+0.06),2), round(sum(ws_wholesale_cost-0.04),2)
+        |from web_sales
+        |group by a order by a
+        |""".stripMargin
+    compareResultsAgainstVanillaSpark(sql, true, _ => {})
+    sql =
+      """
+        |select cast(ws_sales_price as Int) a, round(sum(ws_sales_price),2),
+        |round(sum(ws_sales_price+0.06),2), round(sum(ws_sales_price-0.04),2)
+        |from web_sales
+        |group by a order by a
+        |""".stripMargin
+    compareResultsAgainstVanillaSpark(sql, true, _ => {})
+  }
 }
 // scalastyle:on line.size.limit
