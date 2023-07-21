b60bb01aefd07932a123a8d783a8ce5ca1cde2fb
zhejiangxiaomai <zhenhui.zhao@intel.com>
2023-07-21 10:13:07 +0800

    use revert dot

--- ep/build-velox/src/get_velox.sh
+++ ep/build-velox/src/get_velox.sh
@@ -16,8 +16,8 @@
 
 set -exu
 
-VELOX_REPO=https://github.com/oap-project/velox.git
-VELOX_BRANCH=main
+VELOX_REPO=https://github.com/zhejiangxiaomai/velox.git
+VELOX_BRANCH=revert_dot
 VELOX_HOME=""
 
 #Set on run gluten on HDFS
