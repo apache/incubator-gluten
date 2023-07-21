7a1828e7fcf766367ef2f42fb274d4b7e475e4ae
zhanglistar <zhanglinuxstar@gmail.com>
2023-07-21 19:09:50 +0800

    fix ut

--- cpp-ch/local-engine/Functions/SparkFunctionMonthsBetween.cpp
+++ cpp-ch/local-engine/Functions/SparkFunctionMonthsBetween.cpp
@@ -10,6 +10,7 @@
 #include <Common/DateLUT.h>
 #include <Common/Exception.h>
 #include <Common/logger_useful.h>
+#include "Core/Field.h"
 #include "base/Decimal.h"
 #include "base/types.h"
 
@@ -57,31 +58,31 @@ ALWAYS_INLINE Float64 roundTo8IfNeed(bool round, Float64 res)
     return round ? std::round(res * 1e8) / 1e8 : res;
 }
 
-Float64 monthsBetween(DateTime64 x, DateTime64 y, const DateLUTImpl & timezone_x, const DateLUTImpl & timezone_y, bool round)
+Float64 monthsBetween(DateTime64 x, DateTime64 y, const DateLUTImpl & timezone, bool round)
 {
     // we know that spark use microseconds, maybe round to 8 digits after point
     x /= 1000000;
     y /= 1000000;
-    int x_year = timezone_x.toYear(x);
-    int y_year = timezone_y.toYear(y);
-    auto x_month = timezone_x.toMonth(x);
-    auto y_month = timezone_y.toMonth(y);
-    auto x_day = timezone_x.toDayOfMonth(x);
-    auto y_day = timezone_y.toDayOfMonth(y);
+    int x_year = timezone.toYear(x);
+    int y_year = timezone.toYear(y);
+    auto x_month = timezone.toMonth(x);
+    auto y_month = timezone.toMonth(y);
+    auto x_day = timezone.toDayOfMonth(x);
+    auto y_day = timezone.toDayOfMonth(y);
     auto month_diff = static_cast<Float64>(x_year * 12 + x_month - y_year * 12 - y_month);
     if (x_day == y_day)
         return roundTo8IfNeed(round, month_diff);
 
-    int x_to_month_end = timezone_x.daysInMonth(x);
+    int x_to_month_end = timezone.daysInMonth(x);
     x_to_month_end -= x_day;
-    int y_to_month_end = timezone_y.daysInMonth(y);
+    int y_to_month_end = timezone.daysInMonth(y);
     y_to_month_end -= y_day;
     if (x_to_month_end == 0 && y_to_month_end == 0)
         return roundTo8IfNeed(round, month_diff);
 
     int day_diff = static_cast<int>(x_day) - y_day;
-    auto x_seconds_in_day= x - timezone_x.makeDate(x_year, x_month, x_day);
-    auto y_seconds_in_day= y - timezone_y.makeDate(y_year, y_month, y_day);
+    auto x_seconds_in_day= x - timezone.makeDate(x_year, x_month, x_day);
+    auto y_seconds_in_day= y - timezone.makeDate(y_year, y_month, y_day);
     auto seconds_diff = x_seconds_in_day - y_seconds_in_day;
     auto res = static_cast<Float64>(day_diff * 86400 + seconds_diff)/2678400.0 + month_diff;
     return roundTo8IfNeed(round, res);
@@ -98,26 +99,27 @@ DB::ColumnPtr SparkFunctionMonthsBetween::executeImpl(
     auto res = result_type->createColumn();
     res->reserve(rows);
 
-    const auto & timezone_x = extractTimeZoneFromFunctionArguments(arguments, 3, 0);
-    const auto & timezone_y = extractTimeZoneFromFunctionArguments(arguments, 3, 1);
-
     for (size_t i = 0; i < rows; ++i)
     {
         DB::Field x_value;
         DB::Field y_value;
-        DB::Field round_value;
         x.get(i, x_value);
         y.get(i, y_value);
-        round_off.get(i, round_value);
         if (x_value.isNull() || y_value.isNull()) [[unlikely]]
             res->insertDefault();
         else
+        {
+            DB::Field round_value;
+            round_off.get(i, round_value);
+            DB::Field timezone_value;
+            arguments[3].column->get(i, timezone_value);
+            auto & timezone = DateLUT::instance(timezone_value.safeGet<DB::String>());
             res->insert(monthsBetween(
                 static_cast<DateTime64>(x_value.safeGet<DateTime64>()),
                 static_cast<DateTime64>(y_value.safeGet<DateTime64>()),
-                timezone_x,
-                timezone_y,
+                timezone,
                 static_cast<bool>(round_value.safeGet<DB::UInt8>())));
+        }
     }
     return res;
 }
