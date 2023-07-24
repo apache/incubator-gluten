3643dbb1410b6fa24ce1c15ddc8433d76517923f
Ma Jian <jian1.ma@intel.com>
2023-07-24 13:46:55 +0800

    support array_join

--- shims/common/src/main/scala/io/glutenproject/expression/ExpressionNames.scala
+++ shims/common/src/main/scala/io/glutenproject/expression/ExpressionNames.scala
@@ -202,6 +202,7 @@ object ExpressionNames {
   final val ARRAY_CONTAINS = "array_contains"
   final val ARRAY_MAX = "array_max"
   final val ARRAY_MIN = "array_min"
+  final val ARRAY_JOIN = "array_join"
   final val SORT_ARRAY = "sort_array"
   final val ARRAYS_OVERLAP = "arrays_overlap"
   final val ARRAY_POSITION = "array_position"
