ed7b5b1d7c58c7316b1db45a45e63e1021da01ba
PHILO-HE <feilong.he@intel.com>
2023-07-25 20:16:33 +0800

    Fix after rebase

--- gluten-core/src/main/scala/io/glutenproject/expression/ExpressionConverter.scala
+++ gluten-core/src/main/scala/io/glutenproject/expression/ExpressionConverter.scala
@@ -261,40 +261,41 @@ object ExpressionConverter extends SQLConfHelper with Logging {
   }
 
   def normalFunctionExpressionTransform(
-    expression: Expression,
-    attributes: Seq[Attribute],
-    substraitFunctioName: String): ExpressionTransformer = {
-      if (expression.children.length == 1) {
-        val childTransformer = replaceWithExpressionTransformer(
-          expression.children(0),
-          attributes)
-        new UnaryExpressionTransformer(substraitFunctioName, childTransformer, expression)
-      } else if (expression.children.length == 2) {
-        new BinaryExpressionTransformer(
-          substraitFunctioName,
-          replaceWithExpressionTransformer(expression.children(0), attributes),
-          replaceWithExpressionTransformer(expression.children(1), attributes),
-          expression)
-      } else if (expression.children.length == 3) {
-        new TernaryExpressionTransformer(
-          substraitFunctioName,
-          replaceWithExpressionTransformer(expression.children(0), attributes),
-          replaceWithExpressionTransformer(expression.children(1), attributes),
-          replaceWithExpressionTransformer(expression.children(2), attributes),
-          expression)
-      } else if (expression.children.length == 3) {
-        new QuaternaryExpressionTransformer(
-          substraitFunctioName,
-          replaceWithExpressionTransformer(expression.children(0), attributes),
-          replaceWithExpressionTransformer(expression.children(1), attributes),
-          replaceWithExpressionTransformer(expression.children(2), attributes),
-          replaceWithExpressionTransformer(expression.children(3), attributes),
-          expression)
-      } else {
-        throw new UnsupportedOperationException(
-          s"Unsupported function: ${expression.prettyName} with ${expression.children.length} " +
-            s"children.")
-      }
+      expression: Expression,
+      attributes: Seq[Attribute],
+      substraitFunctioName: String): ExpressionTransformer = {
+    if (expression.children.length == 1) {
+      val childTransformer = replaceWithExpressionTransformer(expression.children(0), attributes)
+      new UnaryExpressionTransformer(substraitFunctioName, childTransformer, expression)
+    } else if (expression.children.length == 2) {
+      new BinaryExpressionTransformer(
+        substraitFunctioName,
+        replaceWithExpressionTransformer(expression.children(0), attributes),
+        replaceWithExpressionTransformer(expression.children(1), attributes),
+        expression
+      )
+    } else if (expression.children.length == 3) {
+      new TernaryExpressionTransformer(
+        substraitFunctioName,
+        replaceWithExpressionTransformer(expression.children(0), attributes),
+        replaceWithExpressionTransformer(expression.children(1), attributes),
+        replaceWithExpressionTransformer(expression.children(2), attributes),
+        expression
+      )
+    } else if (expression.children.length == 3) {
+      new QuaternaryExpressionTransformer(
+        substraitFunctioName,
+        replaceWithExpressionTransformer(expression.children(0), attributes),
+        replaceWithExpressionTransformer(expression.children(1), attributes),
+        replaceWithExpressionTransformer(expression.children(2), attributes),
+        replaceWithExpressionTransformer(expression.children(3), attributes),
+        expression
+      )
+    } else {
+      throw new UnsupportedOperationException(
+        s"Unsupported function: ${expression.prettyName} with ${expression.children.length} " +
+          s"children.")
+    }
   }
 
   def replaceWithExpressionTransformer(
