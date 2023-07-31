580a23acf0651ae2963124359bf8a037c55e4102
Chang chen <chang.chen@kyligence.io>
2023-07-31 12:02:16 +0800

    [GLUTEN-2520][CORE][VL][CH][UT] Refactor: unify java and scala license checker  (#2521)
    * spotless:
    1. Add license header check for java
    2. Revert spotless configuration to make mvn spotless:apply simple,
    
    * Fix style
    
    * fix celeborn style

--- gluten-core/src/main/java/io/glutenproject/memory/memtarget/spark/Spiller.java
+++ gluten-core/src/main/java/io/glutenproject/memory/memtarget/spark/Spiller.java
@@ -14,7 +14,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package io.glutenproject.memory.memtarget.spark;
 
 import org.apache.spark.memory.MemoryConsumer;
--- gluten-core/src/main/java/io/glutenproject/substrait/plan/PlanNode.java
+++ gluten-core/src/main/java/io/glutenproject/substrait/plan/PlanNode.java
@@ -14,7 +14,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package io.glutenproject.substrait.plan;
 
 import io.glutenproject.substrait.extensions.AdvancedExtensionNode;
