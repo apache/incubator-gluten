acc09fd1a1ffd12a55ad295a5c0031600f69fc7d
Cheng Pan <chengpan@apache.org>
2023-08-01 15:59:28 +0800

    nit

--- gluten-celeborn/src/main/java/org/apache/spark/shuffle/gluten/celeborn/CelebornShuffleManager.java
+++ gluten-celeborn/src/main/java/org/apache/spark/shuffle/gluten/celeborn/CelebornShuffleManager.java
@@ -98,7 +98,7 @@ public class CelebornShuffleManager implements ShuffleManager {
           try {
             try {
               Method method =
-                  // for Celeborn 0.3.0, see CELEBORN-798
+                  // for Celeborn 0.3.1 and above, see CELEBORN-804
                   ShuffleClient.class.getDeclaredMethod(
                       "get",
                       String.class,
@@ -117,7 +117,7 @@ public class CelebornShuffleManager implements ShuffleManager {
                           lifecycleManager.getUserIdentifier());
             } catch (NoSuchMethodException noMethod) {
               Method method =
-                  // for Celeborn 0.3.1 and above, see CELEBORN-804
+                  // for Celeborn 0.3.0, see CELEBORN-798
                   ShuffleClient.class.getDeclaredMethod(
                       "get",
                       String.class,
