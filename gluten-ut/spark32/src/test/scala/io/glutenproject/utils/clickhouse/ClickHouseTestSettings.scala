2a26b6ba20d3e0aa9f933948d17c89cc5a9b7e04
Wenzheng Liu <lwz9103@163.com>
2023-08-04 13:51:04 +0800

    [GLUTEN-1632][CH]Daily Update Clickhouse Version (20230804) (#2603)
    * [GLUTEN-1632][CH]Daily Update Clickhouse Version (20230804)
    
    * disable test "range between should accept int/long values as boundary" due to https://github.com/ClickHouse/ClickHouse/pull/52768
    
    Now, Window frame bounds were checked for RANGE.
    
    ---------
    
    Co-authored-by: kyligence-git <gluten@kyligence.io>
    Co-authored-by: Chang Chen <baibaichen@gmail.com>

--- gluten-ut/spark32/src/test/scala/io/glutenproject/utils/clickhouse/ClickHouseTestSettings.scala
+++ gluten-ut/spark32/src/test/scala/io/glutenproject/utils/clickhouse/ClickHouseTestSettings.scala
@@ -448,6 +448,7 @@ class ClickHouseTestSettings extends BackendTestSettings {
   enableSuite[GlutenDataFrameWindowFramesSuite]
     .exclude(
       "rows between should accept int/long values as boundary",
+      "range between should accept int/long values as boundary",
       "reverse preceding/following range between with aggregation",
       "SPARK-24033: Analysis Failure of OffsetWindowFunction"
     )
