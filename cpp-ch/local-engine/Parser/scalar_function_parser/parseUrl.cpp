9b304542aabd656216f42b3886c023d00bfc3d4c
taiyang-li <654010905@qq.com>
2023-08-09 16:40:21 +0800

    rename functions parser files

--- cpp-ch/local-engine/Parser/scalar_function_parser/parseUrl.cpp
+++ cpp-ch/local-engine/Parser/scalar_function_parser/parseUrl.cpp
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-#include "ParseURLParser.h"
+#include "parseUrl.h"
 #include <iterator>
 #include <Common/Exception.h>
 #include <DataTypes/IDataType.h>
