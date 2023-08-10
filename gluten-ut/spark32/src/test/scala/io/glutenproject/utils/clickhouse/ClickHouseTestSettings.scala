65c4a3f60d4c4d32826a66143c1af62a713881a3
exmy <xumovens@gmail.com>
2023-08-10 14:59:02 +0800

    [GLUTEN-2639][CH] Fix log1p with input arg <= -1.0 (#2640)
    What changes were proposed in this pull request?
    (Fixes: #2639)
    
    How was this patch tested?
    UT

--- gluten-ut/spark32/src/test/scala/io/glutenproject/utils/clickhouse/ClickHouseTestSettings.scala
+++ gluten-ut/spark32/src/test/scala/io/glutenproject/utils/clickhouse/ClickHouseTestSettings.scala
@@ -364,10 +364,6 @@ class ClickHouseTestSettings extends BackendTestSettings {
     )
 
   enableSuite[GlutenMathFunctionsSuite]
-    .exclude(
-      "hex",
-      "log1p"
-    )
 
   enableSuite[GlutenComplexTypesSuite]
 
