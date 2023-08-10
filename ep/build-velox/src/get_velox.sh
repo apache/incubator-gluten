f0c1529d5579388787c59d90b0459835aa70b329
zhejiangxiaomai <zhenhui.zhao@intel.com>
2023-08-10 15:05:01 +0800

    test compatible

--- ep/build-velox/src/get_velox.sh
+++ ep/build-velox/src/get_velox.sh
@@ -16,8 +16,8 @@
 
 set -exu
 
-VELOX_REPO=https://github.com/oap-project/velox.git
-VELOX_BRANCH=main
+VELOX_REPO=https://github.com/zhejiangxiaomai/velox.git
+VELOX_BRANCH=compatibleKind
 VELOX_HOME=""
 
 #Set on run gluten on HDFS
