107cdabbef7f56356891cc77690f44e9d3e907bc
jianghaonan <jianghaonan@baidu.com>
2023-08-15 15:12:38 +0800

    [GLUTEN-2706][VL] Replace unnecessary exception to fallback when use velox native parquet write
    Change-Id: I4ec20640bf2e7a3259ec2fa367927432bb02aadc

--- backends-velox/src/test/scala/org/apache/spark/sql/execution/VeloxParquetWriteSuite.scala
+++ backends-velox/src/test/scala/org/apache/spark/sql/execution/VeloxParquetWriteSuite.scala
@@ -78,7 +78,7 @@ class VeloxParquetWriteSuite extends WholeStageTransformerSuite {
         .toDF("id")
         .createOrReplaceTempView("ctas_temp")
       val df = spark.sql("CREATE TABLE velox_ctas USING PARQUET AS SELECT * FROM ctas_temp")
-      Assert.assertFalse(FallbackUtil.isFallback(df.queryExecution.executedPlan))
+      Assert.assertTrue(FallbackUtil.isFallback(df.queryExecution.executedPlan))
     }
   }
 
@@ -91,7 +91,7 @@ class VeloxParquetWriteSuite extends WholeStageTransformerSuite {
           .selectExpr("id as c1", "id % 7 as p")
           .createOrReplaceTempView("temp")
         val df = spark.sql(s"INSERT OVERWRITE DIRECTORY '$path' USING PARQUET SELECT * FROM temp")
-        Assert.assertFalse(FallbackUtil.isFallback(df.queryExecution.executedPlan))
+        Assert.assertTrue(FallbackUtil.isFallback(df.queryExecution.executedPlan))
     }
   }
 
@@ -102,9 +102,9 @@ class VeloxParquetWriteSuite extends WholeStageTransformerSuite {
         .selectExpr("id as c1", "id % 7 as p")
         .createOrReplaceTempView("bucket_temp")
       val df = spark.sql(
-        "CREATE TABLE bucket USING PARQUET " +
-          "AS SELECT * FROM bucket_temp CLUSTERED BY (p) INTO 7 BUCKETS")
-      Assert.assertFalse(FallbackUtil.isFallback(df.queryExecution.executedPlan))
+        "CREATE TABLE bucket USING PARQUET CLUSTERED BY (p) INTO 7 BUCKETS " +
+          "AS SELECT * FROM bucket_temp")
+      Assert.assertTrue(FallbackUtil.isFallback(df.queryExecution.executedPlan))
     }
   }
 
