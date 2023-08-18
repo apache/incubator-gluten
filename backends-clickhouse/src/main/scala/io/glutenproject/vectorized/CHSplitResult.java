6ff6bc149bcde1865446d6c97f163f4eac21985b
liuneng <1398775315@qq.com>
2023-08-18 10:48:36 +0800

    fix style

--- backends-clickhouse/src/main/scala/io/glutenproject/vectorized/CHSplitResult.java
+++ backends-clickhouse/src/main/scala/io/glutenproject/vectorized/CHSplitResult.java
@@ -16,15 +16,30 @@
  */
 package io.glutenproject.vectorized;
 
-import io.glutenproject.vectorized.SplitResult;
-
 public class CHSplitResult extends SplitResult {
     private final long splitTime;
     private final long diskWriteTime;
     private final long serializationTime;
 
-    public CHSplitResult(long totalComputePidTime, long totalWriteTime, long totalEvictTime, long totalCompressTime, long totalBytesWritten, long totalBytesEvicted, long[] partitionLengths, long[] rawPartitionLengths, long splitTime, long diskWriteTime, long serializationTime) {
-        super(totalComputePidTime, totalWriteTime, totalEvictTime, totalCompressTime, totalBytesWritten, totalBytesEvicted, partitionLengths, rawPartitionLengths);
+    public CHSplitResult(long totalComputePidTime,
+                         long totalWriteTime,
+                         long totalEvictTime,
+                         long totalCompressTime,
+                         long totalBytesWritten,
+                         long totalBytesEvicted,
+                         long[] partitionLengths,
+                         long[] rawPartitionLengths,
+                         long splitTime,
+                         long diskWriteTime,
+                         long serializationTime) {
+        super(totalComputePidTime,
+                totalWriteTime,
+                totalEvictTime,
+                totalCompressTime,
+                totalBytesWritten,
+                totalBytesEvicted,
+                partitionLengths,
+                rawPartitionLengths);
         this.splitTime = splitTime;
         this.diskWriteTime = diskWriteTime;
         this.serializationTime = serializationTime;
