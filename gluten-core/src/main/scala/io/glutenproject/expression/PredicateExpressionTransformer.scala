9618cf0940e648fd6521290a27162726dd71e794
rui-mo <rui.mo@intel.com>
2023-08-25 11:15:27 +0800

    Fix

--- gluten-core/src/main/scala/io/glutenproject/expression/PredicateExpressionTransformer.scala
+++ gluten-core/src/main/scala/io/glutenproject/expression/PredicateExpressionTransformer.scala
@@ -19,8 +19,10 @@ package io.glutenproject.expression
 import io.glutenproject.backendsapi.BackendsApiManager
 import io.glutenproject.expression.ConverterUtils.FunctionConfig
 import io.glutenproject.substrait.expression.{ExpressionBuilder, ExpressionNode}
+
 import org.apache.spark.sql.catalyst.expressions._
 import org.apache.spark.sql.types._
+
 import com.google.common.collect.Lists
 
 import scala.collection.JavaConverters._
