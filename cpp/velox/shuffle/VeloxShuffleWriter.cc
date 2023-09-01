22ee6c620e8c04f7e1f4a4410a3693748feb6c21
PHILO-HE <feilong.he@intel.com>
2023-09-01 15:11:56 +0800

    Initial commit

--- cpp/velox/shuffle/VeloxShuffleWriter.cc
+++ cpp/velox/shuffle/VeloxShuffleWriter.cc
@@ -481,6 +481,9 @@ arrow::Status VeloxShuffleWriter::splitFixedWidthValueBuffer(const facebook::vel
     const auto& dstAddrs = partitionFixedWidthValueAddrs_[col];
 
     switch (arrow::bit_width(arrowColumnTypes_[colIdx]->id())) {
+      case 0: // arrow::NullType::type_id:
+        // No value buffer created for NullType.
+        break;
       case 1: // arrow::BooleanType::type_id:
         RETURN_NOT_OK(splitBoolType(srcAddr, dstAddrs));
         break;
@@ -935,6 +938,16 @@ arrow::Status VeloxShuffleWriter::allocatePartitionBuffer(uint32_t partitionId, 
         partitionBinaryAddrs_[binaryIdx][partitionId] =
             BinaryBuf(valueBuffer->mutable_data(), lengthBuffer->mutable_data(), valueBufferSize);
         buffers = {std::move(validityBuffer), std::move(lengthBuffer), std::move(valueBuffer)};
+        break;
+      }
+      case arrow::NullType::type_id: {
+        std::shared_ptr<arrow::ResizableBuffer> validityBuffer{};
+        ARROW_ASSIGN_OR_RAISE(validityBuffer, arrow::AllocateResizableBuffer(newSize, partitionBufferPool_.get()));
+        // initialize all as false.
+        memset(validityBuffer->mutable_data(), 0, validityBuffer->capacity());
+        partitionValidityAddrs_[i][partitionId] = validityBuffer->mutable_data();
+        // No need to create valueBuffer for NullType.
+        partitionBuffers_[i][partitionId] = {std::move(validityBuffer), nullptr};
         break;
       }
       default: { // fixed-width types
