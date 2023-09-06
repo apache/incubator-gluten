64e4964e71217092c4ec6af84598c159584429d7
xiyu.zk <xiyu.zk@alibaba-inc.com>
2023-09-06 17:39:51 +0800

    [CELEBORN] Unify the celeborn shuffle manager of velox and ch

--- backends-clickhouse/src/main/scala/io/glutenproject/backendsapi/clickhouse/CHSparkPlanExecApi.scala
+++ backends-clickhouse/src/main/scala/io/glutenproject/backendsapi/clickhouse/CHSparkPlanExecApi.scala
@@ -16,6 +16,7 @@
  */
 package io.glutenproject.backendsapi.clickhouse
 
+import io.glutenproject.GlutenConfig
 import io.glutenproject.backendsapi.SparkPlanExecApi
 import io.glutenproject.execution._
 import io.glutenproject.expression._
@@ -52,6 +53,7 @@ import org.apache.spark.sql.types.StructType
 import org.apache.spark.sql.vectorized.ColumnarBatch
 
 import com.google.common.collect.Lists
+import org.apache.commons.lang3.ClassUtils
 
 import java.{lang, util}
 
@@ -231,10 +233,17 @@ class CHSparkPlanExecApi extends SparkPlanExecApi {
   override def createColumnarBatchSerializer(
       schema: StructType,
       metrics: Map[String, SQLMetric]): Serializer = {
-    new CHColumnarBatchSerializer(
-      metrics("avgReadBatchNumRows"),
-      metrics("numOutputRows"),
-      metrics("dataSize"))
+    val readBatchNumRows = metrics("avgReadBatchNumRows")
+    val numOutputRows = metrics("numOutputRows")
+    val dataSize = metrics("dataSize")
+    if (GlutenConfig.getConf.isUseCelebornShuffleManager) {
+      val clazz = ClassUtils.getClass("org.apache.spark.shuffle.CHCelebornColumnarBatchSerializer")
+      val constructor =
+        clazz.getConstructor(classOf[SQLMetric], classOf[SQLMetric], classOf[SQLMetric])
+      constructor.newInstance(readBatchNumRows, numOutputRows, dataSize).asInstanceOf[Serializer]
+    } else {
+      new CHColumnarBatchSerializer(readBatchNumRows, numOutputRows, dataSize)
+    }
   }
 
   /** Create broadcast relation for BroadcastExchangeExec */
---
+++ gluten-celeborn/clickhouse/src/test/resources/queries/tpch-queries-ch/q14.sql
@@ -0,0 +1,14 @@
+SELECT /*+ SHUFFLE_HASH(lineitem) */
+    100.00 * sum(
+        CASE WHEN p_type LIKE 'PROMO%' THEN
+            l_extendedprice * (1 - l_discount)
+        ELSE
+            0
+        END) / sum(l_extendedprice * (1 - l_discount)) AS promo_revenue
+FROM
+    lineitem,
+    part
+WHERE
+    l_partkey = p_partkey
+    AND l_shipdate >= date'1995-09-01'
+    AND l_shipdate < date'1995-09-01' + interval 1 month;
