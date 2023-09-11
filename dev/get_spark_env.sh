e8a52318dba83f75486eadd7080eaea07bfa3fde
xieqi <qi.xie@intel.com>
2023-09-11 21:32:49 +0800

    [GLUTEN-2858][VL] Use custom docker images for CI (#2869)
    To optimize velox backend CI, a custom docker image is introduced, with ccache, maven cache & vcpkg cache pre-installed, spark 3.2/3.3 pre-built, etc.

--- dev/get_spark_env.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if [ ! -d spark331 ];
-then  
-  git clone --depth 1 --branch v3.3.1 https://github.com/apache/spark.git spark331 && cd spark331 && ./build/mvn -Pyarn -DskipTests install 
-else  
-  echo "Skip Spark331 build" 
-fi
