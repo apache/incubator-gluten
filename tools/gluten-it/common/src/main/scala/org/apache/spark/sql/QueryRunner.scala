1f1d0cd71a6556913964edc211c2af88e42c39c6
Hongze Zhang <hongze.zhang@intel.com>
2023-09-13 16:21:15 +0800

    kill rate

--- tools/gluten-it/common/src/main/scala/org/apache/spark/sql/QueryRunner.scala
+++ tools/gluten-it/common/src/main/scala/org/apache/spark/sql/QueryRunner.scala
@@ -16,6 +16,7 @@
  */
 package org.apache.spark.sql
 
+import org.apache.spark.SparkContext
 import org.apache.spark.executor.ExecutorMetrics
 import org.apache.spark.scheduler.{SparkListener, SparkListenerExecutorMetricsUpdate, SparkListenerTaskStart}
 
@@ -24,6 +25,7 @@ import org.apache.commons.lang3.RandomUtils
 
 import java.io.ByteArrayOutputStream
 import java.nio.charset.StandardCharsets
+import java.util.concurrent.atomic.AtomicInteger
 
 object QueryRunner {
   private val availableExecutorMetrics: Set[String] = Set(
@@ -59,42 +61,20 @@ object QueryRunner {
     }
     val sc = spark.sparkContext
     sc.setJobDescription(desc)
+
+    // metrics listener
     val em = new ExecutorMetrics()
-    val metricsListener = new SparkListener {
-      override def onExecutorMetricsUpdate(
-          executorMetricsUpdate: SparkListenerExecutorMetricsUpdate): Unit = {
-        executorMetricsUpdate.executorUpdates.foreach {
-          case (_, peakUpdates) =>
-            em.compareAndUpdatePeakValues(peakUpdates)
-        }
-        super.onExecutorMetricsUpdate(executorMetricsUpdate)
-      }
-    }
+    val metricsListener = new MetricsListener(em)
     sc.addSparkListener(metricsListener)
-    if (randomKillTasks) {
-      sc.addSparkListener(new SparkListener {
-        override def onTaskStart(taskStart: SparkListenerTaskStart): Unit = {
-          val killer = new Thread {
-            override def run(): Unit = {
-              // TODO make this configurable
-              // After 1s - 10s, kill the task
-              val waitMs = RandomUtils.nextLong(1000L, 10000L)
-              Thread.sleep(waitMs)
-              // We have 20% chance to kill the task. Otherwise let the task run
-              if (RandomUtils.nextFloat(0.0f, 1.0f) < 0.2f) {
-                if (sc.isStopped) {
-                  return
-                }
-                println(s"Killing task attempt after $waitMs ms: ${taskStart.taskInfo.taskId}")
-                sc.killTaskAttempt(taskStart.taskInfo.taskId, interruptThread = true)
-              }
-            }
-          }
-          killer.setDaemon(true)
-          killer.start()
-        }
-      })
+
+    // kill task listener
+    val killTaskListener: Option[KillTaskListener] = if (randomKillTasks) {
+      Some(new KillTaskListener(sc))
+    } else {
+      None
     }
+    killTaskListener.foreach(sc.addSparkListener(_))
+
     println(s"Executing SQL query from resource path $queryPath...")
     try {
       val sql = resourceToString(queryPath)
@@ -109,6 +89,12 @@ object QueryRunner {
       RunResult(rows, millis, collectedMetrics)
     } finally {
       sc.removeSparkListener(metricsListener)
+      killTaskListener.foreach(
+        l => {
+          sc.removeSparkListener(l)
+          println(s"Success kill rate ${"%.2f%%".format(
+              100 * l.successRate())} during execution of app: ${sc.applicationId}")
+        })
       sc.setJobDescription(null)
     }
   }
@@ -135,3 +121,48 @@ object QueryRunner {
 }
 
 case class RunResult(rows: Seq[Row], executionTimeMillis: Long, metrics: Map[String, Long])
+
+class MetricsListener(em: ExecutorMetrics) extends SparkListener {
+  override def onExecutorMetricsUpdate(
+      executorMetricsUpdate: SparkListenerExecutorMetricsUpdate): Unit = {
+    executorMetricsUpdate.executorUpdates.foreach {
+      case (_, peakUpdates) =>
+        em.compareAndUpdatePeakValues(peakUpdates)
+    }
+    super.onExecutorMetricsUpdate(executorMetricsUpdate)
+  }
+}
+
+class KillTaskListener(val sc: SparkContext) extends SparkListener {
+  private val killCount = new AtomicInteger(0)
+  private val successCount = new AtomicInteger(0)
+
+  override def onTaskStart(taskStart: SparkListenerTaskStart): Unit = {
+    val killer = new Thread {
+      override def run(): Unit = {
+        // TODO make this configurable
+        // After 1s - 10s, kill the task
+        val waitMs = RandomUtils.nextLong(1000L, 10000L)
+        Thread.sleep(waitMs)
+        // We have 20% chance to kill the task. Otherwise let the task run
+        if (RandomUtils.nextFloat(0.0f, 1.0f) < 0.2f) {
+          if (sc.isStopped) {
+            return
+          }
+          println(s"Killing task attempt after $waitMs ms: ${taskStart.taskInfo.taskId}")
+          val succeeded = sc.killTaskAttempt(taskStart.taskInfo.taskId, interruptThread = true)
+          if (succeeded) {
+            successCount.getAndIncrement()
+          }
+          killCount.getAndIncrement()
+        }
+      }
+    }
+    killer.setDaemon(true)
+    killer.start()
+  }
+
+  def successRate(): Float = {
+    successCount.get().asInstanceOf[Float] / killCount.get()
+  }
+}
