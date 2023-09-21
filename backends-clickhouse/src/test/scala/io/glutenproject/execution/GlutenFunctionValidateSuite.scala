12188dc6aceba2a06af6087577fab617389f006c
taiyang-li <654010905@qq.com>
2023-09-21 11:11:40 +0800

    fix uts

--- backends-clickhouse/src/test/scala/io/glutenproject/execution/GlutenFunctionValidateSuite.scala
+++ backends-clickhouse/src/test/scala/io/glutenproject/execution/GlutenFunctionValidateSuite.scala
@@ -419,27 +419,15 @@ class GlutenFunctionValidateSuite extends WholeStageTransformerSuite {
     withSQLConf(
       SQLConf.OPTIMIZER_EXCLUDED_RULES.key ->
         (ConstantFolding.ruleName + "," + NullPropagation.ruleName)) {
-      // Test decode with 'US-ASCII'
+      // Test codec with 'US-ASCII'
       runQueryAndCompare(
-        "SELECT decode(X'537061726B2053514C', 'US-ASCII')",
+        "SELECT decode(encode('Spark SQL', 'US-ASCII'), 'US-ASCII')",
         noFallBack = false
       )(checkOperatorMatch[ProjectExecTransformer])
 
-      // Test decode with 'UTF-16'
+      // Test codec with 'UTF-16'
       runQueryAndCompare(
-        "SELECT decode(X'FEFF0053007000610072006B002000530051004C', 'UTF-16')",
-        noFallBack = false
-      )(checkOperatorMatch[ProjectExecTransformer])
-
-      // Test encode with 'US-ASCII'
-      runQueryAndCompare(
-        "SELECT hex(encode('Spark SQL', 'US-ASCII'))",
-        noFallBack = false
-      )(checkOperatorMatch[ProjectExecTransformer])
-
-      // Test encode with 'UTF-16'
-      runQueryAndCompare(
-        "SELECT hex(encode('Spark SQL', 'UTF-16'))",
+        "SELECT decode(encode('Spark SQL', 'UTF-16'), 'UTF-16')",
         noFallBack = false
       )(checkOperatorMatch[ProjectExecTransformer])
     }
