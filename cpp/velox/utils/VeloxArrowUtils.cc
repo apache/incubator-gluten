bd8b03f5163c02ec194d3654518a25902304a246
rui-mo <rui.mo@intel.com>
2023-10-13 14:23:53 +0800

    Fix comments

--- cpp/velox/utils/VeloxArrowUtils.cc
+++ cpp/velox/utils/VeloxArrowUtils.cc
@@ -58,7 +58,7 @@ arrow::Status MyMemoryPool::Allocate(int64_t size, int64_t alignment, uint8_t** 
   if (bytes_allocated() + size > capacity_) {
     return arrow::Status::OutOfMemory("malloc of size ", size, " failed");
   }
-  RETURN_NOT_OK(pool_->Allocate(size, out));
+  RETURN_NOT_OK(pool_->Allocate(size, alignment, out));
   stats_.UpdateAllocatedBytes(size);
   return arrow::Status::OK();
 }
@@ -67,14 +67,13 @@ arrow::Status MyMemoryPool::Reallocate(int64_t oldSize, int64_t newSize, int64_t
   if (newSize > capacity_) {
     return arrow::Status::OutOfMemory("malloc of size ", newSize, " failed");
   }
-  // auto old_ptr = *ptr;
-  RETURN_NOT_OK(pool_->Reallocate(oldSize, newSize, ptr));
+  RETURN_NOT_OK(pool_->Reallocate(oldSize, newSize, alignment, ptr));
   stats_.UpdateAllocatedBytes(newSize - oldSize);
   return arrow::Status::OK();
 }
 
 void MyMemoryPool::Free(uint8_t* buffer, int64_t size, int64_t alignment) {
-  pool_->Free(buffer, size);
+  pool_->Free(buffer, size, alignment);
   stats_.UpdateAllocatedBytes(-size);
 }
 
@@ -86,16 +85,16 @@ int64_t MyMemoryPool::max_memory() const {
   return pool_->max_memory();
 }
 
-std::string MyMemoryPool::backend_name() const {
-  return pool_->backend_name();
-}
-
 int64_t MyMemoryPool::total_bytes_allocated() const {
   return pool_->total_bytes_allocated();
 }
 
 int64_t MyMemoryPool::num_allocations() const {
   throw pool_->num_allocations();
+}
+
+std::string MyMemoryPool::backend_name() const {
+  return pool_->backend_name();
 }
 
 } // namespace gluten
