d571c1f026bd4c4e1590a2e7b7c390862954fe93
Rui Mo <rui.mo@intel.com>
2023-10-26 13:23:52 +0800

    [CORE] Simplify hasNext and next functions of iterator (#3490)

--- gluten-core/src/main/java/io/glutenproject/vectorized/GeneralInIterator.java
+++ gluten-core/src/main/java/io/glutenproject/vectorized/GeneralInIterator.java
@@ -22,27 +22,19 @@ import java.util.Iterator;
 
 public abstract class GeneralInIterator implements AutoCloseable {
   protected final Iterator<ColumnarBatch> delegated;
-  private transient ColumnarBatch nextBatch = null;
 
   public GeneralInIterator(Iterator<ColumnarBatch> delegated) {
     this.delegated = delegated;
   }
 
   public boolean hasNext() {
-    while (delegated.hasNext()) {
-      nextBatch = delegated.next();
-      if (nextBatch.numRows() > 0) {
-        // any problem using delegated.hasNext() instead?
-        return true;
-      }
-    }
-    return false;
+    return delegated.hasNext();
   }
 
   @Override
   public void close() throws Exception {}
 
   public ColumnarBatch nextColumnarBatch() {
-    return nextBatch;
+    return delegated.next();
   }
 }
