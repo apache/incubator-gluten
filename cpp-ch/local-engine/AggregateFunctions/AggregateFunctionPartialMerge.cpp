7bc49e61efbaf3bf5c7bbf7836702861aba9e235
Chang Chen <baibaichen@gmail.com>
2023-10-31 08:26:29 +0800

    fix build due to https://github.com/ClickHouse/ClickHouse/pull/54947

--- cpp-ch/local-engine/AggregateFunctions/AggregateFunctionPartialMerge.cpp
+++ cpp-ch/local-engine/AggregateFunctions/AggregateFunctionPartialMerge.cpp
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-#include <AggregateFunctions/AggregateFunctionCombinatorFactory.h>
+#include <AggregateFunctions/Combinators/AggregateFunctionCombinatorFactory.h>
 #include <AggregateFunctions/AggregateFunctionPartialMerge.h>
 #include <DataTypes/DataTypeAggregateFunction.h>
 
