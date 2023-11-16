7acf55bfe0d39e3b52d4a08c00ff9e5aaab96dfa
FelixYBW <binwei.yang@intel.com>
2023-11-15 23:34:16 -0800

    rebase to velox 2023-11-14

--- ep/build-velox/src/get_velox.sh
+++ ep/build-velox/src/get_velox.sh
@@ -16,8 +16,8 @@
 
 set -exu
 
-VELOX_REPO=https://github.com/oap-project/velox.git
-VELOX_BRANCH=2023-11-14
+VELOX_REPO=https://github.com/felixybw/velox.git
+VELOX_BRANCH=2023-11-15
 VELOX_HOME=""
 
 #Set on run gluten on HDFS
