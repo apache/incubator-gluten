f21a96f727ab8368d1de0de25101b41c61d58038
KevinyhZou <37431499+KevinyhZou@users.noreply.github.com>
2023-11-17 11:14:21 +0800

    [GLUTEN-3692][CH]Fix count no result on empty table (#3693)
    What changes were proposed in this pull request?
    (Please fill in changes proposed in this fix)
    
    (Fixes: #3692)
    
    How was this patch tested?
    TEST BY UT

--- cpp-ch/local-engine/Parser/SerializedPlanParser.cpp
+++ cpp-ch/local-engine/Parser/SerializedPlanParser.cpp
@@ -61,9 +61,11 @@
 #include <Parsers/ExpressionListParsers.h>
 #include <Processors/Executors/PullingAsyncPipelineExecutor.h>
 #include <Processors/Formats/Impl/ArrowBlockOutputFormat.h>
+#include <Processors/QueryPlan/AggregatingStep.h>
 #include <Processors/QueryPlan/ExpressionStep.h>
 #include <Processors/QueryPlan/FilterStep.h>
 #include <Processors/QueryPlan/LimitStep.h>
+#include <Processors/QueryPlan/MergingAggregatedStep.h>
 #include <Processors/QueryPlan/Optimizations/QueryPlanOptimizationSettings.h>
 #include <Processors/QueryPlan/QueryPlan.h>
 #include <Processors/QueryPlan/ReadFromPreparedSource.h>
@@ -2232,21 +2234,19 @@ bool LocalExecutor::checkAndSetDefaultBlock(size_t current_block_columns, bool h
     {
         return has_next_blocks;
     }
-    auto cols = currentBlock().getColumnsWithTypeAndName();
-    for (const auto & col : cols)
+    bool should_set_default_value = false;
+    for (auto p :  query_pipeline.getProcessors())
     {
-        String col_name = col.name;
-        DataTypePtr col_type = col.type;
-        if (col_name.compare(0, 4, "sum#") != 0 && col_name.compare(0, 4, "max#") != 0 && col_name.compare(0, 4, "min#") != 0
-            && col_name.compare(0, 6, "count#") != 0)
+        if (p->getName() == "MergingAggregatedTransform")
         {
-            return false;
-        }
-        if (!isInteger(col_type) && !col_type->isNullable())
-        {
-            return false;
+            DB::MergingAggregatedStep * agg_step = static_cast<DB::MergingAggregatedStep *>(p->getQueryPlanStep());
+            auto query_params = agg_step->getParams();
+            should_set_default_value = query_params.keys_size == 0;
         }
     }
+    if (!should_set_default_value)
+        return false;
+    auto cols = currentBlock().getColumnsWithTypeAndName();
     for (size_t i = 0; i < cols.size(); i++)
     {
         const DB::ColumnWithTypeAndName col = cols[i];
