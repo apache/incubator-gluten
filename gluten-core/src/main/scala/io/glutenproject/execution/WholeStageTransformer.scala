b0aba6673fbba6785adb245a9fe6c6e6dc626b4d
joey.ljy <joey.ljy@alibaba-inc.com>
2023-11-17 13:20:51 +0800

    remove distinct

--- gluten-core/src/main/scala/io/glutenproject/execution/WholeStageTransformer.scala
+++ gluten-core/src/main/scala/io/glutenproject/execution/WholeStageTransformer.scala
@@ -256,7 +256,7 @@ case class WholeStageTransformer(child: SparkPlan, materializeInput: Boolean = f
             GlutenPartition(
               index,
               substraitPlan.toByteArray,
-              splitInfos.flatMap(_.preferredLocations().asScala).distinct.toArray)
+              splitInfos.flatMap(_.preferredLocations().asScala).toArray)
         }
         (wsCxt, substraitPlanPartitions)
       }(
