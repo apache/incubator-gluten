50132d0395a8caf646e4d53c1d6d0c06af65f716
Hongze Zhang <hongze.zhang@intel.com>
2023-11-29 12:12:31 +0800

    Update get_velox.sh

--- ep/build-velox/src/get_velox.sh
+++ ep/build-velox/src/get_velox.sh
@@ -16,8 +16,8 @@
 
 set -exu
 
-VELOX_REPO=https://github.com/zhztheplayer/velox.git
-VELOX_BRANCH=tmp
+VELOX_REPO=https://github.com/oap-velox/velox.git
+VELOX_BRANCH=update
 VELOX_HOME=""
 
 #Set on run gluten on HDFS
