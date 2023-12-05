ffb0cb355dedbf661eda7198a72fc4151e7bb675
ulysses-you <ulyssesyou18@gmail.com>
2023-12-05 16:46:18 +0800

    add test

--- backends-velox/src/test/scala/io/glutenproject/execution/VeloxMetricsSuite.scala
+++ backends-velox/src/test/scala/io/glutenproject/execution/VeloxMetricsSuite.scala
@@ -89,4 +89,41 @@ class VeloxMetricsSuite extends VeloxWholeStageTransformerSuite with AdaptiveSpa
       }
     }
   }
+
+  test("test shuffle hash join metrics") {
+    withSQLConf(SQLConf.AUTO_BROADCASTJOIN_THRESHOLD.key -> "-1") {
+      // without preproject
+      runQueryAndCompare(
+        "SELECT * FROM metrics_t1 join metrics_t2 on metrics_t1.c1 = metrics_t2.c1"
+      ) {
+        df =>
+          val smj = find(df.queryExecution.executedPlan) {
+            case _: ShuffledHashJoinExecTransformer => true
+            case _ => false
+          }
+          assert(smj.isDefined)
+          val metrics = smj.get.metrics
+          assert(metrics("numOutputRows").value == 100)
+          assert(metrics("numOutputVectors").value > 0)
+          assert(metrics("numOutputBytes").value > 0)
+      }
+
+      // with preproject
+      runQueryAndCompare(
+        "SELECT * FROM metrics_t1 join metrics_t2 on metrics_t1.c1 + 1 = metrics_t2.c1 + 1"
+      ) {
+        df =>
+          val smj = find(df.queryExecution.executedPlan) {
+            case _: ShuffledHashJoinExecTransformer => true
+            case _ => false
+          }
+          assert(smj.isDefined)
+          val metrics = smj.get.metrics
+          assert(metrics("numOutputRows").value == 100)
+          assert(metrics("numOutputVectors").value > 0)
+          assert(metrics("streamPreProjectionCpuCount").value > 0)
+          assert(metrics("buildPreProjectionCpuCount").value > 0)
+      }
+    }
+  }
 }
