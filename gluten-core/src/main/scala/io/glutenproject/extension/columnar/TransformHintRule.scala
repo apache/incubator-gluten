cbe55314ab96e810ae35a781d921defebd702310
ulysses-you <ulyssesyou18@gmail.com>
2023-12-06 11:18:52 +0800

    Add back SparkShimLoader import

--- gluten-core/src/main/scala/io/glutenproject/extension/columnar/TransformHintRule.scala
+++ gluten-core/src/main/scala/io/glutenproject/extension/columnar/TransformHintRule.scala
@@ -20,6 +20,7 @@ import io.glutenproject.GlutenConfig
 import io.glutenproject.backendsapi.BackendsApiManager
 import io.glutenproject.execution._
 import io.glutenproject.extension.{GlutenPlan, ValidationResult}
+import io.glutenproject.sql.shims.SparkShimLoader
 import io.glutenproject.utils.PhysicalPlanSelector
 
 import org.apache.spark.api.python.EvalPythonExecTransformer
