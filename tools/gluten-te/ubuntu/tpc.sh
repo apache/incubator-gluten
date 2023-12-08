b85a7bfd58970c14d8553f80eccc2b1b2377e2a4
Hongze Zhang <hongze.zhang@intel.com>
2023-12-08 13:59:00 +0800

    fixup

--- tools/gluten-te/ubuntu/tpc.sh
+++ tools/gluten-te/ubuntu/tpc.sh
@@ -108,7 +108,7 @@ then
   JAVA_ARGS="$JAVA_ARGS -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=$JDWP_SERVER_PORT"
 fi
 JAVA_ARGS="$JAVA_ARGS $EXTRA_JAVA_OPTIONS"
-JAVA_ARGS="$JAVA_ARGS -cp /opt/gluten/tools/gluten-it/target/gluten-it-1.0-SNAPSHOT-jar-with-dependencies.jar"
+JAVA_ARGS="$JAVA_ARGS -cp /opt/gluten/tools/gluten-it/package/target/lib/*"
 JAVA_ARGS="$JAVA_ARGS io.glutenproject.integration.tpc.Tpc $TPC_CMD_ARGS"
 
 BASH_ARGS=
