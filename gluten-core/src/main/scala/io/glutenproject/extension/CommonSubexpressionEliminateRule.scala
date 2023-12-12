8d2d28c53ce00b64f6b199a9d7b9f4c8d1acaa13
taiyang-li <654010905@qq.com>
2023-12-12 14:15:23 +0800

    finish poc for project

--- gluten-core/src/main/scala/io/glutenproject/extension/CommonSubexpressionEliminateRule.scala
+++ gluten-core/src/main/scala/io/glutenproject/extension/CommonSubexpressionEliminateRule.scala
@@ -23,9 +23,13 @@ import org.apache.spark.sql.catalyst.plans.logical._
 import org.apache.spark.sql.catalyst.rules.Rule
 import org.apache.spark.sql.internal.SQLConf
 
+import scala.collection.mutable
+
 class CommonSubexpressionEliminateRule(session: SparkSession, conf: SQLConf)
   extends Rule[LogicalPlan]
   with Logging {
+
+  case class AliasAndAttribute(alias: Alias, attribute: Attribute)
 
   override def apply(plan: LogicalPlan): LogicalPlan = {
     if (plan.resolved) {
@@ -42,8 +46,20 @@ class CommonSubexpressionEliminateRule(session: SparkSession, conf: SQLConf)
       other.withNewChildren(children)
   }
 
+  private def replaceWithSubExprEliminationExprs(
+      expr: Expression,
+      subExprEliminationExprs: mutable.HashMap[ExpressionEquals, AliasAndAttribute]): Expression = {
+    val exprEquals = subExprEliminationExprs.get(ExpressionEquals(expr))
+    if (exprEquals.isDefined) {
+      exprEquals.get.attribute
+    } else {
+      expr.mapChildren(replaceWithSubExprEliminationExprs(_, subExprEliminationExprs))
+    }
+  }
+
   private def visitProject(project: Project): Project = {
-    val input = project.child
+    // scalastyle:off println
+
     val equivalentExpressions: EquivalentExpressions = new EquivalentExpressions
     project.projectList.foreach(equivalentExpressions.addExprTree(_))
 
@@ -54,27 +70,28 @@ class CommonSubexpressionEliminateRule(session: SparkSession, conf: SQLConf)
     }
 
     // Put the common expressions into a hash map
-    var subExprEliminationExprs = Set.empty[ExpressionEquals]
+    val subExprEliminationExprs = mutable.HashMap.empty[ExpressionEquals, AliasAndAttribute]
     commonExprs.foreach {
       expr =>
-        val exprEquals = new ExpressionEquals(expr)
-        subExprEliminationExprs += exprEquals
+        val exprEquals = ExpressionEquals(expr)
+        val alias = Alias(expr, expr.toString)()
+        val attribute = alias.toAttribute
+        subExprEliminationExprs.put(exprEquals, AliasAndAttribute(alias, attribute))
     }
+    println(s"subExprEliminationExprs: $subExprEliminationExprs")
+
+    // Generate a pre-project operator
+    val input = project.child
+    var preProjectList = subExprEliminationExprs.values.map(_.alias).toSeq ++ input.output
+    val preProject = Project(preProjectList, input)
+    println(s"preproject: $preProject")
 
     // Replace the common expressions with the first expression that produces it.
-    var newProjectList = project.projectList.map(_.transformDown {
-      case expr: Expression =>
-        val exprEquals = subExprEliminationExprs.find(_.equals(ExpressionEquals(expr)))
-        if (exprEquals.isDefined) {
-          Alias(exprEquals.get.e, expr.toString())()
-        } else {
-          expr
-        }
-      case other => other
-    }).map(_.asInstanceOf[NamedExpression])
-
-    newProjectList ++= subExprEliminationExprs.map(_.e.asInstanceOf[NamedExpression]).toSeq
-
-    Project(newProjectList, input)
+    var newProjectList = project.projectList
+      .map(replaceWithSubExprEliminationExprs(_, subExprEliminationExprs))
+      .map(_.asInstanceOf[NamedExpression])
+    println(s"projectList: $newProjectList")
+    Project(newProjectList, preProject)
+    // scalastyle:on println
   }
 }
