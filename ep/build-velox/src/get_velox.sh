0118d44a1e28843f6c0c9c1c1ec90223568e0a47
Hongze Zhang <hongze.zhang@intel.com>
2023-12-14 12:18:26 +0800

    [VL] Daily Update Velox Version (20231214)

--- ep/build-velox/src/get_velox.sh
+++ ep/build-velox/src/get_velox.sh
@@ -16,8 +16,8 @@
 
 set -exu
 
-VELOX_REPO=https://github.com/oap-project/velox.git
-VELOX_BRANCH=update
+VELOX_REPO=https://github.com/zhztheplayer/velox.git
+VELOX_BRANCH=wip-rebase-231214
 VELOX_HOME=""
 
 #Set on run gluten on HDFS
