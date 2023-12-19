3f699b00f11a3f28cb0e9dd13a992e2542f02f63
ulysses-you <ulyssesyou18@gmail.com>
2023-12-19 20:04:13 +0800

    Initialize velox global memory manager

--- cpp/velox/compute/VeloxBackend.cc
+++ cpp/velox/compute/VeloxBackend.cc
@@ -394,6 +394,8 @@ std::unique_ptr<VeloxBackend> VeloxBackend::instance_ = nullptr;
 
 void VeloxBackend::create(const std::unordered_map<std::string, std::string>& conf) {
   instance_ = std::unique_ptr<VeloxBackend>(new gluten::VeloxBackend(conf));
+  // initialize the global memory manager for current process
+  facebook::velox::memory::MemoryManager::initialize({});
 }
 
 VeloxBackend* VeloxBackend::get() {
