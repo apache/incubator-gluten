e56dbc5771690c9014bde971c35c80a6ed97588a
taiyang-li <654010905@qq.com>
2023-12-21 14:47:32 +0800

    fix failed uts

--- backends-clickhouse/src/test/scala/io/glutenproject/execution/GlutenClickHouseTPCHParquetSuite.scala
+++ backends-clickhouse/src/test/scala/io/glutenproject/execution/GlutenClickHouseTPCHParquetSuite.scala
@@ -2257,15 +2257,12 @@ class GlutenClickHouseTPCHParquetSuite extends GlutenClickHouseTPCHAbstractSuite
     val sql =
       """
         |select p_partkey from (
-        |  select *, row_number() over (partition by p_partkey order by is_new) as rank from(
+        |    select *, row_number() over (partition by p_partkey order by is_new) as rank from(
         |    select p_partkey, 0 as is_new from part where p_partkey is not null
         |    union all
-        |    select p_partkey, p_size as is_new from (
-        |      select p_partkey, p_size,
-        |        row_number() over (partition by p_name order by p_partkey) as rn from part
-        |    ) t0 where rn = 1
+        |    select p_partkey, p_partkey%2 as is_new from part where p_partkey is not null
         |  ) t1
-        |) t2 where rank = 1
+        |) t2 where rank = 1 order by p_partkey limit 100
         |""".stripMargin
     runQueryAndCompare(sql)({ _ => })
   }
