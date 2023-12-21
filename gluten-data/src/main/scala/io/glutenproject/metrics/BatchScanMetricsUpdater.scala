bf4bc95d17cc5d9f6bd93c5fd241855d730c31fb
Zhen Li <10524738+zhli1142015@users.noreply.github.com>
2023-12-21 12:11:50 +0800

    [GLUTEN-4125][VL] add scan preload splits metric (#4126)

--- gluten-data/src/main/scala/io/glutenproject/metrics/BatchScanMetricsUpdater.scala
+++ gluten-data/src/main/scala/io/glutenproject/metrics/BatchScanMetricsUpdater.scala
@@ -50,6 +50,7 @@ class BatchScanMetricsUpdater(val metrics: Map[String, SQLMetric]) extends Metri
       metrics("processedStrides") += operatorMetrics.processedStrides
       metrics("remainingFilterTime") += operatorMetrics.remainingFilterTime
       metrics("ioWaitTime") += operatorMetrics.ioWaitTime
+      metrics("preloadSplits") += operatorMetrics.preloadSplits
     }
   }
 }
