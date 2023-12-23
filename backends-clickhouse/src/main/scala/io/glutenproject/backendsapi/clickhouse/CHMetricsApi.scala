98be6c60f676a4261b5aeac8ab80d2a50afea525
Vivek Bhaskar <vivekwiz@apache.org>
2023-12-23 08:44:12 +0530

    Updated subquery metrics in CH

--- backends-clickhouse/src/main/scala/io/glutenproject/backendsapi/clickhouse/CHMetricsApi.scala
+++ backends-clickhouse/src/main/scala/io/glutenproject/backendsapi/clickhouse/CHMetricsApi.scala
@@ -317,6 +317,7 @@ class CHMetricsApi extends MetricsApi with Logging with LogLevelUtil {
       sparkContext: SparkContext): Map[String, SQLMetric] =
     Map(
       "dataSize" -> SQLMetrics.createMetric(sparkContext, "data size (bytes)"),
+      "numOutputRows" -> SQLMetrics.createMetric(sparkContext, "number of output rows"),
       "collectTime" -> SQLMetrics.createMetric(sparkContext, "time to collect (ms)"))
 
   override def genHashJoinTransformerMetrics(sparkContext: SparkContext): Map[String, SQLMetric] =
