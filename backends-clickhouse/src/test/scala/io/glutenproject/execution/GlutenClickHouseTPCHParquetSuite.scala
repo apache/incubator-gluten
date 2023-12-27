6bd9ae6293119f54b9f5d4eae3a36550390b791a
lgbo <lgbo.ustc@gmail.com>
2023-12-27 13:15:07 +0800

    [GLUTEN-4190][CH] Crush on flattening a const null column
    What changes were proposed in this pull request?
    (Please fill in changes proposed in this fix)
    
    Fixes: #4190
    
    How was this patch tested?
    (Please explain how this patch was tested. E.g. unit tests, integration tests, manual tests)
    
    unit tests
    
    (If this patch involves UI changes, please attach a screenshot; otherwise, remove this)

--- backends-clickhouse/src/test/scala/io/glutenproject/execution/GlutenClickHouseTPCHParquetSuite.scala
+++ backends-clickhouse/src/test/scala/io/glutenproject/execution/GlutenClickHouseTPCHParquetSuite.scala
@@ -2267,5 +2267,14 @@ class GlutenClickHouseTPCHParquetSuite extends GlutenClickHouseTPCHAbstractSuite
         |""".stripMargin
     runQueryAndCompare(sql)({ _ => })
   }
+
+  test("GLUTEN-4190: crush on flattening a const null column") {
+    val sql =
+      """
+        | select n_nationkey, rank() over (partition by n_regionkey, null order by n_nationkey)
+        |from nation
+        |""".stripMargin
+    runQueryAndCompare(sql)({ _ => })
+  }
 }
 // scalastyle:on line.size.limit
