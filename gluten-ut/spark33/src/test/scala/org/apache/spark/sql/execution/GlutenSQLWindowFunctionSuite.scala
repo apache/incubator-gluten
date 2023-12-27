8c408f24ddb346fb45c2d276ae4c2575b0885752
joey.ljy <joey.ljy@alibaba-inc.com>
2023-12-27 18:50:24 +0800

    add pre/post projection for window

--- gluten-ut/spark33/src/test/scala/org/apache/spark/sql/execution/GlutenSQLWindowFunctionSuite.scala
+++ gluten-ut/spark33/src/test/scala/org/apache/spark/sql/execution/GlutenSQLWindowFunctionSuite.scala
@@ -16,6 +16,71 @@
  */
 package org.apache.spark.sql.execution
 
-import org.apache.spark.sql.GlutenSQLTestsTrait
+import io.glutenproject.execution.WindowExecTransformer
 
-class GlutenSQLWindowFunctionSuite extends SQLWindowFunctionSuite with GlutenSQLTestsTrait {}
+import org.apache.spark.sql.GlutenSQLTestsTrait
+import org.apache.spark.sql.Row
+
+class GlutenSQLWindowFunctionSuite extends SQLWindowFunctionSuite with GlutenSQLTestsTrait {
+
+  import testImplicits._
+
+  val customerData: Seq[(Int, Int)] = Seq[(Int, Int)](
+    (4553, 11),
+    (4953, 10),
+    (35403, 5),
+    (35803, 12),
+    (60865, 5),
+    (61065, 13),
+    (127412, 13),
+    (148303, 10),
+    (9954, 5),
+    (95337, 12)
+  )
+
+  test("Literal in window partition by and sort") {
+    withTable("customer") {
+      customerData
+        .toDF("c_custkey", "c_nationkey")
+        .write
+        .format("parquet")
+        .saveAsTable("customer")
+      val query =
+        """
+          |SELECT
+          |  c_custkey,
+          |  row_number() OVER (
+          |    PARTITION BY c_nationkey,
+          |    "a"
+          |    ORDER BY
+          |      c_custkey,
+          |      "a"
+          |  ) AS row_num
+          |FROM
+          |   customer
+          |ORDER BY 1, 2;
+          |""".stripMargin
+      val df = sql(query)
+      checkAnswer(
+        df,
+        Seq(
+          Row(4553, 1),
+          Row(4953, 1),
+          Row(9954, 1),
+          Row(35403, 2),
+          Row(35803, 1),
+          Row(60865, 3),
+          Row(61065, 1),
+          Row(95337, 2),
+          Row(127412, 2),
+          Row(148303, 2))
+      )
+      assert(
+        getExecutedPlan(df).exists {
+          case _: WindowExecTransformer => true
+          case _ => false
+        }
+      )
+    }
+  }
+}
