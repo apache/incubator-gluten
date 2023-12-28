955cc23ac72ef5c58ae52ead75ee76845db6625b
guihuawen <guihuawen>
2023-12-28 15:53:39 +0800

    /home/yarn/java-current/bin/java': free(): invalid pointer: 0x00007f87cd7cac80 *** #4216

--- ep/build-arrow/src/build_arrow.sh
+++ ep/build-arrow/src/build_arrow.sh
@@ -117,19 +117,17 @@ cmake -G Ninja \
 cmake --build . --target install
 popd
 
-CPU_TARGET=${CPU_TARGET:-""}
-if [ "$CPU_TARGET" == "aarch64" ]; then
-  # Arrow C Data Interface CPP libraries
-  pushd java
-  mvn generate-resources -P generate-libs-cdata-all-os -Darrow.c.jni.dist.dir=$ARROW_INSTALL_DIR -N
-  popd
 
-  # Arrow Java libraries
-  pushd java
-  mvn clean install -P arrow-c-data -pl c -am -DskipTests -Dcheckstyle.skip \
-    -Darrow.c.jni.dist.dir=$ARROW_INSTALL_DIR/lib -Dmaven.gitcommitid.skip=true
-  popd
-fi
+# Arrow C Data Interface CPP libraries
+pushd java
+mvn generate-resources -P generate-libs-cdata-all-os -Darrow.c.jni.dist.dir=$ARROW_INSTALL_DIR -N
+popd
+
+# Arrow Java libraries
+pushd java
+mvn clean install -P arrow-c-data -pl c -am -DskipTests -Dcheckstyle.skip \
+-Darrow.c.jni.dist.dir=$ARROW_INSTALL_DIR/lib -Dmaven.gitcommitid.skip=true
+popd
 
 echo "Successfully built Arrow from Source."
 echo $TARGET_BUILD_COMMIT >"${ARROW_HOME}/arrow-commit.cache"
