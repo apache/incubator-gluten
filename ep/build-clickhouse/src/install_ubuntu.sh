dc2c4610aba2b02853c75fd4f192944db1ff7111
Leo Li <lyh-36@163.com>
2024-01-09 20:12:39 +0800

    pin install_ubuntu.sh clang version to 16

--- ep/build-clickhouse/src/install_ubuntu.sh
+++ ep/build-clickhouse/src/install_ubuntu.sh
@@ -20,7 +20,9 @@ echo "Install Prerequisites"
 sudo apt-get install git cmake ccache python3 ninja-build nasm yasm gawk lsb-release wget software-properties-common gnupg
 
 echo "Install and Use the Clang compiler"
-sudo bash -c "$(wget -O - https://apt.llvm.org/llvm.sh)"
+wget https://apt.llvm.org/llvm.sh
+chmod +x llvm.sh
+sudo ./llvm.sh 16
 
 echo "add CC and CXX to .bashrc"
 echo "export CC=clang-16" >> ~/.bashrc
