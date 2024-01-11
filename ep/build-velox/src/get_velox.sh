5d324ca97d68aee6847900f3fa0ba5f9d79eec52
Jia Ke <ke.a.jia@intel.com>
2024-01-12 03:22:21 +0800

    Test

--- ep/build-velox/src/get_velox.sh
+++ ep/build-velox/src/get_velox.sh
@@ -16,8 +16,8 @@
 
 set -exu
 
-VELOX_REPO=https://github.com/oap-project/velox.git
-VELOX_BRANCH=2024_01_10
+VELOX_REPO=https://github.com/JkSelf/velox.git
+VELOX_BRANCH=update
 VELOX_HOME=""
 
 #Set on run gluten on HDFS
