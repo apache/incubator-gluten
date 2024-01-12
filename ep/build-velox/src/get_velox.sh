04bd81ad54b71706263f300319fcb56e0b55c519
glutenperfbot <glutenperfbot@intel-internal.com>
2024-01-12 08:12:44 +0800

    rebase velox 2024-01-12
    Signed-off-by: glutenperfbot <glutenperfbot@intel-internal.com>

--- ep/build-velox/src/get_velox.sh
+++ ep/build-velox/src/get_velox.sh
@@ -16,8 +16,8 @@
 
 set -exu
 
-VELOX_REPO=https://github.com/oap-project/velox.git
-VELOX_BRANCH=2024_01_11
+VELOX_REPO=https://github.com/GlutenPerfBot/velox.git
+VELOX_BRANCH=rebase-2024-01-12
 VELOX_HOME=""
 
 #Set on run gluten on HDFS
