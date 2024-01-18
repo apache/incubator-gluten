wForget/gluten

[VL] Verify supported file system

backends-clickhouse/src/main/scala/io/glutenproject/backendsapi/clickhouse/CHBackend.scala
@@ -32,6 +32,8 @@ import org.apache.spark.sql.execution.aggregate.HashAggregateExec
 import org.apache.spark.sql.internal.SQLConf
 import org.apache.spark.sql.types.{ArrayType, MapType, StructField, StructType}
 
+import org.apache.hadoop.fs.Path
+
 import java.util.Locale
 
 import scala.util.control.Breaks.{break, breakable}
@@ -129,7 +131,8 @@ object CHBackendSettings extends BackendSettingsApi with Logging {
       format: ReadFileFormat,
       fields: Array[StructField],
       partTable: Boolean,
-      paths: Seq[String]): Boolean = {
+      paths: Seq[String],
+      rootPaths: Seq[Path]): Boolean = {
 
     def validateFilePath: Boolean = {
       // Fallback to vanilla spark when the input path

apache/incubator-gluten

backends-clickhouse/src/main/scala/io/glutenproject/backendsapi/clickhouse/CHBackend.scala
@@ -35,6 +35,8 @@ import org.apache.spark.sql.execution.datasources.FileFormat
 import org.apache.spark.sql.internal.SQLConf
 import org.apache.spark.sql.types.{ArrayType, MapType, StructField, StructType}
 
+import org.apache.hadoop.fs.Path
+
 import java.util.Locale
 
 import scala.util.control.Breaks.{break, breakable}
@@ -142,7 +144,8 @@ object CHBackendSettings extends BackendSettingsApi with Logging {
       format: ReadFileFormat,
       fields: Array[StructField],
       partTable: Boolean,
-      paths: Seq[String]): ValidationResult = {
+      paths: Seq[String],
+      rootPaths: Seq[Path]): ValidationResult = {
 
     def validateFilePath: Boolean = {
       // Fallback to vanilla spark when the input path