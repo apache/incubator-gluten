2607ff6c0913b9ef56530123bf9248ddcf39e534
Hongze Zhang <hongze.zhang@intel.com>
2024-01-18 16:53:00 +0800

    fixup

--- backends-velox/src/main/scala/org/apache/spark/sql/catalyst/FlushableHashAggregateRule.scala
+++ backends-velox/src/main/scala/org/apache/spark/sql/catalyst/FlushableHashAggregateRule.scala
@@ -20,7 +20,7 @@ import io.glutenproject.execution.{FlushableHashAggregateExecTransformer, HashAg
 
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.catalyst.expressions.aggregate.{Partial, PartialMerge}
-import org.apache.spark.sql.catalyst.plans.physical.ClusteredDistribution
+import org.apache.spark.sql.catalyst.plans.physical.{ClusteredDistribution, UnspecifiedDistribution}
 import org.apache.spark.sql.catalyst.rules.Rule
 import org.apache.spark.sql.execution.SparkPlan
 import org.apache.spark.sql.execution.exchange.ShuffleExchangeLike
@@ -113,6 +113,11 @@ object FlushableHashAggregateRule {
    * perform optimizations like doing "partial_count(a, b, c)" directly on the output data.
    */
   def isAggInputAlreadyDistributedWithAggKeys(agg: HashAggregateExecTransformer): Boolean = {
-    agg.child.outputPartitioning.satisfies(ClusteredDistribution(agg.groupingExpressions))
+    val distribution = if (agg.groupingExpressions.isEmpty) {
+      UnspecifiedDistribution
+    } else {
+      ClusteredDistribution(agg.groupingExpressions)
+    }
+    agg.child.outputPartitioning.satisfies(distribution)
   }
 }
