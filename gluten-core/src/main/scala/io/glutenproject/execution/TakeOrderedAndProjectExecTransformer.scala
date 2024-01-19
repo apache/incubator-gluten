liujiayi771/gluten

Refactor insertion process of pre/post projection

gluten-core/src/main/scala/io/glutenproject/execution/TakeOrderedAndProjectExecTransformer.scala
@@ -16,7 +16,7 @@
  */
 package io.glutenproject.execution
 
-import io.glutenproject.extension.GlutenPlan
+import io.glutenproject.extension.{ColumnarOverrides, GlutenPlan}
 
 import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.catalyst.InternalRow
@@ -115,8 +115,8 @@ case class TakeOrderedAndProjectExecTransformer(
         finalLimitPlan
       }
 
-      val finalPlan =
-        WholeStageTransformer(projectPlan)(transformStageCounter.incrementAndGet())
+      val finalPlan = ColumnarOverrides.applyRules(
+        WholeStageTransformer(projectPlan)(transformStageCounter.incrementAndGet()))
 
       finalPlan.executeColumnar()
     }

apache/incubator-gluten

gluten-core/src/main/scala/io/glutenproject/execution/TakeOrderedAndProjectExecTransformer.scala
@@ -16,7 +16,7 @@
  */
 package io.glutenproject.execution
 
-import io.glutenproject.extension.GlutenPlan
+import io.glutenproject.extension.{ColumnarOverrides, GlutenPlan}
 
 import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.catalyst.InternalRow
@@ -115,8 +115,8 @@ case class TakeOrderedAndProjectExecTransformer(
         finalLimitPlan
       }
 
-      val finalPlan =
-        WholeStageTransformer(projectPlan)(transformStageCounter.incrementAndGet())
+      val finalPlan = ColumnarOverrides.applyExtendedColumnarPreRules(
+        WholeStageTransformer(projectPlan)(transformStageCounter.incrementAndGet()))
 
       finalPlan.executeColumnar()
     }