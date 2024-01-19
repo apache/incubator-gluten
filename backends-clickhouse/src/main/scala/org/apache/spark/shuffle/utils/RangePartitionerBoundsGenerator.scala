df4ad6b619c89b45309662688e6bdf044bec841c
ulysses-you <ulyssesyou18@gmail.com>
2024-01-19 11:00:43 +0800

    style

--- backends-clickhouse/src/main/scala/org/apache/spark/shuffle/utils/RangePartitionerBoundsGenerator.scala
+++ backends-clickhouse/src/main/scala/org/apache/spark/shuffle/utils/RangePartitionerBoundsGenerator.scala
@@ -166,9 +166,7 @@ class RangePartitionerBoundsGenerator[K: Ordering: ClassTag, V](
         node.put("column_ref", index)
         node.put("data_type", ordering.dataType.toString)
         node.put("is_nullable", ordering.nullable)
-        node.put(
-          "direction",
-          SortExecTransformer.transformSortDirection(ordering))
+        node.put("direction", SortExecTransformer.transformSortDirection(ordering))
         arrayNode.add(node)
     }
   }
