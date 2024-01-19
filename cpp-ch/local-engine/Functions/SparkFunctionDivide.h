9025c6a833bf49382eac7c6a89bfcd73882408ef
kevinyhzou <kevinyunhe8@gmail.com>
2024-01-19 10:04:16 +0800

    improve divide

---
+++ cpp-ch/local-engine/Functions/SparkFunctionDivide.h
@@ -0,0 +1,39 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include <Functions/FunctionBinaryArithmetic.h>
+
+using namespace DB;
+
+namespace local_engine
+{
+
+class SparkFunctionDivide : public DB::FunctionBinaryArithmetic
+{
+public:
+    static constexpr auto name = "sparkDivide";
+    static DB::FunctionPtr create(DB::ContextPtr) { return std::make_shared<SparkFunctionDivide>(); }
+    SparkFunctionDivide() = default;
+    ~SparkFunctionDivide() override = default;
+    String getName() const override { return name; }
+
+    ColumnPtr executeImpl(const ColumnsWithTypeAndName & arguments, const DataTypePtr & result_type, size_t input_rows_count) const override
+    {
+        return nullptr;
+    }
+};
+
+}
