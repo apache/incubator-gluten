cdf1725ea56f20643cb5800b3b45fc46c856e38b
BInwei Yang <binwei.yang@intel.com>
2024-02-13 08:40:43 -0800

    [VL] Daily Update Velox Version (2024-02-13) (#4690)
    picked two PRs from 2024_02_09:
    
    3dcca8db6 Ubuntu: build fmt from source in setup script (#8678)
    ffb086b25 Add support to write to ABFS filesystems. (#7218)
    new PRs in 2024_02_12:
    
    42b10d943 Fix crash in parseSerdeParameters() (#8730)
    7227ff822 Fix arbitrary() to always return the first non-null value in Window operation (#8640)

--- ep/build-velox/src/get_velox.sh
+++ ep/build-velox/src/get_velox.sh
@@ -17,7 +17,7 @@
 set -exu
 
 VELOX_REPO=https://github.com/oap-project/velox.git
-VELOX_BRANCH=2024_02_12
+VELOX_BRANCH=2024_02_13
 VELOX_HOME=""
 
 #Set on run gluten on HDFS
@@ -106,6 +106,8 @@ function process_setup_ubuntu {
   sed -i "s/apt install -y/sudo apt install -y/" ${VELOX_HOME}/scripts/setup-adapters.sh
   if [ $ENABLE_S3 == "ON" ]; then
     sed -i '/^  run_and_time install_folly/a \ \ '${VELOX_HOME}/scripts'/setup-adapters.sh aws' scripts/setup-ubuntu.sh
+    # it's used for velox CI
+    sed -i 's/rpm -i minio-20220526054841.0.0.x86_64.rpm/#rpm -i minio-20220526054841.0.0.x86_64.rpm/g' scripts/setup-adapters.sh
   fi
   if [ $ENABLE_GCS == "ON" ]; then
     sed -i '/^  run_and_time install_folly/a \ \ '${VELOX_HOME}/scripts'/setup-adapters.sh gcs' scripts/setup-ubuntu.sh
