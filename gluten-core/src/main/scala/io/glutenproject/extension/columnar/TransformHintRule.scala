c7f2d7e3699d784b57422be23a5ae8a20842491c
Hongze Zhang <hongze.zhang@intel.com>
2024-02-23 12:39:12 +0800

    fixup

--- gluten-core/src/main/scala/io/glutenproject/extension/columnar/TransformHintRule.scala
+++ gluten-core/src/main/scala/io/glutenproject/extension/columnar/TransformHintRule.scala
@@ -61,6 +61,14 @@ object TransformHints {
 
   val DEBUG = false
 
+  /**
+   * If true, the plan node will be guaranteed fallback to Vanilla plan node
+   * while being implemented.
+   *
+   * If false, the plan still has chance to be turned into "non-transformable"
+   * in any another validation rule. So user should not consider the plan "transformable"
+   * unless all validation rules are passed.
+   */
   def isNotTransformable(plan: SparkPlan): Boolean = {
     getHintOption(plan) match {
       case Some(TRANSFORM_UNSUPPORTED(_, _)) => true
