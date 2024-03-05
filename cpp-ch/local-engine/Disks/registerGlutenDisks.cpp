5af4d791ac745b872735cef9e33ab56a8b4b6cc5
liuneng1994 <neng.liu@kyligence.io>
2024-03-05 11:32:46 +0800

    support disk hdfs

--- cpp-ch/local-engine/Disks/registerGlutenDisks.cpp
+++ cpp-ch/local-engine/Disks/registerGlutenDisks.cpp
@@ -21,49 +21,74 @@
 #include <Disks/ObjectStorages/MetadataStorageFactory.h>
 #include <Disks/ObjectStorages/ObjectStorageFactory.h>
 
+#if USE_HDFS
+#include <Disks/ObjectStorages/GlutenDiskHDFS.h>
+#endif
+
 #include "registerGlutenDisks.h"
 
 namespace local_engine
 {
-using namespace DB;
 #if USE_AWS_S3
-void registerGlutenS3ObjectStorage(ObjectStorageFactory & factory);
+void registerGlutenS3ObjectStorage(DB::ObjectStorageFactory & factory);
 #endif
 
+#if USE_HDFS
+void registerGlutenHDFSObjectStorage(DB::ObjectStorageFactory & factory);
+#endif
 
 void registerGlutenDisks(bool global_skip_access_check)
 {
-    auto & factory = DiskFactory::instance();
-
+    auto & factory = DB::DiskFactory::instance();
     auto creator = [global_skip_access_check](
-    const String & name,
-    const Poco::Util::AbstractConfiguration & config,
-    const String & config_prefix,
-    ContextPtr context,
-    const DisksMap & /* map */,
-    bool, bool) -> DiskPtr
+                       const String & name,
+                       const Poco::Util::AbstractConfiguration & config,
+                       const String & config_prefix,
+                       DB::ContextPtr context,
+                       const DB::DisksMap & /* map */,
+                       bool,
+                       bool) -> DB::DiskPtr
     {
         bool skip_access_check = global_skip_access_check || config.getBool(config_prefix + ".skip_access_check", false);
-        auto object_storage = ObjectStorageFactory::instance().create(name, config, config_prefix, context, skip_access_check);
-        auto metadata_storage = MetadataStorageFactory::instance().create(
-            name, config, config_prefix, object_storage, "local");
+        auto object_storage = DB::ObjectStorageFactory::instance().create(name, config, config_prefix, context, skip_access_check);
+        auto metadata_storage = DB::MetadataStorageFactory::instance().create(name, config, config_prefix, object_storage, "local");
 
-        DiskObjectStoragePtr disk = std::make_shared<DiskObjectStorage>(
-            name,
-            object_storage->getCommonKeyPrefix(),
-            std::move(metadata_storage),
-            std::move(object_storage),
-            config,
-            config_prefix);
+        DB::DiskObjectStoragePtr disk = std::make_shared<DB::DiskObjectStorage>(
+            name, object_storage->getCommonKeyPrefix(), std::move(metadata_storage), std::move(object_storage), config, config_prefix);
 
         disk->startup(context, skip_access_check);
         return disk;
     };
 
+    auto & object_factory = DB::ObjectStorageFactory::instance();
 #if USE_AWS_S3
-    auto & object_factory = ObjectStorageFactory::instance();
     registerGlutenS3ObjectStorage(object_factory);
     factory.registerDiskType("s3_gluten", creator); /// For compatibility
+#endif
+
+#if USE_HDFS
+    auto hdfs_creator = [global_skip_access_check](
+                            const String & name,
+                            const Poco::Util::AbstractConfiguration & config,
+                            const String & config_prefix,
+                            DB::ContextPtr context,
+                            const DB::DisksMap & /* map */,
+                            bool,
+                            bool) -> DB::DiskPtr
+    {
+        bool skip_access_check = global_skip_access_check || config.getBool(config_prefix + ".skip_access_check", false);
+        auto object_storage = DB::ObjectStorageFactory::instance().create(name, config, config_prefix, context, skip_access_check);
+        auto metadata_storage = DB::MetadataStorageFactory::instance().create(name, config, config_prefix, object_storage, "local");
+
+        DB::DiskObjectStoragePtr disk = std::make_shared<local_engine::GlutenDiskHDFS>(
+            name, object_storage->getCommonKeyPrefix(), std::move(metadata_storage), std::move(object_storage), config, config_prefix);
+
+        disk->startup(context, skip_access_check);
+        return disk;
+    };
+
+    registerGlutenHDFSObjectStorage(object_factory);
+    factory.registerDiskType("hdfs_gluten", hdfs_creator); /// For compatibility
 #endif
 }
 }
