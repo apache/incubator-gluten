597a77ea8f9c14108a9b73ac1b7c8394ebf3e271
glutenperfbot <glutenperfbot@intel-internal.com>
2024-03-06 08:18:39 +0000

    [VL] Daily Update Velox Version (2024_03_06)
    Signed-off-by: glutenperfbot <glutenperfbot@intel-internal.com>

--- ep/build-velox/src/get_velox.sh
+++ ep/build-velox/src/get_velox.sh
@@ -16,8 +16,8 @@
 
 set -exu
 
-VELOX_REPO=https://github.com/oap-project/velox.git
-VELOX_BRANCH=2024_03_06
+VELOX_REPO=https://github.com/PHILO-HE/velox.git
+VELOX_BRANCH=2024_03_06-revert
 VELOX_HOME=""
 
 #Set on run gluten on HDFS
