abf26295db7dd246bb3d6fde1e71d5490cc7eeb6
Yang Zhang <yangchuan.zy@alibaba-inc.com>
2024-03-13 11:37:41 +0800

    fix style.

--- backends-velox/src/test/scala/io/glutenproject/execution/VeloxFunctionsValidateSuite.scala
+++ backends-velox/src/test/scala/io/glutenproject/execution/VeloxFunctionsValidateSuite.scala
@@ -451,7 +451,8 @@ class VeloxFunctionsValidateSuite extends VeloxWholeStageTransformerSuite {
   }
 
   test("Test unhex function") {
-    runQueryAndCompare("SELECT unhex(hex(l_partkey)), unhex(hex(l_shipmode)) FROM lineitem limit 1") {
+    runQueryAndCompare(
+      "SELECT unhex(hex(l_partkey)), unhex(hex(l_shipmode)) FROM lineitem limit 1") {
       checkOperatorMatch[ProjectExecTransformer]
     }
   }
