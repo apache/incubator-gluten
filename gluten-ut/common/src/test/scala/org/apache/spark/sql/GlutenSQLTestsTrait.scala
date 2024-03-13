97ff083cbe57c4c4cdae2e752c7cd16e64ae5efc
zjuwangg <zjuwangg@foxmail.com>
2024-03-13 20:07:53 +0800

    Revert "fix failed test"
    This reverts commit e412421711b5ac803c759f9e1a2b0fa3d2390d92.

--- gluten-ut/common/src/test/scala/org/apache/spark/sql/GlutenSQLTestsTrait.scala
+++ gluten-ut/common/src/test/scala/org/apache/spark/sql/GlutenSQLTestsTrait.scala
@@ -112,8 +112,7 @@ object GlutenQueryTest extends Assertions {
       df: DataFrame,
       expectedAnswer: Seq[Row],
       checkToRDD: Boolean = true): Option[String] = {
-    val isSorted = df.logicalPlan.collect { case s: logical.Sort => s }.nonEmpty &&
-      df.logicalPlan.collect { case rr: logical.Repartition => rr }.isEmpty
+    val isSorted = df.logicalPlan.collect { case s: logical.Sort => s }.nonEmpty
     if (checkToRDD) {
       SQLExecution.withSQLConfPropagated(df.sparkSession) {
         df.rdd.count() // Also attempt to deserialize as an RDD [SPARK-15791]
