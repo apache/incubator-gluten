f0a10aa2a9f5d56c0afacdefc8d3c2ef6368c25f
PHILO-HE <feilong.he@intel.com>
2024-03-27 20:05:58 +0800

    [VL] Enable a Spark test for window row frame with constant preceding/following (#3887)

--- gluten-ut/spark34/src/test/scala/io/glutenproject/utils/velox/VeloxTestSettings.scala
+++ gluten-ut/spark34/src/test/scala/io/glutenproject/utils/velox/VeloxTestSettings.scala
@@ -1007,7 +1007,6 @@ class VeloxTestSettings extends BackendTestSettings {
     .exclude("unbounded preceding/following range between with aggregation")
     .exclude("sliding range between with aggregation")
     .exclude("store and retrieve column stats in different time zones")
-    .exclude("rows between should accept int/long values as boundary")
   enableSuite[GlutenDataFrameWriterV2Suite]
   enableSuite[GlutenDatasetAggregatorSuite]
   enableSuite[GlutenDatasetCacheSuite]
