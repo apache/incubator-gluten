2901a1fa2724f7a670bcddf99698a3b3ad40a5ad
Hongze Zhang <hongze.zhang@intel.com>
2024-03-28 13:03:50 +0800

    Revert "fixup ds q8"
    This reverts commit d502399804e6723b6e8c1ca667588e0a13080c54.

--- gluten-core/src/main/scala/io/glutenproject/backendsapi/SparkPlanExecApi.scala
+++ gluten-core/src/main/scala/io/glutenproject/backendsapi/SparkPlanExecApi.scala
@@ -85,11 +85,6 @@ trait SparkPlanExecApi {
   def genHiveTableScanExecTransformer(plan: SparkPlan): HiveTableScanExecTransformer =
     HiveTableScanExecTransformer(plan)
 
-  def genProjectExecTransformer(
-      projectList: Seq[NamedExpression],
-      child: SparkPlan): ProjectExecTransformer =
-    ProjectExecTransformer.createUnsafe(projectList, child)
-
   /** Generate HashAggregateExecTransformer. */
   def genHashAggregateExecTransformer(
       requiredChildDistributionExpressions: Option[Seq[Expression]],
