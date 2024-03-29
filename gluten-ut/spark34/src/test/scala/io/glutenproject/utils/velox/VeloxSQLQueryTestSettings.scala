24dd4bbd6b821a30f05799096165f78d0bfeabef
joey.ljy <joey.ljy@alibaba-inc.com>
2024-03-29 14:44:14 +0800

    Fix rebase

--- gluten-ut/spark34/src/test/scala/io/glutenproject/utils/velox/VeloxSQLQueryTestSettings.scala
+++ gluten-ut/spark34/src/test/scala/io/glutenproject/utils/velox/VeloxSQLQueryTestSettings.scala
@@ -233,17 +233,15 @@ object VeloxSQLQueryTestSettings extends SQLQueryTestSettings {
   )
 
   val OVERWRITE_SQL_QUERY_LIST: Set[String] = Set(
-    // Velox corr has better computation logic but it fails Spark's precision check.
+    // The calculation formulas for corr, skewness, kurtosis, variance, and stddev in Velox differ
+    // slightly from those in Spark, resulting in some differences in the final results.
     // Overwrite below test cases.
     // -- SPARK-24369 multiple distinct aggregations having the same argument set
+    // -- Aggregate with nulls.
     "group-by.sql",
     "udf/udf-group-by.sql",
     // Exception string doesn't match for
     // SELECT (SELECT a FROM (SELECT 1 AS a UNION ALL SELECT 2 AS a) t) AS b
-    "subquery/scalar-subquery/scalar-subquery-select.sql",
-    // The calculation formulas for skewness, kurtosis, variance, and stddev in Velox
-    // differ slightly from those in Spark, resulting in some differences in the final
-    // results. However, the differences are within an acceptable margin of error.
-    "group-by.sql"
+    "subquery/scalar-subquery/scalar-subquery-select.sql"
   )
 }
