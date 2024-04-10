efffd0eca78cdc1e504ba85829d9d1b9f7839673
PHILO-HE <feilong.he@intel.com>
2024-04-10 14:53:00 +0800

    Initial commit

--- backends-velox/src/main/scala/io/glutenproject/backendsapi/velox/ListenerApiImpl.scala
+++ backends-velox/src/main/scala/io/glutenproject/backendsapi/velox/ListenerApiImpl.scala
@@ -35,7 +35,7 @@ import org.apache.commons.lang3.StringUtils
 import scala.sys.process._
 
 class ListenerApiImpl extends ListenerApi {
-  private val ARROW_VERSION = "1500"
+  private val ARROW_VERSION = "1400"
 
   override def onDriverStart(conf: SparkConf): Unit = {
     // sql table cache serializer
@@ -111,9 +111,9 @@ class ListenerApiImpl extends ListenerApi {
     }
     loader
       .newTransaction()
-      .loadAndCreateLink(s"libarrow.so.$ARROW_VERSION.0.0", s"libarrow.so.$ARROW_VERSION", false)
+      .loadAndCreateLink(s"libarrow.so.$ARROW_VERSION.1.0", s"libarrow.so.$ARROW_VERSION", false)
       .loadAndCreateLink(
-        s"libparquet.so.$ARROW_VERSION.0.0",
+        s"libparquet.so.$ARROW_VERSION.1.0",
         s"libparquet.so.$ARROW_VERSION",
         false)
       .commit()
@@ -123,11 +123,11 @@ class ListenerApiImpl extends ListenerApi {
     loader
       .newTransaction()
       .loadAndCreateLink(
-        s"libarrow.$ARROW_VERSION.0.0.dylib",
+        s"libarrow.$ARROW_VERSION.1.0.dylib",
         s"libarrow.$ARROW_VERSION.dylib",
         false)
       .loadAndCreateLink(
-        s"libparquet.$ARROW_VERSION.0.0.dylib",
+        s"libparquet.$ARROW_VERSION.1.0.dylib",
         s"libparquet.$ARROW_VERSION.dylib",
         false)
       .commit()
