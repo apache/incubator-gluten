54a82b5966fb0c41d7fc43fd70a1706beea78687
Hongze Zhang <hongze.zhang@intel.com>
2024-04-11 15:01:56 +0800

    fixup

--- gluten-core/src/main/scala/org/apache/gluten/extension/columnar/enumerated/FilterRemoveRule.scala
+++ gluten-core/src/main/scala/org/apache/gluten/extension/columnar/enumerated/FilterRemoveRule.scala
@@ -27,6 +27,11 @@ import org.apache.spark.sql.execution.SparkPlan
 
 // Removes Gluten filter operator if its no-op. Typically a Gluten filter is no-op when it
 // pushes all of its conditions into the child scan.
+//
+// The rule is needed in RAS since our cost model treats all filter + scan plans with constant cost
+// because the pushed filter is not considered in the model. Removing the filter will make
+// optimizer choose a single scan as the winner sub-plan since a single scan's cost is lower than
+// filter + scan.
 object FilterRemoveRule extends RasRule[SparkPlan] {
   override def shift(node: SparkPlan): Iterable[SparkPlan] = {
     val filter = node.asInstanceOf[FilterExecTransformerBase]
