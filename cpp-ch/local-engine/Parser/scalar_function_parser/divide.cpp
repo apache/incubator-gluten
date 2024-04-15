5e4b14ce6c52ab6a276b231ab3d934bcf1971f07
KevinyhZou <37431499+KevinyhZou@users.noreply.github.com>
2024-04-15 17:55:17 +0800

    [GLUTEN-4483][CH]Improve divide (#5387)
    What changes were proposed in this pull request?
    (Please fill in changes proposed in this fix)
    
    (Fixes: #4483)
    
    How was this patch tested?
    (Please explain how this patch was tested. E.g. unit tests, integration tests, manual tests)
    
    性能测试
    端到端性能测试：test_tbl(a float, b float) 数据量6000W行，其中b取值全部为0 测试SQL: select count(1) from test_tbl where a/b is NULL， 测试三次：
    该PR 优化前：1.381s 1.343s 1.35s;
    该PR优化后：1.073s 1.127s 1.137s
    
    通过开发完成的 benchmark_spark_divide_function.cpp 测试 新的divide 函数， 对比CH 的divide函数，以及过去使用FunctionParser的divide 的性能差异，如下
    
    2024-01-22 16:51:29.252 <Warning> SignalHandler: LD_PRELOAD is not set, SignalHandler is disabled
    2024-01-22T16:51:29+08:00
    Running ./benchmark_local_engine
    Run on (32 X 2100 MHz CPU s)
    CPU Caches:
      L1 Data 32 KiB (x16)
      L1 Instruction 32 KiB (x16)
      L2 Unified 1024 KiB (x16)
      L3 Unified 11264 KiB (x2)
    Load Average: 13.31, 24.04, 42.68
    --------------------------------------------------------------------------------------
    Benchmark                                            Time             CPU   Iterations
    --------------------------------------------------------------------------------------
    BM_CHDivideFunction/iterations:10                  268 ms          260 ms           10
    BM_SparkDivideFunction/iterations:10               243 ms          243 ms           10
    BM_GlutenDivideFunctionParser/iterations:10        322 ms          322 ms           10
    可见， 新的SparkDivideFunction 相对于CH 的Divide 大约有10% 左右的提升，相对于Gluten 的FunctionParser 的实现约有30% ~ 40% 左右的提升

--- cpp-ch/local-engine/Parser/scalar_function_parser/divide.cpp
+++ cpp-ch/local-engine/Parser/scalar_function_parser/divide.cpp
@@ -59,15 +59,8 @@ public:
         
         if (isDecimal(removeNullable(left_arg->result_type)) || isDecimal(removeNullable(right_arg->result_type)))
             return toFunctionNode(actions_dag, "sparkDivideDecimal", {left_arg, right_arg});
-        
-        const auto * divide_node = toFunctionNode(actions_dag, "divide", {left_arg, right_arg});
-        DataTypePtr result_type = divide_node->result_type;
-
-        const auto * zero_node = addColumnToActionsDAG(actions_dag, result_type, removeNullable(result_type)->getDefault());
-        const auto * null_node = addColumnToActionsDAG(actions_dag, makeNullable(result_type), Field{});
-        const auto * is_zero_node = toFunctionNode(actions_dag, "equals", {right_arg, zero_node});
-        const auto * result_node = toFunctionNode(actions_dag, "if", {is_zero_node, null_node, divide_node});
-        return result_node;
+        else
+            return toFunctionNode(actions_dag, "sparkDivide", {left_arg, right_arg});
     }
 };
 
