ba3844bc3fed889098e1a2e643b660fc489ce931
zhaokuo <zhaokuo_game@163.com>
2024-04-18 16:22:53 +0800

    rm log

--- cpp/velox/compute/WholeStageResultIterator.cc
+++ cpp/velox/compute/WholeStageResultIterator.cc
@@ -521,7 +521,6 @@ std::unordered_map<std::string, std::string> WholeStageResultIterator::getQueryC
     configs[velox::core::QueryConfig::kMaxSpillLevel] = std::to_string(veloxCfg_->get<int32_t>(kMaxSpillLevel, 4));
     configs[velox::core::QueryConfig::kMaxSpillFileSize] =
         std::to_string(veloxCfg_->get<uint64_t>(kMaxSpillFileSize, kMaxSpillFileSizeDefault));
-    LOG(INFO) << "[zhaokuo] " << kMaxSpillFileSize << ":" << configs[velox::core::QueryConfig::kMaxSpillFileSize];
     configs[velox::core::QueryConfig::kMaxSpillRunRows] =
         std::to_string(veloxCfg_->get<uint64_t>(kMaxSpillRunRows, 12L * 1024 * 1024));
     configs[velox::core::QueryConfig::kMaxSpillBytes] =
