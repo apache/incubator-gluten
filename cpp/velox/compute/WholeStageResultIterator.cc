3e5742a6b9bf950a68d057162eda5c982ddf25d7
zhaokuo <zhaokuo_game@163.com>
2024-04-18 20:15:56 +0800

    [GLUTEN-5251][VL] Fix inconsistency of the default value for spark.gluten.sql.columnar.backend.velox.maxSpillFileSize (#5450)
    Closes #5251

--- cpp/velox/compute/WholeStageResultIterator.cc
+++ cpp/velox/compute/WholeStageResultIterator.cc
@@ -520,7 +520,7 @@ std::unordered_map<std::string, std::string> WholeStageResultIterator::getQueryC
         std::to_string(veloxCfg_->get<bool>(kOrderBySpillEnabled, true));
     configs[velox::core::QueryConfig::kMaxSpillLevel] = std::to_string(veloxCfg_->get<int32_t>(kMaxSpillLevel, 4));
     configs[velox::core::QueryConfig::kMaxSpillFileSize] =
-        std::to_string(veloxCfg_->get<uint64_t>(kMaxSpillFileSize, 20L * 1024 * 1024));
+        std::to_string(veloxCfg_->get<uint64_t>(kMaxSpillFileSize, 1L * 1024 * 1024 * 1024));
     configs[velox::core::QueryConfig::kMaxSpillRunRows] =
         std::to_string(veloxCfg_->get<uint64_t>(kMaxSpillRunRows, 12L * 1024 * 1024));
     configs[velox::core::QueryConfig::kMaxSpillBytes] =
