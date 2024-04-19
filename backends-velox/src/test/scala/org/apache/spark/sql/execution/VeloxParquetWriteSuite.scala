b80ae27fa9df17d956cae4657b73d0737fe68071
yan ma <yan.ma@intel.com>
2024-04-19 17:53:58 +0800

    [GLUTEN-5341] enable "test write parquet with compression codec" for Spark3.5

--- backends-velox/src/test/scala/org/apache/spark/sql/execution/VeloxParquetWriteSuite.scala
+++ backends-velox/src/test/scala/org/apache/spark/sql/execution/VeloxParquetWriteSuite.scala
@@ -51,11 +51,7 @@ class VeloxParquetWriteSuite extends VeloxWholeStageTransformerSuite {
     }
   }
 
-  // Disable for Spark3.5.
-  testWithSpecifiedSparkVersion(
-    "test write parquet with compression codec",
-    Some("3.2"),
-    Some("3.4")) {
+  testWithSpecifiedSparkVersion("test write parquet with compression codec", Some("3.2")) {
     // compression codec details see `VeloxParquetDatasource.cc`
     Seq("snappy", "gzip", "zstd", "lz4", "none", "uncompressed")
       .foreach {
