774e96b797243ccb66a6944a60feff9c8860008e
kevinyhzou <kevinyunhe8@gmail.com>
2024-04-22 15:17:02 +0800

    Fix ci

--- cpp-ch/local-engine/Functions/SparkFunctionExtractYear.cpp
+++ cpp-ch/local-engine/Functions/SparkFunctionExtractYear.cpp
@@ -14,25 +14,14 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-#include <Common/StringUtils/StringUtils.h>
-#include <Columns/ColumnNullable.h>
-#include <Columns/ColumnString.h>
+#include <Functions/FunctionGetDateData.h>
 #include <DataTypes/DataTypesNumber.h>
-#include <DataTypes/DataTypeNullable.h>
-#include <Functions/FunctionFactory.h>
 
 using namespace DB;
-namespace DB
-{
-namespace ErrorCodes
-{
-    extern const int NUMBER_OF_ARGUMENTS_DOESNT_MATCH;
-    extern const int ILLEGAL_TYPE_OF_ARGUMENT;
-}
-}
+
 namespace local_engine
 {
-class SparkFunctionExtractYear : public IFunction
+class SparkFunctionExtractYear : public FunctionGetDateData<true, false, DataTypeInt32::FieldType>
 {
 public:
     static constexpr auto name = "sparkExtractYear";
@@ -47,44 +36,6 @@ public:
     DataTypePtr getReturnTypeImpl(const ColumnsWithTypeAndName &) const override
     {
         return makeNullable(std::make_shared<DataTypeInt32>());
-    }
-    DB::ColumnPtr executeImpl(const DB::ColumnsWithTypeAndName & arguments, const DB::DataTypePtr &, size_t) const override
-    {
-        if (arguments.size() != 1)
-            throw Exception(ErrorCodes::NUMBER_OF_ARGUMENTS_DOESNT_MATCH, "Function {}'s argument size must be 1", name);
-        
-        const ColumnString * col_str = checkAndGetColumn<ColumnString>(arguments[0].column.get());
-        if (!col_str)
-            throw Exception(ErrorCodes::ILLEGAL_TYPE_OF_ARGUMENT, "Function {}'s 1st argument type must be string", name);
-        
-        auto res_col = ColumnInt32::create(col_str->size(), 0);
-        auto null_map_col = ColumnUInt8::create(col_str->size(), 0);
-        executeInternal(*col_str, res_col->getData(), null_map_col->getData());
-        return ColumnNullable::create(std::move(res_col), std::move(null_map_col));
-    }
-    void executeInternal(const ColumnString & src, PaddedPODArray<Int32> & result_data, PaddedPODArray<UInt8> & null_map) const
-    {
-        for (size_t i = 0; i < src.size(); ++i)
-        {
-            if (!extractYear(src.getDataAt(i), result_data[i]))
-                null_map[i] = 1;
-        }
-    }
-    bool extractYear(const StringRef & s, Int32 & year) const
-    {
-        size_t i = 0;
-        for (; i < s.size; ++i)
-        {
-            char ch = *(s.data + i);
-            if (!isNumericASCII(ch))
-            {
-                if (ch == '-') break;
-                else return false;
-            }
-        }
-        if (i != 4) return false;
-        year += (*(s.data + 0) - '0') * 1000 + (*(s.data + 1) - '0') * 100 + (*(s.data + 2) - '0') * 10 + (*(s.data + 3) - '0'); 
-        return true;
     }
 };
 
