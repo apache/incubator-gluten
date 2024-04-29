23ed3716d3e48d52f767d6b7487400e895854740
Hongze Zhang <hongze.zhang@intel.com>
2024-04-29 16:39:08 +0800

    fixup

--- gluten-core/src/main/scala/org/apache/gluten/extension/columnar/RewriteTransformer.scala
+++ gluten-core/src/main/scala/org/apache/gluten/extension/columnar/RewriteTransformer.scala
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.gluten.extension.columnar.rewrite
+package org.apache.gluten.extension.columnar
 
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.catalyst.rules.Rule
