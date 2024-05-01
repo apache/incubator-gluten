a438de0e7cab7fc9178f45f4a2ed671ae1e099bf
Ankita Victor <anvicto@microsoft.com>
2024-05-01 10:31:02 +0530

    Update branch

--- ep/build-velox/src/get_velox.sh
+++ ep/build-velox/src/get_velox.sh
@@ -16,8 +16,8 @@
 
 set -exu
 
-VELOX_REPO=https://github.com/oap-project/velox.git
-VELOX_BRANCH=2024_04_28
+VELOX_REPO=https://github.com/acvictor/velox.git
+VELOX_BRANCH=acvictor/updateFileHandleGen
 VELOX_HOME=""
 
 #Set on run gluten on HDFS
