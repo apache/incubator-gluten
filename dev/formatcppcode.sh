18a915181c6816abb7295d03788fd2a4aadf56a4
PHILO-HE <feilong.he@intel.com>
2024-05-08 16:25:02 +0800

    [VL] Fix clang-format version (#5650)

--- dev/formatcppcode.sh
+++ dev/formatcppcode.sh
@@ -1,3 +1,3 @@
 cd `dirname $0`
-find ../cpp/core -regex '.*\.\(cc\|hpp\|cu\|c\|h\)' -exec clang-format-12 -style=file -i {} \;
-find ../cpp/velox -regex '.*\.\(cc\|hpp\|cu\|c\|h\)' -exec clang-format-12 -style=file -i {} \;
+find ../cpp/core -regex '.*\.\(cc\|hpp\|cu\|c\|h\)' -exec clang-format-15 -style=file -i {} \;
+find ../cpp/velox -regex '.*\.\(cc\|hpp\|cu\|c\|h\)' -exec clang-format-15 -style=file -i {} \;
