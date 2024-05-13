414d52a888d5f65d2799ddf9306e47ad652bac97
Jia Ke <ke.a.jia@intel.com>
2024-05-13 22:55:04 +0800

    test dense rank

--- ep/build-velox/src/get_velox.sh
+++ ep/build-velox/src/get_velox.sh
@@ -16,8 +16,8 @@
 
 set -exu
 
-VELOX_REPO=https://github.com/oap-project/velox.git
-VELOX_BRANCH=2024_05_12
+VELOX_REPO=https://github.com/JkSelf/velox.git
+VELOX_BRANCH=window-denserank
 VELOX_HOME=""
 
 #Set on run gluten on HDFS
