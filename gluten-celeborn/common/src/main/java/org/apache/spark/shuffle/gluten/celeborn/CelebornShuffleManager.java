e7f6e3b4c70107daeb8a3b17682e0e9dd93ab71c
Leo Li <lyh-36@163.com>
2024-05-16 18:09:24 +0800

    fix celeborn fetch retry

--- gluten-celeborn/common/src/main/java/org/apache/spark/shuffle/gluten/celeborn/CelebornShuffleManager.java
+++ gluten-celeborn/common/src/main/java/org/apache/spark/shuffle/gluten/celeborn/CelebornShuffleManager.java
@@ -259,7 +259,7 @@ public class CelebornShuffleManager implements ShuffleManager {
         }
         @SuppressWarnings("unchecked")
         CelebornShuffleHandle<K, V, V> h = ((CelebornShuffleHandle<K, V, V>) handle);
-        ShuffleClient client =
+        shuffleClient =
             CelebornUtils.getShuffleClient(
                 h.appUniqueId(),
                 h.lifecycleManagerHost(),
@@ -279,7 +279,7 @@ public class CelebornShuffleManager implements ShuffleManager {
                   ShuffleClient.class,
                   CelebornShuffleHandle.class,
                   TaskContext.class,
-                  boolean.class);
+                  Boolean.class);
           shuffleId = (int) celebornShuffleIdMethod.invoke(null, shuffleClient, h, context, true);
 
           Method trackMethod =
