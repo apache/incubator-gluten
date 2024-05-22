83468d5523df18be3ece4985e32c50663bab7cf9
Yuan Zhou <yuan.zhou@intel.com>
2024-05-22 17:28:48 +0800

    update to use new image
    Signed-off-by: Yuan Zhou <yuan.zhou@intel.com>

--- dev/ci-velox-buildstatic.sh
+++ dev/ci-velox-buildstatic.sh
@@ -2,7 +2,7 @@ yum install sudo patch java-1.8.0-openjdk-devel -y
 cd $GITHUB_WORKSPACE/ep/build-velox/src
 ./get_velox.sh
 source /opt/rh/devtoolset-9/enable
-source $GITHUB_WORKSPACE//dev/vcpkg/env.sh
+source /opt/gluten/dev/vcpkg/env.sh
 cd $GITHUB_WORKSPACE/
 sed -i '/^headers/d' ep/build-velox/build/velox_ep/CMakeLists.txt
 export NUM_THREADS=4
