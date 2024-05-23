30db1dd12d2a959396b4fa26f9b980273a3cdbd4
NEUpanning <emmning@163.com>
2024-05-23 11:37:22 +0800

    add unit test for from_unixtime

--- gluten-ut/spark34/src/test/scala/org/apache/spark/sql/catalyst/expressions/GlutenDateExpressionsSuite.scala
+++ gluten-ut/spark34/src/test/scala/org/apache/spark/sql/catalyst/expressions/GlutenDateExpressionsSuite.scala
@@ -381,6 +381,9 @@ class GlutenDateExpressionsSuite extends DateExpressionsSuite with GlutenTestsTr
               FromUnixTime(Literal(-1000L), Literal(fmt2), timeZoneId),
               sdf2.format(new Timestamp(-1000000)))
             checkEvaluation(
+              FromUnixTime(Literal(Long.MAX_VALUE / 1000), Literal(fmt2), timeZoneId),
+              sdf2.format(new Timestamp(Long.MAX_VALUE)))
+            checkEvaluation(
               FromUnixTime(
                 Literal.create(null, LongType),
                 Literal.create(null, StringType),
