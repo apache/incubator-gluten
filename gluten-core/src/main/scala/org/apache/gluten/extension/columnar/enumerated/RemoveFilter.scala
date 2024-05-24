9e7e925a0bf4b8307d5a8e1cf2ed4c4c578f090b
Hongze Zhang <hongze.zhang@intel.com>
2024-05-24 16:38:54 +0800

    fixup

--- gluten-core/src/main/scala/org/apache/gluten/extension/columnar/enumerated/RemoveFilter.scala
+++ gluten-core/src/main/scala/org/apache/gluten/extension/columnar/enumerated/RemoveFilter.scala
@@ -16,13 +16,19 @@
  */
 package org.apache.gluten.extension.columnar.enumerated
 
-import org.apache.gluten.execution.{BasicScanExecTransformer, FilterExecTransformerBase}
+import org.apache.gluten.execution._
+import org.apache.gluten.metrics.{MetricsUpdater, NoopMetricsUpdater}
 import org.apache.gluten.ras.path.Pattern._
 import org.apache.gluten.ras.path.Pattern.Matchers._
 import org.apache.gluten.ras.rule.{RasRule, Shape}
 import org.apache.gluten.ras.rule.Shapes._
+import org.apache.gluten.substrait.SubstraitContext
 
+import org.apache.spark.rdd.RDD
+import org.apache.spark.sql.catalyst.expressions.{Attribute, SortOrder}
+import org.apache.spark.sql.catalyst.plans.physical.Partitioning
 import org.apache.spark.sql.execution.SparkPlan
+import org.apache.spark.sql.vectorized.ColumnarBatch
 
 // Removes Gluten filter operator if its no-op. Typically a Gluten filter is no-op when it
 // pushes all of its conditions into the child scan.
@@ -35,7 +41,8 @@ object RemoveFilter extends RasRule[SparkPlan] {
   override def shift(node: SparkPlan): Iterable[SparkPlan] = {
     val filter = node.asInstanceOf[FilterExecTransformerBase]
     if (filter.isNoop()) {
-      return List(filter.child)
+      val out = NoopFilter(filter.child, filter.output)
+      return List(out)
     }
     List.empty
   }
@@ -46,4 +53,30 @@ object RemoveFilter extends RasRule[SparkPlan] {
         clazz(classOf[FilterExecTransformerBase]),
         leaf(clazz(classOf[BasicScanExecTransformer]))
       ).build())
+
+  // A noop filter placeholder that indicates that all conditions are pushed into scan.
+  //
+  // This operator has zero cost in cost model to avoid planner from choosing the
+  // original filter-scan that doesn't have all conditions pushed down to scan.
+  //
+  // We cannot simplify remove the filter to let planner choose the scan since by vanilla
+  // Spark's definition the filter may have different output nullability than scan. So
+  // we have to keep this empty filter to let the optimized tree have the identical output schema
+  // with the original tree. If we simply remove the filter, possible UBs might be caused. For
+  // example, redundant broadcast exchanges may be added by EnsureRequirements because the
+  // broadcast join detects that its join keys' nullabilities have been changed. Then AQE
+  // re-optimization could be broken by ValidateSparkPlan so that AQE could completely
+  // have no effect as if it's off. This case can be observed by explicitly setting an higher
+  // AQE logger level to make sure the validation log doesn't get suppressed, e.g.,
+  // spark.sql.adaptive.logLevel=ERROR.
+  case class NoopFilter(override val child: SparkPlan, override val output: Seq[Attribute])
+    extends UnaryTransformSupport {
+    override def metricsUpdater(): MetricsUpdater = NoopMetricsUpdater
+    override protected def withNewChildInternal(newChild: SparkPlan): SparkPlan = copy(newChild)
+    override def outputPartitioning: Partitioning = child.outputPartitioning
+    override def outputOrdering: Seq[SortOrder] = child.outputOrdering
+    override def doTransform(context: SubstraitContext): TransformContext =
+      child.asInstanceOf[TransformSupport].doTransform(context)
+    override protected def doExecuteColumnar(): RDD[ColumnarBatch] = child.executeColumnar()
+  }
 }
