dc9cae71a784e3b1c6f8dde306776e3277eb291f
Yuan Zhou <yuan.zhou@intel.com>
2024-05-29 13:51:59 +0800

    [VL] test against meta/velox
    Signed-off-by: Yuan Zhou <yuan.zhou@intel.com>

--- ep/build-velox/src/get_velox.sh
+++ ep/build-velox/src/get_velox.sh
@@ -16,8 +16,8 @@
 
 set -exu
 
-VELOX_REPO=https://github.com/oap-project/velox.git
-VELOX_BRANCH=2024_05_27
+VELOX_REPO=https://github.com/facebookincubator/velox.git
+VELOX_BRANCH=main
 VELOX_HOME=""
 
 #Set on run gluten on HDFS
