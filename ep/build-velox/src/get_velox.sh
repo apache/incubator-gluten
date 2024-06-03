c031770337bb83afe97554381867db48be07374b
zhli1142015 <zhli@microsoft.com>
2024-06-03 22:38:56 +0800

    [DNM] test min&max

--- ep/build-velox/src/get_velox.sh
+++ ep/build-velox/src/get_velox.sh
@@ -16,8 +16,8 @@
 
 set -exu
 
-VELOX_REPO=https://github.com/oap-project/velox.git
-VELOX_BRANCH=2024_06_03
+VELOX_REPO=https://github.com/zhli1142015/velox.git
+VELOX_BRANCH=test_min_max
 VELOX_HOME=""
 
 #Set on run gluten on HDFS
