63a2ea0cc527d6aafcd4f21250fa2fbf9a584f43
kevinyhzou <kevinyunhe8@gmail.com>
2024-06-04 14:55:52 +0800

    remove GMT convert

--- cpp-ch/local-engine/Common/CHUtil.cpp
+++ cpp-ch/local-engine/Common/CHUtil.cpp
@@ -659,13 +659,7 @@ void BackendInitializerUtil::initSettings(std::map<std::string, std::string> & b
         }
         else if (key == SPARK_SESSION_TIME_ZONE)
         {
-            std::string time_zone_val = value;
-            // TimeZone of GMT+/GMT- is not supported in clickhouse backend, but Etc/GMT+ and Etc/GMT- supported
-            if (value.starts_with("GMT+"))
-                time_zone_val = "Etc/GMT-" + value.substr(4);
-            else if (value.starts_with("GMT-"))
-                time_zone_val = "Etc/GMT+" + value.substr(4);
-            
+            String time_zone_val = DateLUT::mappingForJavaTimezone(value);
             settings.set("session_timezone", time_zone_val);
             LOG_DEBUG(&Poco::Logger::get("CHUtil"), "Set settings key:{} value:{}", "session_timezone", time_zone_val);
         }
