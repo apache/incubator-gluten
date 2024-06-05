00dda1b67ec81dc841643e1c02323f32df5525d9
Shuai li <loneylee@live.cn>
2024-06-05 10:10:37 +0800

    [GLUTEN-5959] Fix function replace report an error with null value (#5960)
    What changes were proposed in this pull request?
    Kyligence/ClickHouse#493
    Fix replace null value
    
    (Fixes: #5959)
    
    How was this patch tested?
    test by ut

--- backends-clickhouse/src/test/scala/org/apache/gluten/execution/GlutenFunctionValidateSuite.scala
+++ backends-clickhouse/src/test/scala/org/apache/gluten/execution/GlutenFunctionValidateSuite.scala
@@ -707,41 +707,4 @@ class GlutenFunctionValidateSuite extends GlutenClickHouseWholeStageTransformerS
     }
 
   }
-
-  test("GLUTEN-5821: trim_character support value from column.") {
-    withTable("trim") {
-      sql("create table trim(a String, b String) using parquet")
-      sql("""
-            |insert into trim values ('aba', 'a'),('bba', 'b'),('abcdef', 'abcd')
-            |""".stripMargin)
-
-      val sql_str =
-        s"""select
-           |    trim(both b from a)
-           |  from trim
-          """.stripMargin
-
-      runQueryAndCompare(sql_str) { _ => }
-    }
-  }
-
-  test("GLUTEN-5897: fix regexp_extract with bracket") {
-    withTable("regexp_extract_bracket") {
-      sql("create table regexp_extract_bracket(a String) using parquet")
-      sql(
-        """
-          |insert into regexp_extract_bracket values ('123.123abc-abc'),('123-LOW'),('123]abc-abc')
-          |""".stripMargin)
-
-      val sql_str =
-        s"""select
-           |    regexp_extract(a, '([0-9][[\\.][0-9]]*)', 1)
-           |  , regexp_extract(a, '([0-9][[\\.][0-9]]*)', 1)
-           |  , regexp_extract(a, '([0-9][[]]]*)', 1)
-           |  from regexp_extract_bracket
-          """.stripMargin
-
-      runQueryAndCompare(sql_str) { _ => }
-    }
-  }
 }
