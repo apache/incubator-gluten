8724866f99e441a666504fdfb056ed8279598574
zhli1142015 <zhli@microsoft.com>
2024-06-21 07:57:15 +0800

    test

--- ep/build-velox/src/get_velox.sh
+++ ep/build-velox/src/get_velox.sh
@@ -16,8 +16,8 @@
 
 set -exu
 
-VELOX_REPO=https://github.com/oap-project/velox.git
-VELOX_BRANCH=2024_06_24
+VELOX_REPO=https://github.com/zhli1142015/velox.git
+VELOX_BRANCH=test_simd_re
 VELOX_HOME=""
 
 #Set on run gluten on HDFS
