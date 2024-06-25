74abc5efbba77a8518513f8ca4290c319c09cc3d
Yuqi Gu <guyuqi@apache.org>
2024-06-25 07:21:29 +0000

    Remove deprecated CURRENT_DIR
    Change-Id: I069f7febe92a342a0bc2d843093d1ed7cd1e6226
    Signed-off-by: Yuqi Gu <guyuqi@apache.org>

--- ep/build-velox/src/get_velox.sh
+++ ep/build-velox/src/get_velox.sh
@@ -96,6 +96,8 @@ function process_setup_ubuntu {
   sed -i '/libgmock-dev/d' scripts/setup-ubuntu.sh # resolved by ep/build-velox/build/velox_ep/CMake/resolve_dependency_modules/gtest.cmake
   sed -i 's/github_checkout boostorg\/boost \"\${BOOST_VERSION}\" --recursive/wget_and_untar https:\/\/github.com\/boostorg\/boost\/releases\/download\/boost-1.84.0\/boost-1.84.0.tar.gz boost \&\& cd boost/g' scripts/setup-ubuntu.sh
   if [ $ENABLE_HDFS == "ON" ]; then
+    # TODO: The 'modify_libhdfs3.patch' will be removed once the following PR is merged into the oap-project/libhdfs.
+    # Track: https://github.com/oap-project/libhdfs3/pull/2
     sed -i '/^function install_folly.*/i function install_libhdfs3 {\n  github_checkout oap-project/libhdfs3 master \n git apply ../../../src/modify_libhdfs3.patch\n cmake_install\n}\n' scripts/setup-ubuntu.sh
     sed -i '/^  run_and_time install_folly/a \ \ run_and_time install_libhdfs3' scripts/setup-ubuntu.sh
     sed -i '/ccache /a\  yasm \\' scripts/setup-ubuntu.sh
@@ -137,7 +139,9 @@ function process_setup_centos8 {
   sed -i '/^  run_and_time install_fbthrift/a \  run_and_time install_openssl' scripts/setup-centos8.sh
 
   if [ $ENABLE_HDFS == "ON" ]; then
-    sed -i '/^function install_gflags.*/i function install_libhdfs3 {\n cd "\${DEPENDENCY_DIR}"\n github_checkout oap-project/libhdfs3 master\n git apply ../../../src/modify_libhdfs3.patch\n cd ..\n cmake_install\n}\n' scripts/setup-centos8.sh
+    # TODO: The 'modify_libhdfs3.patch' will be removed once the following PR is merged into the oap-project/libhdfs.
+    # Track: https://github.com/oap-project/libhdfs3/pull/2
+    sed -i '/^function install_gflags.*/i function install_libhdfs3 {\n github_checkout oap-project/libhdfs3 master\n git apply ../../../src/modify_libhdfs3.patch\n cmake_install\n}\n' scripts/setup-centos8.sh
     sed -i '/^  run_and_time install_fbthrift/a \  run_and_time install_libhdfs3' scripts/setup-centos8.sh
     sed -i '/^  dnf_install ninja-build/a\  dnf_install yasm\' scripts/setup-centos8.sh
   fi
@@ -173,7 +177,9 @@ function process_setup_centos7 {
   sed -i '/^  run_and_time install_folly/a \ \ run_and_time install_gtest' scripts/setup-centos7.sh
 
   if [ $ENABLE_HDFS = "ON" ]; then
-    sed -i '/^function install_protobuf.*/i function install_libhdfs3 {\n cd "\${DEPENDENCY_DIR}"\n github_checkout oap-project/libhdfs3 master \n git apply ../../../src/modify_libhdfs3.patch\n cd ..\n cmake_install\n}\n' scripts/setup-centos7.sh
+    # TODO: The 'modify_libhdfs3.patch' will be removed once the following PR is merged into the oap-project/libhdfs.
+    # Track: https://github.com/oap-project/libhdfs3/pull/2
+    sed -i '/^function install_protobuf.*/i function install_libhdfs3 {\n github_checkout oap-project/libhdfs3 master \n git apply ../../../src/modify_libhdfs3.patch\n cmake_install\n}\n' scripts/setup-centos7.sh
     sed -i '/^  run_and_time install_folly/a \ \ run_and_time install_libhdfs3' scripts/setup-centos7.sh
     sed -i '/^dnf_install ccache/a\ \ yasm \\' scripts/setup-centos7.sh
   fi
