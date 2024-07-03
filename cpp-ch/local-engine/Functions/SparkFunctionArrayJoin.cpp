79b48637fba498f8bf7608111b2b75481d1a8b0a
kevinyhzou <kevinyunhe8@gmail.com>
2024-07-03 16:04:12 +0800

    ci fix

--- cpp-ch/local-engine/Functions/SparkFunctionArrayJoin.cpp
+++ cpp-ch/local-engine/Functions/SparkFunctionArrayJoin.cpp
@@ -58,17 +58,16 @@ public:
      {
         if (arguments.size() != 2 && arguments.size() != 3)
             throw Exception(ErrorCodes::NUMBER_OF_ARGUMENTS_DOESNT_MATCH, "Function {} must have 2 or 3 arguments", getName());
-        
+
         const auto * arg_null_col = checkAndGetColumn<ColumnNullable>(arguments[0].column.get());
         const auto * array_col = checkAndGetColumn<ColumnArray>(arg_null_col->getNestedColumnPtr().get());
         if (!array_col)
             throw Exception(ErrorCodes::ILLEGAL_TYPE_OF_ARGUMENT, "Function {} 1st argument must be array type", getName());
-        
+
         auto res_col = ColumnString::create();
         auto null_col = ColumnUInt8::create(array_col->size(), 0);
         PaddedPODArray<UInt8> & null_result = null_col->getData();
         StringRef delim, null_replacement;
-        
         auto checkAndGetConstString = [&](const ColumnPtr & col) -> std::pair<bool, StringRef>
         {
             StringRef res;
@@ -79,7 +78,7 @@ public:
                 {
                     for (size_t i = 0; i < array_col->size(); ++i)
                     {
-                        res_col->insertData("", 0);
+                        res_col->insertDefault();
                         null_result[i] = 1;
                     }
                     return std::pair<bool, StringRef>(false, res);
@@ -106,46 +105,45 @@ public:
                 return ColumnNullable::create(std::move(res_col), std::move(null_col));
             null_replacement = null_replacement_res.second;
         }
-        
         const ColumnNullable * array_nested_col = checkAndGetColumn<ColumnNullable>(&array_col->getData());
         const ColumnString * string_col = checkAndGetColumn<ColumnString>(array_nested_col->getNestedColumnPtr().get());
         const ColumnArray::Offsets & array_offsets = array_col->getOffsets();
         const ColumnString::Offsets & string_offsets = string_col->getOffsets();
         const ColumnString::Chars & string_data = string_col->getChars();
-        size_t current_offset = 0;
+        size_t current_offset = 0, array_pos = 0;
         for (size_t i = 0; i < array_col->size(); ++i)
         {
             String res;
             if (arg_null_col->isNullAt(i))
             {
+                res_col->insertDefault();
                 null_result[i] = 1;
+                current_offset = array_offsets[i];
                 continue;
             }
             size_t array_size = array_offsets[i] - current_offset;
-            size_t data_pos = 0;
-            for (size_t j = 0; j < array_size - 1; ++j)
+            size_t data_pos = array_pos == 0 ? 0 : string_offsets[array_pos - 1];
+            for (size_t j = 0; j < array_size; ++j)
             {
-                if (array_nested_col->isNullAt(j))
+                if (array_nested_col->isNullAt(j + array_pos))
                 {
                     if (null_replacement.data)
                     {
                         res += null_replacement.toString();
-                        res += delim.toString();
+                        if (j != array_size - 1)
+                            res += delim.toString();
                     }
                 }
                 else
                 {
-                    const StringRef s(&string_data[data_pos], string_offsets[j] - data_pos);
+                    const StringRef s(&string_data[data_pos], string_offsets[j + array_pos] - data_pos);
                     res += s.toString();
-                    res += delim.toString();
+                    if (j != array_size - 1)
+                        res += delim.toString();
                 }
-                data_pos = string_offsets[j];
+                data_pos = string_offsets[j + array_pos];
             }
-            if (array_size > 0)
-            {
-                const StringRef s = array_nested_col->isNullAt(array_size - 1) ? null_replacement : StringRef(&string_data[data_pos], string_offsets[array_size - 1] - data_pos);
-                res += s.toString();
-            }
+            array_pos += array_size;
             res_col->insertData(res.data(), res.size());
             current_offset = array_offsets[i];
         }
