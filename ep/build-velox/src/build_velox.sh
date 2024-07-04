ffb693ed17e17b0c2a9c2aff4719ea8309df8e89
j7nhai <146867566+j7nhai@users.noreply.github.com>
2024-07-04 17:22:20 +0800

    [VL] Fix build Velox script incorrectly judged as successful when run make (#6331)

--- ep/build-velox/src/build_velox.sh
+++ ep/build-velox/src/build_velox.sh
@@ -109,6 +109,9 @@ function compile {
     fi
   fi
 
+  # Maybe there is some set option in velox setup script. Run set command again.
+  set -exu  
+
   CXX_FLAGS='-Wno-missing-field-initializers'
   COMPILE_OPTION="-DCMAKE_CXX_FLAGS=\"$CXX_FLAGS\" -DVELOX_ENABLE_PARQUET=ON -DVELOX_BUILD_TESTING=OFF"
   if [ $BUILD_TEST_UTILS == "ON" ]; then
