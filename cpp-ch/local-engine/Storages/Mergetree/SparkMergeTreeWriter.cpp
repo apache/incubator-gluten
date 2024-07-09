f32127b9e8f68b7e4fd12241c256561d70cb7977
liuneng1994 <neng.liu@kyligence.io>
2024-07-09 17:40:12 +0800

    sort files

--- cpp-ch/local-engine/Storages/Mergetree/SparkMergeTreeWriter.cpp
+++ cpp-ch/local-engine/Storages/Mergetree/SparkMergeTreeWriter.cpp
@@ -241,6 +241,7 @@ void SparkMergeTreeWriter::commitPartToRemoteStorageIfNeeded()
         auto src_disk = storage->getStoragePolicy()->getAnyDisk();
         auto dest_disk = dest_storage->getStoragePolicy()->getAnyDisk();
         auto tx = dest_disk->createTransaction();
+        std::sort(files.begin(), files.end());
         for (const auto & file : files)
         {
             auto read_buffer = src_disk->readFile(local_relative_path + "/" + file, read_settings);
