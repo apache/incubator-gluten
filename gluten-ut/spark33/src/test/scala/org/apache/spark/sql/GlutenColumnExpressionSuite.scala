1f1c02f7428dfd12573585722eb25b1bd1fc8bea
gaoyangxiaozhu <gyycoder@gmail.com>
2024-07-10 18:42:49 +0800

    address comment

--- gluten-ut/spark33/src/test/scala/org/apache/spark/sql/GlutenColumnExpressionSuite.scala
+++ gluten-ut/spark33/src/test/scala/org/apache/spark/sql/GlutenColumnExpressionSuite.scala
@@ -22,7 +22,7 @@ import org.apache.spark.sql.functions.{expr, input_file_name}
 class GlutenColumnExpressionSuite extends ColumnExpressionSuite with GlutenSQLTestsTrait {
   import testImplicits._
   testGluten(
-    "input_file_name, input_file_block_start, input_file_block_length with scan is fallback") {
+    "input_file_name, input_file_block_start and input_file_block_length should fall back if scan falls back") {
     withSQLConf(("spark.gluten.sql.columnar.filescan", "false")) {
       withTempPath {
         dir =>
