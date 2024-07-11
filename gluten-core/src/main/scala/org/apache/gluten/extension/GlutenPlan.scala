1cc4ef43e325fe62436fc86a332454d629e55b18
Zhen Li <10524738+zhli1142015@users.noreply.github.com>
2024-07-11 22:07:06 +0800

    [VL] Add schema validation for all operators (#6406)
    [VL] Add schema validation for all operators.

--- gluten-core/src/main/scala/org/apache/gluten/extension/GlutenPlan.scala
+++ gluten-core/src/main/scala/org/apache/gluten/extension/GlutenPlan.scala
@@ -63,6 +63,16 @@ trait GlutenPlan extends SparkPlan with Convention.KnownBatchType with LogLevelU
    * Validate whether this SparkPlan supports to be transformed into substrait node in Native Code.
    */
   final def doValidate(): ValidationResult = {
+    val schemaVaidationResult = BackendsApiManager.getValidatorApiInstance
+      .doSchemaValidate(schema)
+      .map {
+        reason => ValidationResult.notOk(s"Found schema check failure for $schema, due to: $reason")
+      }
+      .getOrElse(ValidationResult.ok)
+    if (!schemaVaidationResult.isValid) {
+      TestStats.addFallBackClassName(this.getClass.toString)
+      return schemaVaidationResult
+    }
     try {
       TransformerState.enterValidation
       val res = doValidateInternal()
