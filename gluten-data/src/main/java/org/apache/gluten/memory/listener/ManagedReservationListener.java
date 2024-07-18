b6c7f98ed50a04253a2eeb6c95508faee7e2883e
Hongze Zhang <hongze.zhang@intel.com>
2024-07-18 16:26:14 +0800

    fixup

--- gluten-data/src/main/java/org/apache/gluten/memory/listener/ManagedReservationListener.java
+++ gluten-data/src/main/java/org/apache/gluten/memory/listener/ManagedReservationListener.java
@@ -23,9 +23,7 @@ import com.google.common.base.Preconditions;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-/**
- * Reserve Spark managed memory.
- */
+/** Reserve Spark managed memory. */
 public class ManagedReservationListener implements ReservationListener {
 
   private static final Logger LOG = LoggerFactory.getLogger(ManagedReservationListener.class);
