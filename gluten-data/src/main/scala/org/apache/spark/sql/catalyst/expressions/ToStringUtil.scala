a9d637400f99d6fac240c1bd4f3a18d9e94bbaf5
Chengcheng Jin <chengcheng.jin@intel.com>
2024-07-18 14:27:13 +0000

    fix inherience

--- gluten-data/src/main/scala/org/apache/spark/sql/catalyst/expressions/ToStringUtil.scala
+++ gluten-data/src/main/scala/org/apache/spark/sql/catalyst/expressions/ToStringUtil.scala
@@ -16,15 +16,50 @@
  */
 package org.apache.spark.sql.catalyst.expressions
 
-import org.apache.spark.sql.types.DataType
+import org.apache.spark.sql.catalyst.InternalRow
+import org.apache.spark.sql.catalyst.expressions.codegen.{CodegenContext, ExprCode}
+import org.apache.spark.sql.types.{DataType, StringType}
 import org.apache.spark.unsafe.types.UTF8String
 
-class ToStringUtil(dataType: DataType, override val timeZoneId: Option[String])
-  extends ToPrettyString(null, timeZoneId) {
-  private[this] lazy val castFunc: Any => Any = castToString(dataType)
+// Similar with ToPrettyString Expression
+// Because case class ToPrettyString is forbid to inheritable by code style
+case class ToStringUtil(inputType: DataType, timeZoneId: Option[String] = None)
+  extends UnaryExpression
+  with TimeZoneAwareExpression
+  with ToStringBase {
+
+  override def dataType: DataType = StringType
+
+  override def nullable: Boolean = false
+
+  override def withTimeZone(timeZoneId: String): ToStringUtil =
+    copy(timeZoneId = Some(timeZoneId))
+
+  override protected def withNewChildInternal(newChild: Expression): Expression =
+    throw new UnsupportedOperationException()
+
+  override protected def leftBracket: String = "{"
+  override protected def rightBracket: String = "}"
+
+  override protected def nullString: String = "NULL"
+
+  override protected def useDecimalPlainString: Boolean = true
+
+  override protected def useHexFormatForBinary: Boolean = true
+
+  private[this] lazy val castFunc: Any => Any = castToString(inputType)
+
+  override def eval(input: InternalRow): Any = {
+    throw new UnsupportedOperationException()
+  }
 
   def evalValue(input: Any): Any = {
     if (input == null) UTF8String.fromString(nullString) else castFunc(input)
   }
 
+  override protected def doGenCode(ctx: CodegenContext, ev: ExprCode): ExprCode = {
+    throw new UnsupportedOperationException()
+  }
+
+  override def child: Expression = null
 }
