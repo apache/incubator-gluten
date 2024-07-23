abd2d6fdcca6764cf60af49cc7648e7dd30641c8
wforget <643348094@qq.com>
2024-07-23 17:45:18 +0800

    [MINOR] Remove duplicate mvn packages

--- dev/package-vcpkg.sh
+++ dev/package-vcpkg.sh
@@ -15,7 +15,3 @@ if [ "$LINUX_OS" == "centos" ]; then
 fi
 source ./dev/vcpkg/env.sh
 ./dev/buildbundle-veloxbe.sh --build_tests=ON --build_benchmarks=ON --enable_s3=ON  --enable_hdfs=ON
-mvn clean package -Pbackends-velox -Pceleborn -Puniffle -Pspark-3.2 -DskipTests
-mvn clean package -Pbackends-velox -Pceleborn -Puniffle -Pspark-3.3 -DskipTests
-mvn clean package -Pbackends-velox -Pceleborn -Puniffle -Pspark-3.4 -DskipTests
-mvn clean package -Pbackends-velox -Pceleborn -Puniffle -Pspark-3.5 -DskipTests
