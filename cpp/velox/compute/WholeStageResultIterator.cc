08115e1184c3a299778228feb14d554278af968c
Hongze Zhang <hongze.zhang@intel.com>
2024-07-23 16:33:51 +0800

    [VL] Daily Update Velox Version (2024_07_23) (#6552)

--- cpp/velox/compute/WholeStageResultIterator.cc
+++ cpp/velox/compute/WholeStageResultIterator.cc
@@ -535,7 +535,7 @@ std::shared_ptr<velox::Config> WholeStageResultIterator::createConnectorConfig()
   configs[velox::connector::hive::HiveConfig::kFileColumnNamesReadAsLowerCaseSession] =
       !veloxCfg_->get<bool>(kCaseSensitive, false) ? "true" : "false";
   configs[velox::connector::hive::HiveConfig::kPartitionPathAsLowerCaseSession] = "false";
-  configs[velox::connector::hive::HiveConfig::kParquetWriteTimestampUnitSession] = "6";
+  configs[velox::parquet::WriterOptions::kParquetSessionWriteTimestampUnit] = "6";
   configs[velox::connector::hive::HiveConfig::kReadTimestampUnitSession] = "6";
   configs[velox::connector::hive::HiveConfig::kMaxPartitionsPerWritersSession] =
       std::to_string(veloxCfg_->get<int32_t>(kMaxPartitions, 10000));
