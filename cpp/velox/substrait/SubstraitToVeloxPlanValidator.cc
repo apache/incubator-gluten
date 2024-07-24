35453da3cfb79d6aa89174e4f555f841eee11b8d
liangyongyuan <liangyongyuan@xiaomi.com>
2024-07-24 16:20:19 +0800

    fix

--- cpp/velox/substrait/SubstraitToVeloxPlanValidator.cc
+++ cpp/velox/substrait/SubstraitToVeloxPlanValidator.cc
@@ -478,7 +478,7 @@ bool SubstraitToVeloxPlanValidator::validate(const ::substrait::GenerateRel& gen
   auto rowType = std::make_shared<RowType>(std::move(names), std::move(types));
   std::vector<core::TypedExprPtr> expressions;
   if (generateRel.has_generator()) {
-    if (!validateExpression(generateRel.generator(), rowType) {
+    if (!validateExpression(generateRel.generator(), rowType)) {
       LOG_VALIDATION_MSG("Input validation fails in GenerateRel.");
       return false;
     }
