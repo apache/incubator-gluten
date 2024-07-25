59b1b7cde65764c05b7b3f2998e9a327045a7393
PHILO-HE <feilong.he@intel.com>
2024-07-25 10:47:18 +0800

    Revert change for testing

--- ep/build-velox/src/get_velox.sh
+++ ep/build-velox/src/get_velox.sh
@@ -16,8 +16,8 @@
 
 set -exu
 
-VELOX_REPO=https://github.com/PHILO-HE/velox.git
-VELOX_BRANCH=2024_07_24-v1
+VELOX_REPO=https://github.com/oap-project/velox.git
+VELOX_BRANCH=2024_07_24
 VELOX_HOME=""
 
 OS=`uname -s`
