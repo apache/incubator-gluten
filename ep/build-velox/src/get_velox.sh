eec91685091f6b0ef53d4a84de097596fb3201c0
joey.ljy <joey.ljy@alibaba-inc.com>
2024-07-28 09:19:08 +0800

    Modify velox repo for test

--- ep/build-velox/src/get_velox.sh
+++ ep/build-velox/src/get_velox.sh
@@ -16,8 +16,8 @@
 
 set -exu
 
-VELOX_REPO=https://github.com/oap-project/velox.git
-VELOX_BRANCH=2024_07_26
+VELOX_REPO=https://github.com/liujiayi771/velox.git
+VELOX_BRANCH=2024_07_26_setup
 VELOX_HOME=""
 
 OS=`uname -s`
