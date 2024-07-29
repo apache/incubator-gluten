03765af4a3de690ce82ba0408f60821ff404961f
Zhen Wang <643348094@qq.com>
2024-07-29 11:04:35 +0800

    [VL] Improve package scripts (#6569)

--- dev/package-vcpkg.sh
+++ dev/package-vcpkg.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-set -ex
+set -eux
 
 CURRENT_DIR=$(cd "$(dirname "$BASH_SOURCE")"; pwd)
 GLUTEN_DIR="$CURRENT_DIR/.."
+LINUX_OS=$(. /etc/os-release && echo ${ID})
+VERSION=$(. /etc/os-release && echo ${VERSION_ID})
+ARCH=`uname -m`
 
 cd "$GLUTEN_DIR"
 if [ "$LINUX_OS" == "centos" ]; then
@@ -13,5 +16,10 @@ if [ "$LINUX_OS" == "centos" ]; then
     source /opt/rh/devtoolset-9/enable
   fi
 fi
+
+# prepare vcpkg environments
 source ./dev/vcpkg/env.sh
-./dev/buildbundle-veloxbe.sh --build_tests=ON --build_benchmarks=ON --enable_s3=ON  --enable_hdfs=ON
+
+# build gluten with velox backend, prompt always respond y
+export PROMPT_ALWAYS_RESPOND=y
+./dev/buildbundle-veloxbe.sh --build_tests=ON --build_benchmarks=ON --enable_s3=ON  --enable_hdfs=ON "$@"
