53abee60869db47cee7edb6c391c9a5fa74a166c
李扬 <654010905@qq.com>
2024-08-05 13:24:21 +0800

    [CH] Hotfix debug building error (#6710)
    * fix debug building
    
    * fix debug building
    
    * fix building

--- cpp-ch/local-engine/Storages/Output/ParquetOutputFormatFile.cpp
+++ cpp-ch/local-engine/Storages/Output/ParquetOutputFormatFile.cpp
@@ -43,7 +43,7 @@ OutputFormatFile::OutputFormatPtr ParquetOutputFormatFile::createOutputFormat(co
     auto res = std::make_shared<OutputFormatFile::OutputFormat>();
     res->write_buffer = write_buffer_builder->build(file_uri);
 
-    auto new_header = creatHeaderWithPreferredSchema(header);
+    auto new_header = createHeaderWithPreferredSchema(header);
     // TODO: align all spark parquet config with ch parquet config
     auto format_settings = DB::getFormatSettings(context);
     auto output_format = std::make_shared<DB::ParquetBlockOutputFormat>(*(res->write_buffer), new_header, format_settings);
