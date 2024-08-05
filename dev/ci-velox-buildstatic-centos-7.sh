c3331e124461fca8e6bd069acf1ae4cc4e6b79b8
PHILO-HE <feilong.he@intel.com>
2024-08-05 14:10:34 +0800

    Disable arrow build

--- dev/ci-velox-buildstatic-centos-7.sh
+++ dev/ci-velox-buildstatic-centos-7.sh
@@ -10,4 +10,7 @@ cd $GITHUB_WORKSPACE/
 source ./dev/vcpkg/env.sh
 sed -i '/^headers/d' ep/build-velox/build/velox_ep/CMakeLists.txt
 export NUM_THREADS=4
-./dev/builddeps-veloxbe.sh --build_tests=OFF --build_benchmarks=OFF --enable_s3=ON --enable_gcs=ON --enable_hdfs=ON --enable_abfs=ON
+# If a pr contains changes for arrow, e.g, upgrading arrow or applying some new changes to arrow
+# source, build_arrow should be set to ON.
+./dev/builddeps-veloxbe.sh --build_tests=OFF --build_benchmarks=OFF --enable_s3=ON \
+                           --enable_gcs=ON --enable_hdfs=ON --enable_abfs=ON --build_arrow=OFF
