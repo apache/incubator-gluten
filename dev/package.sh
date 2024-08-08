b686deaa2e51e57682cff927f47df8b378a83a8e
Zhen Wang <643348094@qq.com>
2024-08-08 10:27:02 +0800

    [GLUTEN-6569][VL] Delete unnecessary gcc9 enabling from package script (#6730)

--- dev/package.sh
+++ dev/package.sh
@@ -9,13 +9,6 @@ VERSION=$(. /etc/os-release && echo ${VERSION_ID})
 ARCH=`uname -m`
 
 cd "$GLUTEN_DIR"
-if [ "$LINUX_OS" == "centos" ]; then
-  if [ "$VERSION" == "8" ]; then
-    source /opt/rh/gcc-toolset-9/enable
-  elif [ "$VERSION" == "7" ]; then
-    source /opt/rh/devtoolset-9/enable
-  fi
-fi
 
 # build gluten with velox backend, prompt always respond y
 export PROMPT_ALWAYS_RESPOND=y
