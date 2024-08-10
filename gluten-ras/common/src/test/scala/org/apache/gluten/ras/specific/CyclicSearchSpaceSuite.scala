2671425859c1063ffe01b87625b0c442cc904391
Preetesh2110 <110464118+Preetesh2110@users.noreply.github.com>
2024-08-10 02:16:54 +0000

    [VL] Add Scala 2.13 support (#6326)
    * [VL] Add Scala 2.13 support
    
    * Fix scalaStyle issues
    
    * Fix Scala Style issues
    
    * Add Spark 3.5.1 and Scala 2.13 test in workflow
    
    * Add run-spark-test-spark35-scala213 job
    
    * Add Spark 3.5.1 and Scala 2.13 test in workflow
    
    * Fix tests failures
    
    * Fix tests failures
    
    * ScalaStyle fix
    
    * Fix SoftAffinitySuite
    
    * Fix ArrowUtil error
    
    * Fix backend-velox scala issues
    
    * Fix ColumnarArrowEvalPythonExec issues
    
    * Fix ColumnarArrowEvalPythonExec issues
    
    * Fix TestOperator.scala for style issues
    
    * Fix TestOperator.scala for style issues
    
    * Fix issues in DeltaRewriteTransformerRules.scala
    
    * DeltaRewriteTransformerRules fix
    
    * Fix style issues
    
    * Fix issues
    
    * Fix issues
    
    * Fix issues
    
    * Fix issues
    
    * Fix issues
    
    * Fix issues
    
    * Fix issues
    
    ---------
    
    Co-authored-by: Hongze Zhang <hongze.zhang@intel.com>

--- gluten-ras/common/src/test/scala/org/apache/gluten/ras/specific/CyclicSearchSpaceSuite.scala
+++ gluten-ras/common/src/test/scala/org/apache/gluten/ras/specific/CyclicSearchSpaceSuite.scala
@@ -65,11 +65,12 @@ abstract class CyclicSearchSpaceSuite extends AnyFunSuite {
       PathFinder.builder(ras, mockState).depth(depth).build().find(can)
     }
 
-    assert(find(node1, 1).map(p => p.plan()) == List(Unary("node1", Group(0))))
-    assert(find(node1, 2).map(p => p.plan()) == List(Unary("node1", Leaf("node2", 1))))
-    assert(find(node1, 3).map(p => p.plan()) == List(Unary("node1", Leaf("node2", 1))))
+    assert(find(node1, 1).map(p => p.plan()).toList == List(Unary("node1", Group(0))))
+    assert(find(node1, 2).map(p => p.plan()).toList == List(Unary("node1", Leaf("node2", 1))))
+    assert(find(node1, 3).map(p => p.plan()).toList == List(Unary("node1", Leaf("node2", 1))))
     assert(
-      find(node1, RasPath.INF_DEPTH).map(p => p.plan()) == List(Unary("node1", Leaf("node2", 1))))
+      find(node1, RasPath.INF_DEPTH).map(p => p.plan()).toList == List(
+        Unary("node1", Leaf("node2", 1))))
   }
 
   test("Cyclic - find best, simple self cycle") {
