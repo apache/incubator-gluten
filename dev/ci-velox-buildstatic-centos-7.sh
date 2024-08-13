0e11ebc373b1ae014331f24376588462f0a680fd
PHILO-HE <he_feilong@126.com>
2024-08-13 22:14:14 +0800

    Fix issue

--- dev/ci-velox-buildstatic-centos-7.sh
+++ dev/ci-velox-buildstatic-centos-7.sh
@@ -2,12 +2,8 @@
 
 set -e
 
-yum install sudo patch java-1.8.0-openjdk-devel -y
-cd $GITHUB_WORKSPACE/ep/build-velox/src
-./get_velox.sh
 source /opt/rh/devtoolset-9/enable
 cd $GITHUB_WORKSPACE/
 source ./dev/vcpkg/env.sh
-sed -i '/^headers/d' ep/build-velox/build/velox_ep/CMakeLists.txt
 export NUM_THREADS=4
 ./dev/builddeps-veloxbe.sh --build_tests=OFF --build_benchmarks=OFF --enable_s3=ON --enable_gcs=ON --enable_hdfs=ON --enable_abfs=ON
