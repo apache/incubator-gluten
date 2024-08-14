ef5f01cba5c6c7ddf57734ca685a458174fc9064
Arnav Balyan <60175178+ArnavBalyan@users.noreply.github.com>
2024-08-14 06:15:49 +0530

    [GLUTEN-6148][CORE] Simplify JniLibLoader loading mechanism for native libraries (#6791)
    Closes #6148

--- backends-velox/src/main/scala/org/apache/gluten/utils/SharedLibraryLoaderCentos7.scala
+++ backends-velox/src/main/scala/org/apache/gluten/utils/SharedLibraryLoaderCentos7.scala
@@ -20,26 +20,26 @@ import org.apache.gluten.vectorized.JniLibLoader
 
 class SharedLibraryLoaderCentos7 extends SharedLibraryLoader {
   override def loadLib(loader: JniLibLoader): Unit = {
-    loader
-      .newTransaction()
-      .loadAndCreateLink("libboost_atomic.so.1.84.0", "libboost_atomic.so", false)
-      .loadAndCreateLink("libboost_thread.so.1.84.0", "libboost_thread.so", false)
-      .loadAndCreateLink("libboost_system.so.1.84.0", "libboost_system.so", false)
-      .loadAndCreateLink("libboost_regex.so.1.84.0", "libboost_regex.so", false)
-      .loadAndCreateLink("libboost_program_options.so.1.84.0", "libboost_program_options.so", false)
-      .loadAndCreateLink("libboost_filesystem.so.1.84.0", "libboost_filesystem.so", false)
-      .loadAndCreateLink("libboost_context.so.1.84.0", "libboost_context.so", false)
-      .loadAndCreateLink("libdouble-conversion.so.1", "libdouble-conversion.so", false)
-      .loadAndCreateLink("libevent-2.0.so.5", "libevent-2.0.so", false)
-      .loadAndCreateLink("libgflags.so.2.2", "libgflags.so", false)
-      .loadAndCreateLink("libglog.so.0", "libglog.so", false)
-      .loadAndCreateLink("libntlm.so.0", "libntlm.so", false)
-      .loadAndCreateLink("libgsasl.so.7", "libgsasl.so", false)
-      .loadAndCreateLink("libprotobuf.so.32", "libprotobuf.so", false)
-      .loadAndCreateLink("libhdfs3.so.1", "libhdfs3.so", false)
-      .loadAndCreateLink("libre2.so.10", "libre2.so", false)
-      .loadAndCreateLink("libzstd.so.1", "libzstd.so", false)
-      .loadAndCreateLink("liblz4.so.1", "liblz4.so", false)
-      .commit()
+    loader.loadAndCreateLink("libboost_atomic.so.1.84.0", "libboost_atomic.so", false)
+    loader.loadAndCreateLink("libboost_thread.so.1.84.0", "libboost_thread.so", false)
+    loader.loadAndCreateLink("libboost_system.so.1.84.0", "libboost_system.so", false)
+    loader.loadAndCreateLink("libboost_regex.so.1.84.0", "libboost_regex.so", false)
+    loader.loadAndCreateLink(
+      "libboost_program_options.so.1.84.0",
+      "libboost_program_options.so",
+      false)
+    loader.loadAndCreateLink("libboost_filesystem.so.1.84.0", "libboost_filesystem.so", false)
+    loader.loadAndCreateLink("libboost_context.so.1.84.0", "libboost_context.so", false)
+    loader.loadAndCreateLink("libdouble-conversion.so.1", "libdouble-conversion.so", false)
+    loader.loadAndCreateLink("libevent-2.0.so.5", "libevent-2.0.so", false)
+    loader.loadAndCreateLink("libgflags.so.2.2", "libgflags.so", false)
+    loader.loadAndCreateLink("libglog.so.0", "libglog.so", false)
+    loader.loadAndCreateLink("libntlm.so.0", "libntlm.so", false)
+    loader.loadAndCreateLink("libgsasl.so.7", "libgsasl.so", false)
+    loader.loadAndCreateLink("libprotobuf.so.32", "libprotobuf.so", false)
+    loader.loadAndCreateLink("libhdfs3.so.1", "libhdfs3.so", false)
+    loader.loadAndCreateLink("libre2.so.10", "libre2.so", false)
+    loader.loadAndCreateLink("libzstd.so.1", "libzstd.so", false)
+    loader.loadAndCreateLink("liblz4.so.1", "liblz4.so", false)
   }
 }
