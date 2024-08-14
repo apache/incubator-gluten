9de4b1034879699af358cb308df6bc8704569b9d
PHILO-HE <feilong.he@intel.com>
2024-08-14 15:00:38 +0800

    Use the new docker and disable arrow build

--- dev/ci-velox-buildstatic-centos-7.sh
+++ dev/ci-velox-buildstatic-centos-7.sh
@@ -6,4 +6,5 @@ source /opt/rh/devtoolset-9/enable
 cd $GITHUB_WORKSPACE/
 source ./dev/vcpkg/env.sh
 export NUM_THREADS=4
-./dev/builddeps-veloxbe.sh --build_tests=OFF --build_benchmarks=OFF --enable_s3=ON --enable_gcs=ON --enable_hdfs=ON --enable_abfs=ON
+./dev/builddeps-veloxbe.sh --build_tests=OFF --build_benchmarks=OFF --build_arrow=OFF --enable_s3=ON \
+                           --enable_gcs=ON --enable_hdfs=ON --enable_abfs=ON
