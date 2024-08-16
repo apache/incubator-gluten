f423c42ad342aa7d579dd4af51e808da899b0f23
PHILO-HE <feilong.he@intel.com>
2024-08-16 17:24:06 +0800

    Initial

--- dev/ci-velox-buildstatic-centos-7.sh
+++ dev/ci-velox-buildstatic-centos-7.sh
@@ -3,8 +3,6 @@
 set -e
 
 source /opt/rh/devtoolset-9/enable
-cd $GITHUB_WORKSPACE/
-source ./dev/vcpkg/env.sh
 export NUM_THREADS=4
-./dev/builddeps-veloxbe.sh --build_tests=OFF --build_benchmarks=OFF --build_arrow=OFF --enable_s3=ON \
-                           --enable_gcs=ON --enable_hdfs=ON --enable_abfs=ON
+./dev/builddeps-veloxbe.sh --enable_vcpkg=ON --build_arrow=OFF --build_tests=ON --build_benchmarks=ON \
+                           --build_examples=ON --enable_s3=ON --enable_gcs=ON --enable_hdfs=ON --enable_abfs=ON
