13b118d8bd3410eaefb233420d0a81df509ef26d
Hongze Zhang <hongze.zhang@intel.com>
2024-08-21 14:36:22 +0800

    Revert "fixup"
    This reverts commit 53db9dbe27fd518d0cc7ca9726bb057ccf4d6074.

--- dev/ci-velox-buildshared-centos-8.sh
+++ dev/ci-velox-buildshared-centos-8.sh
@@ -10,6 +10,7 @@ yum install sudo patch java-1.8.0-openjdk-devel wget -y
 wget https://downloads.apache.org/maven/maven-3/3.8.8/binaries/apache-maven-3.8.8-bin.tar.gz
 tar -xvf apache-maven-3.8.8-bin.tar.gz && mv apache-maven-3.8.8 /usr/lib/maven
 export PATH="${PATH}:/usr/lib/maven/bin"
+echo "PATH=${PATH}:/usr/lib/maven/bin" >> $GITHUB_ENV
 
 source /opt/rh/gcc-toolset-9/enable
 ./dev/builddeps-veloxbe.sh --run_setup_script=OFF --enable_ep_cache=OFF --build_tests=ON \
