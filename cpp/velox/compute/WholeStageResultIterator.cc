0fc9e31b708f795e2c9e49a6076a2642f8df0719
Hongze Zhang <hongze.zhang@intel.com>
2024-08-21 16:54:07 +0800

    [GLUTEN-6908][VL] Fix error when getting output from a Velox task that is under spilling by background thread (#6934)
    (cherry picked from commit 80078cd4c8b36db39128f93c666af1ee2c8d94dd)

--- cpp/velox/compute/WholeStageResultIterator.cc
+++ cpp/velox/compute/WholeStageResultIterator.cc
@@ -194,7 +194,22 @@ std::shared_ptr<ColumnarBatch> WholeStageResultIterator::next() {
   if (task_->isFinished()) {
     return nullptr;
   }
-  velox::RowVectorPtr vector = task_->next();
+  velox::RowVectorPtr vector;
+  while (true) {
+    auto future = velox::ContinueFuture::makeEmpty();
+    auto out = task_->next(&future);
+    if (!future.valid()) {
+      // Not need to wait. Break.
+      vector = std::move(out);
+      break;
+    }
+    // Velox suggested to wait. This might be because another thread (e.g., background io thread) is spilling the task.
+    GLUTEN_CHECK(out == nullptr, "Expected to wait but still got non-null output from Velox task");
+    VLOG(2) << "Velox task " << task_->taskId()
+            << " is busy when ::next() is called. Will wait and try again. Task state: "
+            << taskStateString(task_->state());
+    future.wait();
+  }
   if (vector == nullptr) {
     return nullptr;
   }
