dd368963161d7ac28a41ee07e3bece53576abf9b
shuai-xu <xushuai@bigo.sg>
2024-09-03 18:28:45 +0800

    [GLUTEN-7096] [CH] fix exception when same names in group by

--- gluten-ut/spark33/src/test/scala/org/apache/spark/sql/execution/GlutenSQLAggregateFunctionSuite.scala
+++ gluten-ut/spark33/src/test/scala/org/apache/spark/sql/execution/GlutenSQLAggregateFunctionSuite.scala
@@ -33,4 +33,44 @@ class GlutenSQLAggregateFunctionSuite extends GlutenSQLTestsTrait {
     checkAnswer(df, Seq(Row(3, 5)))
     assert(getExecutedPlan(df).count(_.isInstanceOf[HashAggregateExecBaseTransformer]) == 4)
   }
+
+  testGluten("GLUTEN-7096: Same names in group by may cause exception") {
+    sql("create table if not exists user (day string, rtime int, uid string, owner string)")
+    sql("insert into user values ('2024-09-01', 123, 'user1', 'owner1')")
+    sql("insert into user values ('2024-09-01', 123, 'user1', 'owner1')")
+    sql("insert into user values ('2024-09-02', 567, 'user2', 'owner2')")
+    val query =
+      """
+        |select days, rtime, uid, uid, day1
+        |from (
+        | select day1 as days, rtime, uid, uid, day1
+        | from (
+        |   select distinct coalesce(day, "today") as day1, rtime, uid, owner
+        |   from user where day = '2024-09-01'
+        | )) group by days, rtime, guest_uid, owner_uid, day1
+        |""".stripMargin
+    val df = sql(query)
+    checkAnswer(df, Seq(Row("2024-09-01", 123, "user1", "owner1", "2024-09-01")))
+    sql("drop table if exists user")
+  }
+
+  testGluten("GLUTEN-7096: Same names with different qualifier in group by may cause exception") {
+    sql("create table if not exists user (day string, rtime int, uid string, owner string)")
+    sql("insert into user values ('2024-09-01', 123, 'user1', 'owner1')")
+    sql("insert into user values ('2024-09-01', 123, 'user1', 'owner1')")
+    sql("insert into user values ('2024-09-02', 567, 'user2', 'owner2')")
+    val query =
+      """
+        |select days, rtime, uid, uid, day1
+        |from (
+        | select day1 as days, rtime, uid, uid, day1
+        | from (
+        |   select distinct coalesce(day, "today") as day1, rtime, uid, owner
+        |   from user where day = '2024-09-01'
+        | ) t1 ) t2 group by days, rtime, guest_uid, owner_uid, day1
+        |""".stripMargin
+    val df = sql(query)
+    checkAnswer(df, Seq(Row("2024-09-01", 123, "user1", "owner1", "2024-09-01")))
+    sql("drop table if exists user")
+  }
 }
