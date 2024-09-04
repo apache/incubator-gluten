fc9efeac4b82871e87e520a4466970e79d525208
taiyang-li <654010905@qq.com>
2024-09-04 16:30:11 +0800

    fix style

--- gluten-ut/spark32/src/test/scala/org/apache/gluten/utils/clickhouse/ClickHouseTestSettings.scala
+++ gluten-ut/spark32/src/test/scala/org/apache/gluten/utils/clickhouse/ClickHouseTestSettings.scala
@@ -911,7 +911,9 @@ class ClickHouseTestSettings extends BackendTestSettings {
     .exclude("ParseUrl")
     .exclude("SPARK-33468: ParseUrl in ANSI mode should fail if input string is not a valid url")
     .exclude("FORMAT") // refer https://github.com/apache/incubator-gluten/issues/6765
-    .exclude("soundex unit test") // CH and spark returns different results when input non-ASCII characters
+    .exclude(
+      "soundex unit test"
+    ) // CH and spark returns different results when input non-ASCII characters
     .excludeGlutenTest("SPARK-40213: ascii for Latin-1 Supplement characters")
   enableSuite[GlutenTryCastSuite]
     .exclude("null cast")
