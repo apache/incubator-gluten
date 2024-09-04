e8ee0fb755714339f5eb4ffaf945501ba19d6331
PHILO-HE <feilong.he@intel.com>
2024-09-04 17:35:10 +0800

    Initial

--- dev/vcpkg/ports/duckdb/portfile.cmake
+++ dev/vcpkg/ports/duckdb/portfile.cmake
@@ -22,4 +22,11 @@ vcpkg_configure_cmake(
 )
 vcpkg_install_cmake(ADD_BIN_TO_PATH)
 
+vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake/DuckDB)
+
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/lib/cmake")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/lib/cmake")
+
+vcpkg_cmake_config_fixup(PACKAGE_NAME DuckDB)
+
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
