8ae28298dd0ecdb0ea30110764b8be324f66a648
Yuan Zhou <yuan.zhou@intel.com>
2024-09-06 08:26:12 +0800

    Revert "fix depedency on centos 8"
    This reverts commit 70fea33581d5295f5e0001ffb244760ed00a2b92.
    
    Signed-off-by: Yuan Zhou <yuan.zhou@intel.com>

--- dev/ci-velox-buildshared-centos-8.sh
+++ dev/ci-velox-buildshared-centos-8.sh
@@ -2,6 +2,7 @@
 
 set -e
 
+sudo -E dev/vcpkg/vcpkg.json
 source /opt/rh/gcc-toolset-9/enable
 ./dev/builddeps-veloxbe.sh --run_setup_script=OFF --enable_ep_cache=OFF --build_tests=ON \
     --build_examples=ON --build_benchmarks=ON --build_protobuf=ON
