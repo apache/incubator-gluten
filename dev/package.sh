7df6901e9b373dca8f1d4800ac03b0298723bf9c
PHILO-HE <feilong.he@intel.com>
2024-09-09 14:12:38 +0800

    Explicit disable vcpkg

--- dev/package.sh
+++ dev/package.sh
@@ -12,7 +12,7 @@ cd "$GLUTEN_DIR"
 
 # build gluten with velox backend, prompt always respond y
 export PROMPT_ALWAYS_RESPOND=y
-./dev/buildbundle-veloxbe.sh --build_tests=ON --build_benchmarks=ON --enable_s3=ON  --enable_hdfs=ON "$@"
+./dev/buildbundle-veloxbe.sh --enable_vcpkg=OFF --build_tests=ON --build_benchmarks=ON --enable_s3=ON  --enable_hdfs=ON "$@"
 
 # make thirdparty package
 ./dev/build-thirdparty.sh
