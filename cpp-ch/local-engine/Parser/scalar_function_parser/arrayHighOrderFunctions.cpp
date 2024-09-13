325f6a4bf769c3b4803701378742279b097c4c41
李扬 <654010905@qq.com>
2024-09-13 10:21:24 +0800

    [GLUTEN-6816][CH] support function zip_with with some minor refactors (#7211)
    What changes were proposed in this pull request?
    (Please fill in changes proposed in this fix)
    
    (Fixes: #6816)
    
    How was this patch tested?
    GlutenDataFrameFunctionsSuite
    
    "arrays zip_with function - for non-primitive types"
    "arrays zip_with function - for non-primitive types"
    "arrays zip_with function - invalid"

--- cpp-ch/local-engine/Parser/scalar_function_parser/arrayHighOrderFunctions.cpp
+++ cpp-ch/local-engine/Parser/scalar_function_parser/arrayHighOrderFunctions.cpp
@@ -30,17 +30,18 @@
 
 namespace DB::ErrorCodes
 {
-    extern const int LOGICAL_ERROR;
+    extern const int SIZES_OF_COLUMNS_DOESNT_MATCH;
+    extern const int BAD_ARGUMENTS;
 }
 
 namespace local_engine
 {
-class ArrayFilter : public FunctionParser
+class FunctionParserArrayFilter : public FunctionParser
 {
 public:
     static constexpr auto name = "filter";
-    explicit ArrayFilter(SerializedPlanParser * plan_parser_) : FunctionParser(plan_parser_) {}
-    ~ArrayFilter() override = default;
+    explicit FunctionParserArrayFilter(SerializedPlanParser * plan_parser_) : FunctionParser(plan_parser_) {}
+    ~FunctionParserArrayFilter() override = default;
 
     String getName() const override { return name; }
 
@@ -69,14 +70,14 @@ public:
         return toFunctionNode(actions_dag, ch_func_name, {parsed_args[1], parsed_args[0], index_array_node});
     }
 };
-static FunctionParserRegister<ArrayFilter> register_array_filter;
+static FunctionParserRegister<FunctionParserArrayFilter> register_array_filter;
 
-class ArrayTransform : public FunctionParser
+class FunctionParserArrayTransform : public FunctionParser
 {
 public:
     static constexpr auto name = "transform";
-    explicit ArrayTransform(SerializedPlanParser * plan_parser_) : FunctionParser(plan_parser_) {}
-    ~ArrayTransform() override = default;
+    explicit FunctionParserArrayTransform(SerializedPlanParser * plan_parser_) : FunctionParser(plan_parser_) {}
+    ~FunctionParserArrayTransform() override = default;
     String getName() const override { return name; }
     String getCHFunctionName(const substrait::Expression_ScalarFunction & scalar_function) const override
     {
@@ -115,14 +116,14 @@ public:
         return toFunctionNode(actions_dag, ch_func_name, {parsed_args[1], parsed_args[0], index_array_node});
     }
 };
-static FunctionParserRegister<ArrayTransform> register_array_map;
+static FunctionParserRegister<FunctionParserArrayTransform> register_array_map;
 
-class ArrayAggregate : public FunctionParser
+class FunctionParserArrayAggregate : public FunctionParser
 {
 public:
     static constexpr auto name = "aggregate";
-    explicit ArrayAggregate(SerializedPlanParser * plan_parser_) : FunctionParser(plan_parser_) {}
-    ~ArrayAggregate() override = default;
+    explicit FunctionParserArrayAggregate(SerializedPlanParser * plan_parser_) : FunctionParser(plan_parser_) {}
+    ~FunctionParserArrayAggregate() override = default;
     String getName() const override { return name; }
     String getCHFunctionName(const substrait::Expression_ScalarFunction & scalar_function) const override
     {
@@ -134,9 +135,11 @@ public:
         auto ch_func_name = getCHFunctionName(substrait_func);
         auto parsed_args = parseFunctionArguments(substrait_func, actions_dag);
         assert(parsed_args.size() == 3);
+
         const auto * function_type = typeid_cast<const DataTypeFunction *>(parsed_args[2]->result_type.get());
         if (!function_type)
-            throw DB::Exception(DB::ErrorCodes::LOGICAL_ERROR, "The third argument of aggregate function must be a lambda function");
+            throw DB::Exception(DB::ErrorCodes::BAD_ARGUMENTS, "The third argument of aggregate function must be a lambda function");
+
         if (!parsed_args[1]->result_type->equals(*(function_type->getReturnType())))
         {
             parsed_args[1] = ActionsDAGUtil::convertNodeType(
@@ -160,14 +163,14 @@ public:
         return toFunctionNode(actions_dag, "if", {is_null_node, null_node, func_node});
     }
 };
-static FunctionParserRegister<ArrayAggregate> register_array_aggregate;
+static FunctionParserRegister<FunctionParserArrayAggregate> register_array_aggregate;
 
-class ArraySort : public FunctionParser
+class FunctionParserArraySort : public FunctionParser
 {
 public:
     static constexpr auto name = "array_sort";
-    explicit ArraySort(SerializedPlanParser * plan_parser_) : FunctionParser(plan_parser_) {}
-    ~ArraySort() override = default;
+    explicit FunctionParserArraySort(SerializedPlanParser * plan_parser_) : FunctionParser(plan_parser_) {}
+    ~FunctionParserArraySort() override = default;
     String getName() const override { return name; }
     String getCHFunctionName(const substrait::Expression_ScalarFunction & scalar_function) const override
     {
@@ -180,7 +183,8 @@ public:
         auto parsed_args = parseFunctionArguments(substrait_func, actions_dag);
 
         if (parsed_args.size() != 2)
-            throw DB::Exception(DB::ErrorCodes::LOGICAL_ERROR, "array_sort function must have two arguments");
+            throw DB::Exception(DB::ErrorCodes::SIZES_OF_COLUMNS_DOESNT_MATCH, "array_sort function must have two arguments");
+
         if (isDefaultCompare(substrait_func.arguments()[1].value().scalar_function()))
         {
             return toFunctionNode(actions_dag, ch_func_name, {parsed_args[0]});
@@ -304,6 +308,34 @@ private:
         return is_if_both_null_else(lambda_body);
     }
 };
-static FunctionParserRegister<ArraySort> register_array_sort;
+static FunctionParserRegister<FunctionParserArraySort> register_array_sort;
+
+class FunctionParserZipWith: public FunctionParser
+{
+public:
+    static constexpr auto name = "zip_with";
+    explicit FunctionParserZipWith(SerializedPlanParser * plan_parser_) : FunctionParser(plan_parser_) {}
+    ~FunctionParserZipWith() override = default;
+    String getName() const override { return name; }
+
+    const DB::ActionsDAG::Node *
+    parse(const substrait::Expression_ScalarFunction & substrait_func, DB::ActionsDAG & actions_dag) const override
+    {
+        /// Parse spark zip_with(arr1, arr2, func) as CH arrayMap(func, arrayZipUnaligned(arr1, arr2))
+        auto parsed_args = parseFunctionArguments(substrait_func, actions_dag);
+        if (parsed_args.size() != 3)
+            throw DB::Exception(DB::ErrorCodes::SIZES_OF_COLUMNS_DOESNT_MATCH, "zip_with function must have three arguments");
+
+        auto lambda_args = collectLambdaArguments(*plan_parser, substrait_func.arguments()[2].value().scalar_function());
+        if (lambda_args.size() != 2)
+            throw DB::Exception(DB::ErrorCodes::SIZES_OF_COLUMNS_DOESNT_MATCH, "The lambda function in zip_with must have two arguments");
+
+        const auto * array_zip_unaligned = toFunctionNode(actions_dag, "arrayZipUnaligned", {parsed_args[0], parsed_args[1]});
+        const auto * array_map = toFunctionNode(actions_dag, "arrayMap", {parsed_args[2], array_zip_unaligned});
+        return convertNodeTypeIfNeeded(substrait_func, array_map, actions_dag);
+    }
+};
+static FunctionParserRegister<FunctionParserZipWith> register_zip_with;
+
 
 }
