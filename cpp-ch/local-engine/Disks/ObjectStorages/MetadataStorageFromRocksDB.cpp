a7c9567bc7acdd14c8474c3d2dc99d1cfd062b56
LiuNeng <1398775315@qq.com>
2024-09-14 16:17:09 +0800

    [CH] Support rocksdb disk metadata (#7239)
    What changes were proposed in this pull request?
    Added rocksdb-based disk metadata management to solve the problem of too many small mergetree disk metadata files. It also supports automatic cleanup of expired data.
    
    How was this patch tested?
    unit tests

---
+++ cpp-ch/local-engine/Disks/ObjectStorages/MetadataStorageFromRocksDB.cpp
@@ -0,0 +1,316 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include "MetadataStorageFromRocksDB.h"
+#if USE_ROCKSDB
+#include <Disks/ObjectStorages/DiskObjectStorageMetadata.h>
+#include <Disks/ObjectStorages/MetadataStorageFromRocksDBTransactionOperations.h>
+#include <Disks/ObjectStorages/StaticDirectoryIterator.h>
+#include <Interpreters/Context.h>
+#include <Storages/MergeTree/MetaDataHelper.h>
+#include <rocksdb/db.h>
+#include <Common/QueryContext.h>
+
+namespace local_engine
+{
+static std::string getObjectKeyCompatiblePrefix(
+    const DB::IObjectStorage & object_storage, const Poco::Util::AbstractConfiguration & config, const String & config_prefix)
+{
+    return config.getString(config_prefix + ".key_compatibility_prefix", object_storage.getCommonKeyPrefix());
+}
+
+DB::MetadataStoragePtr MetadataStorageFromRocksDB::create(
+    const std::string &,
+    const Poco::Util::AbstractConfiguration & config,
+    const std::string & config_prefix,
+    DB::ObjectStoragePtr object_storage)
+{
+    auto metadata_path = config.getString(config_prefix + ".metadata_path");
+    size_t clean_meta_task_interval_seconds = config.getUInt(config_prefix + ".clean_meta_task_interval_seconds", 60 * 60 * 12);
+    fs::create_directories(metadata_path);
+    auto key_compatibility_prefix = getObjectKeyCompatiblePrefix(*object_storage, config, config_prefix);
+    return std::make_shared<MetadataStorageFromRocksDB>(
+        key_compatibility_prefix, metadata_path, object_storage, clean_meta_task_interval_seconds);
+}
+
+MetadataStorageFromRocksDB::MetadataStorageFromRocksDB(
+    const String & compatible_key_prefix,
+    const String & rocksdb_dir,
+    DB::ObjectStoragePtr & object_storage_,
+    size_t metadata_clean_task_interval_seconds_)
+    : compatible_key_prefix(compatible_key_prefix)
+    , rocksdb_dir(rocksdb_dir)
+    , object_storage(object_storage_)
+    , metadata_clean_task_interval_seconds(metadata_clean_task_interval_seconds_)
+{
+    rocksdb::Options options;
+    options.create_if_missing = true;
+    throwRockDBErrorNotOk(rocksdb::DB::Open(options, rocksdb_dir, &rocksdb));
+    metadata_clean_task = QueryContext::globalContext()->getSchedulePool().createTask(
+        "MetadataStorageFromRocksDB", [this] { cleanOutdatedMetadataThreadFunc(); });
+    metadata_clean_task->scheduleAfter(metadata_clean_task_interval_seconds * 1000);
+    logger = getLogger("MetadataStorageFromRocksDB");
+}
+
+DB::MetadataTransactionPtr MetadataStorageFromRocksDB::createTransaction()
+{
+    return std::make_shared<MetadataStorageFromRocksDBTransaction>(*this);
+}
+
+const std::string & MetadataStorageFromRocksDB::getPath() const
+{
+    return rocksdb_dir;
+}
+
+DB::MetadataStorageType MetadataStorageFromRocksDB::getType() const
+{
+    return DB::MetadataStorageType::None;
+}
+
+bool MetadataStorageFromRocksDB::exists(const std::string & path) const
+{
+    return exist(getRocksDB(), path);
+}
+
+bool MetadataStorageFromRocksDB::isFile(const std::string & path) const
+{
+    auto data = getData(getRocksDB(), path);
+    return data != RocksDBCreateDirectoryOperation::DIR_DATA;
+}
+
+bool MetadataStorageFromRocksDB::isDirectory(const std::string & path) const
+{
+    auto data = getData(getRocksDB(), path);
+    return data == RocksDBCreateDirectoryOperation::DIR_DATA;
+}
+
+uint64_t MetadataStorageFromRocksDB::getFileSize(const std::string & path) const
+{
+    return readMetadata(path)->getTotalSizeBytes();
+}
+
+Poco::Timestamp MetadataStorageFromRocksDB::getLastModified(const std::string & /*path*/) const
+{
+    return {};
+}
+
+bool MetadataStorageFromRocksDB::supportsChmod() const
+{
+    return false;
+}
+
+bool MetadataStorageFromRocksDB::supportsStat() const
+{
+    return false;
+}
+
+std::vector<std::string> MetadataStorageFromRocksDB::listDirectory(const std::string & path) const
+{
+    return listKeys(getRocksDB(), path);
+}
+
+DB::DirectoryIteratorPtr MetadataStorageFromRocksDB::iterateDirectory(const std::string & path) const
+{
+    auto files = listKeys(getRocksDB(), path);
+    std::vector<std::filesystem::path> paths;
+    paths.reserve(files.size());
+    for (const auto & file : files)
+        paths.emplace_back(file);
+    return std::make_unique<DB::StaticDirectoryIterator>(std::move(paths));
+}
+
+uint32_t MetadataStorageFromRocksDB::getHardlinkCount(const std::string & /*path*/) const
+{
+    return 0;
+}
+
+DB::StoredObjects MetadataStorageFromRocksDB::getStorageObjects(const std::string & path) const
+{
+    auto metadata = readMetadata(path);
+    const auto & keys_with_meta = metadata->getKeysWithMeta();
+
+    DB::StoredObjects objects;
+    objects.reserve(keys_with_meta.size());
+    for (const auto & [object_key, object_meta] : keys_with_meta)
+        objects.emplace_back(object_key.serialize(), path, object_meta.size_bytes, object_meta.offset);
+
+    return objects;
+}
+
+DB::DiskObjectStorageMetadataPtr MetadataStorageFromRocksDB::readMetadata(const std::string & path) const
+{
+    std::shared_lock lock(metadata_mutex);
+    return readMetadataUnlocked(path, lock);
+}
+
+DB::DiskObjectStorageMetadataPtr
+MetadataStorageFromRocksDB::readMetadataUnlocked(const std::string & path, std::unique_lock<DB::SharedMutex> &) const
+{
+    auto metadata = std::make_unique<DB::DiskObjectStorageMetadata>(compatible_key_prefix, path);
+    auto str = getData(getRocksDB(), path);
+    metadata->deserializeFromString(str);
+    return metadata;
+}
+
+DB::DiskObjectStorageMetadataPtr
+MetadataStorageFromRocksDB::readMetadataUnlocked(const std::string & path, std::shared_lock<DB::SharedMutex> &) const
+{
+    auto metadata = std::make_unique<DB::DiskObjectStorageMetadata>(compatible_key_prefix, path);
+    auto str = getData(getRocksDB(), path);
+    metadata->deserializeFromString(str);
+    return metadata;
+}
+
+std::string MetadataStorageFromRocksDB::readFileToString(const std::string & path) const
+{
+    return getData(getRocksDB(), path);
+}
+
+void MetadataStorageFromRocksDB::shutdown()
+{
+    metadata_clean_task->deactivate();
+    if (rocksdb)
+    {
+        rocksdb->Close();
+        rocksdb = nullptr;
+    }
+}
+
+void MetadataStorageFromRocksDB::cleanOutdatedMetadataThreadFunc()
+{
+    LOG_INFO(logger, "start to clean disk metadata in rocksdb.");
+    std::queue<String> part_queue;
+    size_t total_count_remove = 0;
+    auto removeParts = [&]
+    {
+        while (!part_queue.empty())
+        {
+            auto meta_name = part_queue.front();
+            part_queue.pop();
+            std::filesystem::path meta_path(meta_name);
+            auto part_path = meta_path.parent_path();
+            auto files = listDirectory(part_path);
+            total_count_remove += (files.size() + 1);
+            getRocksDB().DeleteRange({}, part_path.generic_string(), files.back());
+            getRocksDB().Delete({}, files.back());
+        }
+    };
+    auto * it = getRocksDB().NewIterator({});
+    String prev_key;
+    String prev_data;
+    for (it->SeekToFirst(); it->Valid(); it->Next())
+    {
+        auto file_name = it->key().ToString();
+        // mark outdated part
+        if (isMergeTreePartMetaDataFile(file_name))
+        {
+            auto objects = getStorageObjects(it->key().ToString());
+            if (!object_storage->exists(objects.front()))
+                part_queue.push(file_name);
+        }
+        // clean empty directory
+        if (!prev_key.empty() && !file_name.starts_with(prev_key) && prev_data == RocksDBCreateDirectoryOperation::DIR_DATA)
+        {
+            getRocksDB().Delete({}, prev_key);
+            total_count_remove ++;
+        }
+        if (part_queue.size() > 10000)
+        {
+            removeParts();
+        }
+    }
+    removeParts();
+    rocksdb::Slice begin(nullptr, 0);
+    rocksdb::Slice end(nullptr, 0);
+    rocksdb::Status s = getRocksDB().CompactRange({}, &begin, &end);
+    LOG_INFO(logger, "Clean meta finish, totally clean {} meta", total_count_remove);
+    metadata_clean_task->scheduleAfter(metadata_clean_task_interval_seconds * 1000);
+}
+
+DB::SharedMutex & MetadataStorageFromRocksDB::getMetadataMutex() const
+{
+    return metadata_mutex;
+}
+
+rocksdb::DB & MetadataStorageFromRocksDB::getRocksDB() const
+{
+    if (!rocksdb)
+        throw DB::Exception(DB::ErrorCodes::LOGICAL_ERROR, "RocksDB is not initialized");
+    return *rocksdb;
+}
+
+void MetadataStorageFromRocksDBTransaction::commit()
+{
+    commitImpl(metadata_storage.getMetadataMutex());
+}
+
+const DB::IMetadataStorage & MetadataStorageFromRocksDBTransaction::getStorageForNonTransactionalReads() const
+{
+    return metadata_storage;
+}
+
+bool MetadataStorageFromRocksDBTransaction::supportsChmod() const
+{
+    return false;
+}
+
+void MetadataStorageFromRocksDBTransaction::createEmptyMetadataFile(const std::string & path)
+{
+    auto metadata = std::make_unique<DB::DiskObjectStorageMetadata>(metadata_storage.compatible_key_prefix, path);
+    writeStringToFile(path, metadata->serializeToString());
+}
+
+void MetadataStorageFromRocksDBTransaction::createMetadataFile(const std::string & path, DB::ObjectStorageKey key, uint64_t size_in_bytes)
+{
+    auto metadata = std::make_unique<DB::DiskObjectStorageMetadata>(metadata_storage.compatible_key_prefix, path);
+    metadata->addObject(std::move(key), size_in_bytes);
+
+    auto data = metadata->serializeToString();
+    if (!data.empty())
+        addOperation(std::make_unique<RocksDBWriteFileOperation>(path, metadata_storage.getRocksDB(), data));
+}
+
+void MetadataStorageFromRocksDBTransaction::writeStringToFile(const std::string & path, const std::string & data)
+{
+    addOperation(std::make_unique<RocksDBWriteFileOperation>(path, metadata_storage.getRocksDB(), data));
+}
+
+void MetadataStorageFromRocksDBTransaction::createDirectory(const std::string & path)
+{
+    addOperation(std::make_unique<RocksDBCreateDirectoryOperation>(path, metadata_storage.getRocksDB()));
+}
+
+void MetadataStorageFromRocksDBTransaction::createDirectoryRecursive(const std::string & path)
+{
+    addOperation(std::make_unique<RocksDBCreateDirectoryRecursiveOperation>(path, metadata_storage.getRocksDB()));
+}
+
+void MetadataStorageFromRocksDBTransaction::removeDirectory(const std::string & path)
+{
+    addOperation(std::make_unique<RocksDBRemoveDirectoryOperation>(path, metadata_storage.getRocksDB()));
+}
+
+void MetadataStorageFromRocksDBTransaction::removeRecursive(const std::string & path)
+{
+    addOperation(std::make_unique<RocksDBRemoveRecursiveOperation>(path, metadata_storage.getRocksDB()));
+}
+
+void MetadataStorageFromRocksDBTransaction::unlinkFile(const std::string & path)
+{
+    addOperation(std::make_unique<RocksDBUnlinkFileOperation>(path, metadata_storage.getRocksDB()));
+}
+}
+#endif
---
+++ cpp-ch/local-engine/Disks/ObjectStorages/MetadataStorageFromRocksDBTransactionOperations.cpp
@@ -0,0 +1,177 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include <config.h>
+#if USE_ROCKSDB
+#include "MetadataStorageFromRocksDBTransactionOperations.h"
+#include <ranges>
+
+namespace DB
+{
+namespace ErrorCodes
+{
+extern const int INVALID_STATE;
+}
+}
+
+namespace local_engine
+{
+
+void throwRockDBErrorNotOk(const rocksdb::Status & status)
+{
+    if (!status.ok())
+        throw DB::Exception(DB::ErrorCodes::INVALID_STATE, "Access rocksdb failed: {}", status.ToString());
+}
+
+bool exist(rocksdb::DB & db, const std::string & path)
+{
+    std::string data;
+    auto status = db.Get({}, path, &data);
+    if (status.IsNotFound())
+        return false;
+    throwRockDBErrorNotOk(status);
+    return status.ok();
+}
+
+String getData(rocksdb::DB & db, const std::string & path)
+{
+    std::string data;
+    throwRockDBErrorNotOk(db.Get({}, path, &data));
+    return data;
+}
+
+std::vector<String> listKeys(rocksdb::DB & db, const std::string & path)
+{
+    std::vector<String> result;
+    auto *it = db.NewIterator({});
+    for (it->Seek(path); it->Valid() && it->key().starts_with(path); it->Next())
+    {
+        if (it->key() == path)
+            continue;
+        result.push_back(it->key().ToString());
+    }
+    return result;
+}
+
+void RocksDBWriteFileOperation::execute(std::unique_lock<DB::SharedMutex> &)
+{
+    auto status = db.Get({}, path, &prev_data);
+    if (status.IsNotFound())
+        existed = false;
+    else
+        throwRockDBErrorNotOk(status);
+    db.Put({}, path, data);
+}
+
+void RocksDBWriteFileOperation::undo(std::unique_lock<DB::SharedMutex> &)
+{
+    if (existed)
+        throwRockDBErrorNotOk(db.Put({}, path, prev_data));
+    else
+        throwRockDBErrorNotOk(db.Delete({}, path));
+}
+
+void RocksDBCreateDirectoryOperation::execute(std::unique_lock<DB::SharedMutex> &)
+{
+    existed = exist(db, path);
+    if (existed)
+        return;
+    throwRockDBErrorNotOk(db.Put({}, path, DIR_DATA));
+}
+
+void RocksDBCreateDirectoryOperation::undo(std::unique_lock<DB::SharedMutex> &)
+{
+    if (existed) return;
+    throwRockDBErrorNotOk(db.Delete({}, path));
+}
+
+void RocksDBCreateDirectoryRecursiveOperation::execute(std::unique_lock<DB::SharedMutex> & )
+{
+    namespace fs = std::filesystem;
+    fs::path p(path);
+    while (!exist(db, p.string()))
+    {
+        paths_created.push_back(p);
+        if (!p.has_parent_path())
+            break;
+        p = p.parent_path();
+    }
+    for (const auto & path_to_create : paths_created | std::views::reverse)
+        throwRockDBErrorNotOk(db.Put({}, path_to_create, RocksDBCreateDirectoryOperation::DIR_DATA));
+}
+
+void RocksDBCreateDirectoryRecursiveOperation::undo(std::unique_lock<DB::SharedMutex> & )
+{
+    for (const auto & path_created : paths_created)
+        throwRockDBErrorNotOk(db.Delete({}, path_created));
+}
+
+void RocksDBRemoveDirectoryOperation::execute(std::unique_lock<DB::SharedMutex> &)
+{
+    auto *it = db.NewIterator({});
+    bool empty_dir = true;
+    for (it->Seek(path); it->Valid() && it->key().starts_with(path); it->Next())
+    {
+        if (it->key() != path && it->key().starts_with(path))
+        {
+            empty_dir = false;
+            break;
+        }
+        if (it->key() == path)
+            existed = true;
+    }
+    if (!empty_dir)
+    {
+        throw DB::Exception(DB::ErrorCodes::INVALID_STATE, "Directory {} is not empty", path);
+    }
+    if (existed)
+        throwRockDBErrorNotOk(db.Delete({}, path));
+}
+
+void RocksDBRemoveDirectoryOperation::undo(std::unique_lock<DB::SharedMutex> &)
+{
+    if (existed)
+        throwRockDBErrorNotOk(db.Put({}, path, RocksDBCreateDirectoryOperation::DIR_DATA));
+}
+
+void RocksDBRemoveRecursiveOperation::execute(std::unique_lock<DB::SharedMutex> &)
+{
+    auto *it = db.NewIterator({});
+    for (it->Seek(path); it->Valid() && it->key().starts_with(path); it->Next())
+    {
+        files.emplace(it->key().ToString(), it->value().ToString());
+        throwRockDBErrorNotOk(db.Delete({}, it->key()));
+    }
+}
+
+void RocksDBRemoveRecursiveOperation::undo(std::unique_lock<DB::SharedMutex> &)
+{
+    for (const auto & [key, value] : files)
+        throwRockDBErrorNotOk(db.Put({}, key, value));
+}
+
+void RocksDBUnlinkFileOperation::execute(std::unique_lock<DB::SharedMutex> &)
+{
+    prev_data = getData(db, path);
+    throwRockDBErrorNotOk(db.Delete({}, path));
+}
+
+void RocksDBUnlinkFileOperation::undo(std::unique_lock<DB::SharedMutex> &)
+{
+    throwRockDBErrorNotOk(db.Put({}, path, prev_data));
+}
+}
+#endif
