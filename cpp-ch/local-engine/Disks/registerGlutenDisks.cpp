apache/incubator-gluten

commit a7c9567bc7acdd14c8474c3d2dc99d1cfd062b56

[CH] Support rocksdb disk metadata (#7239)
What changes were proposed in this pull request?
Added rocksdb-based disk metadata management to solve the problem of too many small mergetree disk metadata files. It also supports automatic cleanup of expired data.

How was this patch tested?
unit tests

cpp-ch/local-engine/Disks/registerGlutenDisks.cpp
@@ -29,6 +29,10 @@
 #include <Disks/ObjectStorages/GlutenDiskS3.h>
 #endif
 
+#if USE_ROCKSDB
+#include <Disks/ObjectStorages/MetadataStorageFromRocksDB.h>
+#endif
+
 #include "registerGlutenDisks.h"
 
 namespace local_engine
@@ -60,7 +64,18 @@ void registerGlutenDisks(bool global_skip_access_check)
                                           const Poco::Util::AbstractConfiguration & conf, DB::ContextPtr ctx) -> DB::ObjectStoragePtr
         { return DB::ObjectStorageFactory::instance().create(name, conf, config_prefix, ctx, skip_access_check); };
         auto object_storage = DB::ObjectStorageFactory::instance().create(name, config, config_prefix, context, skip_access_check);
-        auto metadata_storage = DB::MetadataStorageFactory::instance().create(name, config, config_prefix, object_storage, "local");
+        DB::MetadataStoragePtr metadata_storage;
+        auto metadata_type = DB::MetadataStorageFactory::getMetadataType(config, config_prefix, "local");
+        if (metadata_type == "rocksdb")
+        {
+#if USE_ROCKSDB
+            metadata_storage = MetadataStorageFromRocksDB::create(name, config, config_prefix, object_storage);
+#else
+            throw Exception(ErrorCodes::NOT_IMPLEMENTED, "RocksDB metadata storage is not enabled in the build");
+#endif
+        }
+        else
+            metadata_storage = DB::MetadataStorageFactory::instance().create(name, config, config_prefix, object_storage, "local");
 
         DB::DiskObjectStoragePtr disk = std::make_shared<local_engine::GlutenDiskS3>(
             name,
@@ -96,7 +111,18 @@ void registerGlutenDisks(bool global_skip_access_check)
                                           const Poco::Util::AbstractConfiguration & conf, DB::ContextPtr ctx) -> DB::ObjectStoragePtr
         { return DB::ObjectStorageFactory::instance().create(name, conf, config_prefix, ctx, skip_access_check); };
         auto object_storage = object_storage_creator(config, context);
-        auto metadata_storage = DB::MetadataStorageFactory::instance().create(name, config, config_prefix, object_storage, "local");
+        DB::MetadataStoragePtr metadata_storage;
+        auto metadata_type = DB::MetadataStorageFactory::getMetadataType(config, config_prefix,  "local");
+        if (metadata_type == "rocksdb")
+        {
+#if USE_ROCKSDB
+            metadata_storage = MetadataStorageFromRocksDB::create(name, config, config_prefix, object_storage);
+#else
+            throw Exception(ErrorCodes::NOT_IMPLEMENTED, "RocksDB metadata storage is not enabled in the build");
+#endif
+        }
+        else
+            metadata_storage = DB::MetadataStorageFactory::instance().create(name, config, config_prefix, object_storage, "local");
 
         DB::DiskObjectStoragePtr disk = std::make_shared<local_engine::GlutenDiskHDFS>(
             name,

backends-clickhouse/src/test/scala/org/apache/gluten/execution/GlutenClickHouseWholeStageTransformerSuite.scala
@@ -153,6 +153,36 @@ class GlutenClickHouseWholeStageTransformerSuite extends WholeStageTransformerSu
           "spark.gluten.sql.columnar.backend.ch.runtime_config.storage_configuration.policies.__hdfs_main.volumes.main.disk",
           "hdfs_cache")
         .set(
+          "spark.gluten.sql.columnar.backend.ch.runtime_config.storage_configuration.disks.hdfs2.type",
+          "hdfs_gluten")
+        .set(
+          "spark.gluten.sql.columnar.backend.ch.runtime_config.storage_configuration.disks.hdfs2.endpoint",
+          HDFS_URL_ENDPOINT + "/")
+        .set(
+          "spark.gluten.sql.columnar.backend.ch.runtime_config.storage_configuration.disks.hdfs2.metadata_path",
+          HDFS_METADATA_PATH)
+        .set(
+          "spark.gluten.sql.columnar.backend.ch.runtime_config.storage_configuration.disks.hdfs2.metadata_type",
+          "rocksdb")
+        .set(
+          "spark.gluten.sql.columnar.backend.ch.runtime_config.storage_configuration.disks.hdfs_cache2.type",
+          "cache")
+        .set(
+          "spark.gluten.sql.columnar.backend.ch.runtime_config.storage_configuration.disks.hdfs_cache2.disk",
+          "hdfs2")
+        .set(
+          "spark.gluten.sql.columnar.backend.ch.runtime_config.storage_configuration.disks.hdfs_cache2.path",
+          HDFS_CACHE_PATH)
+        .set(
+          "spark.gluten.sql.columnar.backend.ch.runtime_config.storage_configuration.disks.hdfs_cache2.max_size",
+          "10Gi")
+        .set(
+          "spark.gluten.sql.columnar.backend.ch.runtime_config.storage_configuration.policies.__hdfs_main_rocksdb.volumes",
+          "main")
+        .set(
+          "spark.gluten.sql.columnar.backend.ch.runtime_config.storage_configuration.policies.__hdfs_main_rocksdb.volumes.main.disk",
+          "hdfs_cache2")
+        .set(
           "spark.gluten.sql.columnar.backend.ch.runtime_config.hdfs.dfs_client_read_shortcircuit",
           "false")
         .set("spark.gluten.sql.columnar.backend.ch.runtime_config.hdfs.dfs_default_replica", "1")

cpp-ch/CMakeLists.txt
@@ -105,16 +105,15 @@ else()
       -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE} -DENABLE_PROTOBUF=ON
       -DENABLE_TESTS=OFF -DENABLE_JEMALLOC=ON -DENABLE_MULTITARGET_CODE=ON
       -DENABLE_EXTERN_LOCAL_ENGINE=ON -DENABLE_ODBC=OFF -DENABLE_CAPNP=OFF
-      -DENABLE_ROCKSDB=OFF -DENABLE_GRPC=OFF -DENABLE_RUST=OFF -DENABLE_H3=OFF
-      -DENABLE_AMQPCPP=OFF -DENABLE_CASSANDRA=OFF -DENABLE_KAFKA=OFF
-      -DENABLE_NATS=OFF -DENABLE_LIBPQXX=OFF -DENABLE_NURAFT=OFF
-      -DENABLE_DATASKETCHES=OFF -DENABLE_SQLITE=OFF -DENABLE_S2_GEOMETRY=OFF
-      -DENABLE_ANNOY=OFF -DENABLE_ULID=OFF -DENABLE_MYSQL=OFF
-      -DENABLE_BCRYPT=OFF -DENABLE_LDAP=OFF -DENABLE_MSGPACK=OFF
-      -DUSE_REPLXX=OFF -DENABLE_CLICKHOUSE_ALL=OFF -DENABLE_GWP_ASAN=OFF
-      -DCOMPILER_FLAGS='-fvisibility=hidden -fvisibility-inlines-hidden' -S
-      ${CH_SOURCE_DIR} -G Ninja -B ${CH_BINARY_DIR} && cmake --build
-      ${CH_BINARY_DIR} --target libch\"
+      -DENABLE_GRPC=OFF -DENABLE_RUST=OFF -DENABLE_H3=OFF -DENABLE_AMQPCPP=OFF
+      -DENABLE_CASSANDRA=OFF -DENABLE_KAFKA=OFF -DENABLE_NATS=OFF
+      -DENABLE_LIBPQXX=OFF -DENABLE_NURAFT=OFF -DENABLE_DATASKETCHES=OFF
+      -DENABLE_SQLITE=OFF -DENABLE_S2_GEOMETRY=OFF -DENABLE_ANNOY=OFF
+      -DENABLE_ULID=OFF -DENABLE_MYSQL=OFF -DENABLE_BCRYPT=OFF -DENABLE_LDAP=OFF
+      -DENABLE_MSGPACK=OFF -DUSE_REPLXX=OFF -DENABLE_CLICKHOUSE_ALL=OFF
+      -DENABLE_GWP_ASAN=OFF -DCOMPILER_FLAGS='-fvisibility=hidden
+      -fvisibility-inlines-hidden' -S ${CH_SOURCE_DIR} -G Ninja -B
+      ${CH_BINARY_DIR} && cmake --build ${CH_BINARY_DIR} --target libch\"
     OUTPUT _build_ch)
 endif()
 

cpp-ch/local-engine/Disks/ObjectStorages/CompactObjectStorageDiskTransaction.cpp
@@ -22,36 +22,11 @@
 
 namespace local_engine
 {
-int getFileOrder(const std::string & path)
-{
-    if (path.ends_with("columns.txt"))
-        return 1;
-    if (path.ends_with("metadata_version.txt"))
-        return 2;
-    if (path.ends_with("count.txt"))
-        return 3;
-    if (path.ends_with("default_compression_codec.txt"))
-        return 4;
-    if (path.ends_with("checksums.txt"))
-        return 5;
-    if (path.ends_with("uuid.txt"))
-        return 6;
-    if (path.ends_with(".cmrk3") || path.ends_with(".cmrk2") || path.ends_with(".cmrk1") ||
-        path.ends_with(".mrk3") || path.ends_with(".mrk2") || path.ends_with(".mrk1"))
-        return 10;
-    if (path.ends_with("idx"))
-        return 20;
-    if (path.ends_with("bin"))
-        return 1000;
-    return 100;
-}
 
 bool isMetaDataFile(const std::string & path)
 {
     return !path.ends_with("bin");
 }
-
-using FileMappings = std::vector<std::pair<String, std::shared_ptr<DB::TemporaryFileOnDisk>>>;
 
 void CompactObjectStorageDiskTransaction::commit()
 {

cpp-ch/local-engine/Disks/ObjectStorages/MetadataStorageFromRocksDB.cpp
@@ -1,0 +1,316 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include "MetadataStorageFromRocksDB.h"
+#if USE_ROCKSDB
+#include <Disks/ObjectStorages/DiskObjectStorageMetadata.h>
+#include <Disks/ObjectStorages/MetadataStorageFromRocksDBTransactionOperations.h>
+#include <Disks/ObjectStorages/StaticDirectoryIterator.h>
+#include <Interpreters/Context.h>
+#include <Storages/MergeTree/MetaDataHelper.h>
+#include <rocksdb/db.h>
+#include <Common/QueryContext.h>
+
+namespace local_engine
+{
+static std::string getObjectKeyCompatiblePrefix(
+    const DB::IObjectStorage & object_storage, const Poco::Util::AbstractConfiguration & config, const String & config_prefix)
+{
+    return config.getString(config_prefix + ".key_compatibility_prefix", object_storage.getCommonKeyPrefix());
+}
+
+DB::MetadataStoragePtr MetadataStorageFromRocksDB::create(
+    const std::string &,
+    const Poco::Util::AbstractConfiguration & config,
+    const std::string & config_prefix,
+    DB::ObjectStoragePtr object_storage)
+{
+    auto metadata_path = config.getString(config_prefix + ".metadata_path");
+    size_t clean_meta_task_interval_seconds = config.getUInt(config_prefix + ".clean_meta_task_interval_seconds", 60 * 60 * 12);
+    fs::create_directories(metadata_path);
+    auto key_compatibility_prefix = getObjectKeyCompatiblePrefix(*object_storage, config, config_prefix);
+    return std::make_shared<MetadataStorageFromRocksDB>(
+        key_compatibility_prefix, metadata_path, object_storage, clean_meta_task_interval_seconds);
+}
+
+MetadataStorageFromRocksDB::MetadataStorageFromRocksDB(
+    const String & compatible_key_prefix,
+    const String & rocksdb_dir,
+    DB::ObjectStoragePtr & object_storage_,
+    size_t metadata_clean_task_interval_seconds_)
+    : compatible_key_prefix(compatible_key_prefix)
+    , rocksdb_dir(rocksdb_dir)
+    , object_storage(object_storage_)
+    , metadata_clean_task_interval_seconds(metadata_clean_task_interval_seconds_)
+{
+    rocksdb::Options options;
+    options.create_if_missing = true;
+    throwRockDBErrorNotOk(rocksdb::DB::Open(options, rocksdb_dir, &rocksdb));
+    metadata_clean_task = QueryContext::globalContext()->getSchedulePool().createTask(
+        "MetadataStorageFromRocksDB", [this] { cleanOutdatedMetadataThreadFunc(); });
+    metadata_clean_task->scheduleAfter(metadata_clean_task_interval_seconds * 1000);
+    logger = getLogger("MetadataStorageFromRocksDB");
+}
+
+DB::MetadataTransactionPtr MetadataStorageFromRocksDB::createTransaction()
+{
+    return std::make_shared<MetadataStorageFromRocksDBTransaction>(*this);
+}
+
+const std::string & MetadataStorageFromRocksDB::getPath() const
+{
+    return rocksdb_dir;
+}
+
+DB::MetadataStorageType MetadataStorageFromRocksDB::getType() const
+{
+    return DB::MetadataStorageType::None;
+}
+
+bool MetadataStorageFromRocksDB::exists(const std::string & path) const
+{
+    return exist(getRocksDB(), path);
+}
+
+bool MetadataStorageFromRocksDB::isFile(const std::string & path) const
+{
+    auto data = getData(getRocksDB(), path);
+    return data != RocksDBCreateDirectoryOperation::DIR_DATA;
+}
+
+bool MetadataStorageFromRocksDB::isDirectory(const std::string & path) const
+{
+    auto data = getData(getRocksDB(), path);
+    return data == RocksDBCreateDirectoryOperation::DIR_DATA;
+}
+
+uint64_t MetadataStorageFromRocksDB::getFileSize(const std::string & path) const
+{
+    return readMetadata(path)->getTotalSizeBytes();
+}
+
+Poco::Timestamp MetadataStorageFromRocksDB::getLastModified(const std::string & /*path*/) const
+{
+    return {};
+}
+
+bool MetadataStorageFromRocksDB::supportsChmod() const
+{
+    return false;
+}
+
+bool MetadataStorageFromRocksDB::supportsStat() const
+{
+    return false;
+}
+
+std::vector<std::string> MetadataStorageFromRocksDB::listDirectory(const std::string & path) const
+{
+    return listKeys(getRocksDB(), path);
+}
+
+DB::DirectoryIteratorPtr MetadataStorageFromRocksDB::iterateDirectory(const std::string & path) const
+{
+    auto files = listKeys(getRocksDB(), path);
+    std::vector<std::filesystem::path> paths;
+    paths.reserve(files.size());
+    for (const auto & file : files)
+        paths.emplace_back(file);
+    return std::make_unique<DB::StaticDirectoryIterator>(std::move(paths));
+}
+
+uint32_t MetadataStorageFromRocksDB::getHardlinkCount(const std::string & /*path*/) const
+{
+    return 0;
+}
+
+DB::StoredObjects MetadataStorageFromRocksDB::getStorageObjects(const std::string & path) const
+{
+    auto metadata = readMetadata(path);
+    const auto & keys_with_meta = metadata->getKeysWithMeta();
+
+    DB::StoredObjects objects;
+    objects.reserve(keys_with_meta.size());
+    for (const auto & [object_key, object_meta] : keys_with_meta)
+        objects.emplace_back(object_key.serialize(), path, object_meta.size_bytes, object_meta.offset);
+
+    return objects;
+}
+
+DB::DiskObjectStorageMetadataPtr MetadataStorageFromRocksDB::readMetadata(const std::string & path) const
+{
+    std::shared_lock lock(metadata_mutex);
+    return readMetadataUnlocked(path, lock);
+}
+
+DB::DiskObjectStorageMetadataPtr
+MetadataStorageFromRocksDB::readMetadataUnlocked(const std::string & path, std::unique_lock<DB::SharedMutex> &) const
+{
+    auto metadata = std::make_unique<DB::DiskObjectStorageMetadata>(compatible_key_prefix, path);
+    auto str = getData(getRocksDB(), path);
+    metadata->deserializeFromString(str);
+    return metadata;
+}
+
+DB::DiskObjectStorageMetadataPtr
+MetadataStorageFromRocksDB::readMetadataUnlocked(const std::string & path, std::shared_lock<DB::SharedMutex> &) const
+{
+    auto metadata = std::make_unique<DB::DiskObjectStorageMetadata>(compatible_key_prefix, path);
+    auto str = getData(getRocksDB(), path);
+    metadata->deserializeFromString(str);
+    return metadata;
+}
+
+std::string MetadataStorageFromRocksDB::readFileToString(const std::string & path) const
+{
+    return getData(getRocksDB(), path);
+}
+
+void MetadataStorageFromRocksDB::shutdown()
+{
+    metadata_clean_task->deactivate();
+    if (rocksdb)
+    {
+        rocksdb->Close();
+        rocksdb = nullptr;
+    }
+}
+
+void MetadataStorageFromRocksDB::cleanOutdatedMetadataThreadFunc()
+{
+    LOG_INFO(logger, "start to clean disk metadata in rocksdb.");
+    std::queue<String> part_queue;
+    size_t total_count_remove = 0;
+    auto removeParts = [&]
+    {
+        while (!part_queue.empty())
+        {
+            auto meta_name = part_queue.front();
+            part_queue.pop();
+            std::filesystem::path meta_path(meta_name);
+            auto part_path = meta_path.parent_path();
+            auto files = listDirectory(part_path);
+            total_count_remove += (files.size() + 1);
+            getRocksDB().DeleteRange({}, part_path.generic_string(), files.back());
+            getRocksDB().Delete({}, files.back());
+        }
+    };
+    auto * it = getRocksDB().NewIterator({});
+    String prev_key;
+    String prev_data;
+    for (it->SeekToFirst(); it->Valid(); it->Next())
+    {
+        auto file_name = it->key().ToString();
+        // mark outdated part
+        if (isMergeTreePartMetaDataFile(file_name))
+        {
+            auto objects = getStorageObjects(it->key().ToString());
+            if (!object_storage->exists(objects.front()))
+                part_queue.push(file_name);
+        }
+        // clean empty directory
+        if (!prev_key.empty() && !file_name.starts_with(prev_key) && prev_data == RocksDBCreateDirectoryOperation::DIR_DATA)
+        {
+            getRocksDB().Delete({}, prev_key);
+            total_count_remove ++;
+        }
+        if (part_queue.size() > 10000)
+        {
+            removeParts();
+        }
+    }
+    removeParts();
+    rocksdb::Slice begin(nullptr, 0);
+    rocksdb::Slice end(nullptr, 0);
+    rocksdb::Status s = getRocksDB().CompactRange({}, &begin, &end);
+    LOG_INFO(logger, "Clean meta finish, totally clean {} meta", total_count_remove);
+    metadata_clean_task->scheduleAfter(metadata_clean_task_interval_seconds * 1000);
+}
+
+DB::SharedMutex & MetadataStorageFromRocksDB::getMetadataMutex() const
+{
+    return metadata_mutex;
+}
+
+rocksdb::DB & MetadataStorageFromRocksDB::getRocksDB() const
+{
+    if (!rocksdb)
+        throw DB::Exception(DB::ErrorCodes::LOGICAL_ERROR, "RocksDB is not initialized");
+    return *rocksdb;
+}
+
+void MetadataStorageFromRocksDBTransaction::commit()
+{
+    commitImpl(metadata_storage.getMetadataMutex());
+}
+
+const DB::IMetadataStorage & MetadataStorageFromRocksDBTransaction::getStorageForNonTransactionalReads() const
+{
+    return metadata_storage;
+}
+
+bool MetadataStorageFromRocksDBTransaction::supportsChmod() const
+{
+    return false;
+}
+
+void MetadataStorageFromRocksDBTransaction::createEmptyMetadataFile(const std::string & path)
+{
+    auto metadata = std::make_unique<DB::DiskObjectStorageMetadata>(metadata_storage.compatible_key_prefix, path);
+    writeStringToFile(path, metadata->serializeToString());
+}
+
+void MetadataStorageFromRocksDBTransaction::createMetadataFile(const std::string & path, DB::ObjectStorageKey key, uint64_t size_in_bytes)
+{
+    auto metadata = std::make_unique<DB::DiskObjectStorageMetadata>(metadata_storage.compatible_key_prefix, path);
+    metadata->addObject(std::move(key), size_in_bytes);
+
+    auto data = metadata->serializeToString();
+    if (!data.empty())
+        addOperation(std::make_unique<RocksDBWriteFileOperation>(path, metadata_storage.getRocksDB(), data));
+}
+
+void MetadataStorageFromRocksDBTransaction::writeStringToFile(const std::string & path, const std::string & data)
+{
+    addOperation(std::make_unique<RocksDBWriteFileOperation>(path, metadata_storage.getRocksDB(), data));
+}
+
+void MetadataStorageFromRocksDBTransaction::createDirectory(const std::string & path)
+{
+    addOperation(std::make_unique<RocksDBCreateDirectoryOperation>(path, metadata_storage.getRocksDB()));
+}
+
+void MetadataStorageFromRocksDBTransaction::createDirectoryRecursive(const std::string & path)
+{
+    addOperation(std::make_unique<RocksDBCreateDirectoryRecursiveOperation>(path, metadata_storage.getRocksDB()));
+}
+
+void MetadataStorageFromRocksDBTransaction::removeDirectory(const std::string & path)
+{
+    addOperation(std::make_unique<RocksDBRemoveDirectoryOperation>(path, metadata_storage.getRocksDB()));
+}
+
+void MetadataStorageFromRocksDBTransaction::removeRecursive(const std::string & path)
+{
+    addOperation(std::make_unique<RocksDBRemoveRecursiveOperation>(path, metadata_storage.getRocksDB()));
+}
+
+void MetadataStorageFromRocksDBTransaction::unlinkFile(const std::string & path)
+{
+    addOperation(std::make_unique<RocksDBUnlinkFileOperation>(path, metadata_storage.getRocksDB()));
+}
+}
+#endif

cpp-ch/local-engine/Disks/ObjectStorages/MetadataStorageFromRocksDB.h
@@ -1,0 +1,97 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#pragma once
+#include <config.h>
+#if USE_ROCKSDB
+#include <Disks/DiskLocal.h>
+#include <Disks/ObjectStorages/DiskObjectStorageMetadata.h>
+#include <Disks/ObjectStorages/IMetadataStorage.h>
+#include <Disks/ObjectStorages/MetadataOperationsHolder.h>
+#include <rocksdb/db.h>
+
+namespace local_engine
+{
+class MetadataStorageFromRocksDB final : public DB::IMetadataStorage
+{
+    friend class MetadataStorageFromRocksDBTransaction;
+
+public:
+    static DB::MetadataStoragePtr create(
+        const std::string & name,
+        const Poco::Util::AbstractConfiguration & config,
+        const std::string & config_prefix,
+        DB::ObjectStoragePtr object_storage);
+    MetadataStorageFromRocksDB(const String & compatible_key_prefix, const String & rocksdb_dir, DB::ObjectStoragePtr & object_storage, size_t metadata_clean_task_interval_seconds);
+    DB::MetadataTransactionPtr createTransaction() override;
+    const std::string & getPath() const override;
+    DB::MetadataStorageType getType() const override;
+    bool exists(const std::string & path) const override;
+    bool isFile(const std::string & path) const override;
+    bool isDirectory(const std::string & path) const override;
+    uint64_t getFileSize(const std::string & path) const override;
+    Poco::Timestamp getLastModified(const std::string & path) const override;
+    bool supportsChmod() const override;
+    bool supportsStat() const override;
+    std::vector<std::string> listDirectory(const std::string & path) const override;
+    DB::DirectoryIteratorPtr iterateDirectory(const std::string & path) const override;
+    uint32_t getHardlinkCount(const std::string & path) const override;
+    DB::StoredObjects getStorageObjects(const std::string & path) const override;
+    DB::DiskObjectStorageMetadataPtr readMetadata(const std::string & path) const;
+    DB::DiskObjectStorageMetadataPtr readMetadataUnlocked(const std::string & path, std::unique_lock<DB::SharedMutex> & lock) const;
+    DB::DiskObjectStorageMetadataPtr readMetadataUnlocked(const std::string & path, std::shared_lock<DB::SharedMutex> & lock) const;
+    std::string readFileToString(const std::string & path) const override;
+    void shutdown() override;
+    void cleanOutdatedMetadataThreadFunc();
+
+private:
+    DB::SharedMutex & getMetadataMutex() const;
+    rocksdb::DB & getRocksDB() const;
+
+    using RocksDBPtr = rocksdb::DB *;
+    RocksDBPtr rocksdb = nullptr;
+    mutable DB::SharedMutex metadata_mutex;
+    String compatible_key_prefix;
+    String rocksdb_dir;
+    DB::ObjectStoragePtr object_storage;
+    size_t metadata_clean_task_interval_seconds;
+    DB::BackgroundSchedulePool::TaskHolder metadata_clean_task;
+    LoggerPtr logger;
+};
+
+class MetadataStorageFromRocksDBTransaction final : public DB::IMetadataTransaction, private DB::MetadataOperationsHolder
+{
+public:
+    MetadataStorageFromRocksDBTransaction(const MetadataStorageFromRocksDB & metadata_storage_) : metadata_storage(metadata_storage_) { }
+
+    void commit() override;
+    const DB::IMetadataStorage & getStorageForNonTransactionalReads() const override;
+    bool supportsChmod() const override;
+    void createEmptyMetadataFile(const std::string & path) override;
+    void createMetadataFile(const std::string & path, DB::ObjectStorageKey key, uint64_t size_in_bytes) override;
+
+    void writeStringToFile(const std::string &, const std::string &) override;
+    void createDirectory(const std::string &) override;
+    void createDirectoryRecursive(const std::string &) override;
+    void removeDirectory(const std::string &) override;
+    void removeRecursive(const std::string &) override;
+    void unlinkFile(const std::string &) override;
+
+private:
+    const MetadataStorageFromRocksDB & metadata_storage;
+};
+}
+#endif

commit 3da190e7fd6fa899f5b22513af314771f9be2969

Fix build due to ENABLE_ROCKSDB=OFF caused by https://github.com/apache/incubator-gluten/pull/7239

cpp-ch/local-engine/Disks/registerGlutenDisks.cpp
@@ -71,7 +71,7 @@ void registerGlutenDisks(bool global_skip_access_check)
 #if USE_ROCKSDB
             metadata_storage = MetadataStorageFromRocksDB::create(name, config, config_prefix, object_storage);
 #else
-            throw Exception(ErrorCodes::NOT_IMPLEMENTED, "RocksDB metadata storage is not enabled in the build");
+            throw DB::Exception(DB::ErrorCodes::NOT_IMPLEMENTED, "RocksDB metadata storage is not enabled in the build");
 #endif
         }
         else
@@ -118,7 +118,7 @@ void registerGlutenDisks(bool global_skip_access_check)
 #if USE_ROCKSDB
             metadata_storage = MetadataStorageFromRocksDB::create(name, config, config_prefix, object_storage);
 #else
-            throw Exception(ErrorCodes::NOT_IMPLEMENTED, "RocksDB metadata storage is not enabled in the build");
+            throw DB::Exception(DB::ErrorCodes::NOT_IMPLEMENTED, "RocksDB metadata storage is not enabled in the build");
 #endif
         }
         else