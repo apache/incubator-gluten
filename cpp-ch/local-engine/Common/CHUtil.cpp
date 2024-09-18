apache/incubator-gluten

commit a7c9567bc7acdd14c8474c3d2dc99d1cfd062b56

[CH] Support rocksdb disk metadata (#7239)
What changes were proposed in this pull request?
Added rocksdb-based disk metadata management to solve the problem of too many small mergetree disk metadata files. It also supports automatic cleanup of expired data.

How was this patch tested?
unit tests

cpp-ch/local-engine/Disks/registerGlutenDisks.cpp
@@ -29,6 +29,10 @@
 #include <Disks/ObjectStorages/GlutenDiskS3.h>
 #endif
 
+#if USE_ROCKSDB
+#include <Disks/ObjectStorages/MetadataStorageFromRocksDB.h>
+#endif
+
 #include "registerGlutenDisks.h"
 
 namespace local_engine
@@ -60,7 +64,18 @@ void registerGlutenDisks(bool global_skip_access_check)
                                           const Poco::Util::AbstractConfiguration & conf, DB::ContextPtr ctx) -> DB::ObjectStoragePtr
         { return DB::ObjectStorageFactory::instance().create(name, conf, config_prefix, ctx, skip_access_check); };
         auto object_storage = DB::ObjectStorageFactory::instance().create(name, config, config_prefix, context, skip_access_check);
-        auto metadata_storage = DB::MetadataStorageFactory::instance().create(name, config, config_prefix, object_storage, "local");
+        DB::MetadataStoragePtr metadata_storage;
+        auto metadata_type = DB::MetadataStorageFactory::getMetadataType(config, config_prefix, "local");
+        if (metadata_type == "rocksdb")
+        {
+#if USE_ROCKSDB
+            metadata_storage = MetadataStorageFromRocksDB::create(name, config, config_prefix, object_storage);
+#else
+            throw Exception(ErrorCodes::NOT_IMPLEMENTED, "RocksDB metadata storage is not enabled in the build");
+#endif
+        }
+        else
+            metadata_storage = DB::MetadataStorageFactory::instance().create(name, config, config_prefix, object_storage, "local");
 
         DB::DiskObjectStoragePtr disk = std::make_shared<local_engine::GlutenDiskS3>(
             name,
@@ -96,7 +111,18 @@ void registerGlutenDisks(bool global_skip_access_check)
                                           const Poco::Util::AbstractConfiguration & conf, DB::ContextPtr ctx) -> DB::ObjectStoragePtr
         { return DB::ObjectStorageFactory::instance().create(name, conf, config_prefix, ctx, skip_access_check); };
         auto object_storage = object_storage_creator(config, context);
-        auto metadata_storage = DB::MetadataStorageFactory::instance().create(name, config, config_prefix, object_storage, "local");
+        DB::MetadataStoragePtr metadata_storage;
+        auto metadata_type = DB::MetadataStorageFactory::getMetadataType(config, config_prefix,  "local");
+        if (metadata_type == "rocksdb")
+        {
+#if USE_ROCKSDB
+            metadata_storage = MetadataStorageFromRocksDB::create(name, config, config_prefix, object_storage);
+#else
+            throw Exception(ErrorCodes::NOT_IMPLEMENTED, "RocksDB metadata storage is not enabled in the build");
+#endif
+        }
+        else
+            metadata_storage = DB::MetadataStorageFactory::instance().create(name, config, config_prefix, object_storage, "local");
 
         DB::DiskObjectStoragePtr disk = std::make_shared<local_engine::GlutenDiskHDFS>(
             name,

backends-clickhouse/src/test/scala/org/apache/gluten/execution/GlutenClickHouseWholeStageTransformerSuite.scala
@@ -153,6 +153,36 @@ class GlutenClickHouseWholeStageTransformerSuite extends WholeStageTransformerSu
           "spark.gluten.sql.columnar.backend.ch.runtime_config.storage_configuration.policies.__hdfs_main.volumes.main.disk",
           "hdfs_cache")
         .set(
+          "spark.gluten.sql.columnar.backend.ch.runtime_config.storage_configuration.disks.hdfs2.type",
+          "hdfs_gluten")
+        .set(
+          "spark.gluten.sql.columnar.backend.ch.runtime_config.storage_configuration.disks.hdfs2.endpoint",
+          HDFS_URL_ENDPOINT + "/")
+        .set(
+          "spark.gluten.sql.columnar.backend.ch.runtime_config.storage_configuration.disks.hdfs2.metadata_path",
+          HDFS_METADATA_PATH)
+        .set(
+          "spark.gluten.sql.columnar.backend.ch.runtime_config.storage_configuration.disks.hdfs2.metadata_type",
+          "rocksdb")
+        .set(
+          "spark.gluten.sql.columnar.backend.ch.runtime_config.storage_configuration.disks.hdfs_cache2.type",
+          "cache")
+        .set(
+          "spark.gluten.sql.columnar.backend.ch.runtime_config.storage_configuration.disks.hdfs_cache2.disk",
+          "hdfs2")
+        .set(
+          "spark.gluten.sql.columnar.backend.ch.runtime_config.storage_configuration.disks.hdfs_cache2.path",
+          HDFS_CACHE_PATH)
+        .set(
+          "spark.gluten.sql.columnar.backend.ch.runtime_config.storage_configuration.disks.hdfs_cache2.max_size",
+          "10Gi")
+        .set(
+          "spark.gluten.sql.columnar.backend.ch.runtime_config.storage_configuration.policies.__hdfs_main_rocksdb.volumes",
+          "main")
+        .set(
+          "spark.gluten.sql.columnar.backend.ch.runtime_config.storage_configuration.policies.__hdfs_main_rocksdb.volumes.main.disk",
+          "hdfs_cache2")
+        .set(
           "spark.gluten.sql.columnar.backend.ch.runtime_config.hdfs.dfs_client_read_shortcircuit",
           "false")
         .set("spark.gluten.sql.columnar.backend.ch.runtime_config.hdfs.dfs_default_replica", "1")

cpp-ch/CMakeLists.txt
@@ -105,16 +105,15 @@ else()
       -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE} -DENABLE_PROTOBUF=ON
       -DENABLE_TESTS=OFF -DENABLE_JEMALLOC=ON -DENABLE_MULTITARGET_CODE=ON
       -DENABLE_EXTERN_LOCAL_ENGINE=ON -DENABLE_ODBC=OFF -DENABLE_CAPNP=OFF
-      -DENABLE_ROCKSDB=OFF -DENABLE_GRPC=OFF -DENABLE_RUST=OFF -DENABLE_H3=OFF
-      -DENABLE_AMQPCPP=OFF -DENABLE_CASSANDRA=OFF -DENABLE_KAFKA=OFF
-      -DENABLE_NATS=OFF -DENABLE_LIBPQXX=OFF -DENABLE_NURAFT=OFF
-      -DENABLE_DATASKETCHES=OFF -DENABLE_SQLITE=OFF -DENABLE_S2_GEOMETRY=OFF
-      -DENABLE_ANNOY=OFF -DENABLE_ULID=OFF -DENABLE_MYSQL=OFF
-      -DENABLE_BCRYPT=OFF -DENABLE_LDAP=OFF -DENABLE_MSGPACK=OFF
-      -DUSE_REPLXX=OFF -DENABLE_CLICKHOUSE_ALL=OFF -DENABLE_GWP_ASAN=OFF
-      -DCOMPILER_FLAGS='-fvisibility=hidden -fvisibility-inlines-hidden' -S
-      ${CH_SOURCE_DIR} -G Ninja -B ${CH_BINARY_DIR} && cmake --build
-      ${CH_BINARY_DIR} --target libch\"
+      -DENABLE_GRPC=OFF -DENABLE_RUST=OFF -DENABLE_H3=OFF -DENABLE_AMQPCPP=OFF
+      -DENABLE_CASSANDRA=OFF -DENABLE_KAFKA=OFF -DENABLE_NATS=OFF
+      -DENABLE_LIBPQXX=OFF -DENABLE_NURAFT=OFF -DENABLE_DATASKETCHES=OFF
+      -DENABLE_SQLITE=OFF -DENABLE_S2_GEOMETRY=OFF -DENABLE_ANNOY=OFF
+      -DENABLE_ULID=OFF -DENABLE_MYSQL=OFF -DENABLE_BCRYPT=OFF -DENABLE_LDAP=OFF
+      -DENABLE_MSGPACK=OFF -DUSE_REPLXX=OFF -DENABLE_CLICKHOUSE_ALL=OFF
+      -DENABLE_GWP_ASAN=OFF -DCOMPILER_FLAGS='-fvisibility=hidden
+      -fvisibility-inlines-hidden' -S ${CH_SOURCE_DIR} -G Ninja -B
+      ${CH_BINARY_DIR} && cmake --build ${CH_BINARY_DIR} --target libch\"
     OUTPUT _build_ch)
 endif()
 

cpp-ch/local-engine/Disks/ObjectStorages/CompactObjectStorageDiskTransaction.cpp
@@ -22,36 +22,11 @@
 
 namespace local_engine
 {
-int getFileOrder(const std::string & path)
-{
-    if (path.ends_with("columns.txt"))
-        return 1;
-    if (path.ends_with("metadata_version.txt"))
-        return 2;
-    if (path.ends_with("count.txt"))
-        return 3;
-    if (path.ends_with("default_compression_codec.txt"))
-        return 4;
-    if (path.ends_with("checksums.txt"))
-        return 5;
-    if (path.ends_with("uuid.txt"))
-        return 6;
-    if (path.ends_with(".cmrk3") || path.ends_with(".cmrk2") || path.ends_with(".cmrk1") ||
-        path.ends_with(".mrk3") || path.ends_with(".mrk2") || path.ends_with(".mrk1"))
-        return 10;
-    if (path.ends_with("idx"))
-        return 20;
-    if (path.ends_with("bin"))
-        return 1000;
-    return 100;
-}
 
 bool isMetaDataFile(const std::string & path)
 {
     return !path.ends_with("bin");
 }
-
-using FileMappings = std::vector<std::pair<String, std::shared_ptr<DB::TemporaryFileOnDisk>>>;
 
 void CompactObjectStorageDiskTransaction::commit()
 {

commit f1f6c2cf75d8a6c95f70e7c675bf7bdb94480d1c

[GLUTEN-1632][CH]Daily Update Clickhouse Version (20240918) (#7260)
* [GLUTEN-1632][CH]Daily Update Clickhouse Version (20240918)

* Fix build due to ENABLE_ROCKSDB=OFF caused by https://github.com/apache/incubator-gluten/pull/7239
* Fix UT build due to https://github.com/apache/incubator-gluten/pull/6723
* Fix UT build due to https://github.com/apache/incubator-gluten/pull/7193
* Fix Build due to https://github.com/ClickHouse/ClickHouse/pull/69298

---------

Co-authored-by: kyligence-git <gluten@kyligence.io>
Co-authored-by: Chang Chen <baibaichen@gmail.com>

cpp-ch/clickhouse.version
@@ -1,3 +1,3 @@
 CH_ORG=Kyligence
-CH_BRANCH=rebase_ch/20240906
-CH_COMMIT=4513a954b95
+CH_BRANCH=rebase_ch/20240918
+CH_COMMIT=cc6de0f1995

cpp-ch/local-engine/Common/CHUtil.cpp
@@ -21,7 +21,6 @@
 #include <memory>
 #include <optional>
 #include <unistd.h>
-
 #include <AggregateFunctions/Combinators/AggregateFunctionCombinatorFactory.h>
 #include <AggregateFunctions/registerAggregateFunctions.h>
 #include <Columns/ColumnArray.h>
@@ -53,7 +52,6 @@
 #include <Parser/SubstraitParserUtils.h>
 #include <Planner/PlannerActionsVisitor.h>
 #include <Processors/Chunk.h>
-#include <Processors/Formats/IOutputFormat.h>
 #include <Processors/QueryPlan/ExpressionStep.h>
 #include <Processors/QueryPlan/QueryPlan.h>
 #include <QueryPipeline/QueryPipelineBuilder.h>

cpp-ch/local-engine/Disks/registerGlutenDisks.cpp
@@ -71,7 +71,7 @@ void registerGlutenDisks(bool global_skip_access_check)
 #if USE_ROCKSDB
             metadata_storage = MetadataStorageFromRocksDB::create(name, config, config_prefix, object_storage);
 #else
-            throw Exception(ErrorCodes::NOT_IMPLEMENTED, "RocksDB metadata storage is not enabled in the build");
+            throw DB::Exception(DB::ErrorCodes::NOT_IMPLEMENTED, "RocksDB metadata storage is not enabled in the build");
 #endif
         }
         else
@@ -118,7 +118,7 @@ void registerGlutenDisks(bool global_skip_access_check)
 #if USE_ROCKSDB
             metadata_storage = MetadataStorageFromRocksDB::create(name, config, config_prefix, object_storage);
 #else
-            throw Exception(ErrorCodes::NOT_IMPLEMENTED, "RocksDB metadata storage is not enabled in the build");
+            throw DB::Exception(DB::ErrorCodes::NOT_IMPLEMENTED, "RocksDB metadata storage is not enabled in the build");
 #endif
         }
         else

cpp-ch/local-engine/Parser/LocalExecutor.cpp
@@ -18,7 +18,7 @@
 #include "LocalExecutor.h"
 
 #include <Core/Settings.h>
-#include <Parser/SerializedPlanParser.h>
+#include <Interpreters/Context.h>
 #include <Processors/Executors/PipelineExecutor.h>
 #include <QueryPipeline/printPipeline.h>
 #include <Common/QueryContext.h>

cpp-ch/local-engine/Parser/RelParsers/CrossRelParser.cpp
@@ -24,7 +24,6 @@
 #include <Join/BroadCastJoinBuilder.h>
 #include <Join/StorageJoinFromReadBuffer.h>
 #include <Parser/AdvancedParametersParseUtil.h>
-#include <Parser/SerializedPlanParser.h>
 #include <Parsers/ASTIdentifier.h>
 #include <Processors/QueryPlan/ExpressionStep.h>
 #include <Processors/QueryPlan/FilterStep.h>

cpp-ch/local-engine/Parser/RelParsers/ExpandRelParser.cpp
@@ -21,7 +21,6 @@
 #include <Operator/ExpandStep.h>
 #include <Parser/ExpandField.h>
 #include <Parser/RelParsers/RelParser.h>
-#include <Parser/SerializedPlanParser.h>
 #include <Processors/QueryPlan/QueryPlan.h>
 #include <Common/logger_useful.h>
 

cpp-ch/local-engine/Parser/RelParsers/FetchRelParser.cpp
@@ -17,9 +17,9 @@
 
 #include <memory>
 #include <optional>
-#include <Parser/SerializedPlanParser.h>
+#include <Parser/RelParsers/RelParser.h>
 #include <Processors/QueryPlan/LimitStep.h>
-#include "RelParser.h"
+
 namespace local_engine
 {
 class FetchRelParser : public RelParser

cpp-ch/local-engine/Parser/RelParsers/JoinRelParser.cpp
@@ -30,7 +30,6 @@
 #include <Join/StorageJoinFromReadBuffer.h>
 #include <Operator/EarlyStopStep.h>
 #include <Parser/AdvancedParametersParseUtil.h>
-#include <Parser/SerializedPlanParser.h>
 #include <Parsers/ASTIdentifier.h>
 #include <Processors/QueryPlan/ExpressionStep.h>
 #include <Processors/QueryPlan/FilterStep.h>

cpp-ch/local-engine/Parser/RelParsers/ProjectRelParser.cpp
@@ -15,7 +15,8 @@
  * limitations under the License.
  */
 #include "ProjectRelParser.h"
-#include <Interpreters/ArrayJoinAction.h>
+
+#include <Interpreters/ArrayJoin.h>
 #include <Operator/EmptyProjectStep.h>
 #include <Processors/QueryPlan/ArrayJoinStep.h>
 #include <Processors/QueryPlan/ExpressionStep.h>
@@ -198,9 +199,12 @@ ProjectRelParser::parseGenerate(DB::QueryPlanPtr query_plan, const substrait::Re
         }
 
         /// ARRAY JOIN
-        NameSet array_joined_columns{findArrayJoinNode(splitted_actions_dags.array_join)->result_name};
-        auto array_join_action = std::make_shared<ArrayJoinAction>(array_joined_columns, generate_rel.outer(), getContext());
-        auto array_join_step = std::make_unique<ArrayJoinStep>(query_plan->getCurrentDataStream(), array_join_action);
+        Names array_joined_columns{findArrayJoinNode(splitted_actions_dags.array_join)->result_name};
+        ArrayJoin array_join;
+        array_join.columns = std::move(array_joined_columns);
+        array_join.is_left = generate_rel.outer();
+        auto array_join_step = std::make_unique<ArrayJoinStep>(
+            query_plan->getCurrentDataStream(), std::move(array_join), false, getContext()->getSettingsRef().max_block_size);
         array_join_step->setStepDescription("ARRAY JOIN In Generate");
         steps.emplace_back(array_join_step.get());
         query_plan->addStep(std::move(array_join_step));

cpp-ch/local-engine/Parser/RelParsers/ReadRelParser.cpp
@@ -17,6 +17,20 @@
 
 #include "ReadRelParser.h"
 #include <memory>
+#include <Core/Block.h>
+#include <Core/Settings.h>
+#include <Interpreters/Context.h>
+#include <Operator/BlocksBufferPoolTransform.h>
+#include <Parser/RelParsers/MergeTreeRelParser.h>
+#include <Parser/SubstraitParserUtils.h>
+#include <Parser/TypeParser.h>
+#include <Processors/QueryPlan/ReadFromPreparedSource.h>
+#include <Storages/SourceFromJavaIter.h>
+#include <Storages/SubstraitSource/SubstraitFileSource.h>
+#include <Storages/SubstraitSource/SubstraitFileSourceStep.h>
+#include <google/protobuf/wrappers.pb.h>
+#include <Common/BlockTypeUtils.h>
+
 
 namespace DB::ErrorCodes
 {

cpp-ch/local-engine/Parser/RelParsers/ReadRelParser.h
@@ -15,24 +15,12 @@
  * limitations under the License.
  */
 #pragma once
-#include <Core/Block.h>
-#include <Core/Settings.h>
-#include <Interpreters/Context.h>
-#include <Operator/BlocksBufferPoolTransform.h>
-#include <Parser/RelParsers/MergeTreeRelParser.h>
-#include <Parser/SerializedPlanParser.h>
-#include <Parser/SubstraitParserUtils.h>
-#include <Parser/TypeParser.h>
-#include <Processors/QueryPlan/ReadFromPreparedSource.h>
-#include <Storages/SourceFromJavaIter.h>
-#include <Storages/SubstraitSource/SubstraitFileSource.h>
-#include <Storages/SubstraitSource/SubstraitFileSourceStep.h>
-#include <google/protobuf/wrappers.pb.h>
-#include <Common/BlockTypeUtils.h>
+#include <Parser/RelParsers/RelParser.h>
 #include <Common/JNIUtils.h>
 
 namespace local_engine
 {
+
 class ReadRelParser : public RelParser
 {
 public:

cpp-ch/local-engine/Parser/RelParsers/RelParser.cpp
@@ -17,13 +17,10 @@
 #include "RelParser.h"
 
 #include <string>
-#include <google/protobuf/wrappers.pb.h>
-
 #include <AggregateFunctions/AggregateFunctionFactory.h>
 #include <DataTypes/DataTypeAggregateFunction.h>
 #include <DataTypes/IDataType.h>
-#include <DataTypes/DataTypeNullable.h>
-#include <Poco/Logger.h>
+#include <google/protobuf/wrappers.pb.h>
 #include <Poco/StringTokenizer.h>
 #include <Common/Exception.h>
 #include <Common/logger_useful.h>

cpp-ch/local-engine/Parser/RelParsers/RelParser.h
@@ -27,6 +27,7 @@
 #include <base/types.h>
 #include <substrait/extensions/extensions.pb.h>
 #include <substrait/plan.pb.h>
+
 namespace local_engine
 {
 /// parse a single substrait relation
@@ -52,34 +53,37 @@ protected:
     inline SerializedPlanParser * getPlanParser() const { return plan_parser; }
     inline ContextPtr getContext() const { return plan_parser->context; }
 
-    inline String getUniqueName(const std::string & name) { return plan_parser->getUniqueName(name); }
+    inline String getUniqueName(const std::string & name) const { return plan_parser->getUniqueName(name); }
 
-    inline const std::unordered_map<std::string, std::string> & getFunctionMapping() { return plan_parser->function_mapping; }
+    inline const std::unordered_map<std::string, std::string> & getFunctionMapping() const { return plan_parser->function_mapping; }
 
     // Get function signature name.
     std::optional<String> parseSignatureFunctionName(UInt32 function_ref);
     // Get coresponding function name in ClickHouse.
     std::optional<String> parseFunctionName(UInt32 function_ref, const substrait::Expression_ScalarFunction & function);
 
-    const DB::ActionsDAG::Node * parseArgument(ActionsDAG & action_dag, const substrait::Expression & rel)
+    const DB::ActionsDAG::Node * parseArgument(ActionsDAG & action_dag, const substrait::Expression & rel) const
     {
         return plan_parser->parseExpression(action_dag, rel);
     }
 
-    const DB::ActionsDAG::Node * parseExpression(ActionsDAG & action_dag, const substrait::Expression & rel)
+    const DB::ActionsDAG::Node * parseExpression(ActionsDAG & action_dag, const substrait::Expression & rel) const
     {
         return plan_parser->parseExpression(action_dag, rel);
     }
-    DB::ActionsDAG expressionsToActionsDAG(const std::vector<substrait::Expression> & expressions, const DB::Block & header)
+    DB::ActionsDAG expressionsToActionsDAG(const std::vector<substrait::Expression> & expressions, const DB::Block & header) const
     {
         return plan_parser->expressionsToActionsDAG(expressions, header, header);
     }
-    std::pair<DataTypePtr, Field> parseLiteral(const substrait::Expression_Literal & literal) { return plan_parser->parseLiteral(literal); }
+    std::pair<DataTypePtr, Field> parseLiteral(const substrait::Expression_Literal & literal) const
+    {
+        return plan_parser->parseLiteral(literal);
+    }
     // collect all steps for metrics
     std::vector<IQueryPlanStep *> steps;
 
     const ActionsDAG::Node *
-    buildFunctionNode(ActionsDAG & action_dag, const String & function, const DB::ActionsDAG::NodeRawConstPtrs & args)
+    buildFunctionNode(ActionsDAG & action_dag, const String & function, const DB::ActionsDAG::NodeRawConstPtrs & args) const
     {
         return plan_parser->toFunctionNode(action_dag, function, args);
     }

cpp-ch/local-engine/Parser/RelParsers/WindowGroupLimitRelParser.h
@@ -17,7 +17,6 @@
 #pragma once
 #include <optional>
 #include <Parser/RelParsers/RelParser.h>
-#include <Parser/SerializedPlanParser.h>
 #include <Processors/QueryPlan/QueryPlan.h>
 #include <Poco/Logger.h>
 #include <Common/logger_useful.h>

cpp-ch/local-engine/Parser/SerializedPlanParser.cpp
@@ -85,7 +85,6 @@
 #include <Common/JNIUtils.h>
 #include <Common/logger_useful.h>
 #include <Common/typeid_cast.h>
-#include "RelParsers/RelParser.h"
 
 namespace DB
 {

cpp-ch/local-engine/Shuffle/PartitionWriter.h
@@ -21,7 +21,6 @@
 #include <Core/Block.h>
 #include <Core/Settings.h>
 #include <Interpreters/TemporaryDataOnDisk.h>
-#include <Parser/SerializedPlanParser.h>
 #include <Shuffle/ShuffleCommon.h>
 #include <jni/CelebornClient.h>
 #include <Common/GlutenConfig.h>
@@ -68,7 +67,7 @@ public:
     PartitionWriter(const SplitOptions& options, LoggerPtr logger_);
     virtual ~PartitionWriter() = default;
 
-    void initialize(SplitResult * split_result_, const Block & output_header_)
+    void initialize(SplitResult * split_result_, const DB::Block & output_header_)
     {
         if (!init)
         {
@@ -107,8 +106,8 @@ protected:
 
     /// Only valid in celeborn partition writer
     size_t last_partition_id;
-    SplitResult* split_result = nullptr;
-    Block output_header;
+    SplitResult * split_result = nullptr;
+    DB::Block output_header;
     LoggerPtr logger = nullptr;
     bool init = false;
 };

cpp-ch/local-engine/Shuffle/ShuffleCommon.cpp
@@ -16,7 +16,6 @@
  */
 #include <Shuffle/ShuffleCommon.h>
 #include <Storages/IO/AggregateSerializationUtils.h>
-#include <Parser/SerializedPlanParser.h>
 #include <Poco/StringTokenizer.h>
 
 namespace local_engine

cpp-ch/local-engine/tests/gluten_test_util.cpp
@@ -21,6 +21,7 @@
 #include <IO/ReadBuffer.h>
 #include <IO/ReadBufferFromFile.h>
 #include <Interpreters/ActionsVisitor.h>
+#include <Parser/LocalExecutor.h>
 #include <Parser/SerializedPlanParser.h>
 #include <Parser/SubstraitParserUtils.h>
 #include <Parsers/ExpressionListParsers.h>

cpp-ch/local-engine/tests/gtest_ch_storages.cpp
@@ -15,7 +15,7 @@
  * limitations under the License.
  */
 #include <Functions/FunctionFactory.h>
-#include <Parser/MergeTreeRelParser.h>
+#include <Parser/RelParsers/MergeTreeRelParser.h>
 #include <Processors/Executors/PipelineExecutor.h>
 #include <QueryPipeline/QueryPipelineBuilder.h>
 #include <Storages/MergeTree/SparkMergeTreeMeta.h>

cpp-ch/local-engine/tests/gtest_clickhouse_pr_verify.cpp
@@ -18,6 +18,7 @@
 #include <incbin.h>
 #include <Core/Settings.h>
 #include <Interpreters/Context.h>
+#include <Parser/LocalExecutor.h>
 #include <Parser/SerializedPlanParser.h>
 #include <Parser/SubstraitParserUtils.h>
 #include <gtest/gtest.h>

cpp-ch/local-engine/tests/gtest_local_engine.cpp
@@ -24,7 +24,6 @@
 #include <Formats/FormatFactory.h>
 #include <Interpreters/Context.h>
 #include <Parser/CHColumnToSparkRow.h>
-#include <Parser/SerializedPlanParser.h>
 #include <Parser/SparkRowToCHColumn.h>
 #include <Parser/SubstraitParserUtils.h>
 #include <Processors/Executors/PipelineExecutor.h>

cpp-ch/local-engine/tests/gtest_parquet_columnindex.cpp
@@ -14,26 +14,23 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
-
 #include "config.h"
 #if USE_PARQUET
 #include <charconv>
 #include <ranges>
 #include <string>
 #include <Columns/ColumnString.h>
-#include <DataTypes/DataTypeString.h>
+#include <IO/ReadBufferFromFile.h>
 #include <Interpreters/ActionsVisitor.h>
 #include <Interpreters/Context.h>
 #include <Interpreters/ExpressionActions.h>
-#include <Parser/SerializedPlanParser.h>
 #include <Parsers/ExpressionListParsers.h>
-#include <Processors/Formats/Impl/ArrowBufferedStreams.h>
 #include <Storages/Parquet/ArrowUtils.h>
 #include <Storages/Parquet/ColumnIndexFilter.h>
 #include <Storages/Parquet/ParquetConverter.h>
 #include <Storages/Parquet/RowRanges.h>
 #include <Storages/Parquet/VectorizedParquetRecordReader.h>
+#include <boost/iterator/counting_iterator.hpp>
 #include <gtest/gtest.h>
 #include <parquet/page_index.h>
 #include <parquet/schema.h>
@@ -41,7 +38,6 @@
 #include <tests/gluten_test_util.h>
 #include <Common/BlockTypeUtils.h>
 #include <Common/QueryContext.h>
-
 
 #    define ASSERT_DURATION_LE(secs, stmt) \
         { \

cpp-ch/local-engine/tests/gtest_parquet_columnindex_bug.cpp
@@ -18,9 +18,10 @@
 #include <incbin.h>
 #include <Core/Settings.h>
 #include <Interpreters/Context.h>
-#include <Parser/SerializedPlanParser.h>
+#include <Parser/LocalExecutor.h>
 #include <Parser/SubstraitParserUtils.h>
 #include <gtest/gtest.h>
+#include <substrait/plan.pb.h>
 #include <Common/DebugUtils.h>
 #include <Common/GlutenConfig.h>
 #include <Common/QueryContext.h>

cpp-ch/local-engine/tests/gtest_parquet_read.cpp
@@ -21,7 +21,6 @@
 
 #include <ranges>
 #include <Core/Range.h>
-#include <Common/BlockTypeUtils.h>
 #include <DataTypes/DataTypeArray.h>
 #include <DataTypes/DataTypeDate32.h>
 #include <DataTypes/DataTypeDateTime.h>
@@ -32,7 +31,6 @@
 #include <Interpreters/ActionsDAG.h>
 #include <Interpreters/ActionsVisitor.h>
 #include <Interpreters/ExpressionActions.h>
-#include <Parser/SerializedPlanParser.h>
 #include <Processors/Executors/PullingPipelineExecutor.h>
 #include <Processors/Formats/Impl/ArrowColumnToCHColumn.h>
 #include <Processors/Formats/Impl/ParquetBlockInputFormat.h>
@@ -44,6 +42,7 @@
 #include <parquet/arrow/reader.h>
 #include <parquet/level_conversion.h>
 #include <tests/gluten_test_util.h>
+#include <Common/BlockTypeUtils.h>
 
 using namespace DB;
 

cpp-ch/local-engine/tests/gtest_parser.cpp
@@ -19,6 +19,7 @@
 #include <testConfig.h>
 #include <Core/Settings.h>
 #include <Interpreters/Context.h>
+#include <Parser/LocalExecutor.h>
 #include <Parser/SerializedPlanParser.h>
 #include <Parser/SubstraitParserUtils.h>
 #include <Parser/TypeParser.h>
@@ -45,8 +46,8 @@ TEST(LocalExecutor, ReadCSV)
     auto plan = local_engine::JsonStringToMessage<substrait::Plan>(EMBEDDED_PLAN(_readcsv_plan));
 
     auto query_plan = parser.parse(plan);
-    const auto pipeline = parser.buildQueryPipeline(*query_plan);
-    LocalExecutor local_executor{std::move(query_plan), QueryPipelineBuilder::getPipeline(std::move(*pipeline))};
+    auto pipeline = parser.buildQueryPipeline(*query_plan);
+    LocalExecutor local_executor{std::move(query_plan), std::move(pipeline)};
 
     EXPECT_TRUE(local_executor.hasNext());
     const Block & x = *local_executor.nextColumnar();

cpp-ch/local-engine/tests/gtest_write_pipeline.cpp
@@ -19,18 +19,20 @@
 #include <incbin.h>
 #include <testConfig.h>
 #include <Core/Settings.h>
+#include <DataTypes/DataTypeNullable.h>
 #include <Disks/ObjectStorages/HDFS/HDFSObjectStorage.h>
 #include <Interpreters/Context.h>
 #include <Interpreters/InterpreterCreateQuery.h>
 #include <Interpreters/Squashing.h>
-#include <Parser/SerializedPlanParser.h>
+#include <Parser/LocalExecutor.h>
+#include <Parser/RelParsers/WriteRelParser.h>
 #include <Parser/SubstraitParserUtils.h>
 #include <Parser/TypeParser.h>
-#include <Parser/WriteRelParser.h>
 #include <Parsers/ASTFunction.h>
 #include <Parsers/ParserCreateQuery.h>
 #include <Parsers/ParserQuery.h>
 #include <Parsers/parseQuery.h>
+#include <Processors/Chunk.h>
 #include <Processors/Transforms/DeduplicationTokenTransforms.h>
 #include <Storages/MergeTree/MergeTreeSettings.h>
 #include <Storages/MergeTree/MergeTreeSink.h>
@@ -40,6 +42,7 @@
 #include <Storages/ObjectStorage/StorageObjectStorageSink.h>
 #include <Storages/Output/FileWriterWrappers.h>
 #include <gtest/gtest.h>
+#include <substrait/plan.pb.h>
 #include <Common/CHUtil.h>
 #include <Common/DebugUtils.h>
 #include <Common/QueryContext.h>