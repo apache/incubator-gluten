fe2b86186982ede5300c5689a7a1790a49785f48
zml1206 <zhuml1206@gmail.com>
2024-09-23 08:13:14 +0800

    update

--- gluten-arrow/src/main/scala/org/apache/spark/sql/utils/SparkArrowUtil.scala
+++ gluten-arrow/src/main/scala/org/apache/spark/sql/utils/SparkArrowUtil.scala
@@ -18,24 +18,16 @@ package org.apache.spark.sql.utils
 
 import org.apache.gluten.expression.ConverterUtils
 
-import org.apache.spark.sql.internal.SQLConf
 import org.apache.spark.sql.types._
 
 import org.apache.arrow.vector.complex.MapVector
 import org.apache.arrow.vector.types.{DateUnit, FloatingPointPrecision, IntervalUnit, TimeUnit}
 import org.apache.arrow.vector.types.pojo.{ArrowType, Field, FieldType, Schema}
 
-import java.util.Locale
-
 import scala.collection.JavaConverters._
 
 /** Originated from org.apache.spark.sql.util.ArrowUtils. */
 object SparkArrowUtil {
-
-  def normalizeColName(name: String): String = {
-    val caseSensitive = SQLConf.get.caseSensitiveAnalysis
-    if (caseSensitive) name else name.toLowerCase(Locale.ROOT)
-  }
 
   /** Maps data type from Spark to Arrow. NOTE: timeZoneId required for TimestampTypes */
   def toArrowType(dt: DataType, timeZoneId: String): ArrowType = dt match {
