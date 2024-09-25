5580fbe25f3af1d38f524df2165df5e89c94cb0b
Hongze Zhang <hongze.zhang@intel.com>
2024-09-25 15:43:49 +0800

    fixup

--- gluten-arrow/src/main/java/org/apache/gluten/execution/LoadArrowDataExec.scala
+++ gluten-arrow/src/main/java/org/apache/gluten/execution/LoadArrowDataExec.scala
@@ -3,9 +3,11 @@ package org.apache.gluten.execution
 import org.apache.gluten.columnarbatch.ArrowBatches.{ArrowJavaBatch, ArrowNativeBatch}
 import org.apache.gluten.columnarbatch.ColumnarBatches
 import org.apache.gluten.memory.arrow.alloc.ArrowBufferAllocators
+
 import org.apache.spark.sql.execution.SparkPlan
 import org.apache.spark.sql.vectorized.ColumnarBatch
 
+/** Converts input data with batch type [[ArrowNativeBatch]] to type [[ArrowJavaBatch]]. */
 case class LoadArrowDataExec(override val child: SparkPlan)
   extends ColumnarToColumnarExec(ArrowNativeBatch, ArrowJavaBatch) {
   override protected def mapIterator(in: Iterator[ColumnarBatch]): Iterator[ColumnarBatch] = {
--- gluten-arrow/src/main/java/org/apache/gluten/execution/OffloadArrowDataExec.scala
+++ gluten-arrow/src/main/java/org/apache/gluten/execution/OffloadArrowDataExec.scala
@@ -3,9 +3,11 @@ package org.apache.gluten.execution
 import org.apache.gluten.columnarbatch.ArrowBatches.{ArrowJavaBatch, ArrowNativeBatch}
 import org.apache.gluten.columnarbatch.ColumnarBatches
 import org.apache.gluten.memory.arrow.alloc.ArrowBufferAllocators
+
 import org.apache.spark.sql.execution.SparkPlan
 import org.apache.spark.sql.vectorized.ColumnarBatch
 
+/** Converts input data with batch type [[ArrowJavaBatch]] to type [[ArrowNativeBatch]]. */
 case class OffloadArrowDataExec(override val child: SparkPlan)
   extends ColumnarToColumnarExec(ArrowJavaBatch, ArrowNativeBatch) {
   override protected def mapIterator(in: Iterator[ColumnarBatch]): Iterator[ColumnarBatch] = {
