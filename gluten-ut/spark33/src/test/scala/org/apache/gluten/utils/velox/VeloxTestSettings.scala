624752de220672b7c1ecfe882419fe8bc79156b3
JiaKe <ke.a.jia@intel.com>
2024-09-26 14:09:57 +0800

    [GLUTEN-6887][VL] Daily Update Velox Version (2024_09_26)  (#7355)

--- gluten-ut/spark33/src/test/scala/org/apache/gluten/utils/velox/VeloxTestSettings.scala
+++ gluten-ut/spark33/src/test/scala/org/apache/gluten/utils/velox/VeloxTestSettings.scala
@@ -162,7 +162,6 @@ class VeloxTestSettings extends BackendTestSettings {
     .exclude("random")
     .exclude("SPARK-9127 codegen with long seed")
   enableSuite[GlutenRegexpExpressionsSuite]
-    .excludeByPrefix("LIKE Pattern ESCAPE")
   enableSuite[GlutenSortShuffleSuite]
   enableSuite[GlutenSortOrderExpressionsSuite]
   enableSuite[GlutenStringExpressionsSuite]
