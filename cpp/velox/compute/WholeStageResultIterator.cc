5ad1c6926a56d97de88765396cd4ffafd29f0b63
NEUpanning <emmning@163.com>
2024-09-27 10:49:41 +0800

    Enable Spark legacy date formatter if spark.sql.legacy.timeParserPolicy is set to 'LEGACY'.

--- cpp/velox/compute/WholeStageResultIterator.cc
+++ cpp/velox/compute/WholeStageResultIterator.cc
@@ -520,6 +520,11 @@ std::unordered_map<std::string, std::string> WholeStageResultIterator::getQueryC
 
     configs[velox::core::QueryConfig::kSparkPartitionId] = std::to_string(taskInfo_.partitionId);
 
+    // Enable Spark legacy date formatter if spark.sql.legacy.timeParserPolicy is set to 'LEGACY'.
+    if (veloxCfg_->get<std::string>(kSparkLegacyTimeParserPolicy, "") == "LEGACY") {
+      configs[velox::core::QueryConfig::kSparkLegacyDateFormatter] = "true";
+    }
+
   } catch (const std::invalid_argument& err) {
     std::string errDetails = err.what();
     throw std::runtime_error("Invalid conf arg: " + errDetails);
