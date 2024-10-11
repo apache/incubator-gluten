0369a6b0dbffa27830727a0951b08260ed26c278
李扬 <654010905@qq.com>
2024-10-11 10:59:52 +0800

    [GLUTEN-7240][CH] Fix all failed uts in GlutenComplexTypeSuite (#7242)
    * fix failed uts
    
    * rename files
    
    * fix failed uts
    
    * fix failed uts
    
    * update version
    
    * update version
    
    * update version

--- cpp-ch/local-engine/Parser/scalar_function_parser/getArrayItem.cpp
+++ cpp-ch/local-engine/Parser/scalar_function_parser/getArrayItem.cpp
@@ -14,19 +14,66 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+#include <DataTypes/IDataType.h>
+#include <Parser/FunctionParser.h>
 
-#include <Parser/scalar_function_parser/arrayElement.h>
+#include <Core/Field.h>
+#include <DataTypes/DataTypeArray.h>
+#include <DataTypes/DataTypeNullable.h>
+#include <Functions/FunctionHelpers.h>
+
+namespace DB
+{
+namespace ErrorCodes
+{
+    extern const int NUMBER_OF_ARGUMENTS_DOESNT_MATCH;
+}
+}
 
 namespace local_engine
 {
-    class FunctionParserGetArrayItem : public FunctionParserArrayElement
-    {
-    public:
-        explicit FunctionParserGetArrayItem(SerializedPlanParser * plan_parser_) : FunctionParserArrayElement(plan_parser_) { }
-        ~FunctionParserGetArrayItem() override = default;
-        static constexpr auto name = "get_array_item";
-        String getName() const override { return name; }
-    };
 
-    static FunctionParserRegister<FunctionParserGetArrayItem> register_get_array_item;
+class FunctionParserGetArrayItem : public FunctionParser
+{
+public:
+    explicit FunctionParserGetArrayItem(SerializedPlanParser * plan_parser_) : FunctionParser(plan_parser_) { }
+    ~FunctionParserGetArrayItem() override = default;
+
+    static constexpr auto name = "get_array_item";
+    String getName() const override { return name; }
+
+    const ActionsDAG::Node * parse(const substrait::Expression_ScalarFunction & substrait_func, ActionsDAG & actions_dag) const override
+    {
+        /**
+            parse get_array_item(arr, idx) as
+            if (idx <= 0)
+                null
+            else
+                arrayElementOrNull(arr, idx)
+        */
+        auto parsed_args = parseFunctionArguments(substrait_func, actions_dag);
+        if (parsed_args.size() != 2)
+            throw Exception(DB::ErrorCodes::NUMBER_OF_ARGUMENTS_DOESNT_MATCH, "Function {} requires exactly two arguments", getName());
+
+        const auto * arr_arg = parsed_args[0];
+        const auto * idx_arg = parsed_args[1];
+        const DataTypeArray * arr_type = checkAndGetDataType<DataTypeArray>(removeNullable(arr_arg->result_type).get());
+        if (!arr_type)
+            throw Exception(DB::ErrorCodes::BAD_ARGUMENTS, "First argument of function {} must be an array", getName());
+
+        /// arrayElementOrNull(arrary, idx)
+        const auto * array_element_node = toFunctionNode(actions_dag, "arrayElementOrNull", {arr_arg, idx_arg});
+
+        /// idx <= 0
+        const auto * zero_node = addColumnToActionsDAG(actions_dag, std::make_shared<DataTypeInt32>(), 0);
+        const auto * idx_le_zero_node = toFunctionNode(actions_dag, "lessOrEquals", {idx_arg, zero_node});
+
+        const auto * null_node = addColumnToActionsDAG(actions_dag, makeNullable(arr_type->getNestedType()), Field{});
+        const auto * if_node = toFunctionNode(actions_dag, "if", {idx_le_zero_node, null_node, array_element_node});
+        return convertNodeTypeIfNeeded(substrait_func, if_node, actions_dag);
+    }
+};
+
+static FunctionParserRegister<FunctionParserGetArrayItem> register_get_array_item;
+
 }
