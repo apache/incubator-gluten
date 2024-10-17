a7908ffb618be2abe32e0e03c8dc340fd51620b3
Zhen Wang <643348094@qq.com>
2024-10-17 16:23:21 +0800

    [GLUTEN-7559][VL][UNIFFLE] Set rss.enabled to true in UniffleShuffleManager (#7560)

--- gluten-uniffle/velox/src/main/java/org/apache/spark/shuffle/gluten/uniffle/UniffleShuffleManager.java
+++ gluten-uniffle/velox/src/main/java/org/apache/spark/shuffle/gluten/uniffle/UniffleShuffleManager.java
@@ -36,6 +36,8 @@ public class UniffleShuffleManager extends RssShuffleManager {
 
   public UniffleShuffleManager(SparkConf conf, boolean isDriver) {
     super(conf, isDriver);
+    // FIXME: remove this after https://github.com/apache/incubator-uniffle/pull/2193
+    conf.set(RssSparkConfig.RSS_ENABLED.key(), "true");
   }
 
   @Override
