51d40771a3c3eb1888bb7c15ac46e0a33b7541c9
lgbo-ustc <lgbo.ustc@gmail.com>
2024-10-22 18:09:07 +0800

    stage

--- backends-clickhouse/src/main/scala/org/apache/gluten/extension/LazyExpandRule.scala
+++ backends-clickhouse/src/main/scala/org/apache/gluten/extension/LazyExpandRule.scala
@@ -24,9 +24,6 @@ import org.apache.spark.sql.catalyst.expressions._
 import org.apache.spark.sql.catalyst.rules.Rule
 import org.apache.spark.sql.execution._
 import org.apache.spark.sql.execution.SparkPlan
-import org.apache.spark.sql.types._
-
-import java.security.KeyStore.Entry.Attribute
 
 /*
  * For aggregation with grouping sets, we need to expand the grouping sets
@@ -67,23 +64,13 @@ case class LazyExpandRule(session: SparkSession) extends Rule[SparkPlan] with Lo
           projectOutputAttributes,
           advisoryPartitionSize
         ) =>
-      logError(s"xxx visit expand shuffle ${shuffle.getClass.getName}")
       if (
         projections.exists(
           projection =>
             projection.forall(
-              e => !e.isInstanceOf[Literal] || e.asInstanceOf[Literal].value != null))
+              e => !e.isInstanceOf[Literal] || e.asInstanceOf[Literal].value != null)) &&
+        groupingExpressions.forall(_.isInstanceOf[Attribute])
       ) {
-        logError(s"xxx requiredChildDistributionExpressions:$requiredChildDistributionExpressions")
-        logError(
-          s"xxx groupingExpressions:$groupingExpressions, " +
-            s"is all attr: ${groupingExpressions.forall(_.isInstanceOf[Attribute])}")
-        logError(s"xxx aggregateExpressions:$aggregateExpressions")
-        logError(s"xxx aggregateAttributes:$aggregateAttributes")
-        logError(
-          s"xxx resultExpressions:$resultExpressions, " +
-            s"is all attr: ${resultExpressions.forall(_.isInstanceOf[Attribute])}")
-
         // Build a new hash aggregate node. Need to replace the grouping keys with attributes from
         // expand node's input. aggregateExpressions and aggregateExpressions doesn't neeed to be
         // applied the replacement, since all the attributes in them are from the input of the
@@ -111,43 +98,35 @@ case class LazyExpandRule(session: SparkSession) extends Rule[SparkPlan] with Lo
           child
         )
         val hashAggregateOutput = newAggregate.output
-        logError(s"xxx new hash aggregate: $newAggregate")
 
         // build a new expand node
-        logError(s"xxx projections:$projections")
-        logError(s"xxx output:$output")
-        logError(s"xxx keys position ${findGroupingKeysPosition(output, groupingExpressions)}")
-        var group_id: Long = 0
-        val newProjections = projections.map {
-          projection =>
-            val res = hashAggregateOutput ++ Seq(Literal(group_id, LongType))
-            group_id += 1
-            res
-        }
-        val nexExpandOutput = resultExpressions.map(_.toAttribute)
-        val newExpand = ExpandExecTransformer(newProjections, nexExpandOutput, newAggregate)
+        val newExpandOutput = shuffle.child
+          .asInstanceOf[CHHashAggregateExecTransformer]
+          .output
+        val newExpandProjectionTemplate =
+          newExpandOutput.map(e => attributesToReplace.getOrElse(e.name, e))
+        val newExpandProjections = buildNewExpandProjections(
+          groupingExpressions,
+          projections,
+          output,
+          newExpandProjectionTemplate
+        )
+        val newExpand = ExpandExecTransformer(newExpandProjections, newExpandOutput, newAggregate)
         logError(s"xxx new expand: $newExpand")
-        shuffle
+        ColumnarShuffleExchangeExec(
+          outputPartitioning,
+          newExpand,
+          shuffleOrigin,
+          projectOutputAttributes,
+          advisoryPartitionSize)
       } else {
         // It may be a case that involves two distinct aggregations.e.g.
         // select k, count(distinct v1), count(distinct v2) from t group by k with cube
         shuffle
       }
-  }
-
-  def findGroupingKeysPosition(
-      originalExpandOutput: Seq[Attribute],
-      groupingExpressions: Seq[NamedExpression]): Seq[Int] = {
-    groupingExpressions.map {
-      case ne: NamedExpression =>
-        val index = originalExpandOutput.indexWhere(_.semanticEquals(ne.toAttribute))
-        if (index == -1) {
-          throw new IllegalStateException(s"Can't find ${ne.toAttribute} in $originalExpandOutput")
-        }
-        index
-      case other =>
-        throw new IllegalStateException(s"Can't handle $other")
-    }
+    case node =>
+      logError(s"xxx node: ${node.getClass}")
+      node
   }
 
   def buildReplaceAttributeMapForAggregate(
@@ -166,9 +145,34 @@ case class LazyExpandRule(session: SparkSession) extends Rule[SparkPlan] with Lo
 
   def buildNewExpandProjections(
       originalGroupingExpressions: Seq[NamedExpression],
-      originalAggregateOutput: Seq[Attribute],
-      newAggregateOutput: Seq[Attribute],
       originalExpandProjections: Seq[Seq[Expression]],
-      originalExpandOutput: Seq[Attribute]): Seq[Seq[Expression]] = {}
+      originalExpandOutput: Seq[Attribute],
+      newExpandOutput: Seq[Attribute]): Seq[Seq[Expression]] = {
+    var groupingKeysPosition = Map[String, Int]()
+    originalGroupingExpressions.foreach {
+      e =>
+        e match {
+          case ne: NamedExpression =>
+            val index = originalExpandOutput.indexWhere(_.semanticEquals(ne.toAttribute))
+            if (index != -1) {
+              groupingKeysPosition += (ne.name -> index)
+            }
+          case _ =>
+        }
+    }
+
+    val newExpandProjections = originalExpandProjections.map {
+      projection =>
+        val res = newExpandOutput.map {
+          attr =>
+            groupingKeysPosition.get(attr.name) match {
+              case Some(attrPos) => projection(attrPos)
+              case None => attr
+            }
+        }
+        res
+    }
+    newExpandProjections
+  }
 
 }
