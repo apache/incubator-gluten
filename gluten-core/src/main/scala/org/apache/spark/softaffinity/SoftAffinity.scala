e7f5ca479f88631d14667fc3012a6846d334f3c5
xuedongluan <luanxuedong2009@gmail.com>
2024-10-22 16:11:15 +0800

    Move scala class to scala package and fix minor typo in comment

--- gluten-core/src/main/scala/org/apache/spark/softaffinity/SoftAffinity.scala
+++ gluten-core/src/main/scala/org/apache/spark/softaffinity/SoftAffinity.scala
@@ -19,6 +19,7 @@ package org.apache.spark.softaffinity
 import org.apache.gluten.GlutenConfig
 import org.apache.gluten.logging.LogLevelUtil
 import org.apache.gluten.softaffinity.{AffinityManager, SoftAffinityManager}
+
 import org.apache.spark.internal.Logging
 import org.apache.spark.scheduler.ExecutorCacheTaskLocation
 import org.apache.spark.sql.execution.datasources.FilePartition
