805f1e086ad23b1f5f720da746d7a094864dcb49
kevinyhzou <kevinyunhe8@gmail.com>
2024-10-28 14:52:13 +0800

    fix ci

--- gluten-ut/spark32/src/test/scala/org/apache/gluten/utils/clickhouse/ClickHouseTestSettings.scala
+++ gluten-ut/spark32/src/test/scala/org/apache/gluten/utils/clickhouse/ClickHouseTestSettings.scala
@@ -284,6 +284,8 @@ class ClickHouseTestSettings extends BackendTestSettings {
     .exclude("function to_date")
     .exclude("unix_timestamp")
     .exclude("to_unix_timestamp")
+    .exclude("to_timestamp")
+    .excludeGlutenTest("to_timestamp")
     .exclude("to_timestamp with microseconds precision")
     .exclude("SPARK-30668: use legacy timestamp parser in to_timestamp")
     .exclude("SPARK-30766: date_trunc of old timestamps to hours and days")
