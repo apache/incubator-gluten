58db1304468e475425c126685424cbe7982600ce
PHILO-HE <feilong.he@intel.com>
2024-11-08 17:08:14 +0800

    [VL] Clean up some legacy code and correct minimum required GCC version (#7865)

--- dev/ci-velox-buildstatic-centos-7.sh
+++ dev/ci-velox-buildstatic-centos-7.sh
@@ -2,8 +2,6 @@
 
 set -e
 
-# TODO: will be removed after docker is updated.
-sudo yum install -y devtoolset-11
 source /opt/rh/devtoolset-11/enable
 export NUM_THREADS=4
 ./dev/builddeps-veloxbe.sh --enable_vcpkg=ON --build_arrow=OFF --build_tests=OFF --build_benchmarks=OFF \
