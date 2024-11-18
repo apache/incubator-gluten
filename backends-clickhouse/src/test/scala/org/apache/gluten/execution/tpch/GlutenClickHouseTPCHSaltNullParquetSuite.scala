ad3b63bf7ade76c49fc1c2a68011d07f2fd662ab
kevinyhzou <kevinyunhe8@gmail.com>
2024-11-18 10:59:43 +0800

    fix review

--- backends-clickhouse/src/test/scala/org/apache/gluten/execution/tpch/GlutenClickHouseTPCHSaltNullParquetSuite.scala
+++ backends-clickhouse/src/test/scala/org/apache/gluten/execution/tpch/GlutenClickHouseTPCHSaltNullParquetSuite.scala
@@ -2209,22 +2209,24 @@ class GlutenClickHouseTPCHSaltNullParquetSuite extends GlutenClickHouseTPCHAbstr
         |(7, '1970-01-01 00:00:00'),
         |(8, '2024-3-2'),
         |(9, '2024-03-2'),
-        |(10, '2024-03')
+        |(10, '2024-03'),
+        |(11, '2024-03-02 11:22:33')
         |""".stripMargin
     spark.sql(create_table_sql)
     spark.sql(insert_data_sql)
 
     val select_sql = "select id, to_date(data) from test_tbl_3135"
     compareResultsAgainstVanillaSpark(select_sql, true, { _ => })
+
     withSQLConf(("spark.sql.legacy.timeParserPolicy" -> "corrected")) {
       compareResultsAgainstVanillaSpark(
-        "select id, to_date('2025-07-22 10:00:00', 'yyyy-MM-dd') from test_tbl_3135 where id = 1",
+        "select id, to_date(data, 'yyyy-MM-dd') from test_tbl_3135 where id = 11",
         true,
         { _ => })
     }
     withSQLConf(("spark.sql.legacy.timeParserPolicy" -> "legacy")) {
       compareResultsAgainstVanillaSpark(
-        "select id, to_date('2025-07-22 10:00:00', 'yyyy-MM-dd') from test_tbl_3135 where id = 1",
+        "select id, to_date(data, 'yyyy-MM-dd') from test_tbl_3135 where id = 11",
         true,
         { _ => })
     }
