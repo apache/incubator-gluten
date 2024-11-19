5478b856abf776dbc9d04038cf2480636c192d51
Jia Ke <ke.a.jia@intel.com>
2024-11-19 22:27:29 +0800

    window oom test

--- ep/build-velox/src/get_velox.sh
+++ ep/build-velox/src/get_velox.sh
@@ -16,8 +16,8 @@
 
 set -exu
 
-VELOX_REPO=https://github.com/oap-project/velox.git
-VELOX_BRANCH=2024_11_18
+VELOX_REPO=https://github.com/JkSelf/velox.git
+VELOX_BRANCH=window-oom-test
 VELOX_HOME=""
 
 OS=`uname -s`
