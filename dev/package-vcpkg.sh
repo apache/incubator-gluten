be17f7eeaa1628ed8fdff4fb27ea4ae6d45416c7
Wei-Ting Chen <weiting.chen@intel.com>
2024-11-20 15:45:40 +0800

    [VL][1.2] Port #6560 #6569 #6730 #7117 for vcpkg issue fix (#7974)
    * [MINOR] Remove duplicate mvn packages (#6560)
    
    * [VL] Improve package scripts (#6569)
    
    * [GLUTEN-6569][VL] Delete unnecessary gcc9 enabling from package script (#6730)
    
    * [GLUTEN-7118][VL] Fix duckdb target issue when vcpkg is enabled (#7117)
    
    ---------
    
    Co-authored-by: Zhen Wang <643348094@qq.com>
    Co-authored-by: PHILO-HE <feilong.he@intel.com>

--- dev/package-vcpkg.sh
+++ dev/package-vcpkg.sh
@@ -1,21 +1,27 @@
 #!/bin/bash
 
-set -ex
+set -eux
 
 CURRENT_DIR=$(cd "$(dirname "$BASH_SOURCE")"; pwd)
 GLUTEN_DIR="$CURRENT_DIR/.."
+LINUX_OS=$(. /etc/os-release && echo ${ID})
+VERSION=$(. /etc/os-release && echo ${VERSION_ID})
+ARCH=`uname -m`
 
 cd "$GLUTEN_DIR"
 if [ "$LINUX_OS" == "centos" ]; then
   if [ "$VERSION" == "8" ]; then
     source /opt/rh/gcc-toolset-9/enable
   elif [ "$VERSION" == "7" ]; then
+    export MANPATH=""
     source /opt/rh/devtoolset-9/enable
   fi
 fi
+
+# prepare vcpkg environments
 source ./dev/vcpkg/env.sh
-./dev/buildbundle-veloxbe.sh --build_tests=ON --build_benchmarks=ON --enable_s3=ON  --enable_hdfs=ON
-mvn clean package -Pbackends-velox -Pceleborn -Puniffle -Pspark-3.2 -DskipTests
-mvn clean package -Pbackends-velox -Pceleborn -Puniffle -Pspark-3.3 -DskipTests
-mvn clean package -Pbackends-velox -Pceleborn -Puniffle -Pspark-3.4 -DskipTests
-mvn clean package -Pbackends-velox -Pceleborn -Puniffle -Pspark-3.5 -DskipTests
+
+# build gluten with velox backend, prompt always respond y
+export PROMPT_ALWAYS_RESPOND=y
+
+./dev/buildbundle-veloxbe.sh --build_tests=ON --build_arrow=OFF --build_benchmarks=ON --enable_s3=ON  --enable_hdfs=ON "$@"
