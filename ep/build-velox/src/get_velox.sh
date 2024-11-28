48a84e795fbda40b90dfde3ec492f4363d016b7f
Jia Ke <ke.a.jia@intel.com>
2024-11-28 21:11:08 +0800

    test

--- ep/build-velox/src/get_velox.sh
+++ ep/build-velox/src/get_velox.sh
@@ -16,8 +16,8 @@
 
 set -exu
 
-VELOX_REPO=https://github.com/oap-project/velox.git
-VELOX_BRANCH=2024_11_27
+VELOX_REPO=https://github.com/JkSelf/velox.git
+VELOX_BRANCH=viewfs-support
 VELOX_HOME=""
 
 OS=`uname -s`
