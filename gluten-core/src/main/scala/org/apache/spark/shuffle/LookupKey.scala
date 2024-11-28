7b76f374fd8ada6f006fde4dc90d7692561875a5
Hongze Zhang <hongze.zhang@intel.com>
2024-11-28 14:28:32 +0800

    fixup

--- gluten-core/src/main/scala/org/apache/spark/shuffle/LookupKey.scala
+++ gluten-core/src/main/scala/org/apache/spark/shuffle/LookupKey.scala
@@ -2,6 +2,10 @@ package org.apache.spark.shuffle
 
 import org.apache.spark.ShuffleDependency
 
+/**
+ * Required during shuffle manager registration to determine whether the shuffle manager should be
+ * used for the particular shuffle dependency.
+ */
 trait LookupKey {
   def accepts[K, V, C](dependency: ShuffleDependency[K, V, C]): Boolean
 }
