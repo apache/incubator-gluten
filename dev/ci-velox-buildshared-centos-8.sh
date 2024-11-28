de609dad585ec811fc9cdc9c5a26e9c962c66cda
PHILO-HE <feilong.he@intel.com>
2024-11-28 17:05:21 +0800

    Revert to previous docker

--- dev/ci-velox-buildshared-centos-8.sh
+++ dev/ci-velox-buildshared-centos-8.sh
@@ -3,5 +3,5 @@
 set -e
 
 source /opt/rh/gcc-toolset-9/enable
-./dev/builddeps-veloxbe.sh --run_setup_script=ON --build_arrow=OFF --enable_ep_cache=OFF --build_tests=ON \
+./dev/builddeps-veloxbe.sh --run_setup_script=OFF --enable_ep_cache=OFF --build_tests=ON \
     --build_examples=ON --build_benchmarks=ON --build_protobuf=ON
