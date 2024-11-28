f634e76f2db692b8b3f3cc1352a0f28b3847947c
Terry Wang <zjuwangg@foxmail.com>
2024-11-28 22:00:46 +0800

    [draft]Add unsafe bytes buffer array to store broadcast content in offheap

---
+++ backends-velox/src/main/scala/org/apache/spark/sql/execution/unsafe/UnsafeBytesBufferArray.scala
@@ -0,0 +1,150 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.spark.sql.execution.unsafe
+
+import org.apache.spark.internal.Logging
+import org.apache.spark.memory.{MemoryConsumer, MemoryMode, TaskMemoryManager}
+import org.apache.spark.unsafe.Platform
+import org.apache.spark.unsafe.array.LongArray
+
+import java.security.MessageDigest
+
+/**
+ * Used to store broadcast variable off-heap memory for broadcast variable.
+ * The underlying data structure is a
+ *
+ * @param arraySize
+ * underlying array[array[byte]]'s length
+ * @param bytesBufferLengths
+ * underlying array[array[byte]] per array's length
+ * @param totalBytes
+ * all bytes buffer's length plus together
+ */
+class UnsafeBytesBufferArray(
+     arraySize: Int,
+     bytesBufferLengths: Array[Int],
+     totalBytes: Long,
+     tmm: TaskMemoryManager)
+  extends MemoryConsumer(tmm, MemoryMode.OFF_HEAP)
+    with Logging {
+  /**
+   * A single array to store all bytesBufferArray's value, it's inited once
+   * when first time get accessed.
+   */
+  private var longArray: LongArray = _
+
+  /**
+   * Index the start of each byteBuffer's offset to underlying LongArray's initial position.
+   */
+  private val bytesBufferOffset = new Array[Int](arraySize)
+
+  {
+    assert(arraySize == bytesBufferLengths.length)
+    assert(totalBytes >= 0)
+    if (arraySize > 0) {
+      for (i <- 0 until arraySize) {
+        bytesBufferOffset(i) =
+          if (i > 0) bytesBufferLengths(i - 1) + bytesBufferOffset(i - 1) else 0
+      }
+    }
+  }
+
+  override def spill(l: Long, memoryConsumer: MemoryConsumer): Long = 0L
+
+  /**
+   * Put bytesBuffer at specified array index.
+   * @param index
+   * @param bytesBuffer
+   */
+  def putBytesBufferAtIndex(index: Int, bytesBuffer: Array[Byte]): Unit = this.synchronized {
+    assert(index < arraySize)
+    assert(bytesBuffer.length == bytesBufferLengths(index))
+    // first to allocate underlying long array
+    if (null == longArray && index == 0) {
+      log.debug(s"allocate array $totalBytes, actual longArray size ${(totalBytes + 7) / 8}")
+      longArray = allocateArray((totalBytes + 7) / 8)
+    }
+    if (log.isDebugEnabled) {
+      log.debug(s"put bytesBuffer at index $index bytesBuffer's length is ${bytesBuffer.length}")
+      log.debug(
+        s"bytesBuffer at index $index " +
+          s"digest ${calculateMD5(bytesBuffer).mkString("Array(", ", ", ")")}")
+    }
+    Platform.copyMemory(
+      bytesBuffer,
+      Platform.BYTE_ARRAY_OFFSET,
+      longArray.getBaseObject,
+      longArray.getBaseOffset + bytesBufferOffset(index),
+      bytesBufferLengths(index))
+  }
+
+  /**
+   * Used to debug input/output bytes.
+   *
+   * @param bytesBuffer
+   * @return
+   */
+  private def calculateMD5(bytesBuffer: Array[Byte]): Array[Byte] = try {
+    val md = MessageDigest.getInstance("MD5")
+    md.digest(bytesBuffer)
+  } catch {
+    case e: Throwable =>
+      log.warn("error when calculateMD5", e)
+      new Array[Byte](0)
+  }
+
+  /**
+   * Get bytesBuffer at specified index.
+   * @param index
+   * @return
+   */
+  def getBytesBufferAtIndex(index: Int): Array[Byte] = {
+    assert(index < arraySize)
+    if (null == longArray) {
+      return new Array[Byte](0)
+    }
+    val bytes = new Array[Byte](bytesBufferLengths(index))
+    log.debug(s"get bytesBuffer at index $index bytesBuffer length ${bytes.length}")
+    Platform.copyMemory(
+      longArray.getBaseObject,
+      longArray.getBaseOffset + bytesBufferOffset(index),
+      bytes,
+      Platform.BYTE_ARRAY_OFFSET,
+      bytesBufferLengths(index))
+    if (log.isDebugEnabled) {
+      log.debug(
+        s"get bytesBuffer at index $index " +
+          s"digest ${calculateMD5(bytes).mkString("Array(", ", ", ")")}")
+    }
+    bytes
+  }
+
+  /**
+   * It's needed once the broadcast variable is not used
+   */
+    override def finalize(): Unit = {
+    try {
+      if (longArray != null) {
+        log.debug(s"BytesArrayInOffheap finalize $arraySize")
+        freeArray(longArray)
+        longArray = null
+      }
+    } finally {
+      super.finalize();
+    }
+  }
+}
