8be8c6a2a0023292b1222ae51a0f05af490c15a2
zml1206 <zhuml1206@gmail.com>
2024-12-05 14:43:54 +0800

    fix

--- gluten-substrait/src/main/scala/org/apache/gluten/extension/columnar/enumerated/RasOffload.scala
+++ gluten-substrait/src/main/scala/org/apache/gluten/extension/columnar/enumerated/RasOffload.scala
@@ -137,7 +137,7 @@ object RasOffload {
                     offloadedPlan
                   }
                 } catch {
-                  case GlutenNotSupportException =>
+                  case _: GlutenNotSupportException =>
                     // TODO: Tag the original plan with fallback reason. This is a non-trivial work
                     //  in RAS as the query plan we got here may be a copy so may not propagate tags
                     //  to original plan.
