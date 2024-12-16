581dcca0a97c62e441ca6892b41c08e375da2b6f
Yuan Zhou <yuan.zhou@intel.com>
2024-12-16 17:03:54 +0800

    [DNM] test compiler issue
    Signed-off-by: Yuan Zhou <yuan.zhou@intel.com>

--- ep/build-velox/src/get_velox.sh
+++ ep/build-velox/src/get_velox.sh
@@ -16,8 +16,8 @@
 
 set -exu
 
-VELOX_REPO=https://github.com/oap-project/velox.git
-VELOX_BRANCH=2024_12_14
+VELOX_REPO=https://github.com/zhouyuan/velox.git
+VELOX_BRANCH=wip_test_compiler
 VELOX_HOME=""
 
 OS=`uname -s`
