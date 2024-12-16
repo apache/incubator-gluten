apache/incubator-gluten

commit b722f39eefbb4c8e7ab95683b35abc7adf736a70

[GLUTEN-6887][VL] Daily Update Velox Version (2024_12_13) (#8223)
Upstream Velox's New Commits:

ac134400b by Zac Wen, feat: Use Velox fs for ssd cache checkpoint file (11783)
d9a601220 by Jia Ke, fix: Refactor package namespace in HdfsFileSystem (11585)
63fcb2769 by PHILO-HE, build: Remove test component of lib icu (11826)
8d0d244c3 by Jacob Wujciak-Jens, build: Update CI container (11796)
a775a6ce9 by Wei He, fix(fuzzer): Mark IPADDRESS and IPPREFIX types as unsuppported input types in PrestoQueryRunner (11820)
1deda1721 by Wei He, fix(fuzzer): Fix SQL translation of between() in PrestoQueryRunner (11819)
98b149767 by Pedro Eugenio Rocha Pedreira, feat(planbuilder): Accept schema for tableWriter() (11829)
3e4233e5d by Krishna Pai, fix: json_parse tape failure (11831)
109ef3b65 by aditi-pandit, refactor: Change C style casts to C++ style (Part 4) (11687)
0761ec1c4 by Yizhuo Liang, fix: Make ScanAndSort.cpp example up-to-date (11803)
cf2314039 by wypb, fix: Support timestamp type partition filter (11754)

cpp/velox/memory/VeloxMemoryManager.cc
@@ -335,17 +335,23 @@ bool VeloxMemoryManager::tryDestructSafe() {
 
   // Velox memory manager considered safe to destruct when no alive pools.
   if (veloxMemoryManager_) {
-    if (veloxMemoryManager_->numPools() > 2) {
+    if (veloxMemoryManager_->numPools() > 3) {
+      GLUTEN_CHECK(false, "Unreachable code");
       return false;
     }
-    if (veloxMemoryManager_->numPools() == 2) {
+    if (veloxMemoryManager_->numPools() == 3) {
       // Assert the pool is spill pool
       // See https://github.com/facebookincubator/velox/commit/e6f84e8ac9ef6721f527a2d552a13f7e79bdf72e
+      // https://github.com/facebookincubator/velox/commit/ac134400b5356c5ba3f19facee37884aa020afdc
       int32_t spillPoolCount = 0;
+      int32_t cachePoolCount = 0;
       int32_t tracePoolCount = 0;
       veloxMemoryManager_->testingDefaultRoot().visitChildren([&](velox::memory::MemoryPool* child) -> bool {
         if (child == veloxMemoryManager_->spillPool()) {
           spillPoolCount++;
+        }
+        if (child == veloxMemoryManager_->cachePool()) {
+          cachePoolCount++;
         }
         if (child == veloxMemoryManager_->tracePool()) {
           tracePoolCount++;
@@ -353,9 +359,10 @@ bool VeloxMemoryManager::tryDestructSafe() {
         return true;
       });
       GLUTEN_CHECK(spillPoolCount == 1, "Illegal pool count state: spillPoolCount: " + std::to_string(spillPoolCount));
+      GLUTEN_CHECK(cachePoolCount == 1, "Illegal pool count state: cachePoolCount: " + std::to_string(cachePoolCount));
       GLUTEN_CHECK(tracePoolCount == 1, "Illegal pool count state: tracePoolCount: " + std::to_string(tracePoolCount));
     }
-    if (veloxMemoryManager_->numPools() < 2) {
+    if (veloxMemoryManager_->numPools() < 3) {
       GLUTEN_CHECK(false, "Unreachable code");
     }
   }

ep/build-velox/src/get_velox.sh
@@ -17,7 +17,7 @@
 set -exu
 
 VELOX_REPO=https://github.com/oap-project/velox.git
-VELOX_BRANCH=2024_12_12
+VELOX_BRANCH=2024_12_13
 VELOX_HOME=""
 
 OS=`uname -s`

commit 1b581c7523261e54bea7e44ed8d0731aa91d1995

[VL] Fix crash when there are unreleased memory pools during termintating a Velox task (#8243)
A quick fix for a crash issue introduced by #8223.

Error:

terminate called after throwing an instance of 'gluten::GlutenException'
  what():  Unreachable code

cpp/velox/memory/VeloxMemoryManager.cc
@@ -336,7 +336,8 @@ bool VeloxMemoryManager::tryDestructSafe() {
   // Velox memory manager considered safe to destruct when no alive pools.
   if (veloxMemoryManager_) {
     if (veloxMemoryManager_->numPools() > 3) {
-      GLUTEN_CHECK(false, "Unreachable code");
+      VLOG(2) << "Attempt to destruct VeloxMemoryManager failed because there are " << veloxMemoryManager_->numPools()
+              << " outstanding memory pools.";
       return false;
     }
     if (veloxMemoryManager_->numPools() == 3) {