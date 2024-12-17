24099c042244ba18bc3d803d823412a9471725bb
zml1206 <zhuml1206@gmail.com>
2024-12-17 13:36:57 +0800

    fix style

--- gluten-ut/spark34/src/test/scala/org/apache/spark/sql/GlutenDateFunctionsSuite.scala
+++ gluten-ut/spark34/src/test/scala/org/apache/spark/sql/GlutenDateFunctionsSuite.scala
@@ -323,9 +323,11 @@ class GlutenDateFunctionsSuite extends DateFunctionsSuite with GlutenSQLTestsTra
     // invalid format
     checkAnswer(df.select(to_date(col("s"), "yyyy-hh-MM")), Seq(Row(null), Row(null), Row(null)))
     // velox getTimestamp function does not throw exception when format is "yyyy-dd-aa".
-    // val e = intercept[SparkUpgradeException](df.select(to_date(col("s"), "yyyy-dd-aa")).collect())
+    // val e =
+    //   intercept[SparkUpgradeException](df.select(to_date(col("s"), "yyyy-dd-aa")).collect())
     // assert(e.getCause.isInstanceOf[IllegalArgumentException])
-    // assert(e.getMessage.contains("You may get a different result due to the upgrading to Spark"))
+    // assert(
+    //   e.getMessage.contains("You may get a different result due to the upgrading to Spark"))
 
     // February
     val x1 = "2016-02-29"
