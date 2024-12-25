db402a7342928090816f63e6b6e8690eebd9c940
Chang Chen <baibaichen@gmail.com>
2024-12-25 13:37:38 +0800

    [Minor Refactor] pass by const reference instead of pass by value

--- cpp-ch/local-engine/Storages/MergeTree/StorageMergeTreeFactory.h
+++ cpp-ch/local-engine/Storages/MergeTree/StorageMergeTreeFactory.h
@@ -65,7 +65,7 @@ public:
     static StorageMergeTreeFactory & instance();
     static void freeStorage(const DB::StorageID & id, const String & snapshot_id = "");
     static SparkStorageMergeTreePtr
-    getStorage(const DB::StorageID& id, const String & snapshot_id, MergeTreeTable merge_tree_table,
+    getStorage(const DB::StorageID& id, const String & snapshot_id, const MergeTreeTable & merge_tree_table,
         const std::function<SparkStorageMergeTreePtr()> & creator);
     static DB::DataPartsVector getDataPartsByNames(const DB::StorageID & id, const String & snapshot_id, std::unordered_set<String> part_name);
     static void init_cache_map()
