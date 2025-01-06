711981943af9229d84bf13e5c4e0150cdc22bf8b
Jia Ke <ke.a.jia@intel.com>
2025-01-07 00:12:29 +0800

    Fix q14ab perf down

--- ep/build-velox/src/get_velox.sh
+++ ep/build-velox/src/get_velox.sh
@@ -16,8 +16,8 @@
 
 set -exu
 
-VELOX_REPO=https://github.com/oap-project/velox.git
-VELOX_BRANCH=2025_01_05
+VELOX_REPO=https://github.com/JkSelf/velox.git
+VELOX_BRANCH=smj-perf
 VELOX_HOME=""
 
 OS=`uname -s`
