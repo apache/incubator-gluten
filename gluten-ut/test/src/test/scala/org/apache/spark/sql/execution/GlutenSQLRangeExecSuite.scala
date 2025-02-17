3b05cdb0a0fa4e5dee0c8d02f9be710bef76305e
taiyang-li <654010905@qq.com>
2025-02-17 10:39:19 +0800

    fix ut

--- gluten-ut/test/src/test/scala/org/apache/spark/sql/execution/GlutenSQLRangeExecSuite.scala
+++ gluten-ut/test/src/test/scala/org/apache/spark/sql/execution/GlutenSQLRangeExecSuite.scala
@@ -16,7 +16,6 @@
  */
 package org.apache.spark.sql.execution
 
-import org.apache.gluten.config.GlutenConfig
 import org.apache.gluten.execution.ColumnarRangeBaseExec
 import org.apache.gluten.utils.BackendTestUtils
 
