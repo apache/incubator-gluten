6f03fc94e6009f91811f78b9c798237dda0311ce
Chang Chen <baibaichen@gmail.com>
2025-02-24 17:04:01 +0800

    [Test] set path to "./" instead of default path "/"

--- cpp-ch/local-engine/tests/gtest_write_pipeline_mergetree.cpp
+++ cpp-ch/local-engine/tests/gtest_write_pipeline_mergetree.cpp
@@ -108,7 +108,6 @@ TEST(MergeTree, ClickhouseMergeTree)
     ThreadStatus thread_status;
 
     const auto context = DB::Context::createCopy(QueryContext::globalContext());
-    context->setPath("./");
     const Settings & settings = context->getSettingsRef();
 
     const std::string query
@@ -198,7 +197,6 @@ TEST(MergeTree, SparkMergeTree)
     ThreadStatus thread_status;
 
     const auto context = DB::Context::createCopy(QueryContext::globalContext());
-    context->setPath("./");
     const Settings & settings = context->getSettingsRef();
 
     const auto extension_table = local_engine::JsonStringToMessage<substrait::ReadRel::ExtensionTable>(EMBEDDED_PLAN(_1_mergetree_));
