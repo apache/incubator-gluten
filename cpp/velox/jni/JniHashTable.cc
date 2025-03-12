d2b9c03d516af0b02e688da39a8d6a039afc8630
Ke Jia <ke.a.jia@intel.com>
2025-03-12 18:36:44 +0000

    log

--- cpp/velox/jni/JniHashTable.cc
+++ cpp/velox/jni/JniHashTable.cc
@@ -26,6 +26,7 @@
 #include "substrait/type.pb.h"
 #include "velox/core/PlanNode.h"
 #include "velox/type/Type.h"
+#include <iostream>
 
 namespace gluten {
 
@@ -68,6 +69,7 @@ std::shared_ptr<facebook::velox::exec::HashTableBuilder> nativeHashTableBuild(
     bool hasNullKeyValues,
     std::vector<std::shared_ptr<ColumnarBatch>>& batches,
     std::shared_ptr<facebook::velox::memory::MemoryPool> memoryPool) {
+  std::cout << "nativeHashTableBuild is running" << "\n";
   auto rowType = std::make_shared<facebook::velox::RowType>(std::move(names), std::move(veloxTypeList));
 
   auto sJoin = static_cast<substrait::JoinRel_JoinType>(joinType);
