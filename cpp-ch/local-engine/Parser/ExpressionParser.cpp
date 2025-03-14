05eac08b57f0ef9286d8a9171be7b6cd2d1a0e5d
kevinyhzou <37431499+KevinyhZou@users.noreply.github.com>
2025-03-14 17:20:43 +0800

    [GLUTEN-3289][CH]Fix cast float to string (#8092)
    * Fix cast float to string
    
    * rebase

--- cpp-ch/local-engine/Parser/ExpressionParser.cpp
+++ cpp-ch/local-engine/Parser/ExpressionParser.cpp
@@ -337,6 +337,8 @@ ExpressionParser::NodeRawConstPtr ExpressionParser::parseExpression(ActionsDAG &
                 String function_name = "sparkCastFloatTo" + denull_output_type->getName();
                 result_node = toFunctionNode(actions_dag, function_name, args);
             }
+            else if (isFloat(denull_input_type) && isString(denull_output_type))
+                result_node = toFunctionNode(actions_dag, "sparkCastFloatToString", args);
             else if ((isDecimal(denull_input_type) || isNativeNumber(denull_input_type)) && substrait_type.has_decimal())
             {
                 int precision = substrait_type.decimal().precision();
