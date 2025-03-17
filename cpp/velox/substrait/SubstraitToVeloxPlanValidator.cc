26de4728ab75808ebc711624efffcc875f737a2f
wforget <643348094@qq.com>
2025-03-17 17:23:07 +0800

    test

--- cpp/velox/substrait/SubstraitToVeloxPlanValidator.cc
+++ cpp/velox/substrait/SubstraitToVeloxPlanValidator.cc
@@ -243,7 +243,7 @@ bool SubstraitToVeloxPlanValidator::isAllowedCast(const TypePtr& fromType, const
   // 4. Certain complex types are not allowed.
 
   // Don't support isIntervalYearMonth.
-  if (fromType->isIntervalYearMonth() || toType->isIntervalYearMonth()) {
+  if (errorType->isIntervalYearMonth() || toType->isIntervalYearMonth()) {
     LOG_VALIDATION_MSG("Casting involving INTERVAL_YEAR_MONTH is not supported.");
     return false;
   }
