90617a188b7cd69bc39a321767e12d7a7a84bd36
Arnav Balyan <60175178+ArnavBalyan@users.noreply.github.com>
2025-03-18 08:01:01 +0530

    [GLUTEN-3620][VL] Add config to fallback RangeExec (#8913)
    spark.gluten.sql.columnar.range

--- gluten-substrait/src/main/scala/org/apache/gluten/extension/columnar/validator/Validators.scala
+++ gluten-substrait/src/main/scala/org/apache/gluten/extension/columnar/validator/Validators.scala
@@ -186,6 +186,7 @@ object Validators {
           if !(glutenConf.enableColumnarSample && BackendsApiManager.getSettings
             .supportSampleExec()) =>
         fail(p)
+      case p: RangeExec if !glutenConf.enableColumnarRange => fail(p)
       case _ => pass()
     }
   }
