87a159abd6175f6f3c51624636ae331a5c572ced
Zhichao Zhang <zhangzc@apache.org>
2025-03-19 16:19:18 +0800

    [GLUTEN-8872][CH][Part-3] Fix reading bug for the update operation with the deletion vectors (#9029)
    [CH][Part-3] Fix reading bug for the update operation with the deletion vectors
    
    Fix reading bug for the update operation with the deletion vectors:
    Add the '__delta_internal_is_row_deleted' column even if there is no DV for the parquet files

--- cpp-ch/local-engine/Storages/SubstraitSource/Delta/DeltaReader.cpp
+++ cpp-ch/local-engine/Storages/SubstraitSource/Delta/DeltaReader.cpp
@@ -22,6 +22,14 @@
 #include <Storages/Parquet/ParquetMeta.h>
 #include <Storages/SubstraitSource/Delta/DeltaParquetMeta.h>
 
+namespace DB
+{
+namespace ErrorCodes
+{
+    extern const int BAD_ARGUMENTS;
+}
+}
+
 using namespace DB;
 
 namespace local_engine::delta
@@ -35,25 +43,30 @@ std::unique_ptr<DeltaReader> DeltaReader::create(
     const String & row_index_ids_encoded,
     const String & row_index_filter_type)
 {
-    assert(row_index_filter_type == DeltaDVBitmapConfig::DELTA_ROW_INDEX_FILTER_TYPE_IF_CONTAINED);
+    std::shared_ptr<DeltaDVBitmapConfig> bitmap_config_;
+    if (!row_index_ids_encoded.empty() && !row_index_filter_type.empty())
+    {
+        if (row_index_filter_type != DeltaDVBitmapConfig::DELTA_ROW_INDEX_FILTER_TYPE_IF_CONTAINED)
+            throw DB::Exception(DB::ErrorCodes::BAD_ARGUMENTS, "Row index filter type does not support : {}", row_index_filter_type);
 
-    auto bitmap_config_ = std::make_shared<DeltaDVBitmapConfig>();
-    rapidjson::Document doc;
-    doc.Parse(row_index_ids_encoded.c_str());
-    assert(!doc.HasParseError());
-    if (doc.HasMember("storageType") && doc["storageType"].IsString())
-        bitmap_config_->storage_type = doc["storageType"].GetString();
-    if (doc.HasMember("pathOrInlineDv") && doc["pathOrInlineDv"].IsString())
-        bitmap_config_->path_or_inline_dv = doc["pathOrInlineDv"].GetString();
-    if (doc.HasMember("offset") && doc["offset"].IsInt())
-        bitmap_config_->offset = doc["offset"].GetInt();
-    if (doc.HasMember("sizeInBytes") && doc["sizeInBytes"].IsInt())
-        bitmap_config_->size_in_bytes = doc["sizeInBytes"].GetInt();
-    if (doc.HasMember("cardinality") && doc["cardinality"].IsInt64())
-        bitmap_config_->cardinality = doc["cardinality"].GetInt64();
-    if (doc.HasMember("maxRowIndex") && doc["maxRowIndex"].IsInt64())
-        bitmap_config_->max_row_index = doc["maxRowIndex"].GetInt64();
-
+        bitmap_config_ = std::make_shared<DeltaDVBitmapConfig>();
+        rapidjson::Document doc;
+        doc.Parse(row_index_ids_encoded.c_str());
+        if (doc.HasParseError())
+            throw DB::Exception(DB::ErrorCodes::BAD_ARGUMENTS, "Invalid JSON in row_index_ids_encoded: {}", doc.GetParseError());
+        if (doc.HasMember("storageType") && doc["storageType"].IsString())
+            bitmap_config_->storage_type = doc["storageType"].GetString();
+        if (doc.HasMember("pathOrInlineDv") && doc["pathOrInlineDv"].IsString())
+            bitmap_config_->path_or_inline_dv = doc["pathOrInlineDv"].GetString();
+        if (doc.HasMember("offset") && doc["offset"].IsInt())
+            bitmap_config_->offset = doc["offset"].GetInt();
+        if (doc.HasMember("sizeInBytes") && doc["sizeInBytes"].IsInt())
+            bitmap_config_->size_in_bytes = doc["sizeInBytes"].GetInt();
+        if (doc.HasMember("cardinality") && doc["cardinality"].IsInt64())
+            bitmap_config_->cardinality = doc["cardinality"].GetInt64();
+        if (doc.HasMember("maxRowIndex") && doc["maxRowIndex"].IsInt64())
+            bitmap_config_->max_row_index = doc["maxRowIndex"].GetInt64();
+    }
     return std::make_unique<DeltaReader>(file_, to_read_header_, output_header_, input_format_, bitmap_config_);
 }
 
@@ -66,15 +79,15 @@ DeltaReader::DeltaReader(
     : NormalFileReader(file_, to_read_header_, output_header_, input_format_)
     , bitmap_config(bitmap_config_)
 {
-    bitmap_array = std::make_unique<DeltaDVRoaringBitmapArray>(file->getContext());
-    bitmap_array->rb_read(bitmap_config->path_or_inline_dv, bitmap_config->offset, bitmap_config->size_in_bytes);
+    if (bitmap_config)
+    {
+        bitmap_array = std::make_unique<DeltaDVRoaringBitmapArray>(file->getContext());
+        bitmap_array->rb_read(bitmap_config->path_or_inline_dv, bitmap_config->offset, bitmap_config->size_in_bytes);
+    }
 }
 
 Chunk DeltaReader::doPull()
 {
-    if (!bitmap_array)
-        return NormalFileReader::doPull();
-
     while (true)
     {
         Chunk chunk = NormalFileReader::doPull();
@@ -90,20 +103,27 @@ Chunk DeltaReader::doPull()
 
 void DeltaReader::deleteRowsByDV(Chunk & chunk) const
 {
-    assert(bitmap_array);
     size_t num_rows = chunk.getNumRows();
     size_t deleted_row_pos = readHeader.getPositionByName(DeltaParquetVirtualMeta::DELTA_INTERNAL_IS_ROW_DELETED);
-    size_t tmp_row_id_pos_output = readHeader.getPositionByName(ParquetVirtualMeta::TMP_ROWINDEX);
     DB::DataTypePtr deleted_row_type = DeltaParquetVirtualMeta::getMetaColumnType(readHeader);
-    size_t tmp_row_id_pos = chunk.getNumColumns() - 1;
-    if (tmp_row_id_pos_output < tmp_row_id_pos)
-        tmp_row_id_pos = tmp_row_id_pos_output;
-
     auto deleted_row_column_nest = DB::ColumnInt8::create(num_rows);
     auto & vec = deleted_row_column_nest->getData();
 
-    for (int i = 0; i < num_rows; i++)
-        vec[i] = bitmap_array->rb_contains(chunk.getColumns()[tmp_row_id_pos]->get64(i));
+    if (bitmap_array)
+    {
+        size_t tmp_row_id_pos_output = readHeader.getPositionByName(ParquetVirtualMeta::TMP_ROWINDEX);
+        size_t tmp_row_id_pos = chunk.getNumColumns() - 1;
+        if (tmp_row_id_pos_output < tmp_row_id_pos)
+            tmp_row_id_pos = tmp_row_id_pos_output;
+        for (int i = 0; i < num_rows; i++)
+            vec[i] = bitmap_array->rb_contains(chunk.getColumns()[tmp_row_id_pos]->get64(i));
+    }
+    else
+    {
+        // the bitmap array is null, set the values of the '__delta_internal_is_row_deleted' column to the false
+        for (int i = 0; i < num_rows; i++)
+            vec[i] = false;
+    }
 
     DB::ColumnPtr deleted_row_column;
     if (deleted_row_type->isNullable())
