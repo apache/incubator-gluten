52f2a806d3acf040da44a58a4763ae879318c6b5
arnavb <arnavb@uber.com>
2025-03-24 16:28:04 +0000

    update

--- ep/build-velox/src/get_velox.sh
+++ ep/build-velox/src/get_velox.sh
@@ -16,8 +16,8 @@
 
 set -exu
 
-VELOX_REPO=https://github.com/oap-project/velox.git
-VELOX_BRANCH=2025_03_24
+VELOX_REPO=https://github.com/ArnavBalyan/velox.git
+VELOX_BRANCH=arnavb/pretty-string
 VELOX_HOME=""
 
 OS=`uname -s`
