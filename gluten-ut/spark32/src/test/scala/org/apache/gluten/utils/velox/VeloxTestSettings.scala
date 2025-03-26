79fb0bd713f20020453991f441a0366441b026d5
Hongze Zhang <hongze.zzz123@gmail.com>
2025-03-26 22:10:16 +0100

    fixup

--- gluten-ut/spark32/src/test/scala/org/apache/gluten/utils/velox/VeloxTestSettings.scala
+++ gluten-ut/spark32/src/test/scala/org/apache/gluten/utils/velox/VeloxTestSettings.scala
@@ -364,6 +364,8 @@ class VeloxTestSettings extends BackendTestSettings {
     // Need to support MAP<NullType, NullType>
     .exclude(
       "SPARK-27619: When spark.sql.legacy.allowHashOnMapType is true, hash can be used on Maptype")
+    .exclude("SPARK-17515: CollectLimit.execute() should perform per-partition limits")
+    .exclude("SPARK-19650: An action on a Command should not trigger a Spark job")
   enableSuite[GlutenDatasetAggregatorSuite]
   enableSuite[GlutenDatasetOptimizationSuite]
   enableSuite[GlutenDatasetPrimitiveSuite]
@@ -416,6 +418,7 @@ class VeloxTestSettings extends BackendTestSettings {
     .exclude("File source v2: support passing data filters to FileScan without partitionFilters")
     // DISABLED: GLUTEN-4893 Vanilla UT checks scan operator by exactly matching the class type
     .exclude("File source v2: support partition pruning")
+    .excludeGlutenTest("SPARK-25237 compute correct input metrics in FileScanRDD")
   enableSuite[GlutenEnsureRequirementsSuite]
     // Rewrite to change the shuffle partitions for optimizing repartition
     .excludeByPrefix("SPARK-35675")
