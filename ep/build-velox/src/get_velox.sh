30be9552952f4ff7ac579a1255908bcd70866d1d
Chengcheng Jin <Chengcheng.jin@ibm.com>
2025-03-28 08:53:55 +0000

    test

--- ep/build-velox/src/get_velox.sh
+++ ep/build-velox/src/get_velox.sh
@@ -16,8 +16,8 @@
 
 set -exu
 
-VELOX_REPO=https://github.com/oap-project/velox.git
-VELOX_BRANCH=2025_03_31
+VELOX_REPO=https://github.com/jinchengchenghh/velox.git
+VELOX_BRANCH=unsafe
 VELOX_HOME=""
 
 OS=`uname -s`
