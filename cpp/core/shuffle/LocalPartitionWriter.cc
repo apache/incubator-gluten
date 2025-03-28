dc057c2553308b9cbe5b6a209a8e61654d8483b9
Yuming Wang <yumwang@apache.org>
2025-03-28 01:15:57 -0500

    Update LocalPartitionWriter.cc

--- cpp/core/shuffle/LocalPartitionWriter.cc
+++ cpp/core/shuffle/LocalPartitionWriter.cc
@@ -388,7 +388,8 @@ std::string LocalPartitionWriter::nextSpilledFileDir() {
 arrow::Result<std::shared_ptr<arrow::io::OutputStream>> LocalPartitionWriter::openFile(const std::string& file) {
   std::shared_ptr<arrow::io::FileOutputStream> fout;
   auto fd = open(file.c_str(), O_WRONLY | O_CREAT | O_TRUNC);
-  // Set the shuffle file permissions to 0644 to keep it consistent with the permissions of the built-in shuffler manager in Spark.
+  // Set the shuffle file permissions to 0644 to keep it consistent with the permissions of
+  // the built-in shuffler manager in Spark.
   fchmod(fd, 0644);
   ARROW_ASSIGN_OR_RAISE(fout, arrow::io::FileOutputStream::Open(fd));
   if (options_.bufferedWrite) {
