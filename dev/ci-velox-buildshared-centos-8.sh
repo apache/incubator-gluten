c70c3d157cfe8ed5339c39fcd0e1efaeb2e093b7
Yuan <yuanzhou@apache.org>
2025-04-03 10:44:29 +0100

    build arrow cpp
    Signed-off-by: Yuan <yuanzhou@apache.org>

--- dev/ci-velox-buildshared-centos-8.sh
+++ dev/ci-velox-buildshared-centos-8.sh
@@ -18,5 +18,5 @@
 set -e
 
 source /opt/rh/gcc-toolset-11/enable
-./dev/builddeps-veloxbe.sh --run_setup_script=OFF --build_arrow=OFF --build_tests=ON \
+./dev/builddeps-veloxbe.sh --run_setup_script=OFF --build_arrow=ON --build_tests=ON \
     --build_examples=ON --build_benchmarks=ON
