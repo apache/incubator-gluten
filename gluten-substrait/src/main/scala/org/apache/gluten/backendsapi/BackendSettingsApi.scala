c0440701ad694aa8682862502ae876e78a3e9402
Ke Jia <ke.a.jia@intel.com>
2025-04-07 12:13:20 +0000

    bhj optimization to ensure the hash table built once per executor

--- gluten-substrait/src/main/scala/org/apache/gluten/backendsapi/BackendSettingsApi.scala
+++ gluten-substrait/src/main/scala/org/apache/gluten/backendsapi/BackendSettingsApi.scala
@@ -81,7 +81,7 @@ trait BackendSettingsApi {
     GlutenConfig.get.enableColumnarShuffle
   }
 
-  def enableJoinKeysRewrite(): Boolean = true
+  def enableHashTableBuildOncePerExecutor(): Boolean = true
 
   def supportHashBuildJoinTypeOnLeft: JoinType => Boolean = {
     case _: InnerLike | RightOuter | FullOuter => true
