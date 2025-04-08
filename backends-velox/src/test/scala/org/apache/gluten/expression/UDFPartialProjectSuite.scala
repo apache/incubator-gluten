6fff5a4b04a43be1b1fd9c86a4ec6554d408ade8
Chengcheng Jin <Chengcheng.jin@ibm.com>
2025-04-08 15:20:53 +0100

    update test

--- backends-velox/src/test/scala/org/apache/gluten/expression/UDFPartialProjectSuite.scala
+++ backends-velox/src/test/scala/org/apache/gluten/expression/UDFPartialProjectSuite.scala
@@ -85,7 +85,7 @@ abstract class UDFPartialProjectSuite extends WholeStageTransformerSuite {
 
   test("test subquery") {
     runQueryAndCompare(
-      "select plus_two(" +
+      "select plus_one(" +
         "(select plus_one(count(*)) from (values (1)) t0(inner_c))) as col " +
         "from (values (2),(3)) t1(outer_c)") {
       checkGlutenOperatorMatch[ColumnarPartialProjectExec]
