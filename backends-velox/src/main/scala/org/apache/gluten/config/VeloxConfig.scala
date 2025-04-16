8f6846537d3b67271300ad1edf21e5b39f5ba1c1
Ke Jia <ke.a.jia@intel.com>
2025-04-16 15:21:11 +0000

    Resolve comments

--- backends-velox/src/main/scala/org/apache/gluten/config/VeloxConfig.scala
+++ backends-velox/src/main/scala/org/apache/gluten/config/VeloxConfig.scala
@@ -565,7 +565,7 @@ object VeloxConfig {
     buildConf("spark.gluten.velox.buildHashTableOncePerExecutor.enabled")
       .internal()
       .doc(
-        "Experimental: When enabled, the hash table is " +
+        "When enabled, the hash table is " +
           "constructed once per executor. If not enabled, " +
           "the hash table is rebuilt for each task.")
       .booleanConf
