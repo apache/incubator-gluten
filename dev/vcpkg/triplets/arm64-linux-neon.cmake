e5528bb2c78db92a177f43441cb34e3358e42511
zhangzimo <zhangzimofighting@163.com>
2025-04-24 14:40:49 +0800

    [fixed]format arm64-linux-neon.cmake fixed[fixed]

--- dev/vcpkg/triplets/arm64-linux-neon.cmake
+++ dev/vcpkg/triplets/arm64-linux-neon.cmake
@@ -3,8 +3,8 @@ set(VCPKG_TARGET_ARCHITECTURE arm64)
 set(VCPKG_CRT_LINKAGE dynamic)
 set(VCPKG_LIBRARY_LINKAGE static)
 
-set(VCPKG_CMAKE_SYSTEM_NAME Linux) 
+set(VCPKG_CMAKE_SYSTEM_NAME Linux)
 
-set(VCPKG_C_FLAGS "-march=armv8-a+crc ")
-set(VCPKG_CXX_FLAGS "-march=armv8-a+crc -std=c++17 ")
+set(VCPKG_C_FLAGS "-march=armv8-a+crc")
+set(VCPKG_CXX_FLAGS "-march=armv8-a+crc -std=c++17")
 set(VCPKG_LINKER_FLAGS "-static-libstdc++ -static-libgcc")
