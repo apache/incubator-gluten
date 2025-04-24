27e91ef2901d8f0fbd72414fbba121dd5132ace3
zmdaodao <zhangzimofighting@163.com>
2025-04-25 06:27:59 +0800

    [GLUTEN-8981][VL] Add vcpkg triplet for building Gluten on arm (#8982)
    * [BUILD][VL]dd the arm64-linux-neon.cmake file to manually modify VCPKG_CXX_FLAGS on the arm platform. fixed Environment variable VCPKG_FORCE_SYSTEM_BINARIES[VL][BUILD]
    
    * [fixed]format arm64-linux-neon.cmake fixed[fixed]

--- dev/vcpkg/env.sh
+++ dev/vcpkg/env.sh
@@ -10,7 +10,7 @@ SCRIPT_ROOT="$(realpath "$(dirname "${BASH_SOURCE[0]}")")"
 
 export VCPKG_ROOT="$SCRIPT_ROOT/.vcpkg"
 export VCPKG="$SCRIPT_ROOT/.vcpkg/vcpkg"
-export VCPKG_TRIPLET=$([ "${CPU_TARGET:-}" = "aarch64" ] && echo "arm64-linux-release" || echo "x64-linux-avx")
+export VCPKG_TRIPLET=$([ "${CPU_TARGET:-}" = "aarch64" ] && echo "arm64-linux-neon" || echo "x64-linux-avx")
 export VCPKG_TRIPLET_INSTALL_DIR=${SCRIPT_ROOT}/vcpkg_installed/${VCPKG_TRIPLET}
 
 ${SCRIPT_ROOT}/init.sh "$@"
