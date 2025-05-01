48ef09e87f5ac5734b08816bfc496f7cc8cfc957
Yuan Zhou <yuan.zhou@ibm.com>
2025-05-01 17:18:38 +0100

    build arrow = on
    Signed-off-by: Yuan Zhou <yuan.zhou@ibm.com>

--- dev/ci-velox-buildstatic-centos-7.sh
+++ dev/ci-velox-buildstatic-centos-7.sh
@@ -19,5 +19,5 @@ set -e
 
 source /opt/rh/devtoolset-11/enable
 export NUM_THREADS=4
-./dev/builddeps-veloxbe.sh --enable_vcpkg=ON --build_arrow=OFF --build_tests=OFF --build_benchmarks=OFF \
+./dev/builddeps-veloxbe.sh --enable_vcpkg=ON --build_arrow=ON --build_tests=OFF --build_benchmarks=OFF \
                            --build_examples=OFF --enable_s3=ON --enable_gcs=ON --enable_hdfs=ON --enable_abfs=ON
