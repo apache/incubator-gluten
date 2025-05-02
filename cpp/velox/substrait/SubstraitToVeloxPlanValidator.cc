e497d6236ac0edd8c56151e48c91890f008de49c
arnavb <arnavb@uber.com>
2025-05-02 22:37:58 +0000

    update

--- cpp/velox/substrait/SubstraitToVeloxPlanValidator.cc
+++ cpp/velox/substrait/SubstraitToVeloxPlanValidator.cc
@@ -245,9 +245,8 @@ bool isSupportedArrayCast(const TypePtr& fromType, const TypePtr& toType) {
     return kAllowedArrayElementKinds.count(fromType->kind()) > 0;
   }
 
-  if (toType->kind() == TypeKind::DOUBLE) {
-    if (fromType->kind() == TypeKind::INTEGER || fromType->kind() == TypeKind::BIGINT ||
-        fromType->kind() == TypeKind::SMALLINT || fromType->kind() == TypeKind::TINYINT) {
+  if (toType->isDouble()) {
+    if (fromType->isInteger() || fromType->isBigint() || fromType->isSmallint() || fromType->isTinyint()) {
       return true;
     }
   }
