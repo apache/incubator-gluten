106c85310b62cc2e1c621e24f319d8b9c8327333
zhli1142015 <zhli@microsoft.com>
2025-05-09 22:56:56 +0800

    [Core] Support Jvm memory shrinking for DynamicOffHeapSizingMemoryTarget

--- gluten-core/src/main/scala/org/apache/gluten/GlutenPlugin.scala
+++ gluten-core/src/main/scala/org/apache/gluten/GlutenPlugin.scala
@@ -110,6 +110,19 @@ private object GlutenDriverPlugin extends Logging {
 
     // Get the off-heap size set by user.
     val offHeapSize = conf.getSizeAsBytes(GlutenCoreConfig.SPARK_OFFHEAP_SIZE_KEY)
+    val offHeapSize = if (conf.getBoolean(GlutenConfig.DYNAMIC_OFFHEAP_SIZING_ENABLED.key, false)) {
+      val onHeapSize: Long =
+        if (conf.contains(GlutenConfig.SPARK_ONHEAP_SIZE_KEY)) {
+          conf.getSizeAsBytes(GlutenConfig.SPARK_ONHEAP_SIZE_KEY)
+        } else {
+          // 1GB default
+          1024 * 1024 * 1024
+        }
+      ((onHeapSize - (300 * 1024 * 1024)) *
+        conf.getDouble(GlutenConfig.DYNAMIC_OFFHEAP_SIZING_MEMORY_FRACTION.key, 0.6d)).toLong
+    } else {
+      conf.getSizeAsBytes(GlutenCoreConfig.SPARK_OFFHEAP_SIZE_KEY)
+    }
 
     // Set off-heap size in bytes.
     conf.set(GlutenCoreConfig.COLUMNAR_OFFHEAP_SIZE_IN_BYTES, offHeapSize)
