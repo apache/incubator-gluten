a3ef3bc215ae88ca6cb52f8a66bb4027aab6d993
Jin Chengcheng <chengcheng.jin@intel.com>
2025-05-09 14:50:29 +0100

    [VL] Minor: Print Velox plan before execution (#9498)

--- cpp/velox/compute/VeloxRuntime.cc
+++ cpp/velox/compute/VeloxRuntime.cc
@@ -159,6 +159,9 @@ std::shared_ptr<ResultIterator> VeloxRuntime::createResultIterator(
   VeloxPlanConverter veloxPlanConverter(
       inputs, memoryManager()->getLeafMemoryPool().get(), sessionConf, *localWriteFilesTempPath());
   veloxPlan_ = veloxPlanConverter.toVeloxPlan(substraitPlan_, std::move(localFiles_));
+  LOG_IF(INFO, debugModeEnabled_ && taskInfo_.has_value())
+      << "############### Velox plan for task " << taskInfo_.value() << " ###############" << std::endl
+      << veloxPlan_->toString(true, true);
 
   // Scan node can be required.
   std::vector<std::shared_ptr<SplitInfo>> scanInfos;
