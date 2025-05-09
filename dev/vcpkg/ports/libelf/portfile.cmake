e9f65148a37bfc155b4b0c7bed60ce287cf0e37b
Arnav Balyan <60175178+ArnavBalyan@users.noreply.github.com>
2025-05-09 06:38:57 +0530

    update (#9550)
    Move config.guess to gitweb since it's more reliable wdyt.

--- dev/vcpkg/ports/libelf/portfile.cmake
+++ dev/vcpkg/ports/libelf/portfile.cmake
@@ -11,8 +11,13 @@ vcpkg_extract_source_archive(
 )
 
 # Update config.guess and config.sub
-file(DOWNLOAD "https://git.savannah.gnu.org/cgit/config.git/plain/config.guess" "${SOURCE_PATH}/config.guess")
-file(DOWNLOAD "https://git.savannah.gnu.org/cgit/config.git/plain/config.sub" "${SOURCE_PATH}/config.sub")
+file(DOWNLOAD "https://git.savannah.gnu.org/gitweb/?p=config.git;a=blob_plain;f=config.guess;hb=HEAD"
+     "${SOURCE_PATH}/config.guess"
+     STATUS status_guess)
+
+file(DOWNLOAD "https://git.savannah.gnu.org/gitweb/?p=config.git;a=blob_plain;f=config.sub;hb=HEAD"
+     "${SOURCE_PATH}/config.sub"
+     STATUS status_sub)
 
 vcpkg_configure_make(SOURCE_PATH ${SOURCE_PATH} AUTOCONFIG)
 vcpkg_install_make()
