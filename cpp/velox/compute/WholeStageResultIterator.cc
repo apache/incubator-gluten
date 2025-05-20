1f6154b11bec2a28232fe4ad15633fdf5ce7a3e8
Hongze Zhang <hongze.zzz123@gmail.com>
2025-05-20 22:04:34 +0100

    [VL] Increase the spill start partition bit to 48 to avoid overlapping with the hash bits (#9703)
    Velox now set this number to 48 by default. The patch does the same update on Gluten's code.
    
    It's known that the check VELOX_CHECK_LT(sizeBits_ - 1, spillInputStartPartitionBit); in Velox code could be failed if this option is set with a small number.

--- cpp/velox/compute/WholeStageResultIterator.cc
+++ cpp/velox/compute/WholeStageResultIterator.cc
@@ -526,7 +526,7 @@ std::unordered_map<std::string, std::string> WholeStageResultIterator::getQueryC
     configs[velox::core::QueryConfig::kSpillReadBufferSize] =
         std::to_string(veloxCfg_->get<int32_t>(kSpillReadBufferSize, 1L * 1024 * 1024));
     configs[velox::core::QueryConfig::kSpillStartPartitionBit] =
-        std::to_string(veloxCfg_->get<uint8_t>(kSpillStartPartitionBit, 29));
+        std::to_string(veloxCfg_->get<uint8_t>(kSpillStartPartitionBit, 48));
     configs[velox::core::QueryConfig::kSpillNumPartitionBits] =
         std::to_string(veloxCfg_->get<uint8_t>(kSpillPartitionBits, 3));
     configs[velox::core::QueryConfig::kSpillableReservationGrowthPct] =
