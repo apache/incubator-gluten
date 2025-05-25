5a8508f6afd52dcfc902a7d27f411911264b1c40
binwei <binwei.y@gmail.com>
2025-05-24 17:48:45 -0700

    fix

--- cpp/velox/operators/serializer/VeloxRowToColumnarConverter.cc
+++ cpp/velox/operators/serializer/VeloxRowToColumnarConverter.cc
@@ -19,6 +19,7 @@
 #include "memory/VeloxColumnarBatch.h"
 #include "velox/vector/FlatVector.h"
 #include "velox/vector/arrow/Bridge.h"
+#include "velox/row/UnsafeRowFast.h"
 
 using namespace facebook::velox;
 namespace gluten {
@@ -276,7 +277,7 @@ VeloxRowToColumnarConverter::convert(int64_t numRows, int64_t* rowLength, uint8_
     data.emplace_back(std::string_view(reinterpret_cast<const char*>(memoryAddress + offset), rowLength[i]));
     offset += rowLength[i];
   }
-  auto vp = row::UnsafeRowDeserializer::deserialize(data, rowType_, pool_.get());
+  auto vp = row::UnsafeRowFast::deserialize(data, rowType_, pool_.get());
   return std::make_shared<VeloxColumnarBatch>(std::dynamic_pointer_cast<RowVector>(vp));
 }
 
