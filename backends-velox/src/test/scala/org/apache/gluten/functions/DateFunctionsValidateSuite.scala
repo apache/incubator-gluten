00481b0eda58660ea66c2f6fd2dd11dfc6b2535a
zml1206 <zhuml1206@gmail.com>
2025-05-30 13:57:14 +0800

    [VL] Add timestampdiff support

--- backends-velox/src/test/scala/org/apache/gluten/functions/DateFunctionsValidateSuite.scala
+++ backends-velox/src/test/scala/org/apache/gluten/functions/DateFunctionsValidateSuite.scala
@@ -278,6 +278,20 @@ abstract class DateFunctionsValidateSuite extends FunctionsValidateSuite {
     }
   }
 
+  testWithMinSparkVersion("timestampdiff", "3.3") {
+    withTempPath {
+      path =>
+        val t1 = Timestamp.valueOf("2020-03-01 00:00:00.500")
+        val t2 = Timestamp.valueOf("2020-02-29 00:00:00.500")
+        Seq((t1, t2)).toDF("t1", "t2").write.parquet(path.getCanonicalPath)
+
+        spark.read.parquet(path.getCanonicalPath).createOrReplaceTempView("time")
+        runQueryAndCompare("select timestampdiff(SECOND, t1, t2) from time") {
+          checkGlutenOperatorMatch[ProjectExecTransformer]
+        }
+    }
+  }
+
   test("to_date") {
     withTempPath {
       path =>
