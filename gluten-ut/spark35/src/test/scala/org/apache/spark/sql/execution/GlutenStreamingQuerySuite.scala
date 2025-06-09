6acfc63f8abee1b824ee2f186e297555812c58ab
Boxuan Li <liboxuan@connect.hku.hk>
2025-06-09 10:18:04 -0700

    Fix wrongplace to put test file

--- gluten-ut/spark35/src/test/scala/org/apache/spark/sql/execution/GlutenStreamingQuerySuite.scala
+++ gluten-ut/spark35/src/test/scala/org/apache/spark/sql/execution/GlutenStreamingQuerySuite.scala
@@ -17,7 +17,6 @@
 package org.apache.spark.sql.execution
 
 import org.apache.gluten.config.GlutenConfig
-
 import org.apache.spark.SparkConf
 import org.apache.spark.sql.GlutenSQLTestsTrait
 import org.apache.spark.sql.streaming._
