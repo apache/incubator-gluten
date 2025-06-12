bf5e3f3ed6e1372aaf454684139bcc007fd9ba26
Yuan <yuanzhou@apache.org>
2025-06-12 04:04:51 +0100

    fix build arrow
    Signed-off-by: Yuan <yuanzhou@apache.org>

--- dev/package.sh
+++ dev/package.sh
@@ -31,7 +31,8 @@ fi
 
 # build gluten with velox backend, prompt always respond y
 export PROMPT_ALWAYS_RESPOND=y
-./dev/buildbundle-veloxbe.sh --enable_vcpkg=OFF --build_tests=ON --build_benchmarks=ON --enable_s3=ON --enable_hdfs=ON build_arrow "$@"
+./dev/build_arrow.sh
+./dev/buildbundle-veloxbe.sh --enable_vcpkg=OFF --build_tests=ON --build_benchmarks=ON --enable_s3=ON --enable_hdfs=ON "$@"
 
 # make thirdparty package
 ./dev/build-thirdparty.sh
