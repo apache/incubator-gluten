da8045a64692fb39287b91cfb176c6a4fd2e36af
Yuan <yuanzhou@apache.org>
2025-06-12 05:48:26 +0100

    fix
    Signed-off-by: Yuan <yuanzhou@apache.org>

--- dev/package.sh
+++ dev/package.sh
@@ -27,7 +27,6 @@ cd "$GLUTEN_DIR"
 
 # build gluten with velox backend, prompt always respond y
 export PROMPT_ALWAYS_RESPOND=y
-./dev/build_arrow.sh
 ./dev/buildbundle-veloxbe.sh --enable_vcpkg=OFF --build_tests=ON --build_benchmarks=ON --enable_s3=ON --enable_hdfs=ON "$@"
 
 # make thirdparty package
