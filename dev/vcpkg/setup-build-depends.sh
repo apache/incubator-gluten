bd579f825614a6c4871e20098c8e659acf12cdee
Mingliang Zhu <zhuml1206@gmail.com>
2025-06-18 14:08:26 +0800

    [VL] Fix wget automake and gcc (#9992)

--- dev/vcpkg/setup-build-depends.sh
+++ dev/vcpkg/setup-build-depends.sh
@@ -48,10 +48,9 @@ install_gcc11_from_source() {
             gcc_install_dir=/usr/local/${gcc_version}
             cd /tmp
             if [ ! -d $gcc_version ]; then
-                wget https://ftp.gnu.org/gnu/gcc/${gcc_version}/${gcc_version}.tar.gz
-                if [ $? -ne 0 ]; then
-                    wget https://www.mirrorservice.org/sites/ftp.gnu.org/gnu/gcc/${gcc_version}/${gcc_version}.tar.gz
-                fi
+                GCC_URL1=https://ftp.gnu.org/gnu/gcc/${gcc_version}/${gcc_version}.tar.gz
+                GCC_URL2=https://www.mirrorservice.org/sites/ftp.gnu.org/gnu/gcc/${gcc_version}/${gcc_version}.tar.gz
+                wget ${GCC_URL1} || wget ${GCC_URL2}
                 tar -xvf ${gcc_version}.tar.gz
             fi
             cd ${gcc_version}
@@ -121,10 +120,9 @@ install_centos_7() {
         mkdir -p /tmp/automake
         AUTOMAKE_URL1="https://ftp.gnu.org/gnu/automake/automake-1.16.5.tar.xz"
         AUTOMAKE_URL2="https://www.mirrorservice.org/sites/ftp.gnu.org/gnu/automake/automake-1.16.5.tar.xz"
-        wget -O - "$AUTOMAKE_URL1" | tar -x --xz -C /tmp/automake --strip-components=1
-        if [ $? -ne 0 ]; then
-            wget -O - "$AUTOMAKE_URL2" | tar -x --xz -C /tmp/automake --strip-components=1
-        fi
+        wget -O /tmp/automake.tar.xz "$AUTOMAKE_URL1" || wget -O /tmp/automake.tar.xz "$AUTOMAKE_URL2"
+        tar -xf /tmp/automake.tar.xz -C /tmp/automake --strip-components=1
+        rm -f /tmp/automake.tar.xz
         cd /tmp/automake
         ./configure
         make install -j
