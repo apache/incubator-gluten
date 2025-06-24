b9fd932ccdf86d584685036ccd7ecfdde5321d87
nimesh.k <nimesh.k@uber.com>
2025-06-24 16:35:00 +0000

    Revert "fixing existence join"
    This reverts commit bb7e454ccd13c8873bfa5d4c560365172021ff69.

--- gluten-substrait/src/main/scala/org/apache/gluten/utils/SubstraitUtil.scala
+++ gluten-substrait/src/main/scala/org/apache/gluten/utils/SubstraitUtil.scala
@@ -72,6 +72,9 @@ object SubstraitUtil {
       case _: InnerLike =>
         CrossRel.JoinType.JOIN_TYPE_INNER
       case LeftOuter =>
+        // since we always assume build right side in substrait,
+        // the left and right relations are exchanged and the
+        // join type is reverted.
         if (needSwitchChildren) {
           CrossRel.JoinType.JOIN_TYPE_RIGHT
         } else {
@@ -83,13 +86,13 @@ object SubstraitUtil {
         } else {
           CrossRel.JoinType.JOIN_TYPE_RIGHT
         }
-      case LeftSemi | ExistenceJoin(_) =>
+      case LeftSemi =>
         CrossRel.JoinType.JOIN_TYPE_LEFT_SEMI
       case FullOuter =>
         CrossRel.JoinType.JOIN_TYPE_OUTER
       case _ =>
         CrossRel.JoinType.UNRECOGNIZED
-  }
+    }
 
   def createEnhancement(output: Seq[Attribute]): com.google.protobuf.Any = {
     val inputTypeNodes = output.map {
