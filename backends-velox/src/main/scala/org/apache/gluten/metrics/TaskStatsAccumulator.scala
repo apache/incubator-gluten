56333699d0b21aded6d6966bb4c7880ea7dc15f8
Rong Ma <rongma1997@gmail.com>
2025-07-03 22:13:17 +0100

    write stats to event log

---
+++ backends-velox/src/main/scala/org/apache/gluten/metrics/TaskStatsAccumulator.scala
@@ -0,0 +1,64 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.gluten.metrics
+
+import org.apache.gluten.metrics.TaskStatsAccumulator.Impl
+
+import org.apache.spark.sql.execution.SparkPlan
+import org.apache.spark.util.AccumulatorV2
+
+case class TaskStatsAccumulator(child: SparkPlan) {
+  val accumulator: Impl = new Impl
+  child.session.sparkContext.register(accumulator, "velox task stats")
+}
+
+object TaskStatsAccumulator {
+  class Impl extends AccumulatorV2[String, String] {
+    private var stats: String = ""
+
+    override def isZero: Boolean = stats.isEmpty
+
+    override def copy(): AccumulatorV2[String, String] = {
+      val newAcc = new Impl()
+      newAcc.stats = this.stats
+      newAcc
+    }
+
+    override def reset(): Unit = {
+      stats = ""
+    }
+
+    override def add(v: String): Unit = {
+      if (stats.isEmpty) {
+        stats = v
+      } else {
+        stats += "\n" + v
+      }
+    }
+
+    override def merge(other: AccumulatorV2[String, String]): Unit = {
+      other match {
+        case o: Impl =>
+          add(o.value)
+        case _ =>
+          throw new IllegalArgumentException("Cannot merge with non-VeloxTaskStatsAccumulator")
+      }
+    }
+
+    override def value: String = stats
+  }
+}
