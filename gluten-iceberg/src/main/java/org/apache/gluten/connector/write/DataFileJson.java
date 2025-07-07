2ff6c467b9eab7319f2bcd94d676c33bc514246c
Chengcheng Jin <Chengcheng.jin@ibm.com>
2025-07-07 21:10:12 +0800

    Fix accept partitionDataJson

--- gluten-iceberg/src/main/java/org/apache/gluten/connector/write/DataFileJson.java
+++ gluten-iceberg/src/main/java/org/apache/gluten/connector/write/DataFileJson.java
@@ -18,22 +18,27 @@ package org.apache.gluten.connector.write;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import java.util.List;
+
 public class DataFileJson {
   @JsonProperty public String path;
 
   @JsonProperty public MetricsJson metrics;
 
-  @JsonProperty int partitionSpecId;
-
   @JsonProperty String content;
 
   @JsonProperty String referencedDataFile;
 
+  // Wait to return
   @JsonProperty String partitionDataJson;
 
   @JsonProperty public long fileSizeInBytes = -1L;
 
   public static class MetricsJson {
     @JsonProperty public long recordCount = -1L;
+  }
+
+  public static class PartitionValueJson {
+    @JsonProperty List<String> partitionValues;
   }
 }
