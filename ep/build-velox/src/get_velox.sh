4511461b43580ba9922381f8b109f0ff7cd927ab
glutenperfbot <glutenperfbot@glutenproject-internal.com>
2025-07-09 01:50:25 +0000

    [GLUTEN-6887][VL] Daily Update Velox Version (2025_07_09)
    Upstream Velox's New Commits:
    d5de1c442 by zml1206, fix: Fmt V11 build errors (#14040)
    99c052eaf by Masha Basmanova, fix: ConstantTypedExpr::localHash/equals/toString for Opaque (#14048)
    cb2d292b5 by Kevin Wilfong, test: Fix MockSharedArbitrationTest.freeUnusedCapacityWhenReclaimMemoryPool (#14036)
    50eef113a by Bikramjeet Vig, misc: Add ability to track stats on non-special form expressions (#14037)
    8dea99db0 by Pedro Eugenio Rocha Pedreira, fix(vector): EnsureWritable should copy buffer views (#14038)
    4e2d702b3 by Oliver Xu, feat: Add bits::negateBit function (#14039)
    d8e1d24e6 by Masha Basmanova, refactor: Decouple SQL parsing from type resolution (#14028)
    5287673f8 by Yi Cheng Lee, refactor: Use SerDeOptions in TextWriter (#14034)
    0845e9f06 by Masha Basmanova, refactor: Modernize code in velox/parse (#14027)
    ff827b7b3 by Eric Jia, feat: Add enableEstimateFlatSize into QueryConfig to control the estimate stats collection (#13944)
    dd864ddaa by Rui Mo, docs: Update Spark function documents (#14002)
    92722499c by Masha Basmanova, fix: ConstantTypedExpr::toString/equals/hash (#14006)
    42475ee8f by Jacob Wujciak-Jens, build: Add .dockerignore file and fix misc issues (#14016)
    
    Signed-off-by: glutenperfbot <glutenperfbot@glutenproject-internal.com>

--- ep/build-velox/src/get_velox.sh
+++ ep/build-velox/src/get_velox.sh
@@ -17,7 +17,7 @@
 set -exu
 
 VELOX_REPO=https://github.com/oap-project/velox.git
-VELOX_BRANCH=2025_07_08
+VELOX_BRANCH=2025_07_09
 VELOX_HOME=""
 RUN_SETUP_SCRIPT=ON
 VELOX_ENHANCED_REPO=https://github.com/oap-project/velox.git
