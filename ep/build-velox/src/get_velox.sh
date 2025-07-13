260dba25e3be0563a552826a5ed294f865dfebf3
Linsong Wang <linsong.wang@ibm.com>
2025-07-13 05:36:03 +0000

    [GLUTEN-6887][VL] Daily Update Velox Version (2025_07_13)
    Upstream Velox's New Commits:
    
    Signed-off-by: Linsong Wang <linsong.wang@ibm.com>

--- ep/build-velox/src/get_velox.sh
+++ ep/build-velox/src/get_velox.sh
@@ -17,11 +17,11 @@
 set -exu
 
 VELOX_REPO=https://github.com/oap-project/velox.git
-VELOX_BRANCH=2025_07_10
+VELOX_BRANCH=2025_07_13
 VELOX_HOME=""
 RUN_SETUP_SCRIPT=ON
 VELOX_ENHANCED_REPO=https://github.com/IBM/velox.git
-VELOX_ENHANCED_BRANCH=main
+VELOX_ENHANCED_BRANCH=ibm-2025_07_13
 ENABLE_ENHANCED_FEATURES=OFF
 
 # Developer use only for testing Velox PR.
