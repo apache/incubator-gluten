ce3b2ba1901a70413ac26ff6e0e714675fb93e78
glutenperfbot <glutenperfbot@glutenproject-internal.com>
2025-07-23 16:06:01 +0000

    [GLUTEN-6887][VL] Daily Update Velox Version (2025_07_23)
    Upstream Velox's New Commits:
    ff41c351f by Kevin Wilfong, fix: Moving a local variable in return statements in BingTileFunctions.h is causing builds to fail (#14190)
    8561e8181 by Henry Edwin Dikeman, velox(feat): unblock Tpch for native query execution (#14187)
    4a257d80a by generatedunixname1030151775592158, ThriftRemoveRefSuffix_velox_functions_remote [B] (#14032)
    fd66afc8c by Xiaoxuan Meng, feat: Extend simple evaluator to support multiple expression executions for fallback projection in connector (#14178)
    20e9a67a6 by Jacob Khaliqi, test(geos): Add StGeometryFromText error tests (#13568)
    5b3f205a7 by Xiao Du, build(docker): Bump Presto Java version (#14176)
    2d8a20aa4 by Rui Mo, fix: Spark covar_samp aggregate function result (#13990)
    ee7f1372f by Oliver Xu, feat: Implement noisy_approx_sfm function (#14117)
    68368d876 by Patrick Sullivan, fix: Make BingTile type non-orderable (#14100)
    797e38911 by Patrick Sullivan, fix: Use int32_t for bing tile udfs (#14097)
    e99075271 by Xiaoxuan Meng, fix: Handle partial success result and throw the first failed request with rodos pushdown (#14157)
    432affda8 by Krishna Pai, fix(builds): Biased aggregator script (#14167)
    
    Signed-off-by: glutenperfbot <glutenperfbot@glutenproject-internal.com>

--- ep/build-velox/src/get_velox.sh
+++ ep/build-velox/src/get_velox.sh
@@ -17,11 +17,11 @@
 set -exu
 
 VELOX_REPO=https://github.com/oap-project/velox.git
-VELOX_BRANCH=2025_07_22
+VELOX_BRANCH=2025_07_23
 VELOX_HOME=""
 RUN_SETUP_SCRIPT=ON
 VELOX_ENHANCED_REPO=https://github.com/IBM/velox.git
-VELOX_ENHANCED_BRANCH=ibm-2025_07_22
+VELOX_ENHANCED_BRANCH=ibm-2025_07_23
 ENABLE_ENHANCED_FEATURES=OFF
 
 # Developer use only for testing Velox PR.
