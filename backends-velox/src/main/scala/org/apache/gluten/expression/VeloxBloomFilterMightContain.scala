156881d7169cf10f1763a6276037120f789f5eaf
Hongze Zhang <hongze.zzz123@gmail.com>
2025-07-23 05:02:10 +0200

    fixup

--- backends-velox/src/main/scala/org/apache/gluten/expression/VeloxBloomFilterMightContain.scala
+++ backends-velox/src/main/scala/org/apache/gluten/expression/VeloxBloomFilterMightContain.scala
@@ -27,7 +27,9 @@ import org.apache.spark.sql.catalyst.expressions.codegen.Block.BlockHelper
 import org.apache.spark.sql.types.DataType
 
 import io.netty.util.internal.PlatformDependent
+import sun.nio.ch.DirectBuffer
 
+import java.io.{IOException, ObjectInputStream, ObjectOutputStream}
 import java.nio.ByteBuffer
 
 /**
@@ -39,6 +41,7 @@ case class VeloxBloomFilterMightContain(
     bloomFilterExpression: Expression,
     valueExpression: Expression)
   extends BinaryExpression {
+  import VeloxBloomFilterMightContain._
 
   private val delegate =
     SparkShimLoader.getSparkShims.newMightContain(bloomFilterExpression, valueExpression)
@@ -63,12 +66,15 @@ case class VeloxBloomFilterMightContain(
   private lazy val bloomFilterData: Array[Byte] =
     bloomFilterExpression.eval().asInstanceOf[Array[Byte]]
 
-  @transient private lazy val bloomFilterBuffer: ByteBuffer = {
+  @transient private lazy val bloomFilterBuffer: SerializableDirectByteBuffer = {
     if (bloomFilterData == null) {
       null
     } else {
-      val buffer = ByteBuffer.allocateDirect(bloomFilterData.length)
-      buffer.put(bloomFilterData)
+      new SerializableDirectByteBuffer(
+        ByteBuffer
+          .allocateDirect(bloomFilterData.length)
+          .put(bloomFilterData)
+          .rewind())
     }
   }
 
@@ -80,20 +86,19 @@ case class VeloxBloomFilterMightContain(
     if (value == null) {
       return null
     }
-    VeloxBloomFilter.mightContainLongOnSerializedBloom(bloomFilterBuffer, value.asInstanceOf[Long])
+    VeloxBloomFilter.mightContainLongOnSerializedBloom(
+      bloomFilterBuffer.get(),
+      value.asInstanceOf[Long])
   }
 
   override protected def doGenCode(ctx: CodegenContext, ev: ExprCode): ExprCode = {
     if (bloomFilterBuffer == null) {
       return ev.copy(isNull = TrueLiteral, value = JavaCode.defaultLiteral(dataType))
     }
-    ctx.addReferenceObj(
+    val bloomBuf = ctx.addReferenceObj(
       "bloomFilterBuffer",
       bloomFilterBuffer
     ) // This field keeps the direct buffer data alive.
-    val bfAddr = ctx.addReferenceObj(
-      "bloomFilterAddress",
-      PlatformDependent.directBufferAddress(bloomFilterBuffer))
     val valueEval = valueExpression.genCode(ctx)
     val code =
       code"""
@@ -102,8 +107,44 @@ case class VeloxBloomFilterMightContain(
       ${CodeGenerator.javaType(dataType)} ${ev.value} = ${CodeGenerator.defaultValue(dataType)};
       if (!${ev.isNull}) {
         ${ev.value} = ${classOf[VeloxBloomFilter].getName}.mightContainLongOnSerializedBloom(
-        (Long) $bfAddr, (Long)${valueEval.value});
+        (Long) ${classOf[PlatformDependent].getName}.directBufferAddress($bloomBuf.get()),
+        (Long)${valueEval.value});
       }"""
     ev.copy(code = code)
+  }
+}
+
+object VeloxBloomFilterMightContain {
+
+  /**
+   * A serializable container for a Java direct byte buffer.
+   *
+   * Note: Keep this public so generated code can access.
+   */
+  class SerializableDirectByteBuffer(buffer: ByteBuffer) extends Serializable {
+    require(buffer.isInstanceOf[DirectBuffer])
+    require(buffer.position() == 0)
+
+    @transient private var byteBuffer: ByteBuffer = buffer
+
+    def get(): ByteBuffer = {
+      byteBuffer
+    }
+
+    @throws[IOException]
+    private def writeObject(out: ObjectOutputStream): Unit = {
+      val data: Array[Byte] = new Array[Byte](byteBuffer.remaining)
+      byteBuffer.duplicate.get(data) // use duplicate to avoid affecting position
+      out.writeInt(data.length)
+      out.write(data)
+    }
+
+    @throws[IOException]
+    private def readObject(in: ObjectInputStream): Unit = {
+      val length: Int = in.readInt
+      val data: Array[Byte] = new Array[Byte](length)
+      in.readFully(data)
+      byteBuffer = ByteBuffer.allocateDirect(length).put(data)
+    }
   }
 }
