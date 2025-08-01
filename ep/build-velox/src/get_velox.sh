17b56c478cc5dafe7a751f2787ef3849594cbb78
Yuan <yuanzhou@apache.org>
2025-08-01 13:46:49 +0100

    [VL] enable cross arch build for ARM
    Signed-off-by: Yuan <yuanzhou@apache.org>

--- ep/build-velox/src/get_velox.sh
+++ ep/build-velox/src/get_velox.sh
@@ -16,8 +16,8 @@
 
 set -exu
 
-VELOX_REPO=https://github.com/oap-project/velox.git
-VELOX_BRANCH=2025_08_01
+VELOX_REPO=https://github.com/zhouyuan/velox.git
+VELOX_BRANCH=wip_arm_common_build_oap
 VELOX_HOME=""
 RUN_SETUP_SCRIPT=ON
 VELOX_ENHANCED_REPO=https://github.com/IBM/velox.git
