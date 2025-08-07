9e29e0ef54e2d2e1ad7c724924ca83e9fa5ea414
Ke Jia <ke.a.jia@intel.com>
2025-08-07 17:57:53 +0800

    test full outer join

--- ep/build-velox/src/get_velox.sh
+++ ep/build-velox/src/get_velox.sh
@@ -16,8 +16,8 @@
 
 set -exu
 
-VELOX_REPO=https://github.com/oap-project/velox.git
-VELOX_BRANCH=2025_08_06
+VELOX_REPO=https://github.com/JkSelf/velox.git
+VELOX_BRANCH=full-join-test
 VELOX_HOME=""
 RUN_SETUP_SCRIPT=ON
 VELOX_ENHANCED_REPO=https://github.com/IBM/velox.git
