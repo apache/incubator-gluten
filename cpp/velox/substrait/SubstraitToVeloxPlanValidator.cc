a023c46d4487c6b0bc03fc89bca8cff11f618de9
wecharyu <yuwq1996@gmail.com>
2025-08-08 17:48:01 +0000

    add unit tests

--- cpp/velox/substrait/SubstraitToVeloxPlanValidator.cc
+++ cpp/velox/substrait/SubstraitToVeloxPlanValidator.cc
@@ -59,7 +59,7 @@ const std::unordered_set<std::string> kRegexFunctions = {
     "rlike"};
 
 const std::unordered_set<std::string> kBlackList =
-    {"split_part", "sequence", "approx_percentile", "get_array_struct_fields", "map_from_arrays", "to_json"};
+    {"split_part", "sequence", "approx_percentile", "get_array_struct_fields", "map_from_arrays"};
 } // namespace
 
 bool SubstraitToVeloxPlanValidator::parseVeloxType(
