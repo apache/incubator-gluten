2a217a4e3958a6d72d7bad893c8a37f48cbe03a2
Chengcheng Jin <Chengcheng.jin@ibm.com>
2025-08-18 11:16:48 +0100

    address comment

--- gluten-iceberg/src/main/scala/org/apache/gluten/execution/IcebergAppendDataExec.scala
+++ gluten-iceberg/src/main/scala/org/apache/gluten/execution/IcebergAppendDataExec.scala
@@ -44,7 +44,7 @@ trait IcebergAppendDataExec extends ColumnarAppendDataExec {
       case FileFormat.ORC => config.getOrDefault(ORC_COMPRESSION, ORC_COMPRESSION_DEFAULT)
       case _ => throw new UnsupportedOperationException()
     }
-    if (codec.toLowerCase() == "uncompressed") {
+    if (codec.equalsIgnoreCase("uncompressed")) {
       "none"
     } else codec
   }
