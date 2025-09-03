2d86feeb3b340dbb9e42f1c59d969f977bede2fa
Ke Jia <ke.a.jia@intel.com>
2025-09-03 18:49:23 +0800

    smj test

--- ep/build-velox/src/get_velox.sh
+++ ep/build-velox/src/get_velox.sh
@@ -16,8 +16,8 @@
 
 set -exu
 
-VELOX_REPO=https://github.com/oap-project/velox.git
-VELOX_BRANCH=2025_09_02
+VELOX_REPO=https://github.com/JkSelf/velox.git
+VELOX_BRANCH=smj-test.0903
 VELOX_HOME=""
 RUN_SETUP_SCRIPT=ON
 VELOX_ENHANCED_REPO=https://github.com/IBM/velox.git
