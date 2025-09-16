71b3836797f351fcb156db1aa1d0c780828a545c
Hongze Zhang <hongze.zzz123@gmail.com>
2025-09-17 01:08:09 +0200

    [VL] Delta native read support with DVs for Delta 2.4 + Spark 3.4

---
+++ backends-velox/src-delta/main/java/org/apache/gluten/substrait/rel/DeltaLocalFilesNode.java
@@ -0,0 +1,71 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.gluten.substrait.rel;
+
+import com.google.protobuf.ByteString;
+import io.substrait.proto.ReadRel;
+
+import java.util.List;
+import java.util.Map;
+
+public class DeltaLocalFilesNode extends LocalFilesNode {
+  private final List<Boolean> ifContainedFlags;
+  private final List<byte[]> serializedDvBitmaps;
+
+  public DeltaLocalFilesNode(
+      Integer index,
+      List<String> paths,
+      List<Long> starts,
+      List<Long> lengths,
+      List<Long> fileSizes,
+      List<Long> modificationTimes,
+      List<Map<String, String>> partitionColumns,
+      List<Map<String, String>> metadataColumns,
+      ReadFileFormat fileFormat,
+      List<String> preferredLocations,
+      Map<String, String> properties,
+      List<Map<String, Object>> otherMetadataColumns,
+      List<Boolean> ifContainedFlags,
+      List<byte[]> serializedDvBitmaps) {
+    super(
+        index,
+        paths,
+        starts,
+        lengths,
+        fileSizes,
+        modificationTimes,
+        partitionColumns,
+        metadataColumns,
+        fileFormat,
+        preferredLocations,
+        properties,
+        otherMetadataColumns);
+    this.ifContainedFlags = ifContainedFlags;
+    this.serializedDvBitmaps = serializedDvBitmaps;
+  }
+
+  @Override
+  protected void processFileBuilder(ReadRel.LocalFiles.FileOrFiles.Builder fileBuilder, int index) {
+    final ReadRel.LocalFiles.FileOrFiles.DeltaReadOptions.Builder deltaOptionsBuilder =
+        ReadRel.LocalFiles.FileOrFiles.DeltaReadOptions.newBuilder();
+    deltaOptionsBuilder.setDvIfContained(ifContainedFlags.get(index));
+    deltaOptionsBuilder.setDvSerializedBitmap(ByteString.copyFrom(serializedDvBitmaps.get(index)));
+    final ReadRel.LocalFiles.FileOrFiles.DeltaReadOptions deltaOptions =
+        deltaOptionsBuilder.build();
+    fileBuilder.setDelta(deltaOptions);
+  }
+}
