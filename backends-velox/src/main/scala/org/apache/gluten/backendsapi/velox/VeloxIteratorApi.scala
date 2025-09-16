b3452a2a46dc847eeb66fcb1310f3d2488397196
Jiaan Geng <beliefer@163.com>
2025-09-16 09:15:12 +0800

    [MINOR] Refactor genFinalStageIterator to follow Scala style (#10709)

--- backends-velox/src/main/scala/org/apache/gluten/backendsapi/velox/VeloxIteratorApi.scala
+++ backends-velox/src/main/scala/org/apache/gluten/backendsapi/velox/VeloxIteratorApi.scala
@@ -289,9 +289,9 @@ class VeloxIteratorApi extends IteratorApi with Logging {
 
     val transKernel = NativePlanEvaluator.create(BackendsApiManager.getBackendName)
     val columnarNativeIterator =
-      new JArrayList[ColumnarBatchInIterator](inputIterators.map {
+      inputIterators.map {
         iter => new ColumnarBatchInIterator(BackendsApiManager.getBackendName, iter.asJava)
-      }.asJava)
+      }
     val spillDirPath = SparkDirectoryUtil
       .get()
       .namespace("gluten-spill")
@@ -302,7 +302,7 @@ class VeloxIteratorApi extends IteratorApi with Logging {
         rootNode.toProtobuf.toByteArray,
         // Final iterator does not contain scan split, so pass empty split info to native here.
         new Array[Array[Byte]](0),
-        columnarNativeIterator,
+        columnarNativeIterator.asJava,
         partitionIndex,
         BackendsApiManager.getSparkPlanExecApiInstance.rewriteSpillPath(spillDirPath),
         enableCudf
