c56748e68d8956dfda08faa608be2fef6150882f
Yuan <yuanzhou@apache.org>
2025-09-29 11:52:25 +0100

    adding install script for 4.0 spark
    Signed-off-by: Yuan <yuanzhou@apache.org>

--- .github/workflows/util/install_spark_resources.sh
+++ .github/workflows/util/install_spark_resources.sh
@@ -113,6 +113,11 @@ case "$1" in
     cd ${INSTALL_DIR} && \
     install_spark "3.5.5" "3" "2.13"
     ;;
+4.0)
+    # Spark-4.0, scala 2.12 // using 2.12 as a hack as 4.0 does not have 2.13 suffix
+    cd ${INSTALL_DIR} && \
+    install_spark "4.0.1" "4" "2.12"
+    ;;
 *)
     echo "Spark version is expected to be specified."
     exit 1
