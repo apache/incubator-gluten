603217f58389b7a2deb92bc377101d1d725f4a0c
glutenperfbot <glutenperfbot@glutenproject-internal.com>
2025-10-02 04:45:44 +0000

    [GLUTEN-6887][VL] Daily Update Velox Version (2025_10_02)
    Upstream Velox's New Commits:
    904378eb6 by Zhen Li, refactor: Use VELOX_USER_RETURN in Spark arithmetic functions (#14936)
    c060962d7 by mbkkt, feat: Add ignoreDuplicates flag to aggregate function metadata (#14976)
    27e95e95e by James Gill, feat(geo): Add optional geometry and radius fields to SpatialJoinNode (#14940)
    d0179ac98 by Jimmy Lu, fix(hive): Fix subfield filter extraction for AND/OR more than 2 arguments (#14979)
    6998a1461 by Natasha Sehgal, qdigest_agg - Update intermediate type to VARBINARY (#15012)
    242fecfc3 by MacVincent Agha-Oko, fix(TextReader): Fix C++ structured binding lambda capture issue in TextReader (#15004)
    
    Signed-off-by: glutenperfbot <glutenperfbot@glutenproject-internal.com>

--- ep/build-velox/src/get_velox.sh
+++ ep/build-velox/src/get_velox.sh
@@ -17,11 +17,11 @@
 set -exu
 
 VELOX_REPO=https://github.com/oap-project/velox.git
-VELOX_BRANCH=2025_10_01
+VELOX_BRANCH=2025_10_02
 VELOX_HOME=""
 RUN_SETUP_SCRIPT=ON
 VELOX_ENHANCED_REPO=https://github.com/IBM/velox.git
-VELOX_ENHANCED_BRANCH=ibm-2025_10_01
+VELOX_ENHANCED_BRANCH=ibm-2025_10_02
 ENABLE_ENHANCED_FEATURES=OFF
 
 # Developer use only for testing Velox PR.
