e10d306679fe32fe814f932dd2affbc23618d121
Ke Jia <ke.a.jia@intel.com>
2025-10-10 18:19:11 +0800

    test fbthrift

--- ep/build-velox/src/get_velox.sh
+++ ep/build-velox/src/get_velox.sh
@@ -17,8 +17,8 @@
 set -exu
 
 CURRENT_DIR=$(cd "$(dirname "$BASH_SOURCE")"; pwd)
-VELOX_REPO=https://github.com/oap-project/velox.git
-VELOX_BRANCH=2025_10_09
+VELOX_REPO=https://github.com/JkSelf/velox.git
+VELOX_BRANCH=fb-thrift
 VELOX_HOME=""
 RUN_SETUP_SCRIPT=ON
 VELOX_ENHANCED_REPO=https://github.com/IBM/velox.git
