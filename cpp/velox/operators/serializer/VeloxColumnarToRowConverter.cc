5d79530f9b75a73585b97307695c69c520a6daa7
Yuan <yuanzhou@apache.org>
2025-10-16 16:50:46 +0100

    [GLUTEN-10833][VL] Reduce memcpy in ColumnToRow (#10824)
    We always fill the buffer with 0 so it's no necessary to keep the old content when doing allocation in C2R
    
    Fixes #10833
    ---------
    
    Signed-off-by: Yuan <yuanzhou@apache.org>

--- cpp/velox/operators/serializer/VeloxColumnarToRowConverter.cc
+++ cpp/velox/operators/serializer/VeloxColumnarToRowConverter.cc
@@ -57,10 +57,8 @@ void VeloxColumnarToRowConverter::refreshStates(facebook::velox::RowVectorPtr ro
     numRows_ = endRow - startRow;
   }
 
-  if (nullptr == veloxBuffers_) {
+  if (nullptr == veloxBuffers_ || veloxBuffers_->capacity() < totalMemorySize) {
     veloxBuffers_ = velox::AlignedBuffer::allocate<uint8_t>(totalMemorySize, veloxPool_.get());
-  } else if (veloxBuffers_->capacity() < totalMemorySize) {
-    velox::AlignedBuffer::reallocate<uint8_t>(&veloxBuffers_, totalMemorySize);
   }
 
   bufferAddress_ = veloxBuffers_->asMutable<uint8_t>();
