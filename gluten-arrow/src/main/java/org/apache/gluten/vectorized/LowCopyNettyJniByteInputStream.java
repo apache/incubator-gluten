bc4d75904cc08f89a658bfa8de6f1025a34dac0b
wforget <643348094@qq.com>
2025-10-22 14:11:29 +0800

    Support shaded netty package for LowCopyNettyJniByteInputStream

--- gluten-arrow/src/main/java/org/apache/gluten/vectorized/LowCopyNettyJniByteInputStream.java
+++ gluten-arrow/src/main/java/org/apache/gluten/vectorized/LowCopyNettyJniByteInputStream.java
@@ -26,6 +26,8 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.lang.reflect.Field;
 import java.nio.ByteBuffer;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * This implementation is targeted to optimize against Spark's {@link
@@ -34,12 +36,15 @@ import java.nio.ByteBuffer;
  */
 public class LowCopyNettyJniByteInputStream implements JniByteInputStream {
 
-  private static final Field FIELD_ByteBufInputStream_buffer;
+  // Cache shaded netty ByteBufInputStream_buffer field, <className, field>
+  private static final Map<String, Field> BYTE_BUF_INPUT_STREAM_BUFFER_FIELD_MAP =
+      new ConcurrentHashMap<>();
 
   static {
     try {
-      FIELD_ByteBufInputStream_buffer = ByteBufInputStream.class.getDeclaredField("buffer");
-      FIELD_ByteBufInputStream_buffer.setAccessible(true);
+      Field bufferField = ByteBufInputStream.class.getDeclaredField("buffer");
+      bufferField.setAccessible(true);
+      BYTE_BUF_INPUT_STREAM_BUFFER_FIELD_MAP.put(ByteBufInputStream.class.getName(), bufferField);
     } catch (NoSuchFieldException e) {
       throw new RuntimeException(e);
     }
@@ -54,11 +59,7 @@ public class LowCopyNettyJniByteInputStream implements JniByteInputStream {
   public LowCopyNettyJniByteInputStream(InputStream in) {
     this.in = in; // to prevent underlying netty buffer from being collected by GC
     final InputStream unwrapped = JniByteInputStreams.unwrapSparkInputStream(in);
-    try {
-      this.byteBuf = (ByteBuf) FIELD_ByteBufInputStream_buffer.get(unwrapped);
-    } catch (IllegalAccessException e) {
-      throw new GlutenException(e);
-    }
+    this.byteBuf = getBuffer(unwrapped);
   }
 
   @Override
@@ -76,16 +77,44 @@ public class LowCopyNettyJniByteInputStream implements JniByteInputStream {
   }
 
   public static boolean isSupported(InputStream in) {
-    if (!(in instanceof ByteBufInputStream)) {
+    if (!isByteBufInputStream(in)) {
       return false;
     }
     ByteBufInputStream bbin = (ByteBufInputStream) in;
-    try {
-      final ByteBuf byteBuf = (ByteBuf) FIELD_ByteBufInputStream_buffer.get(bbin);
-      if (!byteBuf.isDirect()) {
-        return false;
-      }
+    final ByteBuf byteBuf = getBuffer(bbin);
+    return byteBuf.isDirect();
+  }
+
+  private static final String BYTE_BUF_INPUT_STREAM_CLASSNAME = ByteBufInputStream.class.getName();
+
+  private static boolean isByteBufInputStream(InputStream bbin) {
+    String className = bbin.getClass().getName();
+    if (BYTE_BUF_INPUT_STREAM_BUFFER_FIELD_MAP.containsKey(className)) {
       return true;
+    }
+    if (!className.endsWith(BYTE_BUF_INPUT_STREAM_CLASSNAME)) {
+      return false;
+    }
+
+    try {
+      Field field = bbin.getClass().getDeclaredField("buffer");
+      field.setAccessible(true);
+      BYTE_BUF_INPUT_STREAM_BUFFER_FIELD_MAP.put(className, field);
+      return true;
+    } catch (NoSuchFieldException e) {
+      return false;
+    }
+  }
+
+  private static ByteBuf getBuffer(Object bbin) {
+    String className = bbin.getClass().getName();
+    Field field = BYTE_BUF_INPUT_STREAM_BUFFER_FIELD_MAP.get(className);
+    if (field == null) {
+      throw new GlutenException(
+          "Field 'buffer' not cached for shaded netty ByteBufInputStream: " + className);
+    }
+    try {
+      return (ByteBuf) field.get(bbin);
     } catch (IllegalAccessException e) {
       throw new GlutenException(e);
     }
