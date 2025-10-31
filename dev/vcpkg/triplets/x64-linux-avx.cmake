11df097d0faa92dd5b092eed8c3099b2ae530f34
Yuan <yuanzhou@apache.org>
2025-10-31 10:33:19 +0000

    fix snappy
    Signed-off-by: Yuan <yuanzhou@apache.org>

--- dev/vcpkg/triplets/x64-linux-avx.cmake
+++ dev/vcpkg/triplets/x64-linux-avx.cmake
@@ -8,3 +8,7 @@ set(VCPKG_CMAKE_SYSTEM_NAME Linux)
 set(VCPKG_C_FLAGS "-mavx2 -mfma -mavx -mf16c -mlzcnt -mbmi2")
 set(VCPKG_CXX_FLAGS "-mavx2 -mfma -mavx -mf16c -mlzcnt -std=c++17 -mbmi2")
 set(VCPKG_LINKER_FLAGS "-static-libstdc++ -static-libgcc")
+
+if("${PORT}" STREQUAL "snappy")
+    list(APPEND VCPKG_CMAKE_CONFIGURE_OPTIONS -DSNAPPY_HAVE_BMI2=ON)
+endif()
