30560c51c350564b54fd0789c52215fcf3f357af
Hongze Zhang <hongze.zzz123@gmail.com>
2025-11-03 19:19:41 +0100

    [CORE] Exclude dependent components of incompatible components
    This is a follow-up PR for #10942. Related discussion: https://github.com/apache/incubator-gluten/pull/11006#discussion_r2486059910

--- gluten-core/src/test/scala/org/apache/gluten/component/ComponentSuite.scala
+++ gluten-core/src/test/scala/org/apache/gluten/component/ComponentSuite.scala
@@ -27,7 +27,7 @@ import scala.collection.mutable
 class ComponentSuite extends AnyFunSuite with BeforeAndAfterAll {
   import ComponentSuite._
 
-  test("Load order - sanity") {
+  test("Load order") {
     val a = new DummyBackend("A") {}
     val b = new DummyBackend("B") {}
     val c = new DummyComponent("C") {}
@@ -63,6 +63,61 @@ class ComponentSuite extends AnyFunSuite with BeforeAndAfterAll {
     }
   }
 
+  test("Incompatible component") {
+    val a = new DummyBackend("A") {}
+    val b = new DummyBackend("B") {}
+    val c = new DummyComponent("C") {}
+    val d = new DummyComponent("D") {}
+    val e = new DummyComponent("E") {}
+
+    c.dependsOn(a)
+    d.dependsOn(a, b)
+    e.dependsOn(a, d)
+
+    d.setIncompatible()
+
+    a.ensureRegistered()
+    b.ensureRegistered()
+    c.ensureRegistered()
+    d.ensureRegistered()
+    e.ensureRegistered()
+
+    val possibleOrders: Set[Seq[Component]] =
+      Set(
+        Seq(a, b, c),
+        Seq(b, a, c)
+      )
+
+    assert(possibleOrders.contains(Component.sorted().filter(Seq(a, b, c, d, e).contains(_))))
+  }
+
+  test("Incompatible backend") {
+    val a = new DummyBackend("A") {}
+    val b = new DummyBackend("B") {}
+    val c = new DummyComponent("C") {}
+    val d = new DummyComponent("D") {}
+    val e = new DummyComponent("E") {}
+
+    c.dependsOn(a)
+    d.dependsOn(a, b)
+    e.dependsOn(a, d)
+
+    b.setIncompatible()
+
+    a.ensureRegistered()
+    b.ensureRegistered()
+    c.ensureRegistered()
+    d.ensureRegistered()
+    e.ensureRegistered()
+
+    val possibleOrders: Set[Seq[Component]] =
+      Set(
+        Seq(a, c)
+      )
+
+    assert(possibleOrders.contains(Component.sorted().filter(Seq(a, b, c, d, e).contains(_))))
+  }
+
   test("Dependencies not registered") {
     val a = new DummyBackend("A") {}
     val c = new DummyComponent("C") {}
@@ -89,9 +144,21 @@ object ComponentSuite {
     }
   }
 
+  private trait CompatibilityHelper extends Component {
+    private var _isRuntimeCompatible: Boolean = true;
+
+    override def isRuntimeCompatible: Boolean = _isRuntimeCompatible
+
+    def setIncompatible(): Unit = {
+      _isRuntimeCompatible = false
+    }
+  }
+
   abstract private class DummyComponent(override val name: String)
     extends Component
-    with DependencyBuilder {
+    with DependencyBuilder
+    with CompatibilityHelper {
+
     override def buildInfo(): Component.BuildInfo =
       Component.BuildInfo(name, "N/A", "N/A", "N/A")
 
@@ -99,7 +166,10 @@ object ComponentSuite {
     override def injectRules(injector: Injector): Unit = {}
   }
 
-  abstract private class DummyBackend(override val name: String) extends Backend {
+  abstract private class DummyBackend(override val name: String)
+    extends Backend
+    with CompatibilityHelper {
+
     override def buildInfo(): Component.BuildInfo =
       Component.BuildInfo(name, "N/A", "N/A", "N/A")
 
