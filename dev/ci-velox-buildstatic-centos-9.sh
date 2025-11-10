10459d03b4231c2fede275bc38232736a188693c
Yuan <yuanzhou@apache.org>
2025-11-10 14:28:38 +0000

    [GLUTEN-10926][VL] Fix arm pipeline to use centos9 image (#11061)
    it seems vcpkg-centos-8 image is not correctly pushed after #10993
    This patch fixes it to use vcpkg-centos-9 image to make CI green
    
    ---------
    
    Signed-off-by: Yuan <yuanzhou@apache.org>

--- dev/ci-velox-buildstatic-centos-9.sh
+++ dev/ci-velox-buildstatic-centos-9.sh
@@ -23,5 +23,6 @@ if [ "$(uname -m)" = "aarch64" ]; then
     export VCPKG_FORCE_SYSTEM_BINARIES=1;
 fi
 
+export NUM_THREADS=4
 ./dev/builddeps-veloxbe.sh --enable_vcpkg=ON --build_arrow=OFF --build_tests=OFF --build_benchmarks=OFF \
                            --build_examples=OFF --enable_s3=ON --enable_gcs=ON --enable_hdfs=ON --enable_abfs=ON
