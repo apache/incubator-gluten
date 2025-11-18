cd25526a570ba3c9fbe301041cd593ce098ff8f0
binwei yang <binwei.y@gmail.com>
2025-11-17 18:31:40 -0800

    fix for enhanced features tmporarily

--- cpp/velox/jni/VeloxJniWrapper.cc
+++ cpp/velox/jni/VeloxJniWrapper.cc
@@ -560,7 +560,12 @@ Java_org_apache_gluten_datasource_VeloxDataSourceJniWrapper_splitBlockByPartitio
   const auto numRows = inputRowVector->size();
 
   connector::hive::PartitionIdGenerator idGen(
-      asRowType(inputRowVector->type()), partitionColIndicesVec, 128, pool.get());
+      asRowType(inputRowVector->type()), partitionColIndicesVec, 128, pool.get()
+#ifdef GLUTEN_ENABLE_ENHANCED_FEATURES
+      ,
+      true
+#endif    
+    );
   raw_vector<uint64_t> partitionIds{};
   idGen.run(inputRowVector, partitionIds);
   GLUTEN_CHECK(partitionIds.size() == numRows, "Mismatched number of partition ids");
