802f588c76ba8e1c1f8b0fec03c1463ad2f13540
Chang chen <changchen@microsoft.com>
2025-12-01 03:03:01 +0000

    [Scala 2.13][IntelliJ] Refactor to use Symbol for column references to fix compilation error in Scala 2.13 with IntelliJ compiler: symbol literal is deprecated; use Symbol("i")

--- gluten-ut/spark40/src/test/scala/org/apache/spark/sql/connector/GlutenDataSourceV2Suite.scala
+++ gluten-ut/spark40/src/test/scala/org/apache/spark/sql/connector/GlutenDataSourceV2Suite.scala
@@ -38,25 +38,25 @@ class GlutenDataSourceV2Suite extends DataSourceV2Suite with GlutenSQLTestsBaseT
             val df = spark.read.format(cls.getName).load()
             checkAnswer(df, Seq(Row(1, 4), Row(1, 4), Row(3, 6), Row(2, 6), Row(4, 2), Row(4, 2)))
 
-            val groupByColA = df.groupBy('i).agg(sum('j))
+            val groupByColA = df.groupBy(Symbol("i")).agg(sum(Symbol("j")))
             checkAnswer(groupByColA, Seq(Row(1, 8), Row(2, 6), Row(3, 6), Row(4, 4)))
             assert(collectFirst(groupByColA.queryExecution.executedPlan) {
               case e: ColumnarShuffleExchangeExec => e
             }.isEmpty)
 
-            val groupByColAB = df.groupBy('i, 'j).agg(count("*"))
+            val groupByColAB = df.groupBy(Symbol("i"), Symbol("j")).agg(count("*"))
             checkAnswer(groupByColAB, Seq(Row(1, 4, 2), Row(2, 6, 1), Row(3, 6, 1), Row(4, 2, 2)))
             assert(collectFirst(groupByColAB.queryExecution.executedPlan) {
               case e: ColumnarShuffleExchangeExec => e
             }.isEmpty)
 
-            val groupByColB = df.groupBy('j).agg(sum('i))
+            val groupByColB = df.groupBy(Symbol("j")).agg(sum(Symbol("i")))
             checkAnswer(groupByColB, Seq(Row(2, 8), Row(4, 2), Row(6, 5)))
             assert(collectFirst(groupByColB.queryExecution.executedPlan) {
               case e: ColumnarShuffleExchangeExec => e
             }.isDefined)
 
-            val groupByAPlusB = df.groupBy('i + 'j).agg(count("*"))
+            val groupByAPlusB = df.groupBy(Symbol("i") + Symbol("j")).agg(count("*"))
             checkAnswer(groupByAPlusB, Seq(Row(5, 2), Row(6, 2), Row(8, 1), Row(9, 1)))
             assert(collectFirst(groupByAPlusB.queryExecution.executedPlan) {
               case e: ColumnarShuffleExchangeExec => e
