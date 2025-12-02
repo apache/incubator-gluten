edc4709a375be977dfc6417c0fe7712edc89a60b
Chengcheng Jin <Chengcheng.jin@ibm.com>
2025-12-02 15:13:29 +0000

    fix code style

--- backends-velox/src-iceberg/main/java/org/apache/gluten/TestConfUtil.java
+++ backends-velox/src-iceberg/main/java/org/apache/gluten/TestConfUtil.java
@@ -33,6 +33,6 @@ public class TestConfUtil {
           "false",
           "spark.gluten.ui.enabled",
           "false",
-              "spark.sql.ansi.enabled",
-              "false");
+          "spark.sql.ansi.enabled",
+          "false");
 }
