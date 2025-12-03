9c258bc8377d00f33f96c5fb297728a5a18058da
Rong Ma <marong@apache.org>
2025-12-03 23:04:36 +0800

    fix

--- backends-velox/src/main/scala/org/apache/gluten/backendsapi/velox/VeloxRuleApi.scala
+++ backends-velox/src/main/scala/org/apache/gluten/backendsapi/velox/VeloxRuleApi.scala
@@ -151,6 +151,7 @@ object VeloxRuleApi {
       c => GlutenAutoAdjustStageResourceProfile(new GlutenConfig(c.sqlConf), c.session))
     injector.injectFinal(c => GlutenFallbackReporter(new GlutenConfig(c.sqlConf), c.session))
     injector.injectFinal(_ => RemoveFallbackTagRule())
+    injector.injectFinal(_ => RegenerateTransformStageId())
   }
 
   /**
