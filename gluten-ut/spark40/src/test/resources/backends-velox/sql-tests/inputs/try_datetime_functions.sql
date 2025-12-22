232b3ac300a91155743050e94bdebec3679c92a2
Yuan <yuanzhou@apache.org>
2025-12-22 18:26:18 +0800

    [GLUTEN-11088][VL] Adding SQLQueryTestSuite for Spark-4.0 (#11136)

--- gluten-ut/spark40/src/test/resources/backends-velox/sql-tests/inputs/try_datetime_functions.sql
+++ gluten-ut/spark40/src/test/resources/backends-velox/sql-tests/inputs/try_datetime_functions.sql
@@ -2,3 +2,5 @@ select try_to_timestamp(null), try_to_timestamp('2016-12-31 00:12:00'), try_to_t
 select try_to_timestamp(1);
 select try_to_timestamp('2016-12-31 abc');
 select try_to_timestamp('2019-10-06 10:11:12.', 'yyyy-MM-dd HH:mm:ss.SSSSSS[zzz]');
+select try_to_timestamp("02-29", "MM-dd");
+select try_to_timestamp('22 05 2020 Friday', 'dd MM yyyy EEEEEE');
