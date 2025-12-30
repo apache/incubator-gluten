3b32bda1ed5050108c56269874585d1215c52a75
Chang chen <changchen@apache.org>
2025-12-30 22:45:32 +0800

    [Fix] Refactor Spark version checks in VeloxHashJoinSuite to improve readability and maintainability

--- backends-velox/src/test/scala/org/apache/gluten/execution/VeloxHashJoinSuite.scala
+++ backends-velox/src/test/scala/org/apache/gluten/execution/VeloxHashJoinSuite.scala
@@ -94,7 +94,7 @@ class VeloxHashJoinSuite extends VeloxWholeStageTransformerSuite {
       val wholeStages = plan.collect { case wst: WholeStageTransformer => wst }
       if (isSparkVersionLE("3.2")) {
         assert(wholeStages.length == 1)
-      } else if ( isSparkVersionGE("3.5")) {
+      } else if (isSparkVersionGE("3.5")) {
         assert(wholeStages.length == 5)
       } else {
         assert(wholeStages.length == 3)
