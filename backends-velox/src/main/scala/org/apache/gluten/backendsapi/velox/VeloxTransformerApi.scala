652494dfacecba5be77901d370a7c7adcd860a77
jamxu <jamxu@shein.com>
2026-01-08 19:11:49 +0800

    Revert "update Reference"
    This reverts commit 68f184e4cd61307c1da82e141cfc84a6394cea55.

--- backends-velox/src/main/scala/org/apache/gluten/backendsapi/velox/VeloxTransformerApi.scala
+++ backends-velox/src/main/scala/org/apache/gluten/backendsapi/velox/VeloxTransformerApi.scala
@@ -71,7 +71,7 @@ class VeloxTransformerApi extends TransformerApi with Logging {
       nativeConfMap: JMap[String, String],
       backendPrefix: String): Unit = {
     // S3A configurations that require time units for Velox.
-    // Hadoop-aws versions before 3.3.6 do not include time units by default.
+    // Hadoop-aws versions before 3.4 do not include time units by default.
     // Reference: https://hadoop.apache.org/docs/r3.3.6/hadoop-aws/tools/hadoop-aws/#General_S3A_Client_configuration
     val s3aTimeConfigs = Map(
       "spark.hadoop.fs.s3a.connection.timeout" -> "ms",
