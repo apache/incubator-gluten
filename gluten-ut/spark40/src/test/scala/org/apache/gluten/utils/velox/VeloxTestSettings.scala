5c43de903da4ba2e0923774e3b27fab548fc6ed3
Hongze Zhang <hongze.zzz123@gmail.com>
2026-01-08 14:00:53 +0100

    [GLUTEN-11088][VL] Mark test case `SPARK-47289: extended explain info` as non-fixable
    The Spark 4.0 test case GlutenQueryExecutionSuite.SPARK-47289: extended explain info is not necessarily to be run in Gluten code because it aims to test against vanilla Spark's query plan. The PR corrects the code comment.

--- gluten-ut/spark40/src/test/scala/org/apache/gluten/utils/velox/VeloxTestSettings.scala
+++ gluten-ut/spark40/src/test/scala/org/apache/gluten/utils/velox/VeloxTestSettings.scala
@@ -947,7 +947,8 @@ class VeloxTestSettings extends BackendTestSettings {
     .exclude("Logging plan changes for execution")
     // Rewrite for transformed plan
     .exclude("dumping query execution info to a file - explainMode=formatted")
-    // TODO: fix in Spark-4.0
+    // The case doesn't need to be fixed in Gluten since it's verifying against
+    // vanilla Spark's query plan.
     .exclude("SPARK-47289: extended explain info")
 
   override def getSQLQueryTestSettings: SQLQueryTestSettings = VeloxSQLQueryTestSettings
