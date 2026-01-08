d2fd55138fdb56eecf48a7e015062d68ae88e738
jamxu <jamxu@shein.com>
2026-01-08 20:14:54 +0800

    fix codestyle

--- backends-velox/src/main/scala/org/apache/gluten/backendsapi/velox/VeloxTransformerApi.scala
+++ backends-velox/src/main/scala/org/apache/gluten/backendsapi/velox/VeloxTransformerApi.scala
@@ -72,7 +72,8 @@ class VeloxTransformerApi extends TransformerApi with Logging {
       backendPrefix: String): Unit = {
     // S3A configurations that require time units for Velox.
     // Hadoop-aws versions before 3.4 do not include time units by default.
-    // Reference: https://hadoop.apache.org/docs/r3.3.6/hadoop-aws/tools/hadoop-aws/#General_S3A_Client_configuration
+    // Reference:
+    // https://hadoop.apache.org/docs/r3.3.6/hadoop-aws/tools/hadoop-aws/#General_S3A_Client_configuration
     val s3aTimeConfigs = Map(
       "spark.hadoop.fs.s3a.connection.timeout" -> "ms",
       "spark.hadoop.fs.s3a.connection.establish.timeout" -> "ms",
