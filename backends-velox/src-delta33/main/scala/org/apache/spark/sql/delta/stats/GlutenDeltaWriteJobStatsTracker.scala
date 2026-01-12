8cf539ace9b7cf0ecffeddd44750df67d659f132
Hongze Zhang <hongze.zzz123@gmail.com>
2026-01-12 12:52:01 +0100

    [GLUTEN-10215][VL] Delta write: Native statistics tracker to avoid C2R overhead
    The patch adds a native job statistics tracker for Delta write to eliminate C2R overhead.
    
    More PR description WIP.

--- backends-velox/src-delta33/main/scala/org/apache/spark/sql/delta/stats/GlutenDeltaWriteJobStatsTracker.scala
+++ backends-velox/src-delta33/main/scala/org/apache/spark/sql/delta/stats/GlutenDeltaWriteJobStatsTracker.scala
@@ -16,20 +16,67 @@
  */
 package org.apache.spark.sql.delta.stats
 
-import org.apache.gluten.execution.{PlaceholderRow, TerminalRow, VeloxColumnarToRowExec}
+import org.apache.gluten.backendsapi.BackendsApiManager
+import org.apache.gluten.backendsapi.velox.VeloxBatchType
+import org.apache.gluten.columnarbatch.ColumnarBatches
+import org.apache.gluten.config.GlutenConfig
+import org.apache.gluten.execution._
+import org.apache.gluten.expression.{ConverterUtils, TransformerState}
+import org.apache.gluten.extension.columnar.heuristic.HeuristicTransform
+import org.apache.gluten.extension.columnar.offload.OffloadOthers
+import org.apache.gluten.extension.columnar.rewrite.PullOutPreProject
+import org.apache.gluten.extension.columnar.transition.Convention
+import org.apache.gluten.extension.columnar.validator.{Validator, Validators}
+import org.apache.gluten.iterator.Iterators
+import org.apache.gluten.substrait.SubstraitContext
+import org.apache.gluten.substrait.plan.PlanBuilder
+import org.apache.gluten.vectorized.{ColumnarBatchInIterator, NativePlanEvaluator}
 
+import org.apache.spark.TaskContext
+import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.catalyst.InternalRow
+import org.apache.spark.sql.catalyst.expressions.{Attribute, AttributeReference, Expression, Projection, UnsafeProjection}
+import org.apache.spark.sql.catalyst.expressions.aggregate.{AggregateExpression, Complete, DeclarativeAggregate}
 import org.apache.spark.sql.delta.DeltaIdentityColumnStatsTracker
+import org.apache.spark.sql.execution.{ColumnarCollapseTransformStages, LeafExecNode}
+import org.apache.spark.sql.execution.aggregate.HashAggregateExec
 import org.apache.spark.sql.execution.datasources.{WriteJobStatsTracker, WriteTaskStats, WriteTaskStatsTracker}
 import org.apache.spark.sql.execution.metric.SQLMetric
+import org.apache.spark.sql.types.StringType
+import org.apache.spark.sql.vectorized.ColumnarBatch
+import org.apache.spark.util.{SerializableConfiguration, SparkDirectoryUtil}
+
+import com.google.common.collect.Lists
+import org.apache.hadoop.conf.Configuration
+import org.apache.hadoop.fs.Path
+
+import java.util.UUID
+import java.util.concurrent.{SynchronousQueue, TimeUnit}
+
+import scala.collection.JavaConverters._
+import scala.collection.mutable
 
 class GlutenDeltaJobStatisticsTracker(val delegate: DeltaJobStatisticsTracker)
   extends WriteJobStatsTracker {
   import GlutenDeltaJobStatisticsTracker._
 
+  @transient private val hadoopConf: Configuration = {
+    val clazz = classOf[DeltaJobStatisticsTracker]
+    val method = clazz.getDeclaredField("hadoopConf")
+    method.setAccessible(true)
+    method.get(delegate).asInstanceOf[Configuration]
+  }
+  @transient private val path = delegate.path
+  private val dataCols = delegate.dataCols
+  private val statsColExpr = delegate.statsColExpr
+
+  private val srlHadoopConf = new SerializableConfiguration(hadoopConf)
+  private val rootUri = path.getFileSystem(hadoopConf).makeQualified(path).toUri
+
   override def newTaskInstance(): WriteTaskStatsTracker = {
-    new GlutenDeltaTaskStatisticsTracker(
-      delegate.newTaskInstance().asInstanceOf[DeltaTaskStatisticsTracker])
+    val rootPath = new Path(rootUri)
+    val hadoopConf = srlHadoopConf.value
+    new GlutenDeltaTaskStatisticsTracker(dataCols, statsColExpr, rootPath, hadoopConf)
   }
 
   override def processStats(stats: Seq[WriteTaskStats], jobCommitTime: Long): Unit = {
@@ -49,35 +96,211 @@ private object GlutenDeltaJobStatisticsTracker {
    *
    * TODO: Columnar-based statistics collection.
    */
-  private class GlutenDeltaTaskStatisticsTracker(delegate: DeltaTaskStatisticsTracker)
+  private class GlutenDeltaTaskStatisticsTracker(
+      dataCols: Seq[Attribute],
+      statsColExpr: Expression,
+      rootPath: Path,
+      hadoopConf: Configuration)
     extends WriteTaskStatsTracker {
+    private val accumulators = mutable.Map[String, VeloxStatisticsAccumulator]()
 
-    private val c2r = new VeloxColumnarToRowExec.Converter(new SQLMetric("convertTime"))
-
-    override def newPartition(partitionValues: InternalRow): Unit = {
-      delegate.newPartition(partitionValues)
-    }
+    override def newPartition(partitionValues: InternalRow): Unit = {}
 
     override def newFile(filePath: String): Unit = {
-      delegate.newFile(filePath)
+      accumulators.getOrElseUpdate(
+        filePath,
+        new VeloxStatisticsAccumulator(dataCols, statsColExpr)
+      )
     }
 
     override def closeFile(filePath: String): Unit = {
-      delegate.closeFile(filePath)
+      accumulators(filePath).setFinished
     }
 
     override def newRow(filePath: String, row: InternalRow): Unit = {
       row match {
         case _: PlaceholderRow =>
         case t: TerminalRow =>
-          c2r.toRowIterator(t.batch()).foreach(eachRow => delegate.newRow(filePath, eachRow))
-        case otherRow =>
-          delegate.newRow(filePath, otherRow)
+          accumulators(filePath).appendColumnarBatch(t.batch())
       }
     }
 
     override def getFinalStats(taskCommitTime: Long): WriteTaskStats = {
-      delegate.getFinalStats(taskCommitTime)
+      val stats = accumulators.map {
+        case (path, acc) =>
+          new Path(path).getName -> acc.toJson
+      }.toMap
+      DeltaFileStatistics(stats)
     }
+  }
+
+  private class VeloxStatisticsAccumulator(dataCols: Seq[Attribute], statsColExpr: Expression) {
+    private val c2r = new VeloxColumnarToRowExec.Converter(new SQLMetric("convertTime"))
+    private var resultRequested: Boolean = false
+    private val inputBatchQueue = new SynchronousQueue[ColumnarBatch]()
+    private val outputBatchQueue = new SynchronousQueue[ColumnarBatch]()
+    private val aggregates: Seq[AggregateExpression] = statsColExpr.collect {
+      case ae: AggregateExpression if ae.aggregateFunction.isInstanceOf[DeclarativeAggregate] =>
+        assert(ae.mode == Complete)
+        ae
+    }
+    private val resultExpr: Expression = statsColExpr.transform {
+      case ae: AggregateExpression if ae.aggregateFunction.isInstanceOf[DeclarativeAggregate] =>
+        ae.aggregateFunction.asInstanceOf[DeclarativeAggregate].evaluateExpression
+    }
+    protected val aggBufferAttrs: Seq[Attribute] = statsColExpr.flatMap {
+      case ae: AggregateExpression if ae.aggregateFunction.isInstanceOf[DeclarativeAggregate] =>
+        ae.aggregateFunction.asInstanceOf[DeclarativeAggregate].aggBufferAttributes
+      case _ => None
+    }
+    private val getStats: Projection = UnsafeProjection.create(
+      exprs = Seq(resultExpr),
+      inputSchema = aggBufferAttrs
+    )
+    private val taskContext = TaskContext.get()
+    private val outIterator: Iterator[ColumnarBatch] = {
+      // Constructs an input iterator receiving the input rows first.
+      val inputIterator = new ColumnarBatchInIterator(
+        BackendsApiManager.getBackendName,
+        Iterators
+          .wrap(new Iterator[ColumnarBatch] {
+            private var batch: ColumnarBatch = _
+
+            override def hasNext: Boolean = {
+              assert(batch == null)
+              while (!Thread.currentThread().isInterrupted) {
+                val tmp =
+                  try {
+                    inputBatchQueue.poll(100, TimeUnit.MILLISECONDS)
+                  } catch {
+                    case _: InterruptedException =>
+                      Thread.currentThread().interrupt()
+                      return false;
+                  }
+                if (tmp != null) {
+                  batch = tmp
+                  return true
+                }
+                if (resultRequested) {
+                  return false
+                }
+              }
+              throw new IllegalStateException()
+            }
+
+            override def next(): ColumnarBatch = {
+              assert(batch != null)
+              try {
+                batch
+              } finally {
+                batch = null
+              }
+            }
+          })
+          .recyclePayload(b => b.close())
+          .create()
+          .asJava
+      )
+
+      val inputNode = StatisticsInputNode(dataCols)
+
+      val aggOp = HashAggregateExec(
+        None,
+        false,
+        None,
+        Seq.empty,
+        aggregates,
+        Seq(AttributeReference("stats", StringType)()),
+        0,
+        Seq.empty,
+        inputNode
+      )
+      // Legacy: The legacy transform rule.
+      val offloads = Seq(OffloadOthers()).map(_.toStrcitRule())
+      val validatorBuilder: GlutenConfig => Validator = conf =>
+        Validators.newValidator(conf, offloads)
+      val rewrites = Seq(PullOutPreProject)
+      val config = GlutenConfig.get
+      val transformRule =
+        HeuristicTransform.WithRewrites(validatorBuilder(config), rewrites, offloads)
+      val aggTransformer = ColumnarCollapseTransformStages(config)(transformRule(aggOp))
+        .asInstanceOf[WholeStageTransformer]
+        .child
+        .asInstanceOf[RegularHashAggregateExecTransformer]
+      val substraitContext = new SubstraitContext
+      TransformerState.enterValidation
+      val transformedNode =
+        try {
+          aggTransformer.transform(substraitContext)
+        } finally {
+          TransformerState.finishValidation
+        }
+      val outNames = aggTransformer.output.map(ConverterUtils.genColumnNameWithExprId).asJava
+      val planNode =
+        PlanBuilder.makePlan(substraitContext, Lists.newArrayList(transformedNode.root), outNames)
+
+      val evaluator = NativePlanEvaluator.create(
+        BackendsApiManager.getBackendName,
+        Map.empty[String, String].asJava)
+      val spillDirPath = SparkDirectoryUtil
+        .get()
+        .namespace("gluten-spill")
+        .mkChildDirRoundRobin(UUID.randomUUID.toString)
+        .getAbsolutePath
+      val resItr = evaluator
+        .createKernelWithBatchIterator(
+          planNode.toProtobuf.toByteArray,
+          new Array[Array[Byte]](0),
+          Seq(inputIterator).asJava,
+          0,
+          BackendsApiManager.getSparkPlanExecApiInstance.rewriteSpillPath(spillDirPath)
+        )
+        .asScala
+      resItr
+    }
+
+    private val resultThread = new Thread(
+      new Runnable {
+        override def run(): Unit = {
+          TaskContext.setTaskContext(taskContext)
+          if (outIterator.hasNext) {
+            val next = outIterator.next()
+            assert(next != null)
+            outputBatchQueue.offer(next)
+          }
+        }
+      },
+      s"Gluten Delta Statistics Writer - ${taskContext.taskAttemptId()}"
+    )
+    resultThread.start()
+
+    def appendColumnarBatch(inputBatch: ColumnarBatch): Unit = {
+      // Retains the input batch so it will be shared by the data writer thread
+      // and the statistic writer thread.
+      ColumnarBatches.retain(inputBatch)
+      inputBatchQueue.offer(inputBatch)
+    }
+
+    def setFinished: Unit = {
+      resultRequested = true
+    }
+
+    def toJson: String = {
+      val resBatch = outputBatchQueue.take()
+      assert(resBatch.numRows() == 1)
+      val row = c2r.toRowIterator(resBatch).next()
+      val jsonStats = getStats(row).getString(0)
+      jsonStats
+    }
+  }
+
+  private case class StatisticsInputNode(override val output: Seq[Attribute])
+    extends GlutenPlan
+    with LeafExecNode {
+    override def batchType(): Convention.BatchType = VeloxBatchType
+    override def rowType0(): Convention.RowType = Convention.RowType.None
+    override protected def doExecute(): RDD[InternalRow] = throw new UnsupportedOperationException()
+    override protected def doExecuteColumnar(): RDD[ColumnarBatch] =
+      throw new UnsupportedOperationException()
   }
 }
