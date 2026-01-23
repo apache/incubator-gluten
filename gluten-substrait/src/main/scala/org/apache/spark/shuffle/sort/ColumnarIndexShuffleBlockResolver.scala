f96ca3eb5443e80e4e89fe7d7ff82dd3dc25cb10
Wangyang Guo <wangyang.guo@intel.com>
2026-01-23 19:16:05 +0800

    Add ColumnarIndexShuffleBlockResolver#getSegmentsFromIndex

--- gluten-substrait/src/main/scala/org/apache/spark/shuffle/sort/ColumnarIndexShuffleBlockResolver.scala
+++ gluten-substrait/src/main/scala/org/apache/spark/shuffle/sort/ColumnarIndexShuffleBlockResolver.scala
@@ -19,4 +19,50 @@ package org.apache.spark.shuffle.sort
 import org.apache.spark.SparkConf
 import org.apache.spark.shuffle._
 
-class ColumnarIndexShuffleBlockResolver(conf: SparkConf) extends IndexShuffleBlockResolver(conf) {}
+import java.io.DataInputStream
+import java.io.File
+import java.nio.channels.Channels
+import java.nio.channels.SeekableByteChannel
+
+class ColumnarIndexShuffleBlockResolver(conf: SparkConf) extends IndexShuffleBlockResolver(conf) {
+  private def isNewFormat(index: File): Boolean = {
+    // Simple heuristic to determine new format by appending 1 extra bytes.
+    // Old index file always has length multiple of 8 bytes.
+    if (index.length() % 8 == 1) true else false
+  }
+
+  private def getSegmentsFromIndex(
+      channel: SeekableByteChannel,
+      startId: Int,
+      endId: Int): Seq[(Long, Long)] = {
+    // New Index Format:
+    // To support a partiton index with multiple segments,
+    // the index file is composed of three parts:
+    // 1) Partition Index: index_0, index_1, ... index_N
+    //    Each index_i is an 8-byte long integer representing the byte offset in the file
+    //    For partition i, its segments are stored between index_i and index_(i+1).
+    // 2) Segment Data: [offset_0][size_0][offset_1][size_1]...[offset_N][size_N]
+    //    Each segment is represented by two 8-byte long integers: offset and size.
+    //    offset is the byte offset in the data file, size is the length in bytes of this segment.
+    //    All segments for all partitions are stored sequentially after the partition index.
+    // 3) One extra byte at the end to distinguish from old format.
+    channel.position(startId * 8L)
+    val in = new DataInputStream(Channels.newInputStream(channel))
+    var startOffset = in.readLong()
+    channel.position(endId * 8L)
+    val endOffset = in.readLong()
+    if (endOffset < startOffset || (endOffset - startOffset) % 16 != 0) {
+      throw new IllegalStateException(
+        s"Index file: Invalid index to segments ($startOffset, $endOffset)")
+    }
+    val segmentCount = (endOffset - startOffset) / 16
+    // Read segments
+    channel.position(startOffset)
+    val segments = for (i <- 0 until segmentCount.toInt) yield {
+      val offset = in.readLong()
+      val size = in.readLong()
+      (offset, size)
+    }
+    segments.filter(_._2 > 0) // filter out zero-size segments
+  }
+}
--- gluten-substrait/src/test/scala/org/apache/spark/shuffle/sort/ColumnarIndexShuffleBlockResolverSuite.scala
+++ gluten-substrait/src/test/scala/org/apache/spark/shuffle/sort/ColumnarIndexShuffleBlockResolverSuite.scala
@@ -15,11 +15,93 @@
  * limitations under the License.
  */
 package org.apache.spark.shuffle.sort
-import org.scalatest.BeforeAndAfterAll
+
+import org.apache.spark.SparkConf
+
+import org.scalatest.{BeforeAndAfterAll, BeforeAndAfterEach}
 import org.scalatest.funsuite.AnyFunSuite
 
-class ColumnarIndexShuffleBlockResolverSuite extends AnyFunSuite with BeforeAndAfterAll {
-  test("dummy test") {
-    assert(1 + 1 == 2)
+import java.io.File
+import java.nio.ByteBuffer
+import java.nio.channels.FileChannel
+import java.nio.file.StandardOpenOption
+
+class ColumnarIndexShuffleBlockResolverSuite
+  extends AnyFunSuite
+  with BeforeAndAfterAll
+  with BeforeAndAfterEach {
+
+  private var tmpFile: File = _
+  private var channel: FileChannel = _
+
+  override def beforeAll(): Unit = {
+    tmpFile = File.createTempFile("index", ".bin")
+    val fc = FileChannel.open(tmpFile.toPath, StandardOpenOption.WRITE, StandardOpenOption.READ)
+    // Partition index: 4 partitions, so 5 offsets
+    // Partition 0: 0 segments
+    // Partition 1: 1 segment
+    // Partition 2: 2 segments
+    // Partition 3: 3 segments
+    // Offsets: [indexEnd, seg1Start, seg2Start, seg3Start, segEnd]
+    // Let's say index is 5*8 = 40 bytes, segments start at 40
+    val segOffsets = Array(40L, 40L, 56L, 88L, 136L)
+    val indexBuf = ByteBuffer.allocate(5 * 8)
+    segOffsets.foreach(indexBuf.putLong)
+    indexBuf.flip()
+    fc.write(indexBuf)
+    // Segment data: total 6 segments (1+2+3)
+    val segBuf = ByteBuffer.allocate(6 * 16)
+    // Define a data file with 200 bytes.
+    // Partition 1:
+    segBuf.putLong(0L); segBuf.putLong(10L)
+    // Partition 2:
+    segBuf.putLong(10L); segBuf.putLong(20L)
+    segBuf.putLong(100L); segBuf.putLong(50L)
+    // Partition 3:
+    segBuf.putLong(30L); segBuf.putLong(70L)
+    segBuf.putLong(150L); segBuf.putLong(50L)
+    segBuf.putLong(200L); segBuf.putLong(0L)
+    segBuf.flip()
+    fc.write(segBuf)
+    // Add one extra byte to mark new format
+    fc.write(ByteBuffer.wrap(Array(1.toByte)))
+    fc.close()
+  }
+
+  override def beforeEach(): Unit = {
+    channel = FileChannel.open(tmpFile.toPath, StandardOpenOption.READ)
+  }
+
+  override def afterEach(): Unit = {
+    if (channel != null) {
+      channel.close()
+      channel = null
+    }
+  }
+
+  override def afterAll(): Unit = {
+    if (tmpFile != null) tmpFile.delete()
+  }
+
+  test("getSegmentsFromIndex returns correct segments for complex index file") {
+    val resolver = new ColumnarIndexShuffleBlockResolver(new SparkConf())
+    val method = classOf[ColumnarIndexShuffleBlockResolver].getDeclaredMethod(
+      "getSegmentsFromIndex",
+      classOf[java.nio.channels.SeekableByteChannel],
+      classOf[Int],
+      classOf[Int])
+    method.setAccessible(true)
+    // Partition 0: 0 segments
+    val segs0 =
+      method.invoke(resolver, channel, Int.box(0), Int.box(1)).asInstanceOf[Seq[(Long, Long)]]
+    assert(segs0.isEmpty)
+    // Partition 1 - 2: 3 segment
+    val segs1 =
+      method.invoke(resolver, channel, Int.box(1), Int.box(3)).asInstanceOf[Seq[(Long, Long)]]
+    assert(segs1 == Seq((0L, 10L), (10L, 20L), (100L, 50L)))
+    // Partition 3: 2 segments, empty segment will be filter out
+    val segs3 =
+      method.invoke(resolver, channel, Int.box(3), Int.box(4)).asInstanceOf[Seq[(Long, Long)]]
+    assert(segs3 == Seq((30L, 70L), (150L, 50L)))
   }
 }
