55a2332c5b18db3944c5cede196046819a7f9d9f
Jin Chengcheng <Chengcheng.Jin@ibm.com>
2026-01-28 17:55:41 +0800

    [GLUTEN-10933][VL] BroadcastExchange outputs cudf::table (#11441)

--- cpp/velox/compute/VeloxRuntime.cc
+++ cpp/velox/compute/VeloxRuntime.cc
@@ -52,6 +52,10 @@ DECLARE_bool(velox_memory_pool_capacity_transfer_across_tasks);
 #include "operators/writer/VeloxParquetDataSourceABFS.h"
 #endif
 
+#ifdef GLUTEN_ENABLE_GPU
+#include "operators/serializer/VeloxGpuColumnarBatchSerializer.h"
+#endif
+
 using namespace facebook;
 
 namespace gluten {
@@ -307,6 +311,11 @@ std::shared_ptr<ShuffleReader> VeloxRuntime::createShuffleReader(
 std::unique_ptr<ColumnarBatchSerializer> VeloxRuntime::createColumnarBatchSerializer(struct ArrowSchema* cSchema) {
   auto arrowPool = memoryManager()->defaultArrowMemoryPool();
   auto veloxPool = memoryManager()->getLeafMemoryPool();
+#ifdef GLUTEN_ENABLE_GPU
+  if (veloxCfg_->get<bool>(kCudfEnabled, kCudfEnabledDefault)) {
+    return std::make_unique<VeloxGpuColumnarBatchSerializer>(arrowPool, veloxPool, cSchema);
+  }
+#endif
   return std::make_unique<VeloxColumnarBatchSerializer>(arrowPool, veloxPool, cSchema);
 }
 
