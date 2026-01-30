37fd3e56462dbc4652cdd9f433864521774915c2
Ke Jia <ke.jia@ibm.com>
2026-01-30 09:37:17 +0000

    enable bhj filter pushdown

--- gluten-substrait/src/main/scala/org/apache/gluten/backendsapi/BackendSettingsApi.scala
+++ gluten-substrait/src/main/scala/org/apache/gluten/backendsapi/BackendSettingsApi.scala
@@ -82,6 +82,8 @@ trait BackendSettingsApi {
     GlutenConfig.get.enableColumnarShuffle
   }
 
+  def enableJoinKeysRewrite(): Boolean = true
+
   def enableHashTableBuildOncePerExecutor(): Boolean = true
 
   def supportHashBuildJoinTypeOnLeft: JoinType => Boolean = {
