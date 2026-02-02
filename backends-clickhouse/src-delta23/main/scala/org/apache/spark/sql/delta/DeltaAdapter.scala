7071d863c29fce0bd39123bb5af3719eafe41c58
zhanglistar <zhanglinuxstar@gmail.com>
2026-02-02 15:19:26 +0800

    Fix scala code format in CH CI.

--- backends-clickhouse/src-delta23/main/scala/org/apache/spark/sql/delta/DeltaAdapter.scala
+++ backends-clickhouse/src-delta23/main/scala/org/apache/spark/sql/delta/DeltaAdapter.scala
@@ -15,6 +15,7 @@
  * limitations under the License.
  */
 package org.apache.spark.sql.delta
+
 import org.apache.spark.sql.catalyst.expressions.{Attribute, Expression}
 import org.apache.spark.sql.delta.stats.DeltaScan
 
