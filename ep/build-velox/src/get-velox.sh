2b581ec84ff23eface510f213d42e2d97e961195
Hongze Zhang <hongze.zzz123@gmail.com>
2026-02-25 17:56:17 +0100

    fixup

--- ep/build-velox/src/get-velox.sh
+++ ep/build-velox/src/get-velox.sh
@@ -18,7 +18,7 @@ set -exu
 
 CURRENT_DIR=$(cd "$(dirname "$BASH_SOURCE")"; pwd)
 VELOX_REPO=https://github.com/zhztheplayer/velox.git
-VELOX_BRANCH=wip-string-rtfilter
+VELOX_BRANCH=wip-probe-filter
 VELOX_ENHANCED_BRANCH=ibm-2026_02_24
 VELOX_HOME=""
 RUN_SETUP_SCRIPT=ON
