020a574b678b9eac5dc6e4604bc13beddb7b3137
yizhouyang <yizhouyang@tencent.com>
2026-02-25 10:54:53 +0800

    feat:support gluten-level approx_percentile

--- gluten-substrait/src/main/scala/org/apache/gluten/config/GlutenConfig.scala
+++ gluten-substrait/src/main/scala/org/apache/gluten/config/GlutenConfig.scala
@@ -381,6 +381,7 @@ class GlutenConfig(conf: SQLConf) extends GlutenCoreConfig(conf) {
 
   def maxBroadcastTableSize: Long =
     JavaUtils.byteStringAsBytes(conf.getConfString(SPARK_MAX_BROADCAST_TABLE_SIZE, "8GB"))
+  def glutenPercentileFunctionFallback: Boolean = getConf(GLUTEN_PERCENTILE_FUNCTION_FALLBACK)
 }
 
 object GlutenConfig extends ConfigRegistry {
@@ -1585,4 +1586,14 @@ object GlutenConfig extends ConfigRegistry {
           "total size of small files is below this threshold.")
       .doubleConf
       .createWithDefault(0.5)
+
+  val GLUTEN_PERCENTILE_FUNCTION_FALLBACK =
+    buildConf("spark.gluten.sql.glutenPercentileFunctionStrategy")
+      .doc(
+        "Chooses whether percentile algorithm falls back " +
+          "for the percentile function. kll is provided by open-source velox, but " +
+          "we can always disable this function and fallback.")
+      .internal()
+      .booleanConf
+      .createWithDefault(false)
 }
