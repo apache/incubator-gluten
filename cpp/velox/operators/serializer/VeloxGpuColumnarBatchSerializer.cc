e591948a49541a8806dc8c9ce711ed873e1e1c31
Yuan <yuanzhou@apache.org>
2026-03-04 19:51:08 +0000

    fix
    Signed-off-by: Yuan <yuanzhou@apache.org>

--- cpp/velox/operators/serializer/VeloxGpuColumnarBatchSerializer.cc
+++ cpp/velox/operators/serializer/VeloxGpuColumnarBatchSerializer.cc
@@ -45,7 +45,7 @@ std::shared_ptr<ColumnarBatch> VeloxGpuColumnarBatchSerializer::deserialize(uint
   auto vb = VeloxColumnarBatchSerializer::deserialize(data, size);
   auto stream = cudf_velox::cudfGlobalStreamPool().get_stream();
   auto table = cudf_velox::with_arrow::toCudfTable(
-    dynamic_pointer_cast<VeloxColumnarBatch>(vb)->getRowVector(), veloxPool_.get(), stream, , get_output_mr());
+    dynamic_pointer_cast<VeloxColumnarBatch>(vb)->getRowVector(), veloxPool_.get(), stream, cudf_velox::get_output_mr());
   stream.synchronize();
   auto vector = std::make_shared<cudf_velox::CudfVector>(
       veloxPool_.get(), rowType_, size, std::move(table), stream);
