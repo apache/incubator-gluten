22e84bc98dbd8e79dc298d2314203e511ae7b67b
Yuan <yuanzhou@apache.org>
2026-03-11 14:58:14 +0000

    fix
    Signed-off-by: Yuan <yuanzhou@apache.org>

--- ep/build-velox/src/setup-centos8.sh
+++ ep/build-velox/src/setup-centos8.sh
@@ -213,6 +213,7 @@ function install_velox_deps {
   run_and_time install_protobuf
   run_and_time install_fmt
   run_and_time install_fast_float
+  run_and_time install_xxhash
   run_and_time install_folly
   run_and_time install_fizz
   run_and_time install_wangle
