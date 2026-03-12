WangGuangxin/gluten

Add support for paimon from master
This change introduces all of the necessary paimon-specific
changes that were included in olap/gluten's master branch
but were not ported over when switching to OSS gluten's
main branch

The following changes were made

- PaimonScanTransformer is now AbstractPaimonScanTransformer
in order to support a other backend's implementation for paimon.
Bolt-specific features are now included in an extended class
BoltPaimonScanTransformer to facilitate the bolt-specific
requirements.

- Added support for adding extension info from the scan
transformer into the advanced_extension field for a ReadRel.
Also added protos for the paimon-specific advanced extension
which are automatically generated and compiled for both Java
and C++ versions.
- The above also coincides with adding support for passing
the "tableParameters" field of a HiveTableHandle when
converting the substrait plan into bolt.

- Added protos specific to paimon splits in order to serialize
and deserialize paimon-specific split information from gluten
into bolt. Previously this was done in a hacky way using
strings and comma-separated lists. This new version uses
protobuf both on the Java and C++ side to communicate
paimon-specific split information via a LocalFiles' file_format
field oneof definition in algebra.proto. One of the new options
added is for "PaimonReadOptions" which contains the required
paimon information.

- Added a bolt-specific paimon suite which mirrors all of
the previous test cases from the master branch.

cpp/bolt/substrait/SubstraitToBoltPlan.cc
@@ -18,6 +18,7 @@
 #include "SubstraitToBoltPlan.h"
 #include <bolt/common/compression/Compression.h>
 
+#include "PaimonTableEnhancement.pb.h"
 #include "TypeUtils.h"
 #include "VariantToVectorConverter.h"
 #include "operators/plannodes/RowVectorStream.h"
@@ -1335,6 +1336,25 @@ core::PlanNodePtr SubstraitToBoltPlanConverter::toBoltPlan(const ::substrait::Re
     SubstraitParser::parseColumnTypes(baseSchema, columnTypes);
   }
 
+  std::unordered_map<std::string, std::string> tableParameters;
+  if (readRel.has_advanced_extension()) {
+    const auto& ext = readRel.advanced_extension();
+    if (ext.has_enhancement()) {
+      gluten::PaimonTableEnhancement paimonParameters;
+      if (ext.enhancement().UnpackTo(&paimonParameters)) {
+        for (const auto& item : *(paimonParameters.mutable_table_properties())) {
+          tableParameters.emplace(item.first, item.second);
+        }
+      }
+    }
+    std::ostringstream paramstream;
+    for (const auto& item : tableParameters) {
+      paramstream << "[" << item.first << "=" << item.second << "];";
+    }
+    LOG(INFO) << "Adding table parameters: " << paramstream.str();
+  }
+
+
   // Bolt requires Filter Pushdown must being enabled.
   bool filterPushdownEnabled = true;
   auto names = colNameList;
@@ -1351,7 +1371,7 @@ core::PlanNodePtr SubstraitToBoltPlanConverter::toBoltPlan(const ::substrait::Re
   }
   bytedance::bolt::connector::hive::SubfieldFilters subfieldFilters;
   tableHandle = std::make_shared<connector::hive::HiveTableHandle>(
-      connectorId, "hive_table", filterPushdownEnabled, std::move(subfieldFilters), remainingFilter, dataColumns);
+      connectorId, "hive_table", filterPushdownEnabled, std::move(subfieldFilters), remainingFilter, dataColumns, tableParameters);
 
   // Get assignments and out names.
   std::vector<std::string> outNames;

apache/incubator-gluten

cpp/bolt/substrait/SubstraitToBoltPlan.cc
@@ -18,6 +18,7 @@
 #include "SubstraitToBoltPlan.h"
 #include <bolt/common/compression/Compression.h>
 
+#include "PaimonTableEnhancement.pb.h"
 #include "TypeUtils.h"
 #include "VariantToVectorConverter.h"
 #include "operators/plannodes/RowVectorStream.h"
@@ -1335,6 +1336,25 @@ core::PlanNodePtr SubstraitToBoltPlanConverter::toBoltPlan(const ::substrait::Re
     SubstraitParser::parseColumnTypes(baseSchema, columnTypes);
   }
 
+  std::unordered_map<std::string, std::string> tableParameters;
+  if (readRel.has_advanced_extension()) {
+    const auto& ext = readRel.advanced_extension();
+    if (ext.has_enhancement()) {
+      gluten::PaimonTableEnhancement paimonParameters;
+      if (ext.enhancement().UnpackTo(&paimonParameters)) {
+        for (const auto& item : *(paimonParameters.mutable_table_properties())) {
+          tableParameters.emplace(item.first, item.second);
+        }
+      }
+    }
+    std::ostringstream paramstream;
+    for (const auto& item : tableParameters) {
+      paramstream << "[" << item.first << "=" << item.second << "];";
+    }
+    LOG(INFO) << "Adding table parameters: " << paramstream.str();
+  }
+
+
   // Bolt requires Filter Pushdown must being enabled.
   bool filterPushdownEnabled = true;
   auto names = colNameList;
@@ -1356,7 +1376,7 @@ core::PlanNodePtr SubstraitToBoltPlanConverter::toBoltPlan(const ::substrait::Re
   }
   bytedance::bolt::connector::hive::SubfieldFilters subfieldFilters;
   tableHandle = std::make_shared<connector::hive::HiveTableHandle>(
-      connectorId, "hive_table", filterPushdownEnabled, std::move(subfieldFilters), remainingFilter, tableSchema);
+      connectorId, "hive_table", filterPushdownEnabled, std::move(subfieldFilters), remainingFilter, tableSchema, tableParameters);
 
   // Get assignments and out names.
   std::vector<std::string> outNames;